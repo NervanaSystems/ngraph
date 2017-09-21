3691c57777b758a0aa83cb5af321374a728e6a8f
Yixing Lao <yixing.lao@intel.com>
2017-09-21 11:08:29 -0700

    include Eigen with double quotes

--- src/ngraph/runtime/eigen/tensor_view.cpp
+++ src/ngraph/runtime/eigen/tensor_view.cpp
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // ----------------------------------------------------------------------------
 
-#include <Eigen/Dense>
 #include <memory>
 
+#include "Eigen/Dense"
 #include "ngraph/ngraph.hpp"
 
 using namespace Eigen;
--- test/eigen.cpp
+++ test/eigen.cpp
@@ -13,18 +13,18 @@
 // ----------------------------------------------------------------------------
 
 #include <iostream>
-#include <Eigen/Dense>
 
+#include "Eigen/Dense"
 #include "gtest/gtest.h"
 
 using Eigen::MatrixXd;
 
 TEST(eigen, simple)
 {
-    MatrixXd m(2,2);
-    m(0,0) = 3;
-    m(1,0) = 2.5;
-    m(0,1) = -1;
-    m(1,1) = m(1,0) + m(0,1);
-    EXPECT_FLOAT_EQ(m(1,1), 1.5);
+    MatrixXd m(2, 2);
+    m(0, 0) = 3;
+    m(1, 0) = 2.5;
+    m(0, 1) = -1;
+    m(1, 1) = m(1, 0) + m(0, 1);
+    EXPECT_FLOAT_EQ(m(1, 1), 1.5);
 }
