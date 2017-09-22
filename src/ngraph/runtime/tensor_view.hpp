da8d8a2b7894445a3f097b57ab5e86c07ac8cd8c
Scott Cyphers <diyessi@users.noreply.github.com>
2017-09-22 07:01:43 -0700

    Remove eigen dependencies. (#140)

--- src/ngraph/runtime/tensor_view.hpp
+++ src/ngraph/runtime/tensor_view.hpp
@@ -14,17 +14,68 @@
 
 #pragma once
 
+#include <memory>
+#include <vector>
+
+#include "ngraph/shape.hpp"
+#include "ngraph/types/element_type.hpp"
+
 namespace ngraph
 {
     namespace runtime
     {
-        // Actual tensor views are parameterized on element type
-        class PrimaryTensorView
+        template <typename ET>
+        class ParameterizedTensorView;
+
+        class TensorView
         {
         public:
-            virtual ~PrimaryTensorView() {}
+            virtual ~TensorView() {}
+            template <typename ET>
+            ParameterizedTensorView<ET>* get_parameterized_tensor()
+            {
+                return dynamic_cast<ParameterizedTensorView<ET>*>(this);
+            }
         };
 
-        using PTVs = std::vector<std::shared_ptr<ngraph::runtime::PrimaryTensorView>>;
+        template <typename ET>
+        class ParameterizedTensorView : public ngraph::runtime::TensorView
+        {
+        public:
+            ParameterizedTensorView(const ngraph::Shape& shape)
+                : m_vector(ngraph::shape_size(shape), 0)
+            {
+            }
+
+            virtual ~ParameterizedTensorView() {}
+
+            // Standard definitions from vector
+            using element_type           = ET;
+            using value_type             = typename ET::type;
+            using storage_type           = std::vector<value_type>;
+            using size_type              = typename storage_type::size_type;
+            using difference_type        = typename storage_type::difference_type;
+            using reference              = typename storage_type::reference;
+            using const_reference        = typename storage_type::const_reference;
+            using pointer                = typename storage_type::pointer;
+            using const_pointer          = typename storage_type::const_pointer;
+            using iterator               = typename storage_type::iterator;
+            using const_iterator         = typename storage_type::const_iterator;
+            using reverse_iterator       = typename storage_type::reverse_iterator;
+            using const_reverse_iterator = typename storage_type::const_reverse_iterator;
+
+            template <typename T>
+            ParameterizedTensorView<ET>& operator=(const std::vector<T>& value)
+            {
+                get_vector() = value;
+                return *this;
+            }
+
+            // For getting the data out
+            storage_type& get_vector() { return m_vector; }
+
+        protected:
+            storage_type m_vector;
+        };
     }
 }
--- test/execute.cpp
+++ test/execute.cpp
@@ -27,24 +27,24 @@ TEST(execute, test_abc)
     auto C     = make_shared<op::Parameter>(element::Float32::element_type(), shape);
     auto f     = make_shared<Function>((A + B) * C, op::Parameters{A, B, C});
 
-    auto external = make_shared<ngraph::runtime::eigen::ExternalFunction>(f);
+    auto external = make_shared<ngraph::runtime::ExternalFunction>(f);
     auto cf       = external->make_call_frame();
 
     // Create some tensors for input/output
-    auto a      = make_shared<runtime::eigen::PrimaryTensorView<element::Float32>>(shape);
+    auto a      = ngraph::runtime::make_tensor<element::Float32>(shape);
     *a          = vector<float>{1, 2, 3, 4};
-    auto b      = make_shared<runtime::eigen::PrimaryTensorView<element::Float32>>(shape);
+    auto b      = ngraph::runtime::make_tensor<element::Float32>(shape);
     *b          = vector<float>{5, 6, 7, 8};
-    auto c      = make_shared<runtime::eigen::PrimaryTensorView<element::Float32>>(shape);
+    auto c      = ngraph::runtime::make_tensor<element::Float32>(shape);
     *c          = vector<float>{9, 10, 11, 12};
-    auto result = make_shared<runtime::eigen::PrimaryTensorView<element::Float32>>(shape);
+    auto result = ngraph::runtime::make_tensor<element::Float32>(shape);
 
-    (*cf)(runtime::PTVs{a, b, c}, runtime::PTVs{result});
+    (*cf)({a, b, c}, {result});
     ASSERT_EQ((vector<float>{54, 80, 110, 144}), result->get_vector());
 
-    (*cf)(runtime::PTVs{b, a, c}, runtime::PTVs{result});
+    (*cf)({b, a, c}, {result});
     ASSERT_EQ((vector<float>{54, 80, 110, 144}), result->get_vector());
 
-    (*cf)(runtime::PTVs{a, c, b}, runtime::PTVs{result});
+    (*cf)({a, c, b}, {result});
     ASSERT_EQ((vector<float>{50, 72, 98, 128}), result->get_vector());
 }
