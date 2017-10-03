eb22cbb45c436b93a233e0ff1403e408563fca8d
Scott Cyphers <scott.cyphers@intel.com>
2017-10-03 09:48:58 -0700

    Use weak_ptr for node in inputs/outputs, turn off alignment style.

--- src/ngraph/descriptor/output.hpp
+++ src/ngraph/descriptor/output.hpp
@@ -32,24 +32,26 @@ namespace ngraph
             // Output& operator=(const Output&) = delete;
 
         public:
-            /// @param node Node that owns this output. Not shared to prevent owner loop.
+            /// @param node Node that owns this output.
             /// @param index Position of the output tensor in all output tensors
             /// @param tensor_view The view of this tensor; where the value will be written
-            Output(Node* node, size_t index, const std::shared_ptr<TensorView>& tensor_view);
+            Output(const std::shared_ptr<Node>& node,
+                   size_t index,
+                   const std::shared_ptr<TensorView>& tensor_view);
 
-            std::shared_ptr<Node>       get_node() const;
-            size_t                      get_index() const { return m_index; }
+            std::shared_ptr<Node> get_node() const;
+            size_t get_index() const { return m_index; }
             std::shared_ptr<TensorView> get_tensor_view() const { return m_tensor_view; }
             void add_input(Input* input);
             const std::set<Input*>& get_inputs() const { return m_inputs; }
-            const Tensor&           get_tensor() const;
-            Tensor&                 get_tensor();
+            const Tensor& get_tensor() const;
+            Tensor& get_tensor();
 
         protected:
-            Node*                       m_node;
-            size_t                      m_index;
+            std::weak_ptr<Node> m_node;
+            size_t m_index;
             std::shared_ptr<TensorView> m_tensor_view;
-            std::set<Input*>            m_inputs;
+            std::set<Input*> m_inputs;
         };
     }
 }
