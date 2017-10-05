8146eb87493c132be8b156b01e5e139f3dbab754
Christian Convey <christian.convey@intel.com>
2017-10-05 13:56:07 -0400

    Adds clang-format version checking.

--- maint/check-code-format.sh
+++ maint/check-code-format.sh
@@ -22,14 +22,20 @@ set -u
 # For this reason, this script specifies the exact version of clang-format to be used.
 
 declare CLANG_FORMAT_BASENAME="clang-format-3.9"
+declare REQUIRED_CLANG_FORMAT_VERSION=3.9
 
 declare THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+source "${THIS_SCRIPT_DIR}/bash_lib.sh"
+source "${THIS_SCRIPT_DIR}/clang_format_lib.sh"
+
 declare CLANG_FORMAT_PROG
 if ! CLANG_FORMAT_PROG="$(which "${CLANG_FORMAT_BASENAME}")"; then
-    echo "Unable to find program ${CLANG_FORMAT_BASENAME}" >&2
-    exit 1
+    bash_lib_die "Unable to find program ${CLANG_FORMAT_BASENAME}" >&2
 fi
+
+clang_format_lib_verify_version "${CLANG_FORMAT_PROG}" "${REQUIRED_CLANG_FORMAT_VERSION}"
+bash_lib_status "Verified that '${CLANG_FORMAT_PROG}' has version '${REQUIRED_CLANG_FORMAT_VERSION}'"
 
 declare -a FAILED_FILES=()
 declare NUM_FILES_CHECKED=0
@@ -39,11 +45,10 @@ pushd "${THIS_SCRIPT_DIR}/.."
 declare ROOT_SUBDIR
 for ROOT_SUBDIR in src test; do
     if ! [[ -d "${ROOT_SUBDIR}" ]]; then
-	echo "In directory '$(pwd)', no subdirectory named '${ROOT_SUBDIR}' was found." >&2
-	exit 1
+        bash_lib_die "In directory '$(pwd)', no subdirectory named '${ROOT_SUBDIR}' was found."
     fi
 
-    echo "About to format C/C++ code in directory tree '$(pwd)/${ROOT_SUBDIR}' ..."
+    bash_lib_status "About to format C/C++ code in directory tree '$(pwd)/${ROOT_SUBDIR}' ..."
     declare SRC_FILE
     # Note that we restrict to "-type f" to exclude symlinks. Emacs sometimes
     # creates dangling symlinks with .cpp/.hpp suffixes as a sort of locking
@@ -59,8 +64,7 @@ done
 popd
 
 if [[ ${#FAILED_FILES[@]} -eq 0 ]]; then
-    echo "All ${NUM_FILES_CHECKED}  C/C++ files pass the code-format check."
-    exit 0
+    bash_lib_status "All ${NUM_FILES_CHECKED}  C/C++ files pass the code-format check."
 else
     echo "${#FAILED_FILES[@]} of ${NUM_FILES_CHECKED} source files failed the code-format check:"
     declare FAILED_SRC_FILE
