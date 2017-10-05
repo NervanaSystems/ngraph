d6f387a5fd6bb9657b0dcb462451939ed9b35417
Robert Kimball <robert.kimball@intel.com>
2017-10-05 11:16:49 -0700

    update pass tests

--- test/topological_sort.cpp
+++ test/topological_sort.cpp
@@ -21,6 +21,7 @@
 
 #include "ngraph/log.hpp"
 #include "ngraph/ngraph.hpp"
+#include "ngraph/pass/collect_functions.hpp"
 #include "ngraph/pass/manager.hpp"
 #include "ngraph/pass/topological_sort.hpp"
 #include "ngraph/util.hpp"
@@ -133,4 +134,52 @@ TEST(benchmark, topological_sort)
     ngraph::free_nodes(result);
     timer.stop();
     NGRAPH_INFO << "delete nodes took " << timer.get_milliseconds() << "ms";
+}
+TEST(topological_sort, collect_functions)
+{
+    // First create "f(A,B,C) = (A+B)*C".
+    auto shape = Shape{2, 2};
+    auto A = make_shared<op::Parameter>(element::Float32::element_type(), shape);
+    auto B = make_shared<op::Parameter>(element::Float32::element_type(), shape);
+    auto C = make_shared<op::Parameter>(element::Float32::element_type(), shape);
+    auto rt_f = make_shared<TensorViewType>(element::Float32::element_type(), shape);
+    auto f = make_shared<Function>((A + B) * C, rt_f, op::Parameters{A, B, C}, "f");
+
+    // Now make "g(X,Y,Z) = f(X,Y,Z) + f(X,Y,Z)"
+    auto X = make_shared<op::Parameter>(element::Float32::element_type(), shape);
+    auto Y = make_shared<op::Parameter>(element::Float32::element_type(), shape);
+    auto Z = make_shared<op::Parameter>(element::Float32::element_type(), shape);
+    auto rt_g = make_shared<TensorViewType>(element::Float32::element_type(), shape);
+    auto g = make_shared<Function>(make_shared<op::FunctionCall>(f, Nodes{X, Y, Z}) +
+                                       make_shared<op::FunctionCall>(f, Nodes{X, Y, Z}),
+                                   rt_g,
+                                   op::Parameters{X, Y, Z},
+                                   "g");
+
+    // Now make "h(X,Y,Z) = g(X,Y,Z) + g(X,Y,Z)"
+    auto X1 = make_shared<op::Parameter>(element::Float32::element_type(), shape);
+    auto Y1 = make_shared<op::Parameter>(element::Float32::element_type(), shape);
+    auto Z1 = make_shared<op::Parameter>(element::Float32::element_type(), shape);
+    auto rt_h = make_shared<TensorViewType>(element::Float32::element_type(), shape);
+    auto h = make_shared<Function>(make_shared<op::FunctionCall>(g, Nodes{X1, Y1, Z1}) +
+                                       make_shared<op::FunctionCall>(g, Nodes{X1, Y1, Z1}),
+                                   rt_h,
+                                   op::Parameters{X1, Y1, Z1},
+                                   "h");
+
+    pass::Manager pass_manager;
+    pass_manager.register_pass<pass::CollectFunctions>();
+    pass_manager.run_passes(h);
+
+    set<string> expected = {"f", "g", "h"};
+    auto functions = pass_manager.get_state().get_functions();
+    vector<string> fnames;
+    for (Function* func : functions)
+    {
+        fnames.push_back(func->get_name());
+    }
+    EXPECT_EQ(expected.size(), functions.size());
+    EXPECT_TRUE(contains(fnames, "f"));
+    EXPECT_TRUE(contains(fnames, "g"));
+    EXPECT_TRUE(contains(fnames, "h"));
 }
