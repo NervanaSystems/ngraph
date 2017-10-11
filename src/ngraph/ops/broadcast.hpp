77df23d1e47810ae131b2c18bde66ba8cbbe7d57
Adam Procter <adam.m.procter@intel.com>
2017-10-11 16:54:00 -0700

    Docs for the base classes, broadcast, concat

--- src/ngraph/ops/broadcast.hpp
+++ src/ngraph/ops/broadcast.hpp
@@ -20,15 +20,51 @@ namespace ngraph
 {
     namespace op
     {
+        /// \brief Operation which "adds" axes to an input tensor, replicating elements from the input as needed along the new axes.
+        ///
+        /// Informally, a broadcast "adds" axes to the input tensor, replicating elements from the input tensor as needed to fill the new dimensions.
+        /// The parameter `m_broadcast_axes` indicates which of the output axes is being so added. For example, an output shape of `{2,5,6,2,8}` and
+        /// broadcast axes of `{1,3,4}` means that the input must have shape `{2,6}`.
+        ///
+        /// Formally, given a shape or coordinate \f$S = [d_1,\dots,d_n]\f$ and a set of axis indices \f$A\f$, define \f$\textit{del}(S,A)\f$ to be
+        /// the shape or coordinate obtained by deleting the \f$(a + 1)\f$th dimension from \f$S\f$ for each \f$a \in A\f$. Then given an input
+        /// tensor \f$T\f$ of shape \f$\textit{del}(S,A)\f$ with element type \f$E\f$, broadcasting axes \f$A\f$ produces a tensor \f$T'\f$ of shape
+        /// \f$S\f$ with element type \f$E\f$, where \f$T'[i_1,\dots,i_n] = T[del([i_1,\dots,i_n],A)]\f$.
+        ///
+        /// ## Parameters
+        ///
+        /// |                  | Description                                                              |
+        /// | ---------------- | ------------------------------------------------------------------------ |
+        /// | `shape`          | The shape \f$[d_1,\dots,d_n]\f$ of the broadcasted output.               |
+        /// | `broadcast_axes` | The indices \f$A\f$ in the `shape` of each broadcasted (i.e., new) axis. |
+        ///
+        /// ## Inputs
+        ///
+        /// |       | Type                                                | Description                             |
+        /// | ----- | --------------------------------------------------- | --------------------------------------- |
+        /// | `arg` | \f$E[\mathit{del}([d_1,\dots,d_n],A)]~(n \geq 0)\f$ | A tensor of any shape and element type. |
+        ///
+        /// ## Output
+        ///
+        /// | Type                   | Description                                                                     |
+        /// | ---------------------- | ------------------------------------------------------------------------------- |
+        /// | \f$E[d_1,\dots,d_n]\f$ | The tensor \f$T'\f$, where \f$T'[i_1,\dots,i_n] = T[del([i_1,\dots,i_n],A)]\f$. |
+        ///
+        /// ## Implementation Status
+        ///
+        /// | Backend | Status                                          |
+        /// | ------- | ----------------------------------------------- |
+        /// | NGVM    | Implemented for scalars, matrices, and vectors. |
+        
         class Broadcast : public Builtin
         {
         public:
+            /// \brief Constructs a conversion operation.
             ///
-            /// @param arg The tensor view to be broadcast.
-            /// @param shape The shape of the result
-            /// @param broadcast_axes The axis positions (0-based) in the result that are being broadcast.
-            ///  the remaining axes in shape must be the same as the shape of arg.
-            ///
+            /// \param arg            Node that produces the input tensor to be broadcast.
+            /// \param shape          The shape of the output tensor.
+            /// \param broadcast_axes The axis positions (0-based) in the result that are being broadcast. The
+            ///                        remaining axes in shape must be the same as the shape of arg.
             Broadcast(const std::shared_ptr<Node>& arg,
                       const Shape& shape,
                       const AxisSet& broadcast_axes)
@@ -41,6 +77,7 @@ namespace ngraph
             virtual std::string description() const override { return "Broadcast"; }
             virtual void propagate_types() override;
 
+            /// \return An set containing the indices of the broadcast axes (0-based).
             const AxisSet& get_broadcast_axes() const { return m_broadcast_axes; }
         protected:
             Shape m_shape;
