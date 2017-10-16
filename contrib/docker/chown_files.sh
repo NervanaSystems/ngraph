f0153ec3c5fc9e39dee1402149df60e8dde658f0
Chris Lishka <christopher.r.lishka@intel.com>
2017-10-16 12:54:38 -0700

    Changes/fixes to address code review.  Mainly adding double-quotes around variables and using $(...) expansion where appropriate (and semantically identical to back-quotes).

--- contrib/docker/chown_files.sh
+++ contrib/docker/chown_files.sh
@@ -3,20 +3,20 @@
 # the docker run commands leave output files with root ownership
 # modify the file ownership with the UID of the calling user  
 
-if [ -z $MY_UID ];then
-    MY_UID=`id -u`
+if [ -z "$MY_UID" ];then
+    MY_UID=$(id -u)
 fi
 
-if [ -z $MY_GID ];then
-    MY_GID=`id -g`
+if [ -z "$MY_GID" ];then
+    MY_GID=$(id -g)
 fi
 
-if [ -z $MY_ROOT_DIR ];then
+if [ -z "$MY_ROOT_DIR" ];then
     MY_ROOT_DIR=/root/ngraph-test
 fi
 
-cd $MY_ROOT_DIR
+cd "$MY_ROOT_DIR'
 find . -user root > files_to_chown.txt
-cat files_to_chown.txt | xargs chown ${MY_UID} ${1}
-cat files_to_chown.txt | xargs chgrp ${MY_GID} ${1}
+cat files_to_chown.txt | xargs chown "${MY_UID}" "${1}"
+cat files_to_chown.txt | xargs chgrp "${MY_GID}" "${1}"
 rm files_to_chown.txt
--- contrib/docker/run_as_user.sh
+++ contrib/docker/run_as_user.sh
@@ -6,35 +6,48 @@ if [ -z "$RUN_UID" ] ; then
     echo '       Running as default user (root, in docker)'
     echo ' '
 
-    # Root login shell, which when ended will end the docker session.
-    # For batch use, remove this line.
-    su -
+    exit 1
 
 else
 
-    # Add a user with UID of person running docker (in ${RUN_UID})
-    # Make sure the home directory is owned by the new user
-    # Make sure the user can run sudo (which may or may not be installed)
-    if [ -d /home/dockuser ] ; then
-      chown ${RUN_UID} /home/dockuser
-    fi
-    adduser --disabled-password --gecos 'Docker-User' -u ${RUN_UID} dockuser
-    adduser dockuser sudo
-
-    # If root access is needed in the docker image while running as a normal
-    # user, uncomment this and add 'sudo' as a packaged installed in the dockerfile
-    # echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+    # The username used in the docker container to map the caller UID to
+    #
+    # Note 'dockuser' is used in other scripts, notably Makefile.  If you
+    # choose to change it here, then you need to change it in all other
+    # scripts, or else the builds will break.
+    #
+    DOCK_USER='dockuser'
 
     # We will be su'ing using a non-login shell or command, and preserving
     # the environment.  This is done so that env. variables passed in with
     # "docker run --env ..." are honored.
     # Therefore, we need to reset at least HOME=/root ...
-    export HOME=/home/dockuser
+    #
+    # Note also that /home/dockuser is used in other scripts, notably
+    # Makefile.  If you choose to change it here, then you need to change it
+    # in all other scripts, or else the builds will break.
+    #
+    export HOME="/home/${DOCK_USER}"
+
+    # Make sure the home directory is owned by the new user
+    if [ -d "${HOME}" ] ; then
+      chown "${RUN_UID}" "${HOME}"
+    fi
+
+    # Add a user with UID of person running docker (in ${RUN_UID})
+    # If $HOME does not yet exist, then it will be created
+    adduser --disabled-password --gecos 'Docker-User' -u "${RUN_UID}" "${DOCK_USER}"
+    # Add dockuser to the sudo group
+    adduser "${DOCK_USER}" sudo
+
+    # If root access is needed in the docker image while running as a normal
+    # user, uncomment this and add 'sudo' as a package installed in Dockerfile
+    # echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
     if [ -z "$RUN_CMD" ] ; then  # Launch a shell as dockuser
-      su -m dockuser -c /bin/bash
+      su -m "${DOCK_USER}" -c /bin/bash
     else                         # Run command as dockuser
-      su -m dockuser -c "${RUN_CMD}"
+      su -m "${DOCK_USER}" -c "${RUN_CMD}"
     fi
 
 fi
