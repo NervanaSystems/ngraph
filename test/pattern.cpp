30cb57a84faaeff5a612f9b834e92a40f1989318
nikolay.korovaiko <korovaikon@gmail.com>
2017-10-18 16:11:31 -0700

    clean up

--- test/pattern.cpp
+++ test/pattern.cpp
@@ -31,18 +31,24 @@
 using namespace ngraph;
 using namespace std;
 
+static std::shared_ptr<Node> construct_constant_node(int n)
+{
+    auto shape = Shape{ 1 };
+    auto t = ngraph::runtime::make_tensor<element::Int32>(shape);
+    (*t) = std::vector<int>{ n };
+    return std::make_shared<op::ParameterizedConstant<element::Int32>>(shape, t);
+}
 
 class TestGraphRewrite : public ngraph::pass::GraphRewrite
 {
 public:
-    TestGraphRewrite() : GraphRewrite() 
+    
+    void construct_multiply_by_one()
     {
-        auto shape = Shape{ 1 };
         //pattern #1 : a * 1 = a
-        auto pattern = std::make_shared<pattern::op::Label>([](shared_ptr<Node> n) { return !dynamic_pointer_cast<op::ParameterizedConstant<element::Int32>>(n); });
-        auto t = ngraph::runtime::make_tensor<element::Int32>(shape);
-        (*t) = std::vector<int>{ 1 };
-        auto iconst1_1 = make_shared<op::ParameterizedConstant<element::Int32>>(shape, t);
+        //auto pattern = std::make_shared<pattern::op::Label>(ne_const_pred);
+        auto pattern = std::make_shared<pattern::op::Label>();
+        auto iconst1 = construct_constant_node(1);
 
         NGRAPH_DEBUG << "IN TestGraphRewrite" << std::endl;
 
@@ -73,102 +79,142 @@ public:
             auto const_values = const_node->get_value()->get_vector();
             bool all_ones = std::all_of(begin(const_values), end(const_values), [](int e) {return e == 1; });
 
-            if (!all_ones) 
+            if (!all_ones)
             {
                 NGRAPH_DEBUG << "ALL_ONES" << std::endl;
                 return;
             }
-            
+
             NGRAPH_DEBUG << "BEFORE REPLACE" << std::endl;
             ngraph::pass::GraphRewrite::replace_node(m.match_root(), pattern->get_binded_node());
         };
 
-        auto m = make_shared<pattern::Matcher>(pattern * iconst1_1, callback);
+        auto m = make_shared<pattern::Matcher>(pattern * iconst1, callback);
         this->add_matcher(m);
+    }
+    
+
+    void construct_add_zero()
+    {
+        //pattern #2 : a + 0 = a
+        //auto pattern = std::make_shared<pattern::op::Label>(ne_const_pred);
+        auto pattern = std::make_shared<pattern::op::Label>();
+        auto iconst0 = construct_constant_node(0);
+
+        NGRAPH_DEBUG << "IN TestGraphRewrite" << std::endl;
+
+        ngraph::pattern::gr_callback_fn callback = [pattern](pattern::Matcher& m)
+        {
+            NGRAPH_DEBUG << "IN CALLBACK" << std::endl;
+            assert(m.match_root()->get_arguments().size() == 2);
+
+            size_t const_node_index = m.match_root()->get_arguments().at(0) == pattern->get_binded_node();
+            auto const_node = dynamic_pointer_cast<op::ParameterizedConstant<element::Int32>>(m.match_root()->get_arguments().at(const_node_index));
+            auto second_node = m.match_root()->get_arguments().at(const_node_index);
+            NGRAPH_DEBUG << "second_node " << second_node->description() << " , " << second_node << std::endl;
+            NGRAPH_DEBUG << "pattern " << pattern->get_binded_node()->description() << " , " << pattern->get_binded_node() << std::endl;
+            assert(const_node);
+
+            auto pattern_value_type = dynamic_pointer_cast<const TensorViewType>(pattern->get_binded_node()->get_value_type());
+            auto const_node_value_type = dynamic_pointer_cast<const TensorViewType>(const_node->get_value_type());
+            assert(pattern_value_type && const_node);
+
+
+            if (pattern_value_type->get_element_type() != const_node_value_type->get_element_type() ||
+                pattern_value_type->get_shape() != const_node_value_type->get_shape())
+            {
+                NGRAPH_DEBUG << "TYPE/SHAPE" << std::endl;
+                return;
+            }
+
+            auto const_values = const_node->get_value()->get_vector();
+            bool all_zeros = std::all_of(begin(const_values), end(const_values), [](int e) {return e == 0; });
+
+            if (!all_zeros)
+            {
+                NGRAPH_DEBUG << "ALL_ZEROS" << std::endl;
+                return;
+            }
+
+            NGRAPH_DEBUG << "BEFORE REPLACE" << std::endl;
+            ngraph::pass::GraphRewrite::replace_node(m.match_root(), pattern->get_binded_node());
+        };
+
+        auto m = make_shared<pattern::Matcher>(pattern + iconst0, callback);
+        this->add_matcher(m);
+    }
+    
+    TestGraphRewrite() : GraphRewrite()
+    {
+        construct_multiply_by_one();
+        construct_add_zero();
     }
 
 };
 
+static void run_passes(pass::Manager& pass_manager, shared_ptr<Node> graph, std::vector<shared_ptr<op::Parameter>> parms) 
+{
+    auto shape = Shape{ 1 };
+    auto rt = make_shared<TensorViewType>(element::Int32::element_type(), shape);
+    auto func = make_shared<Function>(graph, rt, op::Parameters{ parms });
+    pass_manager.run_passes(func);
+}
 
 TEST(graph_rewrite, basic)
 {
-    auto shape = Shape{1};
-    auto a = make_shared<op::Parameter>(element::Int32::element_type(), shape);
-    auto b = make_shared<op::Parameter>(element::Int32::element_type(), shape);
-    auto c = make_shared<op::Parameter>(element::Int32::element_type(), shape);
-    auto d = make_shared<op::Parameter>(element::Int32::element_type(), shape);
-
-    auto t = ngraph::runtime::make_tensor<element::Int32>(shape);
-    (*t) = std::vector<int>{ 1 };
-    
+    auto shape = Shape{1};    
     pass::Manager pass_manager;
 
     pass_manager.register_pass<pass::TopologicalSort>();
     pass_manager.register_pass<pass::PropagateTypes>();
     pass_manager.register_pass<TestGraphRewrite>();
-
+    
     {
         auto a = make_shared<op::Parameter>(element::Int32::element_type(), shape);
         auto b = make_shared<op::Parameter>(element::Int32::element_type(), shape);
-        auto iconst1_0 = make_shared<op::ParameterizedConstant<element::Int32>>(shape, t);
-        auto mul = (a * iconst1_0);
-        auto graph = b + mul;
-        auto rt = make_shared<TensorViewType>(element::Int32::element_type(), shape);
-        auto func = make_shared<Function>(graph, rt, op::Parameters{ a, b });
-        pass_manager.run_passes(func);
-        auto graph_args = graph->get_arguments();
+        auto iconst0 = construct_constant_node(0);
+        auto graph = b + (a + iconst0);
+        run_passes(pass_manager, graph, { a, b });
         ASSERT_TRUE(graph->get_arguments().at(1) == a);
     }
 
     {
         auto a = make_shared<op::Parameter>(element::Int32::element_type(), shape);
         auto b = make_shared<op::Parameter>(element::Int32::element_type(), shape);
-        auto iconst1_0 = make_shared<op::ParameterizedConstant<element::Int32>>(shape, t);
-        auto graph = ((((a * iconst1_0) * iconst1_0) * iconst1_0) * iconst1_0) + b;
-        auto rt = make_shared<TensorViewType>(element::Int32::element_type(), shape);
-        auto func = make_shared<Function>(graph, rt, op::Parameters{ a, b });
-        pass_manager.run_passes(func);
+        auto iconst1 = construct_constant_node(1);
+        auto graph = b + (a * iconst1);
+        run_passes(pass_manager, graph, { a, b });
+        ASSERT_TRUE(graph->get_arguments().at(1) == a);
+    }
+
+    {
+        auto a = make_shared<op::Parameter>(element::Int32::element_type(), shape);
+        auto b = make_shared<op::Parameter>(element::Int32::element_type(), shape);
+        auto iconst1 = construct_constant_node(1);
+        auto graph = ((((a * iconst1) * iconst1) * iconst1) * iconst1) + b;
+        run_passes(pass_manager, graph, { a, b });
         ASSERT_TRUE(graph->get_arguments().at(0) == a);
     }
 
     {
-        NGRAPH_DEBUG << "CASCADE " << std::endl;
         auto a = make_shared<op::Parameter>(element::Int32::element_type(), shape);
         auto b = make_shared<op::Parameter>(element::Int32::element_type(), shape);
-        auto iconst1_0 = make_shared<op::ParameterizedConstant<element::Int32>>(shape, t);
-        //auto graph = b + (iconst1_0 * (iconst1_0 * (iconst1_0 * (iconst1_0 * a))));
-        auto graph = b + (iconst1_0 * (iconst1_0 * a));
-        auto rt = make_shared<TensorViewType>(element::Int32::element_type(), shape);
-        auto func = make_shared<Function>(graph, rt, op::Parameters{ a, b });
-        pass_manager.run_passes(func);
+        auto iconst0 = construct_constant_node(0);
+        auto iconst1 = construct_constant_node(1);
+        auto graph = b + (iconst0 + ((a + iconst0) * iconst1));
+        run_passes(pass_manager, graph, { a, b });
         ASSERT_TRUE(graph->get_arguments().at(1) == a);
     }
 
-
-    /*
-    auto pattern = std::make_shared<pattern::op::Label>();
-    auto sum = b + a;
-    auto pattern_mul = pattern_sum * a;
-    auto mul = sum * a;
-
-    ngraph::pattern::gr_callback_fn callback = [pattern](pattern::Matcher& m)
     {
-        pattern->get_binded_node()
-        ngraph::pass::GraphRewrite::replace_node(m.match_root(), m.pattern_node());
-    };
-    a * 1 = a;
-    auto m = make_shared<pattern::Matcher>(pattern + a);
-    auto pattern = std::make_shared<pattern::op::Pattern>(); //marker
-    ASSERT_TRUE(m->match(pattern_mul, mul));
+        auto a = make_shared<op::Parameter>(element::Int32::element_type(), shape);
+        auto b = make_shared<op::Parameter>(element::Int32::element_type(), shape);
+        auto iconst1 = construct_constant_node(1);
+        auto graph = b + (iconst1 * (iconst1 * (iconst1 * (iconst1 * a))));
+        run_passes(pass_manager, graph, { a, b });
+        ASSERT_TRUE(graph->get_arguments().at(1) == a);
+    }
 
-    ngraph::pass::GraphRewrite gr;
-
-    std::list<std::shared_ptr<Node>> nodes{ b, a, sum, mul };
-    gr.run_on_call_graph(nodes);
-    ASSERT_EQ(mul->get_arguments().at(0), pattern);
-    auto& sum_users = sum->users();
-    ASSERT_TRUE(sum_users.find(mul.get()) == sum_users.end());
-    */
 }
 
 
@@ -226,5 +272,4 @@ TEST(pattern, op_op)
 
     auto fconst1_0 = make_shared<op::ParameterizedConstant<element::Float32>>(shape, f);
     ASSERT_FALSE(n.match(pattern * fconst1_0, a * iconst1_1)); //different iconst 
-
 }
