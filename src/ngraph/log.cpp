be6ca906a0708d19486ec2947cc368879991155a
nikolay.korovaiko <korovaikon@gmail.com>
2017-11-08 09:13:11 -0800

    removing tracing changes

--- src/ngraph/log.cpp
+++ src/ngraph/log.cpp
@@ -93,9 +93,7 @@ void nervana::logger::log_item(const string& s)
     queue_condition.notify_one();
 }
 
-nervana::log_helper::log_helper(
-    LOG_TYPE type, const char* file, int line, const char* func, bool ds)
-    : dummy_stream(ds)
+nervana::log_helper::log_helper(LOG_TYPE type, const char* file, int line, const char* func)
 {
     switch (type)
     {
@@ -121,9 +119,6 @@ nervana::log_helper::log_helper(
 
 nervana::log_helper::~log_helper()
 {
-    if (!dummy_stream)
-    {
-        cout << _stream.str() << endl;
-        // logger::log_item(_stream.str());
-    }
+    cout << _stream.str() << endl;
+    // logger::log_item(_stream.str());
 }
