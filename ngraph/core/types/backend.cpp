7653460abaf00c0bcec8cf810e33d6d28a84415b
nishant.b.patel <nishant.b.patel@intel.com>
2017-11-14 15:03:16 -0800

    Modifying test.py and also removing import of NDArray

--- ngraph/core/types/backend.cpp
+++ ngraph/core/types/backend.cpp
@@ -16,11 +16,11 @@ PYBIND11_PLUGIN(clsBackend) {
     py::module mod("clsBackend");
     py::module::import("clsCallFrame");
     py::module::import("clsParameterizedTensorView");
-    py::module::import("clsNDArrayBase");
     using ET = ngraph::element::TraitedType<float>;
 
     py::class_<Backend, std::shared_ptr<Backend>> clsBackend(mod, "clsBackend");
     clsBackend.def("make_call_frame", &Backend::make_call_frame);
+    clsBackend.def("make_primary_tensor_view", &Backend::make_primary_tensor_view);
     clsBackend.def("make_parameterized_tensor_view", (std::shared_ptr<ParameterizedTensorView<ET>> (Backend::*) (const ngraph::Shape& )) &Backend::make_parameterized_tensor_view);
     clsBackend.def("make_parameterized_tensor_view", (std::shared_ptr<ParameterizedTensorView<ET>> (Backend::*) (const NDArrayBase<ET::type>& )) &Backend::make_parameterized_tensor_view);
     return mod.ptr();
--- ngraph/core/types/test.py
+++ ngraph/core/types/test.py
@@ -6,13 +6,17 @@ import clsManager
 
 element_type = clsTraitedType.TraitedTypeF.element_type()
 shape = [2,2]
-a = clsParameter.clsParameter(element_type, shape)
-b = clsParameter.clsParameter(element_type, shape)
-c = clsParameter.clsParameter(element_type, shape)
+A = clsParameter.clsParameter(element_type, shape)
+B = clsParameter.clsParameter(element_type, shape)
+C = clsParameter.clsParameter(element_type, shape)
 value_type = clsTensorViewType.clsTensorViewType(element_type, shape)
-parameter_list = [a, b, c]
-function = clsFunction.clsFunction((a + b)*c, value_type, parameter_list, 'test')
+parameter_list = [A, B, C]
+function = clsFunction.clsFunction((A + B)*C, value_type, parameter_list, 'test')
 manager = clsManager.clsManager.get('NGVM');
 external = manager.compile(function)
 backend = manager.allocate_backend()
 cf = backend.make_call_frame(external)
+a = backend.make_primary_tensor_view(element_type, shape)
+b = backend.make_primary_tensor_view(element_type, shape)
+c = backend.make_primary_tensor_view(element_type, shape)
+result = backend.make_primary_tensor_view(element_type, shape)
