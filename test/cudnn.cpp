479ab682d898b9f7abe9264645b2d520dd0b29e8
Tristan Webb <tristan.webb@intel.com>
2017-11-16 17:01:59 -0800

    Clang CUDA source example to later test compiling
    Clang cuda example from:
    https://gist.github.com/anonymous/855e277884eb6b388cd2f00d956c2fd4

--- test/cudnn.cpp
+++ test/cudnn.cpp
@@ -27,6 +27,7 @@
 #include "ngraph/runtime/gpu/manager.hpp"
 
 using namespace ngraph::runtime::gpu;
+using namespace std;
 
 TEST(cudnn, loadTest)
 {
@@ -40,4 +41,48 @@ TEST(cudnn, loadBackend)
   // auto gpu_backend = new GPUBackend();
   // auto gpu_external_function = new GPUExternalFunction();
   // auto gpu_manager = new GPUManager();
+}
+
+TEST(cudnn, compileTest)
+{
+  // constexpr auto name = "test.cpp";
+  const auto source = R"(
+  #include <iostream>
+
+    __global__ void axpy(float a, float* x, float* y) {
+      y[threadIdx.x] = a * x[threadIdx.x];
+    }
+
+    int main(int argc, char* argv[]) {
+      const int kDataLen = 4;
+
+      float a = 2.0f;
+      float host_x[kDataLen] = {1.0f, 2.0f, 3.0f, 4.0f};
+      float host_y[kDataLen];
+
+      // Copy input data to device.
+      float* device_x;
+      float* device_y;
+      cudaMalloc(&device_x, kDataLen * sizeof(float));
+      cudaMalloc(&device_y, kDataLen * sizeof(float));
+      cudaMemcpy(device_x, host_x, kDataLen * sizeof(float),
+                cudaMemcpyHostToDevice);
+
+      // Launch the kernel.
+      axpy<<<1, kDataLen>>>(a, device_x, device_y);
+
+      // Copy output data to host.
+      cudaDeviceSynchronize();
+      cudaMemcpy(host_y, device_y, kDataLen * sizeof(float),
+                cudaMemcpyDeviceToHost);
+
+      // Print the results.
+      for (int i = 0; i < kDataLen; ++i) {
+        std::cout << "y[" << i << "] = " << host_y[i] << "\n";
+      }
+
+      cudaDeviceReset();
+      return 0;
+    })";
+  EXPECT_EQ(source,source);
 }
