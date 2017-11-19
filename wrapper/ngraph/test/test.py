cb4ea600e0ce520efb82ea079511da8f73b6ba5b
pthoreho <pruthvi.gowda.thorehosur.appajigowda@intel.com>
2017-11-19 14:14:57 -0800

    - Added doc string to the test file and fixed bugs in the test.py

--- wrapper/ngraph/test/test.py
+++ wrapper/ngraph/test/test.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------------
+# Copyright 2016 Nervana Systems Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------
 import numpy as np
 
 import wrapper.ngraph.clsUtil as clsUtil
@@ -35,4 +50,4 @@ result.write(clsUtil.numpy_to_c(result_arr), 0, 16)
 cf.call([a, b, c], [result])
 
 result.read(clsUtil.numpy_to_c(result_arr), 0, 16)
-print result_arr
+#print(result_arr)
