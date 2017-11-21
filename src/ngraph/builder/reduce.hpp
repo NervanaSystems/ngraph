4f8427cecda24d27413e26dacdb6168ba504181d
Matthew Brookhart <matthew.i.brookhart@intel.com>
2017-11-20 17:23:44 -0800

    Make reduce inline, compiles, sum passes test

--- src/ngraph/builder/reduce.hpp
+++ src/ngraph/builder/reduce.hpp
@@ -17,15 +17,33 @@
 
 #include "ngraph/node.hpp"
 #include "ngraph/common.hpp"
+#include "ngraph/ops/constant.hpp"
+#include "ngraph/ops/parameter.hpp"
+#include "ngraph/ops/reduce.hpp"
+#include "ngraph/types/type.hpp"
+#include "ngraph/function.hpp"
 
 namespace ngraph
 {
     namespace builder
     {
         template <typename T>
-        std::shared_ptr<Node> create_reduction(
+        inline std::shared_ptr<Node> create_reduction(
             const std::shared_ptr<Node>& node, const std::string& init_val,
-            const AxisSet& reduction_axes);
+            const AxisSet& reduction_axes) 
+        {
+            const auto& et = node->get_element_type();
+
+            auto f_A = std::make_shared<op::Parameter>(et, Shape{});
+            auto f_B = std::make_shared<op::Parameter>(et, Shape{});
+            auto f_rt = std::make_shared<TensorViewType>(et, Shape{});
+            auto f = std::make_shared<Function>(std::make_shared<T>(f_A, f_B),
+                                                f_rt, op::Parameters{f_A, f_B});
+
+            auto init = std::make_shared<op::Constant>(et, Shape{}, init_val);
+
+            return std::make_shared<op::Reduce>(node, init, f, reduction_axes);
+        }
 
     } // namespace builder
 } // namespace ngraph
