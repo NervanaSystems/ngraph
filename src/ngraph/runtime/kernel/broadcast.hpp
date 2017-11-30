279b538e720268ac07f0110fac9abf9498c88fc8
Adam Procter <adam.m.procter@intel.com>
2017-11-30 14:15:40 -0800

    Simpler kernel for broadcast

--- src/ngraph/runtime/kernel/broadcast.hpp
+++ src/ngraph/runtime/kernel/broadcast.hpp
@@ -25,6 +25,40 @@ namespace ngraph
     {
         namespace kernel
         {
+            // TODO: this function should be moved for re-use.
+            Coordinate project_coordinate(const Coordinate& coord, const AxisSet& deleted_axes)
+            {
+                Coordinate result;
+
+                for (size_t i = 0; i < coord.size(); i++)
+                {
+                    if (deleted_axes.find(i) == deleted_axes.end())
+                    {
+                        result.push_back(coord[i]);
+                    }
+                }
+
+                return result;
+            }
+
+            // TODO: this function should be moved for re-use.
+            size_t index_in_dense_tensor(const Shape& tensor_shape, const Coordinate& coord)
+            {
+                size_t index = 0;
+                size_t stride = 1;
+
+                assert(tensor_shape.size() == coord.size()); // FIXME: don't use assert
+
+                for (size_t i = tensor_shape.size(); i-- > 0;)
+                {
+                    assert(coord[i] <= tensor_shape[i]); // FIXME: don't use assert
+                    index += coord[i] * stride;
+                    stride *= tensor_shape[i];
+                }
+
+                return index;
+            }
+
             template <typename T>
             void broadcast(T* arg,
                            T* out,
@@ -32,45 +66,17 @@ namespace ngraph
                            const Shape& out_shape,
                            const AxisSet& broadcast_axes)
             {
-                // For the outer loop we will walk over the entire input shape.
-                CoordinateIterator arg_iter(in_shape);
+                CoordinateIterator out_iter(out_shape);
 
                 do
                 {
-                    // For the inner loop we will walk across the entire axis for the new broadcast axes, and stay put at the current arg position for the existing axes.
-                    Coordinate arg_coordinate = arg_iter.get_current_coordinate();
+                    auto out_coord = out_iter.get_current_coordinate();
+                    auto out_index = out_iter.get_current_index();
+                    auto in_coord = project_coordinate(out_coord, broadcast_axes);
+                    auto in_index = index_in_dense_tensor(in_shape, in_coord);
 
-                    Strides out_strides(out_shape.size(), 1);
-                    Coordinate out_outer_corner(out_shape.size());
-                    Coordinate out_inner_corner(out_shape.size());
-
-                    size_t arg_pos = 0;
-
-                    for (size_t i = 0; i < out_shape.size(); i++)
-                    {
-                        if (broadcast_axes.find(i) == broadcast_axes.end())
-                        {
-                            // This is an existing axis.
-                            out_outer_corner[i] = arg_coordinate[arg_pos];
-                            out_inner_corner[i] = arg_coordinate[arg_pos];
-                            arg_pos++;
-                        }
-                        else
-                        {
-                            // This is a new broadcast axis.
-                            out_outer_corner[i] = out_shape[i];
-                            out_inner_corner[i] = 0;
-                        }
-                    }
-
-                    CoordinateIterator out_iter(
-                        out_shape, out_strides, out_outer_corner, out_inner_corner);
-
-                    do
-                    {
-                        out[out_iter.get_current_index()] = arg[arg_iter.get_current_index()];
-                    } while (out_iter.increment());
-                } while (arg_iter.increment());
+                    out[out_index] = arg[in_index];
+                } while (out_iter.increment());
             }
         }
     }
