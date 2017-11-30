3a25531e820b028238534583551b7fed76495c62
Jaikrishnan Menon <jaikrishnan.menon@intel.com>
2017-11-30 12:52:24 -0800

    CMake: Rewrite TBB integration

--- cmake/external_tbb.cmake
+++ cmake/external_tbb.cmake
@@ -11,46 +11,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include(ExternalProject)
+#----------------------------------------------------------------------------------------------------------
+# Fetch and configure TBB
+#----------------------------------------------------------------------------------------------------------
 
-#----------------------------------------------------------------------------------------------------------
-# Fetch and install TBB
-#----------------------------------------------------------------------------------------------------------
+set(TBB_GIT_REPO_URL https://github.com/01org/tbb)
+set(TBB_GIT_TAG "tbb_2018")
 
 if(NOT ${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+  configure_file(${CMAKE_SOURCE_DIR}/cmake/tbb_fetch.cmake.in ${CMAKE_CURRENT_BINARY_DIR}/tbb/CMakeLists.txt)
+  execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
+    WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/tbb")
+  execute_process(COMMAND "${CMAKE_COMMAND}" --build .
+    WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/tbb")
 
-    set(TBB_GIT_REPO_URL https://github.com/01org/tbb)
-    set(TBB_GIT_TAG "tbb_2018")
-    set(TBB_INSTALL_DIR ${EXTERNAL_INSTALL_DIR}/tbb)
-
-    # The 'BUILD_BYPRODUCTS' argument was introduced in CMake 3.2.
-    if(${CMAKE_VERSION} VERSION_LESS 3.2)
-        ExternalProject_Add(
-            ext_tbb
-            GIT_REPOSITORY ${TBB_GIT_REPO_URL}
-            GIT_TAG ${TBB_GIT_TAG}
-            CONFIGURE_COMMAND ""
-            BUILD_COMMAND ""
-            UPDATE_COMMAND ""
-            INSTALL_COMMAND ""
-            )
-    else()
-        ExternalProject_Add(
-            ext_tbb
-            GIT_REPOSITORY ${TBB_GIT_REPO_URL}
-            GIT_TAG ${TBB_GIT_TAG}
-            CONFIGURE_COMMAND ""
-            BUILD_COMMAND ""
-            UPDATE_COMMAND ""
-            INSTALL_COMMAND ""
-            BUILD_BYPRODUCTS "${TBB_INSTALL_DIR}/include/tbb/tbb.h"
-            )
-    endif()
-
-    ExternalProject_Get_Property(ext_tbb source_dir binary_dir)
-
-    include(${source_dir}/cmake/TBBBuild.cmake)
-    tbb_build(TBB_ROOT ${source_dir} MAKE_ARGS compiler=clang CONFIG_DIR TBB_DIR)
+    include(${CMAKE_CURRENT_BINARY_DIR}/tbb/tbb-src/cmake/TBBBuild.cmake)
+    tbb_build(TBB_ROOT ${CMAKE_CURRENT_BINARY_DIR}/tbb/tbb-src MAKE_ARGS compiler=clang CONFIG_DIR TBB_DIR)
     find_package(TBB REQUIRED tbb)
-
 endif()
