690148b59256f6accde547bd7a71ed37442a08fd
Adam Procter <adam.m.procter@intel.com>
2017-12-20 15:14:36 -0800

    Docstring repair

--- src/ngraph/ops/reverse.hpp
+++ src/ngraph/ops/reverse.hpp
@@ -38,9 +38,9 @@ namespace ngraph
         ///
         /// ## Output
         ///
-        /// | Type                   | Description                                                                                                                                                                  |
-        /// | ---------------------- | ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
-        /// | \f$E[d_1,\dots,d_n]\f$ | The tensor \f$T\f$, where \f$T[i_1,\dots,i_n] = \texttt{arg}[j_1,\dots,j_n]\f$ and \f$j_k = d_k - i_k - 1\f$\f$ if axis \f$k\f$ is in the reverse set; else \f$j_k = i_k\f$. |
+        /// | Type                   | Description                                                                                                                                                               |
+        /// | ---------------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+        /// | \f$E[d_1,\dots,d_n]\f$ | The tensor \f$T\f$, where \f$T[i_1,\dots,i_n] = \texttt{arg}[j_1,\dots,j_n]\f$ and \f$j_k = d_k - i_k - 1\f$ if axis \f$k\f$ is in the reverse set; else \f$j_k = i_k\f$. |
         class Reverse : public RequiresTensorViewArgs
         {
         public:
