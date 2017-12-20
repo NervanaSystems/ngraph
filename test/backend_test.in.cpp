06f3d30f77fd42f54a4a7f21af4e39567c7b836a
Robert Kimball <robert.kimball@intel.com>
2017-12-19 16:00:14 -0800

    aliased output unit test

--- test/backend_test.in.cpp
+++ test/backend_test.in.cpp
@@ -34,6 +34,41 @@ static void copy_data(shared_ptr<runtime::TensorView> tv, const vector<T>& data)
     tv->write(data.data(), 0, data_size);
 }
 
+TEST(${BACKEND_NAME}, aliased_output)
+{
+    using f32 = element::Float32;
+
+    auto shape = Shape{2, 2};
+    auto A = make_shared<op::Parameter>(f32::element_type(), shape);
+    auto B = make_shared<op::Parameter>(f32::element_type(), shape);
+    auto rt1 = make_shared<TensorViewType>(f32::element_type(), shape);
+    auto rt2 = make_shared<TensorViewType>(f32::element_type(), shape);
+    auto C = A + B;
+    auto f = make_shared<Function>(Nodes{C, C}, ValueTypes{rt1, rt2}, op::Parameters{A, B});
+
+    auto manager = runtime::Manager::get("${BACKEND_NAME}");
+    auto external = manager->compile(f);
+    auto backend = manager->allocate_backend();
+    auto cf = backend->make_call_frame(external);
+
+    // Create some tensors for input/output
+    shared_ptr<runtime::TensorView> a =
+        backend->make_primary_tensor_view(f32::element_type(), shape);
+    shared_ptr<runtime::TensorView> b =
+        backend->make_primary_tensor_view(f32::element_type(), shape);
+    shared_ptr<runtime::TensorView> out1 =
+        backend->make_primary_tensor_view(f32::element_type(), shape);
+    shared_ptr<runtime::TensorView> out2 =
+        backend->make_primary_tensor_view(f32::element_type(), shape);
+
+    copy_data(a, vector<float>{0, 1, 2, 3});
+    copy_data(b, vector<float>{1, 2, 3, 4});
+
+    cf->call({a, b}, {out1, out2});
+    // EXPECT_EQ(result->get_vector<float>(),
+    //           (test::NDArray<float, 2>({{6, 8}, {10, 12}})).get_vector());
+}
+
 TEST(${BACKEND_NAME}, parameter_as_output)
 {
     auto shape = Shape{3, 4};
