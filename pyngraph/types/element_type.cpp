df28eee04c55d30992bb039dc85a4d77a6f78b25
Sang Ik Lee <sang.ik.lee@intel.com>
2018-01-09 15:50:36 -0800

    Use pybind attr for Type.

--- pyngraph/types/element_type.cpp
+++ pyngraph/types/element_type.cpp
@@ -20,29 +20,17 @@
 
 namespace py = pybind11;
 
-const ngraph::element::Type& boolean() { return ngraph::element::boolean; }
-const ngraph::element::Type& f32() { return ngraph::element::f32; }
-const ngraph::element::Type& f64() { return ngraph::element::f64; }
-const ngraph::element::Type& i8() { return ngraph::element::i8; }
-const ngraph::element::Type& i16() { return ngraph::element::i16; }
-const ngraph::element::Type& i32() { return ngraph::element::i32; }
-const ngraph::element::Type& i64() { return ngraph::element::i64; }
-const ngraph::element::Type& u8() { return ngraph::element::u8; }
-const ngraph::element::Type& u16() { return ngraph::element::u16; }
-const ngraph::element::Type& u32() { return ngraph::element::u32; }
-const ngraph::element::Type& u64() { return ngraph::element::u64; }
-
 void regclass_pyngraph_Type(py::module m){
     py::class_<ngraph::element::Type, std::shared_ptr<ngraph::element::Type>> type(m, "Type");
-    type.def_static("boolean", &boolean, py::return_value_policy::reference);
-    type.def_static("f32", &f32, py::return_value_policy::reference);
-    type.def_static("f64", &f64, py::return_value_policy::reference);
-    type.def_static("i8", &i8, py::return_value_policy::reference);
-    type.def_static("i16", &i16, py::return_value_policy::reference);
-    type.def_static("i32", &i32, py::return_value_policy::reference);
-    type.def("i64", &i64, py::return_value_policy::reference);
-    type.def("u8", &u8, py::return_value_policy::reference);
-    type.def("u16", &u16, py::return_value_policy::reference);
-    type.def("u32", &u32, py::return_value_policy::reference);
-    type.def("u64", &u64, py::return_value_policy::reference);
+    type.attr("boolean") = ngraph::element::boolean;
+    type.attr("f32")     = ngraph::element::f32;
+    type.attr("f64")     = ngraph::element::f64;
+    type.attr("i8")      = ngraph::element::i8;
+    type.attr("i16")     = ngraph::element::i16;
+    type.attr("i32")     = ngraph::element::i32;
+    type.attr("i64")     = ngraph::element::i64;
+    type.attr("u8")      = ngraph::element::u8;
+    type.attr("u16")     = ngraph::element::u16;
+    type.attr("u32")     = ngraph::element::u32;
+    type.attr("u64")     = ngraph::element::u64;
 }
