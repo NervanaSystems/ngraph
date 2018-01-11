2182b250a61b4d80e00bd59fbab7b4107c896194
Wenzhe Xue <wenzhe@nervanasys.com>
2018-01-11 11:30:49 -0800

    wip: include mpi path to test CMakeLists

--- test/mlsl.cpp
+++ test/mlsl.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // ----------------------------------------------------------------------------
 
-//#include "mpi.h"
+#include "mpi.h"
 //#include "mlsl.hpp"
 
 #include <gtest/gtest.h>
