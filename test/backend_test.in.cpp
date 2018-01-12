4776e81356c460eec7c4a1783e6116a5da29b083
Matthew Brookhart <matthew.i.brookhart@intel.com>
2018-01-12 15:04:18 -0800

    rename for clarity

--- test/backend_test.in.cpp
+++ test/backend_test.in.cpp
@@ -5164,7 +5164,7 @@ TEST(${BACKEND_NAME}, reduce_window_emulating_max_pool_2d_1channel_1image_stride
 }
 
 template <typename T>
-void make_unary_test()
+void make_unary_empty_test()
 {
     auto shape = Shape{0};
     auto A = make_shared<op::Parameter>(element::f32, shape);
@@ -5183,7 +5183,7 @@ void make_unary_test()
 }
 
 template <typename T>
-void make_binary_test()
+void make_binary_empty_test()
 {
     auto shape = Shape{0};
     auto A = make_shared<op::Parameter>(element::f32, shape);
@@ -5205,27 +5205,27 @@ void make_binary_test()
 
 TEST(${BACKEND_NAME}, zero_sized_abs)
 {
-    make_unary_test<op::Abs>();
+    make_unary_empty_test<op::Abs>();
 }
 TEST(${BACKEND_NAME}, zero_sized_ceiling)
 {
-    make_unary_test<op::Ceiling>();
+    make_unary_empty_test<op::Ceiling>();
 }
 TEST(${BACKEND_NAME}, zero_sized_exp)
 {
-    make_unary_test<op::Exp>();
+    make_unary_empty_test<op::Exp>();
 }
 TEST(${BACKEND_NAME}, zero_sized_floor)
 {
-    make_unary_test<op::Floor>();
+    make_unary_empty_test<op::Floor>();
 }
 TEST(${BACKEND_NAME}, zero_sized_log)
 {
-    make_unary_test<op::Log>();
+    make_unary_empty_test<op::Log>();
 }
 TEST(${BACKEND_NAME}, zero_sized_negative)
 {
-    make_unary_test<op::Negative>();
+    make_unary_empty_test<op::Negative>();
 }
 TEST(${BACKEND_NAME}, zero_sized_not)
 {
@@ -5247,97 +5247,97 @@ TEST(${BACKEND_NAME}, zero_sized_not)
 }
 TEST(${BACKEND_NAME}, zero_sized_sign)
 {
-    make_unary_test<op::Sign>();
+    make_unary_empty_test<op::Sign>();
 }
 TEST(${BACKEND_NAME}, zero_sized_sqrt)
 {
-    make_unary_test<op::Sqrt>();
+    make_unary_empty_test<op::Sqrt>();
 }
 TEST(${BACKEND_NAME}, zero_sized_sin)
 {
-    make_unary_test<op::Sin>();
+    make_unary_empty_test<op::Sin>();
 }
 TEST(${BACKEND_NAME}, zero_sized_sinh)
 {
-    make_unary_test<op::Sinh>();
+    make_unary_empty_test<op::Sinh>();
 }
 TEST(${BACKEND_NAME}, zero_sized_cos)
 {
-    make_unary_test<op::Cos>();
+    make_unary_empty_test<op::Cos>();
 }
 TEST(${BACKEND_NAME}, zero_sized_cosh)
 {
-    make_unary_test<op::Cosh>();
+    make_unary_empty_test<op::Cosh>();
 }
 TEST(${BACKEND_NAME}, zero_sized_tan)
 {
-    make_unary_test<op::Tan>();
+    make_unary_empty_test<op::Tan>();
 }
 TEST(${BACKEND_NAME}, zero_sized_tanh)
 {
-    make_unary_test<op::Tanh>();
+    make_unary_empty_test<op::Tanh>();
 }
 TEST(${BACKEND_NAME}, zero_sized_asin)
 {
-    make_unary_test<op::Asin>();
+    make_unary_empty_test<op::Asin>();
 }
 TEST(${BACKEND_NAME}, zero_sized_acos)
 {
-    make_unary_test<op::Acos>();
+    make_unary_empty_test<op::Acos>();
 }
 TEST(${BACKEND_NAME}, zero_sized_atan)
 {
-    make_unary_test<op::Atan>();
+    make_unary_empty_test<op::Atan>();
 }
 TEST(${BACKEND_NAME}, zero_sized_add)
 {
-    make_binary_test<op::Add>();
+    make_binary_empty_test<op::Add>();
 }
 TEST(${BACKEND_NAME}, zero_sized_divide)
 {
-    make_binary_test<op::Divide>();
+    make_binary_empty_test<op::Divide>();
 }
 TEST(${BACKEND_NAME}, zero_sized_eq)
 {
-    make_binary_test<op::Equal>();
+    make_binary_empty_test<op::Equal>();
 }
 TEST(${BACKEND_NAME}, zero_sized_greater)
 {
-    make_binary_test<op::Greater>();
+    make_binary_empty_test<op::Greater>();
 }
 TEST(${BACKEND_NAME}, zero_sized_greatereq)
 {
-    make_binary_test<op::GreaterEq>();
+    make_binary_empty_test<op::GreaterEq>();
 }
 TEST(${BACKEND_NAME}, zero_sized_less)
 {
-    make_binary_test<op::Less>();
+    make_binary_empty_test<op::Less>();
 }
 TEST(${BACKEND_NAME}, zero_sized_lesseq)
 {
-    make_binary_test<op::LessEq>();
+    make_binary_empty_test<op::LessEq>();
 }
 TEST(${BACKEND_NAME}, zero_sized_maximum)
 {
-    make_binary_test<op::Maximum>();
+    make_binary_empty_test<op::Maximum>();
 }
 TEST(${BACKEND_NAME}, zero_sized_minimum)
 {
-    make_binary_test<op::Minimum>();
+    make_binary_empty_test<op::Minimum>();
 }
 TEST(${BACKEND_NAME}, zero_sized_multiply)
 {
-    make_binary_test<op::Multiply>();
+    make_binary_empty_test<op::Multiply>();
 }
 TEST(${BACKEND_NAME}, zero_sized_not_equal)
 {
-    make_binary_test<op::NotEqual>();
+    make_binary_empty_test<op::NotEqual>();
 }
 TEST(${BACKEND_NAME}, zero_sized_power)
 {
-    make_binary_test<op::Power>();
+    make_binary_empty_test<op::Power>();
 }
 TEST(${BACKEND_NAME}, zero_sized_subtract)
 {
-    make_binary_test<op::Subtract>();
+    make_binary_empty_test<op::Subtract>();
 }
