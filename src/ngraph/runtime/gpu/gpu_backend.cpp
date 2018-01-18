5604e4806a826ea8d554f53935a9d747dd8ccc98
Robert Kimball <robert.kimball@intel.com>
2018-01-18 15:08:35 -0800

    wip

--- src/ngraph/runtime/gpu/gpu_backend.cpp
+++ src/ngraph/runtime/gpu/gpu_backend.cpp
@@ -27,9 +27,9 @@ std::shared_ptr<ngraph::runtime::CallFrame> runtime::gpu::GPU_Backend::make_call
 }
 
 std::shared_ptr<ngraph::runtime::TensorView>
-    runtime::gpu::GPU_Backend::make_primary_tensor_view(const ngraph::element::Type& element_type,
-                                                        const Shape& shape)
+    runtime::gpu::GPU_Backend::make_device_tensor(const ngraph::element::Type& element_type,
+                                                const Shape& shape)
 {
-    auto rc = make_shared<runtime::cpu::CPU_TensorView>(element_type, shape);
+    auto rc = make_shared<runtime::TensorView>(element_type, shape);
     return dynamic_pointer_cast<runtime::TensorView>(rc);
 }
