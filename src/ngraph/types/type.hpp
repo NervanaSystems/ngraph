42b34d5cf74dbe54d1c69929110e8fbb19cbbabf
Scott Cyphers <scott.cyphers@intel.com>
2018-01-23 16:16:44 -0800

    Remove TupleType, ValueType

--- src/ngraph/types/type.hpp
+++ src/ngraph/types/type.hpp
@@ -22,87 +22,30 @@
 
 namespace ngraph
 {
-    class TensorViewType;
-    class TupleType;
-
-    /// ValueType is
-    ///   TensorViewType
-    ///   | TupleType(ValueType[])
-    class ValueType
-    {
-        ValueType(const ValueType&) = delete;
-        ValueType& operator=(const ValueType&) = delete;
-
-    protected:
-        ValueType() {}
-    public:
-        virtual ~ValueType() {}
-        virtual bool operator==(const ValueType& that) const = 0;
-        bool operator!=(const ValueType& that) const;
-        /// Add tensor views in depth-first order.
-        virtual void collect_tensor_views(
-            std::vector<std::shared_ptr<const TensorViewType>>& views) const = 0;
-        virtual const Shape& get_shape() const = 0;
-        virtual const element::Type& get_element_type() const = 0;
-        friend std::ostream& operator<<(std::ostream&, const ValueType&);
-    };
-
     /// Describes a tensor view; an element type and a shape.
-    class TensorViewType : public ValueType, public std::enable_shared_from_this<TensorViewType>
+    class TensorViewType : public std::enable_shared_from_this<TensorViewType>
     {
+        TensorViewType& operator=(const ValueType&) = delete;
+
     public:
         /// /param element_type The type of the tensor elements.
         /// /param shape The shape of the tensor.
         TensorViewType(const element::Type& element_type, const Shape& shape)
-            : ValueType()
-            , m_element_type(element_type)
+            : m_element_type(element_type)
             , m_shape(shape)
         {
         }
 
-        virtual const element::Type& get_element_type() const override { return m_element_type; }
-        virtual const Shape& get_shape() const override { return m_shape; }
-        virtual bool operator==(const ValueType& that) const override;
-        virtual void collect_tensor_views(
-            std::vector<std::shared_ptr<const TensorViewType>>& views) const override;
+        const element::Type& get_element_type() const { return m_element_type; }
+        const Shape& get_shape() const { return m_shape; }
+        bool operator==(const TensorViewType& that) const;
+        bool operator!=(const TensorViewType& that) const;
+        void collect_tensor_views(std::vector<std::shared_ptr<const TensorViewType>>& views) const;
 
         friend std::ostream& operator<<(std::ostream&, const TensorViewType&);
 
     protected:
         const element::Type m_element_type;
         Shape m_shape;
-    };
-
-    /// Describes a tuple of values; a vector of types
-    class TupleType : public ValueType
-    {
-    public:
-        /// Construct empty tuple and add value types later.
-        TupleType() {}
-        /// @param element_types A vector of types for the tuple elements
-        TupleType(const std::vector<std::shared_ptr<const ValueType>>& element_types)
-            : m_element_types(element_types)
-        {
-        }
-
-        const std::vector<std::shared_ptr<const ValueType>> get_element_types() const
-        {
-            return m_element_types;
-        }
-        std::vector<std::shared_ptr<const ValueType>> set_element_types()
-        {
-            return m_element_types;
-        }
-
-        virtual const element::Type& get_element_type() const override;
-
-        virtual bool operator==(const ValueType& that) const override;
-        virtual void collect_tensor_views(
-            std::vector<std::shared_ptr<const TensorViewType>>& views) const override;
-        virtual const Shape& get_shape() const override;
-        friend std::ostream& operator<<(std::ostream&, const TupleType&);
-
-    protected:
-        std::vector<std::shared_ptr<const ValueType>> m_element_types;
     };
 }
