f784b0da17df607195d96f01537ac4b6965e9384
Robert Kimball <robert.kimball@intel.com>
2018-02-03 08:19:01 -0800

    add clone unit test (#442)
    fix clone of function with multiple outputs

--- src/ngraph/graph_util.cpp
+++ src/ngraph/graph_util.cpp
@@ -239,8 +239,12 @@ std::shared_ptr<ngraph::Function> ngraph::clone_function(std::shared_ptr<ngraph:
     // clone function operations
     clone_nodes(func->get_ops(), node_map);
 
-    // get cloned function result and parameters
-    auto cloned_result = node_map.get(func->get_result());
+    // get cloned function results and parameters
+    Nodes cloned_results;
+    for (shared_ptr<Node> node : func->get_results())
+    {
+        cloned_results.push_back(node_map.get(node));
+    }
     std::vector<std::shared_ptr<op::Parameter>> cloned_params;
     for (auto param : func->get_parameters())
     {
@@ -248,5 +252,5 @@ std::shared_ptr<ngraph::Function> ngraph::clone_function(std::shared_ptr<ngraph:
     }
 
     // create and return cloned function
-    return std::make_shared<ngraph::Function>(cloned_result, cloned_params);
+    return std::make_shared<ngraph::Function>(cloned_results, cloned_params);
 }
