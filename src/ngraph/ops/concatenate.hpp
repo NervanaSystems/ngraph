88bc37f147b0de51a174e60029af245c0ae14ab8
Scott Cyphers <diyessi@users.noreply.github.com>
2018-02-13 11:39:05 -0800

    Some more op doc (#491)
    * Some more op doc
    
    * Review comments
    
    * Review comments.
    
    * Review comments

--- src/ngraph/ops/concatenate.hpp
+++ src/ngraph/ops/concatenate.hpp
@@ -25,40 +25,6 @@ namespace ngraph
     namespace op
     {
         /// \brief Concatenation operation.
-        ///
-        /// Given an axis index \f$a\f$ and a rank \f$r \geq 1\f$ where \f$0 \leq a \lt r\f$, and one or more \f$r\f$-tensors
-        /// with the same element type whose shapes are the same except possibly at axis \f$a\f$, the tensors are
-        /// concatenated along axis \f$a\f$.
-        ///
-        /// For example:
-        ///   1. Concatenating matrices on axis 0 (the row axis) stacks the matrices from top to bottom.
-        ///      The number of rows in the resulting matrix is the sum of the number of rows for each
-        ///      input matrix.
-        ///   2. Concatenating matrices on axis 1 (the column axis) concatenates them from left to right.
-        ///      The number of columns in the resulting matrix is the sum of the number of columns for each
-        ///      input matrix.
-        ///   3. Concatenating 3-tensors on axis 2 (the depth axis) stacks them from front to back.
-        ///      The depth of the resulting tensor is the sum of the total depth for each input tensor.
-        ///
-        /// The resulting tensor will have the same rank as the input tensors.
-        ///
-        /// ## Parameters
-        ///
-        /// |                      | Description                                                    |
-        /// | -------------------- | -------------------------------------------------------------- |
-        /// | `concatenation_axis` | The axis \f$a\f$ along which to concatenate the input tensors. |
-        ///
-        /// ## Inputs
-        ///
-        /// |                 | Type                                                          | Description                                                                                                              |
-        /// | --------------- | ------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------------------------ |
-        /// | `args`[\f$i\f$] | \f$E[d_1,\dots,d_{a-1},d^i_a,d_{a+1},\dots,d_n]~(n \geq 1)\f$ | One or more input tensors, all of which have the same element type, and the same shape, except possibly at axis \f$a\f$. |
-        ///
-        /// ## Output
-        ///
-        /// | Type                                                         | Description                                                                                     |
-        /// | ------------------------------------------------------------ | ----------------------------------------------------------------------------------------------- |
-        /// | \f$E[d_1,\dots,d_{a-1},\Sigma_i(d^i_a),d_{a+1},\dots,d_n]\f$ | The tensor \f$T\f$, where \f$T\f$ is the concatenation of the input tensors along axis \f$a\f$. |
         class Concat : public RequiresTensorViewArgs
         {
         public:
