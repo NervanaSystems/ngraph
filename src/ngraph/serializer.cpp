1d136552194512930f8f93d72bbd96dd2250978b
sasadep <sandeep.aswath.narayana@intel.com>
2018-02-13 13:45:27 -0800

    relu in serializer

--- src/ngraph/serializer.cpp
+++ src/ngraph/serializer.cpp
@@ -54,6 +54,7 @@
 #include "ngraph/ops/power.hpp"
 #include "ngraph/ops/reduce.hpp"
 #include "ngraph/ops/reduce_window.hpp"
+#include "ngraph/ops/relu.hpp"
 #include "ngraph/ops/remainder.hpp"
 #include "ngraph/ops/replace_slice.hpp"
 #include "ngraph/ops/reshape.hpp"
@@ -652,6 +653,10 @@ static shared_ptr<ngraph::Function>
         {
             node = make_shared<op::Remainder>(args[0], args[1]);
         }
+        else if (node_op == "Relu")
+        {
+            node = make_shared<op::Relu>(args[0]);
+        }
         else if (node_op == "ReplaceSlice")
         {
             auto lower_bounds = node_js.at("lower_bounds").get<vector<size_t>>();
@@ -995,6 +1000,9 @@ static json write(const Node& n)
         node["function"] = tmp->get_functions()[0]->get_name();
         node["window_shape"] = tmp->get_window_shape();
         node["window_movement_strides"] = tmp->get_window_movement_strides();
+    }
+    else if (node_op == "Relu")
+    {
     }
     else if (node_op == "Remainder")
     {
