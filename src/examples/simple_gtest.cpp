a1962e769182f03e218328e3e380b37aec7698f2
Ashok Emani <ashok.emani@intel.com>
2018-02-14 22:15:19 -0800

    add cmake for nbench, address review comments

--- src/examples/simple_gtest.cpp
+++ src/examples/simple_gtest.cpp
@@ -1,23 +1,28 @@
-// ----------------------------------------------------------------------------
-// Copyright 2017 Nervana Systems Inc.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// ----------------------------------------------------------------------------
+/*******************************************************************************
+* Copyright 2017-2018 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
 
 // Standalone Goole Test example for ngraph.
 // compile and test as follows.
-// g++ -std=c++11 simple_gtest.cc -I$HOME/ngraph_dist/include -L$HOME/ngraph_dist/lib -lngraph -lpthread -lgtest -o /tmp/test
+// g++ -std=c++11 simple_gtest.cpp -I$HOME/ngraph_dist/include -L$HOME/ngraph_dist/lib -pthread -lngraph -lgtest -o /tmp/test
 // env LD_LIBRARY_PATH=$HOME/ngraph_dist/lib /tmp/test
+
 #include <gtest/gtest.h>
-#include "nutils.h"
+#include <ngraph/ngraph.hpp>
+#include "../../test/util/test_tools.hpp"
+#include "nutils.hpp"
 using namespace std;
 using namespace ngraph;
 
