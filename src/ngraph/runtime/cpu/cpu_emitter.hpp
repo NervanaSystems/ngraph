e6e5eb2d9e09e9126c0180c1bd6f4388b4d3ea43
sasadep <sandeep.aswath.narayana@intel.com>
2018-02-16 13:38:42 -0800

    fix confict from rebase

--- src/ngraph/runtime/cpu/cpu_emitter.hpp
+++ src/ngraph/runtime/cpu/cpu_emitter.hpp
@@ -50,7 +50,6 @@ namespace ngraph
                     throw std::runtime_error("Unimplemented op in CPU emitter");
                 }
 
-
                 static void nop(CPU_ExternalFunction* external_function,
                                 codegen::CodeWriter& writer,
                                 const ngraph::Node* node,
