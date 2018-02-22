a22e5387697236eb8796e4b2c608cd3aad2240f1
baojun-nervana <baojun.liu@intel.com>
2018-02-22 15:51:57 -0800

    Added argon backend

--- conftest.py
+++ conftest.py
@@ -17,8 +17,8 @@ import pytest
 
 
 def pytest_addoption(parser):
-    parser.addoption('--backend', default='INTERPRETER',
-                     choices=['INTERPRETER', 'CPU', 'GPU'], help='Select from available backends')
+    parser.addoption('--backend', default='CPU',
+                     choices=['INTERPRETER', 'CPU', 'GPU', 'ARGON'], help='Select from available backends')
 
 
 def pass_method(*args, **kwargs):
@@ -28,4 +28,5 @@ def pass_method(*args, **kwargs):
 def pytest_configure(config):
     config.gpu_skip = pytest.mark.skipif(config.getvalue('backend') == 'GPU')
     config.cpu_skip = pytest.mark.skipif(config.getvalue('backend') == 'CPU')
+    config.argon_skip = pytest.mark.skipif(config.getvalue('backend') == 'ARGON')
     config.interpreter_skip = pytest.mark.skipif(config.getvalue('backend') == 'INTERPRETER')
