de9d850d9aab98a535675a58d723b604859662ba
Adam Rogowiec <adam.rogowiec@intel.com>
2018-02-22 12:53:23 +0100

    Fix function signature type annotation.

--- ngraph_api/ops.py
+++ ngraph_api/ops.py
@@ -214,6 +214,6 @@ def tanh(node, name=None):  # type: (Node, str) -> Node
 # matmul ops
 @nameable_op
 def dot(left_node, right_node, name=None):
-    # type: (Node, Node, int, str) -> Node
+    # type: (Node, Node, str) -> Node
     """Return node which performs matrix multiplication of two input nodes."""
     return Dot(left_node, right_node)
