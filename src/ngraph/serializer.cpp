faa7c3ef41ab5454a818b723cd305c930513d700
pthoreho <pruthvi.gowda.thorehosur.appajigowda@intel.com>
2018-03-02 11:34:38 -0800

    - Added support optimized bn mkldnn implementation in cpu emitter - modified bn unit_test to support new implementation - added layout assignment for bn op - Style Fix
    (cherry picked from commit 7747a40806d62c126059d5c873adcd2e61a0adb0)

--- src/ngraph/serializer.cpp
+++ src/ngraph/serializer.cpp
@@ -325,13 +325,7 @@ static shared_ptr<ngraph::Function>
         else if (node_op == "BatchNorm")
         {
             auto epsilon = node_js.at("eps").get<double>();
-            node = make_shared<op::BatchNorm>(epsilon, args[0], args[1], args[2], args[3], args[4]);
-        }
-        else if (node_op == "BatchNormBackprop")
-        {
-            auto epsilon = node_js.at("eps").get<double>();
-            node = make_shared<op::BatchNormBackprop>(
-                epsilon, args[0], args[1], args[2], args[3], args[4], args[5]);
+            node = make_shared<op::BatchNorm>(epsilon, args[0], args[1], args[2]);
         }
         else if (node_op == "Broadcast")
         {
@@ -488,10 +482,10 @@ static shared_ptr<ngraph::Function>
             shared_ptr<Function> f_ptr = function_map.at(function_name);
             node = make_shared<op::FunctionCall>(f_ptr, args);
         }
-        else if (node_op == "GetOutputElement")
-        {
-            node = make_shared<op::GetOutputElement>(args[0], node_js.at("n").get<size_t>());
-        }
+        // else if (node_op == "GetOutputElement")
+        // {
+        //     node = make_shared<op::GetOutputElement>(args[0]);
+        // }
         else if (node_op == "Greater")
         {
             node = make_shared<op::Greater>(args[0], args[1]);
@@ -841,11 +835,6 @@ static json write(const Node& n)
         auto tmp = dynamic_cast<const op::BatchNorm*>(&n);
         node["eps"] = tmp->get_eps_value();
     }
-    else if (node_op == "BatchNormBackprop")
-    {
-        auto tmp = dynamic_cast<const op::BatchNormBackprop*>(&n);
-        node["eps"] = tmp->get_eps_value();
-    }
     else if (node_op == "Broadcast")
     {
         auto tmp = dynamic_cast<const op::Broadcast*>(&n);
@@ -930,8 +919,6 @@ static json write(const Node& n)
     }
     else if (node_op == "GetOutputElement")
     {
-        auto tmp = dynamic_cast<const op::GetOutputElement*>(&n);
-        node["n"] = tmp->get_n();
     }
     else if (node_op == "Greater")
     {
