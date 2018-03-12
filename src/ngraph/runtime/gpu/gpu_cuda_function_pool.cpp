76a2dbdb9fbd3d28000d0bc2ad6837933f4cd418
Chris Sullivan <chris.sullivan@intel.com>
2018-03-12 11:47:55 -0700

    Updated namespace use in cpp files.

--- src/ngraph/runtime/gpu/gpu_cuda_function_pool.cpp
+++ src/ngraph/runtime/gpu/gpu_cuda_function_pool.cpp
@@ -1,18 +1,18 @@
 /*******************************************************************************
-* Copyright 2017-2018 Intel Corporation
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+ * Copyright 2017-2018 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 
 #include <cctype>
 #include <fstream>
@@ -26,40 +26,33 @@
 
 static const std::string s_output_dir = "gpu_codegen";
 
-namespace ngraph
+using namespace ngraph;
+
+runtime::gpu::CudaFunctionPool& runtime::gpu::CudaFunctionPool::instance()
 {
-    namespace runtime
+    static CudaFunctionPool pool;
+    return pool;
+}
+
+void runtime::gpu::CudaFunctionPool::set(const std::string& name, const std::string& kernel)
+{
+    const char* opts[] = {"--gpu-architecture=compute_35",
+                          "--relocatable-device-code=true"};
+    std::string filename =
+        file_util::path_join(s_output_dir, "cuda_kernel_" + name + "_codegen.cu");
+    std::ofstream out(filename);
+    out << kernel;
+    out.close();
+    m_function_map.insert(
+        {name, CudaFunctionBuilder::get("cuda_" + name, kernel, 2, opts)});
+}
+
+std::shared_ptr<CUfunction> runtime::gpu::CudaFunctionPool::get(const std::string& name)
+{
+    auto it = m_function_map.find(name);
+    if (it != m_function_map.end())
     {
-        namespace gpu
-        {
-            CudaFunctionPool& CudaFunctionPool::instance()
-            {
-                static CudaFunctionPool pool;
-                return pool;
-            }
-
-            void CudaFunctionPool::set(const std::string& name, const std::string& kernel)
-            {
-                const char* opts[] = {"--gpu-architecture=compute_35",
-                                      "--relocatable-device-code=true"};
-                std::string filename =
-                    file_util::path_join(s_output_dir, "cuda_kernel_" + name + "_codegen.cu");
-                std::ofstream out(filename);
-                out << kernel;
-                out.close();
-                m_function_map.insert(
-                    {name, CudaFunctionBuilder::get("cuda_" + name, kernel, 2, opts)});
-            }
-
-            std::shared_ptr<CUfunction> CudaFunctionPool::get(const std::string& name)
-            {
-                auto it = m_function_map.find(name);
-                if (it != m_function_map.end())
-                {
-                    return (*it).second;
-                }
-                return nullptr;
-            }
-        }
+        return (*it).second;
     }
+    return nullptr;
 }
--- src/ngraph/runtime/gpu/gpu_cuda_kernel_builder.cpp
+++ src/ngraph/runtime/gpu/gpu_cuda_kernel_builder.cpp
@@ -16,74 +16,67 @@
 #include "ngraph/runtime/gpu/gpu_cuda_kernel_builder.hpp"
 #include "ngraph/codegen/code_writer.hpp"
 
-namespace ngraph
+using namespace ngraph;
+
+void runtime::gpu::CudaKernelBuilder::get_elementwise_op(codegen::CodeWriter& writer,
+                                                         const std::string& name,
+                                                         const std::string& data_type,
+                                                         const std::string& op,
+                                                         const size_t& num_inputs)
 {
-    namespace runtime
+    writer << "extern \"C\" __global__ void cuda_" << name << "(";
+    for (size_t i = 0; i < num_inputs; i++)
     {
-        namespace gpu
-        {
-            void CudaKernelBuilder::get_elementwise_op(codegen::CodeWriter& writer,
-                                                       const std::string& name,
-                                                       const std::string& data_type,
-                                                       const std::string& op,
-                                                       const size_t& num_inputs)
-            {
-                writer << "extern \"C\" __global__ void cuda_" << name << "(";
-                for (size_t i = 0; i < num_inputs; i++)
-                {
-                    writer << data_type << "* in" << i << ", ";
-                }
-                writer << data_type << "* out,"
-                       << "size_t n)\n";
-                writer << "{\n";
-                writer.indent++;
-                {
-                    writer << "size_t tid = blockIdx.x * blockDim.x + threadIdx.x; \n";
-                    writer << "if (tid < n)\n";
-                    writer << "{\n";
-                    writer.indent++;
-                    {
-                        writer << "out[tid] = " << op << "(";
-                        for (size_t i = 0; i < num_inputs - 1; i++)
-                        {
-                            writer << "in" << i << "[tid], ";
-                        }
-                        writer << "in" << num_inputs - 1 << "[tid]);\n";
-                    }
-                    writer.indent--;
-                    writer << "}\n";
-                }
-                writer.indent--;
-                writer << "}\n";
-
-                return;
-            }
-
-            void CudaKernelBuilder::get_device_helper(codegen::CodeWriter& writer,
-                                                      const std::string& name,
-                                                      const std::string& data_type,
-                                                      const std::string& math_kernel,
-                                                      const size_t& num_inputs)
-            {
-                if (math_kernel.size())
-                {
-                    writer << "__device__ " << data_type << " " << name << "(";
-                    for (size_t i = 0; i < num_inputs - 1; i++)
-                    {
-                        writer << data_type << " x" << i << ", ";
-                    }
-                    writer << data_type << " x" << num_inputs - 1;
-                    writer << ")\n";
-                    writer << "{\n";
-                    writer.indent++;
-                    {
-                        writer << "return " + math_kernel << ";\n";
-                    }
-                    writer.indent--;
-                    writer << "}\n";
-                }
-                return;
-            }
-        }
+        writer << data_type << "* in" << i << ", ";
     }
+    writer << data_type << "* out,"
+           << "size_t n)\n";
+    writer << "{\n";
+    writer.indent++;
+    {
+        writer << "size_t tid = blockIdx.x * blockDim.x + threadIdx.x; \n";
+        writer << "if (tid < n)\n";
+        writer << "{\n";
+        writer.indent++;
+        {
+            writer << "out[tid] = " << op << "(";
+            for (size_t i = 0; i < num_inputs - 1; i++)
+            {
+                writer << "in" << i << "[tid], ";
+            }
+            writer << "in" << num_inputs - 1 << "[tid]);\n";
+        }
+        writer.indent--;
+        writer << "}\n";
+    }
+    writer.indent--;
+    writer << "}\n";
+
+    return;
+}
+
+void runtime::gpu::CudaKernelBuilder::get_device_helper(codegen::CodeWriter& writer,
+                                                        const std::string& name,
+                                                        const std::string& data_type,
+                                                        const std::string& math_kernel,
+                                                        const size_t& num_inputs)
+{
+    if (math_kernel.size())
+    {
+        writer << "__device__ " << data_type << " " << name << "(";
+        for (size_t i = 0; i < num_inputs - 1; i++)
+        {
+            writer << data_type << " x" << i << ", ";
+        }
+        writer << data_type << " x" << num_inputs - 1;
+        writer << ")\n";
+        writer << "{\n";
+        writer.indent++;
+        {
+            writer << "return " + math_kernel << ";\n";
+        }
+        writer.indent--;
+        writer << "}\n";
+    }
+    return;
 }
