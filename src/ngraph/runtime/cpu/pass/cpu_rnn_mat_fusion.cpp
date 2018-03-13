5564673c28fc1b7a4809f8b4cf6ec820f8b9f39a
Louis Feng <louis.feng@intel.com>
2018-03-13 14:13:35 -0700

    WIP.

--- src/ngraph/runtime/cpu/pass/cpu_rnn_mat_fusion.cpp
+++ src/ngraph/runtime/cpu/pass/cpu_rnn_mat_fusion.cpp
@@ -18,6 +18,7 @@
 #include <typeindex>
 #include <typeinfo>
 #include <unordered_map>
+#include <map>
 #include <stack>
 
 #include "ngraph/ops/dot.hpp"
@@ -26,9 +27,11 @@
 #include "ngraph/ops/parameter.hpp"
 #include "cpu_rnn_mat_fusion.hpp"
 
-static ngraph::NodeVector get_users(ngraph::Node& node)
+using namespace ngraph;
+
+static NodeVector get_users(Node& node)
 {
-    ngraph::NodeVector result;
+    NodeVector result;
 
     for (size_t i = 0; i < node.get_output_size(); ++i)
     {
@@ -43,7 +46,55 @@ static ngraph::NodeVector get_users(ngraph::Node& node)
 
 #define TI(x) std::type_index(typeid(x))
 
-void FindDot(std::shared_ptr<ngraph::Node> node, )
+struct DotOps
+{
+    std::shared_ptr<Node> dot;
+    NodeVector path;
+};
+
+void FindValidPathDFS(std::shared_ptr<Node> n, NodeVector path, std::vector<NodeVector>& path_bundle)
+{
+    Node& node = *n;
+    std::cout << "visiting: " << node.get_friendly_name() << std::endl;
+    if (TI(node) == TI(ngraph::op::Dot)) {
+        path.push_back(n);
+        path_bundle.push_back(path);
+        return;
+    }
+    if (TI(node) == TI(ngraph::op::Slice) || TI(node) == TI(ngraph::op::Reshape)) {
+        path.push_back(n);
+        const auto outputs = get_users(node);
+        for (const auto& out : outputs) {
+            FindValidPathDFS(out, path, path_bundle);
+        }
+    }
+}
+
+struct OrderedParams
+{
+public:
+    OrderedParams(const std::shared_ptr<Node>& n1, const std::shared_ptr<Node>& n2) {
+        if (n1 < n2) {
+            m_params.first = n1;
+            m_params.second = n2;
+        }
+        else {
+            m_params.first = n2;
+            m_params.second = n1;
+        }
+    }
+    std::shared_ptr<Node> first() const { return m_params.first; }
+    std::shared_ptr<Node> second() const { return m_params.second; }
+private:
+    friend bool operator< (const OrderedParams& n1, const OrderedParams& n2);
+    std::pair<std::shared_ptr<Node>, std::shared_ptr<Node>> m_params;
+};
+
+bool operator< (const OrderedParams& n1, const OrderedParams& n2)
+{
+    return n1.m_params < n2.m_params;
+}
+
 bool ngraph::runtime::cpu::pass::CPURnnMatFusion::run_on_function(
     std::shared_ptr<ngraph::Function> function)
 {
@@ -57,6 +108,7 @@ bool ngraph::runtime::cpu::pass::CPURnnMatFusion::run_on_function(
     {
         // Work around a warning [-Wpotentially-evaluated-expression]
         Node& node = *n;
+        std::cout << "instance id: " << node.get_instance_id() << std::endl;
         std::string type = "other";
         if (TI(node) == TI(ngraph::op::Parameter)) {
             param_nodes.push_back(n);
@@ -79,9 +131,137 @@ bool ngraph::runtime::cpu::pass::CPURnnMatFusion::run_on_function(
             std::cout << "    out: " << out->get_friendly_name() << std::endl;
         }
     }
+    std::cout << "find all dots" << std::endl;
+    // iterate all parameters and find path to dot op
+    std::map<std::shared_ptr<Node>, std::vector<NodeVector>> dot_ops;
+    std::vector<NodeVector> path_bundle;
     for (auto& p : param_nodes) {
-
+        std::cout << "param: " << p->get_friendly_name() << std::endl;
+        const auto outputs = get_users(*p);
+        for (const auto& out : outputs) {
+            std::cout << "    out: " << out->get_friendly_name() << std::endl;
+        }
+        NodeVector path;
+        path.push_back(p);
+        for (const auto& out : outputs) {
+            FindValidPathDFS(out, path, path_bundle);
+        }
+    }
+    for (const auto& nv : path_bundle) {
+        dot_ops[nv.back()].push_back(nv);
+    }
+    for (auto& d : dot_ops) {
+        std::cout << "dot: " << d.first->get_friendly_name() << std::endl;
+        for (auto& vn : d.second) {
+            std::cout << "  path: " << std::endl;
+            for (auto& n : vn) {
+                std::cout << "   node: " << n->get_friendly_name() << std::endl;
+            }
+        }
+    }
+    std::cout << "remove dots with single path" << std::endl;
+    // remove dot ops with single path
+    for (auto dot_it = dot_ops.cbegin(); dot_it != dot_ops.cend();) {
+        if (dot_it->second.size() < 2) {
+            dot_it = dot_ops.erase(dot_it);
+        }
+        else {
+            ++dot_it;
+        }
+    }
+    for (auto& d : dot_ops) {
+        std::cout << "dot: " << d.first->get_friendly_name() << std::endl;
+        for (auto& vn : d.second) {
+            std::cout << "  path: " << std::endl;
+            for (auto& n : vn) {
+                std::cout << "   node: " << n->get_friendly_name() << std::endl;
+            }
+        }
     }
 
+    std::cout << "find all pairs" << std::endl;
+    std::map<OrderedParams, NodeVector> params;
+    for (auto& d : dot_ops) {
+        //assert(d.second.size() == 2);
+        OrderedParams p(d.second[0].front(), d.second[1].front());
+        params[p].push_back(d.first);
+    }
+    for (auto& p : params) {
+        std::cout << "pair: [" << p.first.first()->get_friendly_name() << ", "
+                  << p.first.second()->get_friendly_name() << "]" << std::endl;
+        for (auto& op : p.second) {
+            std::cout << "op: " << op->get_friendly_name() << std::endl;
+        }
+    }
+    // remove pairs with single op
+    std::cout << "removed single op pairs" << std::endl;
+    for (auto it = params.begin(); it != params.end();) {
+        if (it->second.size() < 2) {
+            it = params.erase(it);
+        }
+        else {
+            ++it;
+        }
+    }
+    for (auto& p : params) {
+        std::cout << "pair: [" << p.first.first()->get_friendly_name() << ", "
+                  << p.first.second()->get_friendly_name() << "]" << std::endl;
+        for (auto& op : p.second) {
+            std::cout << "op: " << op->get_friendly_name() << std::endl;
+        }
+    }
+    // TODO: check consistency
+    // check dot op matrix order
+    // check shape size
+
+    // create new ops
+    for (auto& p : params) {
+
+        auto dot_op = p.second[0];
+        auto p1 = dot_op->get_input_op(0);//p.first.first();
+        auto p2 = dot_op->get_input_op(1);//p.first.second();
+
+        std::cout << "replacing " << p1->get_friendly_name() << " with " << p1->get_input_op(0)->get_friendly_name() << std::endl;
+        std::cout << "replacing " << p2->get_friendly_name() << " with " << p2->get_input_op(0)->get_friendly_name() << std::endl;
+        function->replace_node(p1, p1->get_input_op(0));
+        function->replace_node(p2, p2->get_input_op(0));
+//        for (auto v : p1->get_shape()) {
+//            std::cout << v << " ";
+//        }
+//        std::cout << std::endl;
+//        for (auto v : p2->get_shape()) {
+//            std::cout << v << " ";
+//        }
+//        std::cout << std::endl;
+        for (auto& n : function->get_ordered_ops())
+        {
+            // Work around a warning [-Wpotentially-evaluated-expression]
+            Node& node = *n;
+            std::cout << "instance id: " << node.get_instance_id() << std::endl;
+            std::string type = "other";
+            if (TI(node) == TI(ngraph::op::Parameter)) {
+                param_nodes.push_back(n);
+            }
+            if (TI(node) == TI(ngraph::op::Slice)) {
+                type = "Slice";
+            }
+            if (TI(node) == TI(ngraph::op::Reshape)) {
+                type = "Reshape";
+            }
+            if (TI(node) == TI(ngraph::op::Dot)) {
+                type = "Dot";
+            }
+            std::cout << "node (" << type << "): " << node.get_friendly_name() << std::endl;
+            for (const auto& in : node.get_input_ops()) {
+                std::cout << "    in:  " << in->get_friendly_name() << std::endl;
+            }
+            auto outputs = get_users(node);
+            for (const auto& out : outputs) {
+                std::cout << "    out: " << out->get_friendly_name() << std::endl;
+            }
+        }
+
+//        std::shared_ptr<op::Reshape> n1_reshape = std::make_shared(op::Reshape(A, AxisVector{1, 0}, shape_r)));
+    }
     return clobbered;
 }
