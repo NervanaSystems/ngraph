7177a0b48b344bf23094918984d9a0d45c2994f9
Yixing Lao <yixing.lao@gmail.com>
2018-03-19 14:21:37 -0700

    topolotical-sort based node clustering (#615)

--- src/ngraph/placement.hpp
+++ src/ngraph/placement.hpp
@@ -16,10 +16,25 @@
 
 #pragma once
 
+#include <memory>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "ngraph/log.hpp"
 
 namespace ngraph
 {
+    class Function;
+    class Node;
+
+    namespace op
+    {
+        class Parameter;
+        class Result;
+    }
+
     enum class Placement
     {
         DEFAULT,
@@ -30,4 +45,9 @@ namespace ngraph
     };
 
     std::string placement_to_string(Placement placement);
+
+    // Split function to function(s) with unique placement
+    std::pair<std::vector<std::shared_ptr<Function>>,
+              std::unordered_map<std::shared_ptr<op::Parameter>, std::shared_ptr<op::Result>>>
+        split_function_by_placement(const std::shared_ptr<Function>& f);
 }
