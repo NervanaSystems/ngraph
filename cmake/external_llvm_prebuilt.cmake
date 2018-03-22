1e1634b2785147fb184e95aab0403939d020659d
Sang Ik Lee <sang.ik.lee@intel.com>
2018-03-22 10:14:16 -0700

    Add support for prebuilt LLVM on Mac.

--- cmake/external_llvm_prebuilt.cmake
+++ cmake/external_llvm_prebuilt.cmake
@@ -16,22 +16,34 @@
 
 include(ExternalProject)
 
-if (NGRAPH_CPU_ENABLE AND (${CMAKE_SYSTEM_NAME} MATCHES "Darwin") OR (${CMAKE_SYSTEM_NAME} MATCHES "Windows"))
+if (NGRAPH_CPU_ENABLE AND (${CMAKE_SYSTEM_NAME} MATCHES "Windows"))
     message(FATAL_ERROR "The NGRAPH_USE_PREBUILT_LLVM option is not supported on this platform.")
 endif()
 
 if (NGRAPH_CPU_ENABLE)
     message(STATUS "Fetching LLVM from llvm.org")
 
-    # Override default LLVM binaries
-    if(NOT DEFINED LLVM_TARBALL_URL)
-        set(LLVM_TARBALL_URL http://releases.llvm.org/5.0.1/clang+llvm-5.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz)
-    endif()
+    if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+        # Override default LLVM binaries
+        if(NOT DEFINED LLVM_TARBALL_URL)
+            set(LLVM_TARBALL_URL http://releases.llvm.org/5.0.1/clang+llvm-5.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz)
+        endif()
 
-    if(NOT DEFINED LLVM_SHA1_HASH)
-        set(LLVM_SHA1_HASH 2fddf9a90b182fa594786be6923e58f5ead71e9c)
-    endif()
+        if(NOT DEFINED LLVM_SHA1_HASH)
+            set(LLVM_SHA1_HASH 2fddf9a90b182fa594786be6923e58f5ead71e9c)
+        endif()
+    elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+        # Override default LLVM binaries
+        if(NOT DEFINED LLVM_TARBALL_URL)
+            set(LLVM_TARBALL_URL http://releases.llvm.org/5.0.1/clang+llvm-5.0.1-x86_64-apple-darwin.tar.xz)
+        endif()
 
+        if(NOT DEFINED LLVM_SHA1_HASH)
+            set(LLVM_SHA1_HASH c8d6f64eab0074cd7519266ffe6663922be4105f)
+        endif()
+    else()
+        message(FATAL_ERROR "The NGRAPH_USE_PREBUILT_LLVM option is not supported on this platform.")
+    endif()
     # The 'BUILD_BYPRODUCTS' argument was introduced in CMake 3.2.
     if(${CMAKE_VERSION} VERSION_LESS 3.2)
         ExternalProject_Add(
@@ -61,83 +73,163 @@ if (NGRAPH_CPU_ENABLE)
     set(LLVM_INCLUDE_DIR "${source_dir}/include")  # used by other external projects in current scope
     set(LLVM_LIB_DIR "${source_dir}/lib" PARENT_SCOPE)
 
-    set(LLVM_LINK_LIBS
-        clangTooling
-        clangFrontendTool
-        clangFrontend
-        clangDriver
-        clangSerialization
-        clangCodeGen
-        clangParse
-        clangSema
-        clangStaticAnalyzerFrontend
-        clangStaticAnalyzerCheckers
-        clangStaticAnalyzerCore
-        clangAnalysis
-        clangARCMigrate
-        clangRewriteFrontend
-        clangEdit
-        clangAST
-        clangLex
-        clangBasic
-        LLVMLTO
-        LLVMPasses
-        LLVMObjCARCOpts
-        LLVMSymbolize
-        LLVMDebugInfoPDB
-        LLVMDebugInfoDWARF
-        LLVMMIRParser
-        LLVMCoverage
-        LLVMTableGen
-        LLVMDlltoolDriver
-        LLVMOrcJIT
-        LLVMObjectYAML
-        LLVMLibDriver
-        LLVMOption
-        LLVMX86Disassembler
-        LLVMX86AsmParser
-        LLVMX86CodeGen
-        LLVMGlobalISel
-        LLVMSelectionDAG
-        LLVMAsmPrinter
-        LLVMDebugInfoCodeView
-        LLVMDebugInfoMSF
-        LLVMX86Desc
-        LLVMMCDisassembler
-        LLVMX86Info
-        LLVMX86AsmPrinter
-        LLVMX86Utils
-        LLVMMCJIT
-        LLVMLineEditor
-        LLVMInterpreter
-        LLVMExecutionEngine
-        LLVMRuntimeDyld
-        LLVMCodeGen
-        LLVMTarget
-        LLVMCoroutines
-        LLVMipo
-        LLVMInstrumentation
-        LLVMVectorize
-        LLVMScalarOpts
-        LLVMLinker
-        LLVMIRReader
-        LLVMAsmParser
-        LLVMInstCombine
-        LLVMTransformUtils
-        LLVMBitWriter
-        LLVMAnalysis
-        LLVMProfileData
-        LLVMObject
-        LLVMMCParser
-        LLVMMC
-        LLVMBitReader
-        LLVMCore
-        LLVMBinaryFormat
-        LLVMSupport
-        LLVMDemangle
-        tinfo
-        z
-        m
-        PARENT_SCOPE)
-
+    if(APPLE)
+        set(LLVM_LINK_LIBS
+            clangTooling
+            clangFrontendTool
+            clangFrontend
+            clangDriver
+            clangSerialization
+            clangCodeGen
+            clangParse
+            clangSema
+            clangStaticAnalyzerFrontend
+            clangStaticAnalyzerCheckers
+            clangStaticAnalyzerCore
+            clangAnalysis
+            clangARCMigrate
+            clangRewriteFrontend
+            clangEdit
+            clangAST
+            clangLex
+            clangBasic
+            LLVMLTO
+            LLVMPasses
+            LLVMObjCARCOpts
+            LLVMSymbolize
+            LLVMDebugInfoPDB
+            LLVMDebugInfoDWARF
+            LLVMMIRParser
+            LLVMCoverage
+            LLVMTableGen
+            LLVMDlltoolDriver
+            LLVMOrcJIT
+            LLVMObjectYAML
+            LLVMLibDriver
+            LLVMOption
+            LLVMX86Disassembler
+            LLVMX86AsmParser
+            LLVMX86CodeGen
+            LLVMGlobalISel
+            LLVMSelectionDAG
+            LLVMAsmPrinter
+            LLVMDebugInfoCodeView
+            LLVMDebugInfoMSF
+            LLVMX86Desc
+            LLVMMCDisassembler
+            LLVMX86Info
+            LLVMX86AsmPrinter
+            LLVMX86Utils
+            LLVMMCJIT
+            LLVMLineEditor
+            LLVMInterpreter
+            LLVMExecutionEngine
+            LLVMRuntimeDyld
+            LLVMCodeGen
+            LLVMTarget
+            LLVMCoroutines
+            LLVMipo
+            LLVMInstrumentation
+            LLVMVectorize
+            LLVMScalarOpts
+            LLVMLinker
+            LLVMIRReader
+            LLVMAsmParser
+            LLVMInstCombine
+            LLVMTransformUtils
+            LLVMBitWriter
+            LLVMAnalysis
+            LLVMProfileData
+            LLVMObject
+            LLVMMCParser
+            LLVMMC
+            LLVMBitReader
+            LLVMCore
+            LLVMBinaryFormat
+            LLVMSupport
+            LLVMDemangle
+            curses
+            z
+            m
+            PARENT_SCOPE)
+    else()
+        set(LLVM_LINK_LIBS
+            clangTooling
+            clangFrontendTool
+            clangFrontend
+            clangDriver
+            clangSerialization
+            clangCodeGen
+            clangParse
+            clangSema
+            clangStaticAnalyzerFrontend
+            clangStaticAnalyzerCheckers
+            clangStaticAnalyzerCore
+            clangAnalysis
+            clangARCMigrate
+            clangRewriteFrontend
+            clangEdit
+            clangAST
+            clangLex
+            clangBasic
+            LLVMLTO
+            LLVMPasses
+            LLVMObjCARCOpts
+            LLVMSymbolize
+            LLVMDebugInfoPDB
+            LLVMDebugInfoDWARF
+            LLVMMIRParser
+            LLVMCoverage
+            LLVMTableGen
+            LLVMDlltoolDriver
+            LLVMOrcJIT
+            LLVMObjectYAML
+            LLVMLibDriver
+            LLVMOption
+            LLVMX86Disassembler
+            LLVMX86AsmParser
+            LLVMX86CodeGen
+            LLVMGlobalISel
+            LLVMSelectionDAG
+            LLVMAsmPrinter
+            LLVMDebugInfoCodeView
+            LLVMDebugInfoMSF
+            LLVMX86Desc
+            LLVMMCDisassembler
+            LLVMX86Info
+            LLVMX86AsmPrinter
+            LLVMX86Utils
+            LLVMMCJIT
+            LLVMLineEditor
+            LLVMInterpreter
+            LLVMExecutionEngine
+            LLVMRuntimeDyld
+            LLVMCodeGen
+            LLVMTarget
+            LLVMCoroutines
+            LLVMipo
+            LLVMInstrumentation
+            LLVMVectorize
+            LLVMScalarOpts
+            LLVMLinker
+            LLVMIRReader
+            LLVMAsmParser
+            LLVMInstCombine
+            LLVMTransformUtils
+            LLVMBitWriter
+            LLVMAnalysis
+            LLVMProfileData
+            LLVMObject
+            LLVMMCParser
+            LLVMMC
+            LLVMBitReader
+            LLVMCore
+            LLVMBinaryFormat
+            LLVMSupport
+            LLVMDemangle
+            tinfo
+            z
+            m
+            PARENT_SCOPE)
+    endif()
 endif()
