2426158859c8d9e3726daed37264e1aeb44fd531
Dawn Stone <dawn.stone@intel.com>
2018-04-04 12:28:47 -0700

    adding support for GPU backend to contrib/docker
    added gpu dockerfiles
    
    renamed Dockerfile for centos74
    
    fixed NGRAPH_GPU_ENABLE cmake flag name

--- contrib/docker/build-ngraph-and-test.sh
+++ contrib/docker/build-ngraph-and-test.sh
@@ -24,7 +24,9 @@ echo 'Contents of /home/dockuser:'
 ls -la /home/dockuser
 echo ' '
 
-export CMAKE_OPTIONS_EXTRA=""
+if [ -z ${CMAKE_OPTIONS_EXTRA} ]; then
+    export CMAKE_OPTIONS_EXTRA=''
+fi
 
 # setting for make -j
 if [ -z ${PARALLEL} ] ; then
@@ -41,17 +43,7 @@ if [ -z ${BUILD_SUBDIR} ] ; then
     BUILD_SUBDIR=BUILD
 fi
 
-# Option to build with GPU backend
-# default builds with CPU only
-if [ -z ${NGRAPH_GPU_ENABLE} ] ; then
-    NGRAPH_GPU_ENABLE=false
-fi
-
 # Set up the environment
-if $NGRAPH_GPU_ENABLE; then
-    export CMAKE_OPTIONS_EXTRA="-DNGRAPH_GPU_ENABLE=TRUE"
-fi
-
 export NGRAPH_REPO=/home/dockuser/ngraph-test
 
 if [ -z ${OUTPUT_DIR} ]; then
