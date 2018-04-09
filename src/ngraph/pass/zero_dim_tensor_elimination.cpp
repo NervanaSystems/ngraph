405fad7f70b552d6f60b1215b8041b160e397a23
nikolay.korovaiko <korovaikon@gmail.com>
2018-04-09 11:22:29 -0700

    address Scott's feedback

--- src/ngraph/pass/zero_dim_tensor_elimination.cpp
+++ src/ngraph/pass/zero_dim_tensor_elimination.cpp
@@ -58,16 +58,23 @@ static std::shared_ptr<Node> return_broadcast(std::shared_ptr<Node> n)
     return std::make_shared<op::Broadcast>(n->get_input_op(1), n->get_shape(), axes);
 }
 
-static const std::unordered_map<std::type_index,
-                                std::function<std::shared_ptr<Node>(std::shared_ptr<Node>)>>
-    ops_to_const_values{
-        {TI(ngraph::op::AvgPool), return_zero_const},
-        {TI(ngraph::op::MaxPool), return_zero_const},
-        {TI(ngraph::op::Convolution), return_zero_const},
-        {TI(ngraph::op::Sum), return_zero_const},
-        {TI(ngraph::op::Product), return_one_const},
-        {TI(ngraph::op::Pad), return_broadcast},
-    };
+static std::shared_ptr<Node> get_const_value_for_op(std::shared_ptr<Node> n)
+{
+    static const std::unordered_map<std::type_index,
+                                    std::function<std::shared_ptr<Node>(std::shared_ptr<Node>)>>
+        ops_to_const_values{
+            {TI(ngraph::op::AvgPool), return_zero_const},
+            {TI(ngraph::op::MaxPool), return_zero_const},
+            {TI(ngraph::op::Convolution), return_zero_const},
+            {TI(ngraph::op::Sum), return_zero_const},
+            {TI(ngraph::op::Product), return_one_const},
+            {TI(ngraph::op::Pad), return_broadcast},
+        };
+
+    const Node& node = *n;
+    auto entry = ops_to_const_values.find(TI(node));
+    return entry == ops_to_const_values.end() ? nullptr : entry->second(n);
+}
 
 static bool has_zero_dim(std::shared_ptr<Node> node)
 {
@@ -140,15 +147,12 @@ bool ngraph::pass::ZeroDimTensorElimination::run_on_function(std::shared_ptr<ngr
             continue;
         }
 
-        const Node& node = *n;
-        auto entry = ops_to_const_values.find(TI(node));
-
-        if (entry == ops_to_const_values.end() || !has_zero_dim(n->get_input_op(0)))
+        auto new_node = get_const_value_for_op(n);
+        if (!new_node || !has_zero_dim(n->get_input_op(0)))
         {
             continue;
         }
 
-        auto new_node = entry->second(n);
         replaced = true;
         NGRAPH_DEBUG << " Replacing " << n->get_name() << " with " << new_node->get_name();
         replace_node(n, new_node);
