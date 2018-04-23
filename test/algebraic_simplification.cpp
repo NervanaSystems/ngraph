03a37c9527367d1b46b3c077b0feb8a3406b04e9
nikolay.korovaiko <korovaikon@gmail.com>
2018-04-23 09:21:04 -0700

    negative tests, 0*0, 0*1, 0+0

--- test/algebraic_simplification.cpp
+++ test/algebraic_simplification.cpp
@@ -48,7 +48,7 @@
 using namespace ngraph;
 using namespace std;
 
-TEST(pattern, algebraic_simplification_add_types_shapes)
+TEST(algebraic_simplification, add_types_shapes)
 {
     Shape shapes[] = {Shape{}, Shape{2, 2}, Shape{3, 3, 3}};
     element::Type types[] = {element::i32, element::f32, element::f64};
@@ -85,7 +85,7 @@ TEST(pattern, algebraic_simplification_add_types_shapes)
     }
 }
 
-TEST(pattern, algebraic_simplification_add_broadcast)
+TEST(algebraic_simplification, add_broadcast)
 {
     Shape shape{2, 2};
     pass::Manager pass_manager;
@@ -116,7 +116,7 @@ TEST(pattern, algebraic_simplification_add_broadcast)
     }
 }
 
-TEST(pattern, algebraic_simplification_multiply_zero_one)
+TEST(algebraic_simplification, multiply_zero_one)
 {
     Shape shapes[] = {Shape{}, Shape{2, 2}, Shape{3, 3, 3}};
     element::Type types[] = {element::i32, element::f32, element::f64};
@@ -159,5 +159,118 @@ TEST(pattern, algebraic_simplification_multiply_zero_one)
         {
             ASSERT_EQ(expected.at(i), results.at(i)->get_argument(0));
         }
+    }
+}
+
+TEST(algebraic_simplification, zero_plus_zero_commutativity)
+{
+    Shape shape{};
+    auto type = element::f32;
+    pass::Manager pass_manager;
+    pass_manager.register_pass<pass::VisualizeTree>("before.pdf");
+    pass_manager.register_pass<pass::AlgebraicSimplification>();
+    pass_manager.register_pass<pass::VisualizeTree>("after.pdf");
+
+    auto a = make_shared<op::Parameter>(type, shape);
+    auto b = make_shared<op::Parameter>(type, shape);
+    auto c = make_shared<op::Parameter>(type, shape);
+    auto iconst0 = ngraph::make_constant_from_string("0", type, shape);
+    auto add_a_0 = iconst0 + iconst0;
+    auto add_a_0_0 = iconst0 + iconst0;
+    auto add_b_0 = iconst0 + b;
+    auto add_b_0_0 = iconst0 + b;
+
+    auto f = std::make_shared<Function>(ngraph::NodeVector{a, b, add_a_0_0, c, add_b_0_0},
+                                        op::ParameterVector{a, b, c});
+    pass_manager.run_passes(f);
+
+    ASSERT_TRUE(ngraph::is_zero(f->get_results().at(2)->get_argument(0)));
+    ASSERT_EQ(f->get_results().at(4)->get_argument(0), b);
+}
+
+TEST(algebraic_simplification, zero_multiply_zero_one)
+{
+    Shape shape{};
+    auto type = element::f32;
+    pass::Manager pass_manager;
+    pass_manager.register_pass<pass::VisualizeTree>("before.pdf");
+    pass_manager.register_pass<pass::AlgebraicSimplification>();
+    pass_manager.register_pass<pass::VisualizeTree>("after.pdf");
+
+    auto a = make_shared<op::Parameter>(type, shape);
+    auto b = make_shared<op::Parameter>(type, shape);
+    auto c = make_shared<op::Parameter>(type, shape);
+    auto iconst0 = ngraph::make_constant_from_string("0", type, shape);
+    auto iconst1 = ngraph::make_constant_from_string("1", type, shape);
+    auto add_a_0 = iconst0 * iconst0;
+    auto add_b_0 = iconst1 * iconst0;
+
+    auto f = std::make_shared<Function>(ngraph::NodeVector{a, b, add_a_0, c, add_b_0},
+                                        op::ParameterVector{a, b, c});
+    pass_manager.run_passes(f);
+
+    ASSERT_TRUE(ngraph::is_zero(f->get_results().at(2)->get_argument(0)));
+    ASSERT_TRUE(ngraph::is_zero(f->get_results().at(4)->get_argument(0)));
+}
+
+TEST(algebraic_simplification, add_negative_tests)
+{
+    Shape shape{};
+    auto type = element::f32;
+    pass::Manager pass_manager;
+    pass_manager.register_pass<pass::VisualizeTree>("before.pdf");
+    pass_manager.register_pass<pass::AlgebraicSimplification>();
+    pass_manager.register_pass<pass::VisualizeTree>("after.pdf");
+
+    auto a = make_shared<op::Parameter>(type, shape);
+    auto b = make_shared<op::Parameter>(type, shape);
+    auto c = make_shared<op::Parameter>(type, shape);
+    auto abs_a = make_shared<op::Abs>(a);
+    auto iconst2 = ngraph::make_constant_from_string("2", type, shape);
+    auto add_a_0 = a + iconst2;
+    auto add_a_0_0 = add_a_0 + iconst2;
+    auto add_b_0 = b + abs_a;
+    auto add_b_0_0 = add_b_0 + abs_a;
+
+    auto f = std::make_shared<Function>(ngraph::NodeVector{a, b, add_a_0_0, c, add_b_0_0},
+                                        op::ParameterVector{a, b, c});
+    pass_manager.run_passes(f);
+
+    auto expected = ngraph::NodeVector{a, b, add_a_0_0, c, add_b_0_0};
+    auto results = f->get_results();
+    for (size_t i = 0; i < results.size(); i++)
+    {
+        ASSERT_EQ(expected.at(i), results.at(i)->get_argument(0));
+    }
+}
+
+TEST(algebraic_simplification, multiply_negative_tests)
+{
+    Shape shape{};
+    auto type = element::f32;
+    pass::Manager pass_manager;
+    pass_manager.register_pass<pass::VisualizeTree>("before.pdf");
+    pass_manager.register_pass<pass::AlgebraicSimplification>();
+    pass_manager.register_pass<pass::VisualizeTree>("after.pdf");
+
+    auto a = make_shared<op::Parameter>(type, shape);
+    auto b = make_shared<op::Parameter>(type, shape);
+    auto c = make_shared<op::Parameter>(type, shape);
+    auto abs_a = make_shared<op::Abs>(a);
+    auto iconst2 = ngraph::make_constant_from_string("2", type, shape);
+    auto add_a_0 = a * iconst2;
+    auto add_a_0_0 = add_a_0 * iconst2;
+    auto add_b_0 = b * abs_a;
+    auto add_b_0_0 = add_b_0 * abs_a;
+
+    auto f = std::make_shared<Function>(ngraph::NodeVector{a, b, add_a_0_0, c, add_b_0_0},
+                                        op::ParameterVector{a, b, c});
+    pass_manager.run_passes(f);
+
+    auto expected = ngraph::NodeVector{a, b, add_a_0_0, c, add_b_0_0};
+    auto results = f->get_results();
+    for (size_t i = 0; i < results.size(); i++)
+    {
+        ASSERT_EQ(expected.at(i), results.at(i)->get_argument(0));
     }
 }
