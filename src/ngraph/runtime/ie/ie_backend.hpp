5eb3d7fd3e6a329a837dcf539c5f7474fe72d4ce
root <varun.v.kumar@intel.com>
2018-04-25 04:56:17 -0700

    add bn to the inference engine backend

--- src/ngraph/runtime/ie/ie_backend.hpp
+++ src/ngraph/runtime/ie/ie_backend.hpp
@@ -26,6 +26,7 @@
 #include "ngraph/runtime/tensor_view.hpp"
 
 #include "ngraph/op/avg_pool.hpp"
+#include "ngraph/op/batch_norm.hpp"
 #include "ngraph/op/broadcast.hpp"
 #include "ngraph/op/concat.hpp"
 #include "ngraph/op/constant.hpp"
@@ -55,6 +56,7 @@
 #include "ngraph/runtime/reference/asin.hpp"
 #include "ngraph/runtime/reference/atan.hpp"
 #include "ngraph/runtime/reference/avg_pool.hpp"
+#include "ngraph/runtime/reference/batch_norm.hpp"
 #include "ngraph/runtime/reference/broadcast.hpp"
 #include "ngraph/runtime/reference/ceiling.hpp"
 #include "ngraph/runtime/reference/concat.hpp"
@@ -223,6 +225,33 @@ private:
                                             apb->get_padding_below(),
                                             apb->get_padding_above(),
                                             apb->get_include_padding_in_avg_computation());
+        }
+        else if (node_op == "BatchNorm")
+        {
+            ngraph::op::BatchNorm* bn = dynamic_cast<ngraph::op::BatchNorm*>(&node);
+            if (bn->get_output_size() == 3)
+            {
+                reference::batch_norm_three_outputs<T>(
+                    bn->get_eps_value(),
+                    reinterpret_cast<T*>(args[0]->get_data_ptr()),
+                    reinterpret_cast<T*>(args[1]->get_data_ptr()),
+                    reinterpret_cast<T*>(args[2]->get_data_ptr()),
+                    reinterpret_cast<T*>(out[0]->get_data_ptr()),
+                    reinterpret_cast<T*>(out[1]->get_data_ptr()),
+                    reinterpret_cast<T*>(out[2]->get_data_ptr()),
+                    args[2]->get_shape());
+            }
+            else
+            {
+                reference::batch_norm_one_output<T>(bn->get_eps_value(),
+                                                    reinterpret_cast<T*>(args[0]->get_data_ptr()),
+                                                    reinterpret_cast<T*>(args[1]->get_data_ptr()),
+                                                    reinterpret_cast<T*>(args[2]->get_data_ptr()),
+                                                    reinterpret_cast<T*>(args[3]->get_data_ptr()),
+                                                    reinterpret_cast<T*>(args[4]->get_data_ptr()),
+                                                    reinterpret_cast<T*>(out[0]->get_data_ptr()),
+                                                    args[2]->get_shape());
+            }
         }
         else if (node_op == "Broadcast")
         {
