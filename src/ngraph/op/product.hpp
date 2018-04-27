5d963b3e401e186fcdf86c4f64ecc4b131607a20
Adam Procter <adam.m.procter@intel.com>
2018-04-27 09:39:54 -0700

    Remove single-line comments ending with backslash
    These comments cause warnings on gcc.

--- src/ngraph/op/product.hpp
+++ src/ngraph/op/product.hpp
@@ -30,9 +30,7 @@ namespace ngraph
         /// \f[
         ///     \mathit{product}\left(\{0\},
         ///         \left[ \begin{array}{ccc}
-        ///                1 & 2 \\
-        ///                3 & 4 \\
-        ///                5 & 6 \end{array} \right]\right) =
+        ///                1 & 2 \\ 3 & 4 \\ 5 & 6 \end{array} \right]\right) =
         ///     \left[ (1 * 3 * 5), (2 * 4 * 6) \right] =
         ///     \left[ 15, 48 \right]~~~\text{(dimension 0 (rows) is eliminated)}
         /// \f]
@@ -40,9 +38,7 @@ namespace ngraph
         /// \f[
         ///     \mathit{product}\left(\{1\},
         ///         \left[ \begin{array}{ccc}
-        ///                1 & 2 \\
-        ///                3 & 4 \\
-        ///                5 & 6 \end{array} \right]\right) =
+        ///                1 & 2 \\ 3 & 4 \\ 5 & 6 \end{array} \right]\right) =
         ///     \left[ (1 * 2), (3 * 4), (5 * 6) \right] =
         ///     \left[ 2, 12, 30 \right]~~~\text{(dimension 1 (columns) is eliminated)}
         /// \f]
@@ -50,9 +46,7 @@ namespace ngraph
         /// \f[
         ///     \mathit{product}\left(\{0,1\},
         ///         \left[ \begin{array}{ccc}
-        ///                1 & 2 \\
-        ///                3 & 4 \\
-        ///                5 & 6 \end{array} \right]\right) =
+        ///                1 & 2 \\ 3 & 4 \\ 5 & 6 \end{array} \right]\right) =
         ///      (1 * 2) * (3 * 4) * (5 * 6) =
         ///      720~~~\text{(both dimensions (rows and columns) are eliminated)}
         /// \f]
