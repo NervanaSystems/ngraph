4ec612238e6cfeac92c6c42c47b13695eb3dcf3c
nikolay.korovaiko <korovaikon@gmail.com>
2018-05-01 13:36:39 -0700

    more tests for reverse_sequence

--- test/cpu_test.cpp
+++ test/cpu_test.cpp
@@ -62,7 +62,7 @@ TEST(cpu_test, unhandled_op)
     ASSERT_THROW(backend->compile(f), ngraph_error);
 }
 
-TEST(cpu_test, reverse_sequence)
+TEST(cpu_test, reverse_sequence_n2c3h4w2)
 {
     Shape shape{2, 3, 4, 2};
     auto A = make_shared<op::Parameter>(element::i32, shape);
@@ -108,3 +108,98 @@ TEST(cpu_test, reverse_sequence)
     backend->call(f, {result}, {a});
     EXPECT_EQ(read_vector<int>(result), expected);
 }
+
+TEST(cpu_test, reverse_sequence_n4c3h2w2)
+{
+    Shape shape{4, 3, 2, 2};
+    auto A = make_shared<op::Parameter>(element::i32, shape);
+
+    size_t batch_axis = 0;
+    size_t sequence_axis = 1;
+    Shape sequence_lengths{1,2,3,3};
+    auto rs = std::make_shared<op::ReverseSequence>(A, batch_axis, sequence_axis, sequence_lengths);
+
+    auto f = make_shared<Function>(rs, op::ParameterVector{A});
+
+    auto backend = runtime::Backend::create("CPU");
+
+    // Create some tensors for input/output
+    shared_ptr<runtime::TensorView> a = backend->create_tensor(element::i32, shape);
+    shared_ptr<runtime::TensorView> result = backend->create_tensor(element::i32, shape);
+
+    std::vector<int> input
+    {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
+        31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 
+        41, 42, 43, 44, 45, 46, 47
+    };
+
+    std::vector<int> expected
+    {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+        11, 16, 17, 18, 19, 12, 13, 14, 15, 20, 
+        21, 22, 23, 32, 33, 34, 35, 28, 29, 30, 
+        31, 24, 25, 26, 27, 44, 45, 46, 47, 40, 
+        41, 42, 43, 36, 37, 38, 39
+    };
+
+    copy_data(a, input);
+
+    backend->call(f, {result}, {a});
+    EXPECT_EQ(read_vector<int>(result), expected);
+}
+
+TEST(cpu_test, reverse_sequence_n4d2c3h2w2)
+{
+    Shape shape{4,2,3,2,2};
+    auto A = make_shared<op::Parameter>(element::i32, shape);
+
+    size_t batch_axis = 0;
+    size_t sequence_axis = 2;
+    Shape sequence_lengths{1,2,1,2};
+    auto rs = std::make_shared<op::ReverseSequence>(A, batch_axis, sequence_axis, sequence_lengths);
+
+    auto f = make_shared<Function>(rs, op::ParameterVector{A});
+
+    auto backend = runtime::Backend::create("CPU");
+
+    // Create some tensors for input/output
+    shared_ptr<runtime::TensorView> a = backend->create_tensor(element::i32, shape);
+    shared_ptr<runtime::TensorView> result = backend->create_tensor(element::i32, shape);
+
+    std::vector<int> input
+    {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
+        31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 
+        41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 
+        51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 
+        61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 
+        71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 
+        81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 
+        91, 92, 93, 94, 95
+    };
+
+    std::vector<int> expected
+    {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+        21, 22, 23, 28, 29, 30, 31, 24, 25, 26, 
+        27, 32, 33, 34, 35, 40, 41, 42, 43, 36, 
+        37, 38, 39, 44, 45, 46, 47, 48, 49, 50, 
+        51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 
+        61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 
+        71, 76, 77, 78, 79, 72, 73, 74, 75, 80, 
+        81, 82, 83, 88, 89, 90, 91, 84, 85, 86, 
+        87, 92, 93, 94, 95
+    };
+
+    copy_data(a, input);
+
+    backend->call(f, {result}, {a});
+    EXPECT_EQ(read_vector<int>(result), expected);
+}
+
