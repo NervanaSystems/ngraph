11ec64498ba286383ac2cb2462cac54f29dc7388
Adam Rogowiec <adam.rogowiec@intel.com>
2018-05-08 22:39:55 +0200

    [Py]  (Partially) automatic generation of python API doc (#973)
    * First version of nGraph Python API documentation pages.
    
    * Remove use of (`) apostrophes since they generate Sphinx warnings.
    
    * Fix submodule rendering in Sphinx PythonAPI docs
    
    * Add updated index and conf py files
    
    * Updated version of Python API doc.
    
    * Better description for the autosummary
    
    * FIX: Disable autogeneration of summary files in documentation.
    
    * Remove redundant file containing duplicate information.
    
    * Remove unused Sphinx extensions.
    
    * (Partially) automatic generation of python API doc.
    
    - Detailed documentation is generated automatically
    - Summaries are generated automatically for all files except runtime.py
      which is parsed incorrectly.
    - No additional steps required. Only run `make html`. Under the hood there
      is lunched script which generates all required files to some temporary
      directory under `/tmp/sphinx_auto_py_doc/` and copied to docs directory
      afterwards.
    - Remove `autosummary_generate` from in configuration file since we use
      custom script which performs this step.
    
    * Review update.
    
    * Automatically generated stub files for doc.
    
    - Added just few empty lines which has been removed during recent merge.

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -36,8 +36,6 @@ needs_sphinx = '1.6.5'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.inheritance_diagram',
-    'sphinx.ext.graphviz',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
@@ -218,10 +216,3 @@ rst_epilog = u"""
 # -- autodoc Extension configuration --------------------------------------
 
 autodoc_mock_imports = ['ngraph.impl', 'ngraph.utils']
-
-# -- autosummary Extension configuration ----------------------------------
-
-# This enables autogeneration of standard summary files
-# However we customize them to our needs, thus disable it at the moment.
-
-# autosummary_generate = ['python_api/structure.rst']
