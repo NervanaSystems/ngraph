da24b77cb429f861fef1cfbee52e60b589e6c34d
Nick Korovaiko <korovaikon@gmail.com>
2018-05-15 08:37:08 -0700

    hooking up batchnorm kernels to cpu codegen (#1001)

--- src/ngraph/runtime/cpu/cpu_external_function.cpp
+++ src/ngraph/runtime/cpu/cpu_external_function.cpp
@@ -351,6 +351,7 @@ void runtime::cpu::CPU_ExternalFunction::compile()
 #include "ngraph/runtime/cpu/mkldnn_invoke.hpp"
 #include "ngraph/runtime/reference/and.hpp"
 #include "ngraph/runtime/reference/avg_pool.hpp"
+#include "ngraph/runtime/reference/batch_norm.hpp"
 #include "ngraph/runtime/reference/broadcast.hpp"
 #include "ngraph/runtime/reference/concat.hpp"
 #include "ngraph/runtime/reference/convolution.hpp"
