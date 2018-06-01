b3fa3e9ce8a08e3055c319f0a432b48d7535662a
Yixing Lao <yixing.lao@intel.com>
2018-05-31 20:38:24 -0700

    documentation

--- test/all_close_f.cpp
+++ test/all_close_f.cpp
@@ -110,113 +110,111 @@ float bits_to_float(const string& s)
 //     NGRAPH_INFO << test::close_f(val0, val_2);
 // }
 
+// Test the exact bounds near +0.f
+//
+// With mantissa_bits = 8, tolerance_bits = 2
+//
+//                           Targeted bit
+//                           |
+//                           v
+// 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+// s e e e e e e e e m m m m m m m m m m m m m m m m m m m m m m m
+//               =>|      8      |
+//                           | 2 |<=
+//
+// [Upper bound]
+//                           Add 1 at this bit to get upper bound
+//                           |
+//                           v
+// 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+// +                         1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+// ---------------------------------------------------------------
+// 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+//
+// [Lower bound]
+//                           Minus 1 at this bit
+//                           |
+//                           v
+// 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+// -                         1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+// ---------------------------------------------------------------
+// 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+//
+// Convert to 2's compliment
+// 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+//
+// Mask the sign bit
+// 1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 TEST(all_close_f, mantissa_8_near_0)
 {
-    // Tests the exact bounds near +0.f
-    //
-    // With mantissa_bits = 8, tolerance_bits = 2
-    //
-    //                           Targeted bit
-    //                           |
-    //                           v
-    // 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    // s e e e e e e e e m m m m m m m m m m m m m m m m m m m m m m m
-    //               =>|      8      |
-    //                           | 2 |<=
-    //
-    // [Upper bound]
-    //                           Add 1 at this bit to get upper bound
-    //                           |
-    //                           v
-    // 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    // +                         1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    // ---------------------------------------------------------------
-    // 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    //
-    // [Lower bound]
-    //                           Minus 1 at this bit
-    //                           |
-    //                           v
-    // 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    // -                         1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    // ---------------------------------------------------------------
-    // 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    //
-    // Convert to 2's compliment
-    // 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    //
-    // Mask the sign bit
-    // 1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-
     // 0.f, the ground-truth value
     float expected_val = bits_to_float("00000000000000000000000000000000");
     float computed_val;
 
-    // ~3.67342E-40, upper bound
+    // ~3.67342E-40, the exact upper bound
     computed_val = bits_to_float("00000000000001000000000000000000");
     EXPECT_TRUE(test::close_f(expected_val, computed_val, 8, 2));
 
-    // ~3.67343E-40, bigger than upper bound
+    // ~3.67343E-40, the next representable number bigger than upper bound
     computed_val = bits_to_float("00000000000001000000000000000001");
     EXPECT_TRUE(!test::close_f(expected_val, computed_val, 8, 2));
 
-    // 0.984375f, lower bound
+    // ~-3.67342E-40, the exact lower bound
     computed_val = bits_to_float("10000000000001000000000000000000");
     EXPECT_TRUE(test::close_f(expected_val, computed_val, 8, 2));
 
-    // // 0.9843749404f, smaller than lower bound
-    // computed_val = bits_to_float("00111111011110111111111111111111");
-    // EXPECT_TRUE(!test::close_f(expected_val, computed_val, 8, 2));
+    // ~-3.67343E-40, the next representable number smaller than lower bound
+    computed_val = bits_to_float("10000000000001000000000000000001");
+    EXPECT_TRUE(!test::close_f(expected_val, computed_val, 8, 2));
 }
 
+// Test the exact bounds near 1.f
+//
+// With mantissa_bits = 8, tolerance_bits = 2
+//
+//                           Targeted bit
+//                           |
+//                           v
+// s e e e e e e e e m m m m m m m m m m m m m m m m m m m m m m m
+//               =>|      8      |
+//                           | 2 |<=
+//
+// [Upper bound]
+//                           Add 1 at this bit to get upper bound
+//                           |
+//                           v
+// 0 0 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+// +                         1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+// ---------------------------------------------------------------
+// 0 0 1 1 1 1 1 1 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+//
+// [Lower bound]
+//                           Minus 1 at this bit to get lower bound
+//                           |
+//                           v
+// 0 0 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+// -                         1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+// ---------------------------------------------------------------
+// 0 0 1 1 1 1 1 1 0 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 TEST(all_close_f, mantissa_8_near_1)
 {
-    // Tests the exact bounds near 1.f
-    //
-    // With mantissa_bits = 8, tolerance_bits = 2
-    //
-    //                           Targeted bit
-    //                           |
-    //                           v
-    // s e e e e e e e e m m m m m m m m m m m m m m m m m m m m m m m
-    //               =>|      8      |
-    //                           | 2 |<=
-    //
-    // [Upper bound]
-    //                           Add 1 at this bit to get upper bound
-    //                           |
-    //                           v
-    // 0 0 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    // +                         1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    // ---------------------------------------------------------------
-    // 0 0 1 1 1 1 1 1 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    //
-    // [Lower bound]
-    //                           Minus 1 at this bit to get lower bound
-    //                           |
-    //                           v
-    // 0 0 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    // -                         1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-    // ---------------------------------------------------------------
-    // 0 0 1 1 1 1 1 1 0 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-
     // 1.f, the ground-truth value
     float expected_val = bits_to_float("00111111100000000000000000000000");
     float computed_val;
 
-    // 1.03125f, upper bound
+    // 1.03125f, the exact upper bound
     computed_val = bits_to_float("00111111100001000000000000000000");
     EXPECT_TRUE(test::close_f(expected_val, computed_val, 8, 2));
 
-    // 1.031250119f, bigger than upper bound
+    // 1.031250119f, the next representable number bigger than upper bound
     computed_val = bits_to_float("00111111100001000000000000000001");
     EXPECT_TRUE(!test::close_f(expected_val, computed_val, 8, 2));
 
-    // 0.984375f, lower bound
+    // 0.984375f, the exact lower bound
     computed_val = bits_to_float("00111111011111000000000000000000");
     EXPECT_TRUE(test::close_f(expected_val, computed_val, 8, 2));
 
-    // 0.9843749404f, smaller than lower bound
+    // 0.9843749404f, the next representable number smaller than lower bound
     computed_val = bits_to_float("00111111011110111111111111111111");
     EXPECT_TRUE(!test::close_f(expected_val, computed_val, 8, 2));
 }
