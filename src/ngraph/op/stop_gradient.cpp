c29a6cf4177bd2da154ded41c6aeb88e2756c9eb
Ashok Emani <ashok.emani@intel.com>
2018-05-31 20:38:14 -0700

    add StopGradient op src

---
+++ src/ngraph/op/stop_gradient.cpp
@@ -0,0 +1,49 @@
+/*******************************************************************************
+* Copyright 2017-2018 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#include "ngraph/op/stop_gradient.hpp"
+#include "ngraph/op/broadcast.hpp"
+#include "ngraph/op/constant.hpp"
+
+using namespace std;
+using namespace ngraph;
+
+op::StopGradient::StopGradient(const shared_ptr<Node>& arg)
+    : UnaryElementwiseArithmetic("StopGradient", arg)
+{
+}
+
+shared_ptr<Node> op::StopGradient::copy_with_new_args(const NodeVector& new_args) const
+{
+    if (new_args.size() != 1)
+    {
+        throw ngraph_error("Incorrect number of new arguments");
+    }
+    return make_shared<StopGradient>(new_args.at(0));
+}
+
+void op::StopGradient::generate_adjoints(autodiff::Adjoints& adjoints, const NodeVector& deltas)
+{
+    auto x = get_inputs().at(0).get_output().get_node();
+
+    auto zero = make_shared<op::Constant>(x->get_element_type(), Shape{}, vector<string>{"0"});
+    AxisSet axes;
+    for (size_t i = 0; i < x->get_shape().size(); i++)
+        axes.insert(i);
+    auto zeros = make_shared<op::Broadcast>(zero, x->get_shape(), axes);
+
+    adjoints.add_delta(x, zeros);
+}
