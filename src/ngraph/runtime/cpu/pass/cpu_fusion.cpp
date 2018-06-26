3a94e7b75ed14a0d1edd41bc7e2fc8e97b295a00
Jayaram Bobba <jayaram.bobba@intel.com>
2018-06-26 00:15:11 -0700

    Bug fix to account for inplace input in conv sum fusion

--- src/ngraph/runtime/cpu/pass/cpu_fusion.cpp
+++ src/ngraph/runtime/cpu/pass/cpu_fusion.cpp
@@ -1097,10 +1097,12 @@ void ngraph::runtime::cpu::pass::CPUFusion::construct_conv_bias_add()
             auto add_m = m.get_match_root();
             auto pattern_map = m.get_pattern_map();
             auto conv_m = std::dynamic_pointer_cast<op::ConvolutionBias>(add_m->get_argument(1));
+            auto add_input = add_m->get_argument(0);
 
             if (!conv_m)
             {
                 conv_m = std::dynamic_pointer_cast<op::ConvolutionBias>(add_m->get_argument(0));
+                add_input = add_m->get_argument(1);
             }
 
             //These checks are to make sure a MKLDNN Convolution kernel can be used.
@@ -1163,7 +1165,7 @@ void ngraph::runtime::cpu::pass::CPUFusion::construct_conv_bias_add()
             }
 
             auto conv_add = std::shared_ptr<Node>(
-                new op::ConvolutionBiasAdd(conv_m, pattern_map[add_input], false));
+                new op::ConvolutionBiasAdd(conv_m, add_input, false));
             ngraph::replace_node(m.get_match_root(), conv_add);
             return true;
         };
