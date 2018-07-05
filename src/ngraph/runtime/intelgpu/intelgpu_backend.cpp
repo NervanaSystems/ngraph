88421e2cd7d71807ebaf784c5faa7fac4464f0d8
Dmitry Yershov <dmitry.b.yershov@intel.com>
2018-07-05 01:53:19 +0000

    Backend/API: Implementation of ADD and MUL operations in the compile method for IntelGPU

--- src/ngraph/runtime/intelgpu/intelgpu_backend.cpp
+++ src/ngraph/runtime/intelgpu/intelgpu_backend.cpp
@@ -14,11 +14,42 @@
 * limitations under the License.
 *******************************************************************************/
 
+#include <CPP/eltwise.hpp>
+#include <CPP/input_layout.hpp>
+#include <CPP/layout.hpp>
+#include <CPP/network.hpp>
+#include <CPP/scale.hpp>
+
 #include "ngraph/runtime/intelgpu/intelgpu_backend.hpp"
+#include "ngraph/runtime/intelgpu/intelgpu_layout.hpp"
 #include "ngraph/runtime/intelgpu/intelgpu_tensor_view.hpp"
 
 using namespace std;
 using namespace ngraph;
+
+void do_eltwise_operation(cldnn::topology& topology,
+                          const shared_ptr<Node>& op,
+                          cldnn::eltwise_mode mode)
+{
+    if (op->get_output_size() != 1 || op->get_input_size() != 2)
+    {
+        ostringstream os;
+        os << "eltwise operation \"" << op->description() << "\" input and output sizes mismatch.";
+        throw std::invalid_argument(os.str());
+    }
+
+    std::vector<cldnn::primitive_id> op_add_inputs;
+    for (const descriptor::Input& op_input : op->get_inputs())
+    {
+        const std::string& element_name = op_input.get_tensor().get_name();
+        op_add_inputs.push_back(element_name);
+    }
+
+    const std::string& output_name = op->get_outputs().begin()->get_tensor().get_name();
+
+    cldnn::eltwise op_add(output_name, op_add_inputs, mode);
+    topology.add(op_add);
+}
 
 extern "C" runtime::Backend* new_backend(const char* configuration_string)
 {
@@ -51,7 +82,48 @@ shared_ptr<runtime::TensorView> runtime::intelgpu::IntelGPUBackend::create_tenso
 
 bool runtime::intelgpu::IntelGPUBackend::compile(shared_ptr<Function> func)
 {
-    throw runtime_error("IntelGPUBackend::compile: Not implemented yet");
+    FunctionInstance& instance = ocl_networks[func];
+    if (instance.ocl_network != nullptr)
+    {
+        return true;
+    }
+
+    cldnn::topology topology;
+
+    for (shared_ptr<Node> op : func->get_ops())
+    {
+        if ("Parameter" == op->description())
+        {
+            if (op->get_output_size() != 1 || op->get_input_size() != 0)
+            {
+                throw ngraph_error("Parameter input and output sizes mismatch.");
+            }
+            const std::string& element_name = op->get_output_tensor_view()->get_tensor().get_name();
+            cldnn::layout element_layout =
+                IntelGPULayout::create_cldnn_layout(op->get_element_type(), op->get_shape());
+
+            cldnn::input_layout op_layout(element_name, element_layout);
+            topology.add(op_layout);
+        }
+        else if ("Add" == op->description())
+        {
+            do_eltwise_operation(topology, op, cldnn::eltwise_mode::sum);
+        }
+        else if ("Multiply" == op->description())
+        {
+            do_eltwise_operation(topology, op, cldnn::eltwise_mode::prod);
+        }
+        else
+        {
+            ostringstream os;
+            os << "Unsupported operation \"" << op->description() << '\"';
+            throw std::invalid_argument(os.str());
+        }
+    }
+
+    instance.ocl_network = std::make_shared<cldnn::network>(*ocl_engine, topology);
+
+    return true;
 }
 
 bool runtime::intelgpu::IntelGPUBackend::call(
