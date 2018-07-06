620c20f6a3559f0c2f73345dcaca76810fd4cf25
Robert Kimball <robert.kimball@intel.com>
2018-07-06 12:46:56 -0700

    remove unneeded test

--- python/test/ngraph/test_reduction.py
+++ python/test/ngraph/test_reduction.py
@@ -73,14 +73,3 @@ def test_reduce():
     reduction_function_args = [init_val, ng.impl.op.Subtract, list(reduction_axes)]
     result = run_op_node([input_data], ng.reduce, *reduction_function_args)
     assert np.allclose(result, expected)
-
-    def custom_reduction_function(a, b):
-        return a + b * b
-
-    param1 = ng.impl.op.Parameter(get_element_type(np.float32), Shape([]))
-    param2 = ng.impl.op.Parameter(get_element_type(np.float32), Shape([]))
-    reduction_operation = Function(NodeVector([custom_reduction_function(param1, param2)]),
-                                   [param1, param2], 'reduction_op')
-    reduction_function_args = [init_val, reduction_operation, list(reduction_axes)]
-    result = run_op_node([input_data], ng.reduce, *reduction_function_args)
-    assert np.allclose(result, expected)
