4224145f8265e12f418082d23f07090df99a05c0
amy.zhuang <amy.zhuang@intel.com>
2018-07-20 09:52:31 -0700

    Add NGRAPH_INTRA_OP_PARALLELISM to control size of thread pools.

--- src/ngraph/runtime/cpu/kernel/eigen_thread_pool.cpp
+++ src/ngraph/runtime/cpu/kernel/eigen_thread_pool.cpp
@@ -29,9 +29,16 @@ namespace ngraph
                 static int GetNumCores()
                 {
                     const auto omp_num_threads = std::getenv("OMP_NUM_THREADS");
+                    const auto ngraph_intra_op_parallelism =
+                        std::getenv("NGRAPH_INTRA_OP_PARALLELISM");
                     int count;
 
                     if (omp_num_threads && (count = std::atoi(omp_num_threads)))
+                    {
+                        return count;
+                    }
+                    else if (ngraph_intra_op_parallelism &&
+                             (count == std::atoi(ngraph_intra_op_parallelism)))
                     {
                         return count;
                     }
