44838fa6ce122c014db8378a9b2ce3e6cd82ec36
Chris Lishka <christopher.r.lishka@intel.com>
2018-08-02 15:22:12 -0700

    Put Jenkins repo in a subdirectory and use the requested branch

--- .ci/jenkins/jenkins-trigger.groovy
+++ .ci/jenkins/jenkins-trigger.groovy
@@ -3,19 +3,25 @@
 // which can trigger GitHub jobs more effectively than the GitHub Pull
 // Request Builder (GHPRB) plugin, in our environment.
 
-node('bdw && nogpu') {
+// The original ngraph-unittest job required the following parameters.  We
+// set these up below as global variables, so we do not need to rewrite the
+// original script -- we only need to provide this new trigger hook.
+//
+// ngraph-unittest parameters:
+BRANCH = BRANCH_NAME
+// PR_URL = CHANGE_URL
+// PR_COMMIT_AUTHOR = CHANGE_AUTHOR
+// TRIGGER_URL        <- No longer needed
+JENKINS_BRANCH = "chrisl/new-ngraph-ci-trigger"
+TIMEOUTTIME = 3600
 
-    // The original ngraph-unittest job required the following parameters.  We
-    // set these up below as global variables, so we do not need to rewrite the
-    // original script -- we only need to provide this new trigger hook.
-    //
-    // ngraph-unittest parameters:
-    BRANCH = BRANCH_NAME
-    // PR_URL = CHANGE_URL
-    // PR_COMMIT_AUTHOR = CHANGE_AUTHOR
-    // TRIGGER_URL        <- No longer needed
-    JENKINS_BRANCH = "chrisl/new-ngraph-ci-trigger"
-    TIMEOUTTIME = 3600
+// Constants
+JENKINS_DIR="cje-algo"
+
+
+node("bdw && nogpu") {
+
+    deleteDir()  // Clear the workspace before starting
 
     echo "jenkins-trigger parameters:"
     echo "BRANCH           = ${BRANCH}"
@@ -25,7 +31,10 @@ node('bdw && nogpu') {
     echo "TIMEOUTTIME      = ${TIMEOUTTIME}"
 
     // Clone the cje-algo directory which contains our Jenkins groovy scripts
-    git poll: false, url: 'https://github.intel.com/AIPG/cje-algo'
+    dir(JENKINS_DIR){
+        git(branch: JENKINS_BRANCH, changelog: false, poll: false
+            url: 'https://github.intel.com/AIPG/cje-algo')
+    }
     echo "After cloning cje-algo, workspace looks like:"
     sh "ls -l"
 
