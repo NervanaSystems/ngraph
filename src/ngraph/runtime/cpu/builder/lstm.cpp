fd3687eb0e6abcdd0cc497452bcdde5f00114792
pruthvi <pruthvi.gowda.thorehosur.appajigowda@intel.com>
2018-08-07 13:23:28 -0700

    addressed PR comments

--- src/ngraph/runtime/cpu/builder/lstm.cpp
+++ src/ngraph/runtime/cpu/builder/lstm.cpp
@@ -42,35 +42,9 @@ namespace ngraph
                 if (args.size() != 5 || !lstm_node->get_fused_inputs())
                 {
                     throw ngraph_error(
-                        "Lstm op doesnt have the required number of inputs to emit MKLDNN kernel");
+                        "Lstm op doesnt have the required number of inputs to create MKLDNN "
+                        "kernel");
                 }
-                auto src_sequence_length_max =
-                    static_cast<unsigned long>(lstm_node->get_src_sequence_length());
-                auto direction = static_cast<unsigned long>(lstm_node->get_direction());
-                auto num_fused_layers =
-                    static_cast<unsigned long>(lstm_node->get_num_fused_layers());
-                auto feature_size =
-                    static_cast<unsigned long>(lstm_node->get_src_iter_feature_size());
-                auto batch = static_cast<unsigned long>(lstm_node->get_batch_size());
-                auto lstm_cell_n_gates =
-                    static_cast<unsigned long>(lstm_node->get_gates_per_cell());
-                auto lstm_cell_n_states =
-                    static_cast<unsigned long>(lstm_node->get_num_cell_states());
-
-                if (out[0].get_shape().size() == 2 && (out[0].get_shape()[1] != feature_size))
-                {
-                    throw ngraph_error(
-                        "input slc{ht} feature size is not equal to output dlc{ht} feature size ");
-                }
-
-                if (out[1].get_shape().size() == 2 && (out[1].get_shape()[1] != feature_size) &&
-                    lstm_node->get_num_timesteps() != 1)
-                {
-                    throw ngraph_error(
-                        "input sic{ht_1|ct_1} feature size is not equal to output dlc{ht_1|ct_1} "
-                        "feature size ");
-                }
-
                 auto& functors = external_function->get_functors();
                 auto& tensor_data = external_function->get_tensor_data();
 
@@ -82,49 +56,8 @@ namespace ngraph
                 auto& dst_layer_tensor = tensor_data[out[0].get_name()];
                 auto& dst_iter_tensor = tensor_data[out[1].get_name()];
 
-                Shape src_layer_tz{
-                    src_sequence_length_max,
-                    batch,
-                    static_cast<unsigned long>(lstm_node->get_src_layer_feature_size())};
-                Shape src_iter_tz{
-                    num_fused_layers, direction, lstm_cell_n_states, batch, feature_size};
-                Shape wei_layer_tz{
-                    num_fused_layers,
-                    direction,
-                    static_cast<unsigned long>(lstm_node->get_src_layer_feature_size()),
-                    lstm_cell_n_gates,
-                    feature_size};
-                Shape wei_iter_tz{
-                    num_fused_layers, direction, feature_size, lstm_cell_n_gates, feature_size};
-                Shape bias_tz{num_fused_layers, direction, lstm_cell_n_gates, feature_size};
-                Shape dst_layer_tz{src_sequence_length_max, batch, feature_size};
-                Shape dst_iter_tz{
-                    num_fused_layers, direction, lstm_cell_n_states, batch, feature_size};
-
                 auto& mkldnn_emitter = external_function->get_mkldnn_emitter();
-                // We create the memory descriptors used by the user
-                auto src_layer_md = mkldnn_emitter->build_memory_descriptor(
-                    src_layer_tz, args[0].get_element_type(), mkldnn::memory::format::tnc);
-                auto src_iter_md = mkldnn_emitter->build_memory_descriptor(
-                    src_iter_tz, args[1].get_element_type(), mkldnn::memory::format::ldsnc);
-                auto wei_layer_md = mkldnn_emitter->build_memory_descriptor(
-                    wei_layer_tz, args[2].get_element_type(), mkldnn::memory::format::ldigo);
-                auto wei_iter_md = mkldnn_emitter->build_memory_descriptor(
-                    wei_iter_tz, args[3].get_element_type(), mkldnn::memory::format::ldigo);
-                auto bias_md = mkldnn_emitter->build_memory_descriptor(
-                    bias_tz, args[4].get_element_type(), mkldnn::memory::format::ldgo);
-                auto dst_layer_md = mkldnn_emitter->build_memory_descriptor(
-                    dst_layer_tz, out[0].get_element_type(), mkldnn::memory::format::tnc);
-                auto dst_iter_md = mkldnn_emitter->build_memory_descriptor(
-                    dst_iter_tz, out[1].get_element_type(), mkldnn::memory::format::ldsnc);
-
-                auto lstm_index = mkldnn_emitter->build_rnn_forward(src_layer_md,
-                                                                    src_iter_md,
-                                                                    wei_layer_md,
-                                                                    wei_iter_md,
-                                                                    bias_md,
-                                                                    dst_layer_md,
-                                                                    dst_iter_md);
+                auto lstm_index = mkldnn_emitter->build_rnn<ngraph::op::Lstm>(node, args, out);
                 auto& deps = mkldnn_emitter->get_primitive_deps(lstm_index);
 
                 auto functor = [&, lstm_index](CPURuntimeContext* ctx) {
--- src/ngraph/runtime/cpu/builder/rnn.cpp
+++ src/ngraph/runtime/cpu/builder/rnn.cpp
@@ -37,33 +37,6 @@ namespace ngraph
                         "Rnn is supported only through MKLDNN and doesnt have reference "
                         "INTERPRETER implementation");
                 }
-                const ngraph::op::Rnn* rnn_node = static_cast<const ngraph::op::Rnn*>(node);
-
-                auto src_sequence_length_max =
-                    static_cast<unsigned long>(rnn_node->get_src_sequence_length());
-                auto direction = static_cast<unsigned long>(rnn_node->get_direction());
-                auto num_fused_layers =
-                    static_cast<unsigned long>(rnn_node->get_num_fused_layers());
-                auto feature_size =
-                    static_cast<unsigned long>(rnn_node->get_src_iter_feature_size());
-                auto batch = static_cast<unsigned long>(rnn_node->get_batch_size());
-                auto rnn_cell_n_gates = static_cast<unsigned long>(rnn_node->get_gates_per_cell());
-                auto rnn_cell_n_states =
-                    static_cast<unsigned long>(rnn_node->get_num_cell_states());
-
-                if (out[0].get_shape().size() == 2 && (out[0].get_shape()[1] != feature_size))
-                {
-                    throw ngraph_error(
-                        "input slc{ht} feature size is not equal to output dlc{ht} feature size ");
-                }
-
-                if (out[1].get_shape().size() == 2 && (out[1].get_shape()[1] != feature_size) &&
-                    rnn_node->get_num_timesteps() != 1)
-                {
-                    throw ngraph_error(
-                        "input sic{ht_1|ct_1} feature size is not equal to output dlc{ht_1|ct_1} "
-                        "feature size ");
-                }
 
                 auto& functors = external_function->get_functors();
                 auto& tensor_data = external_function->get_tensor_data();
@@ -76,52 +49,9 @@ namespace ngraph
                 auto& dst_layer_tensor = tensor_data[out[0].get_name()];
                 auto& dst_iter_tensor = tensor_data[out[1].get_name()];
 
-                Shape src_layer_tz{
-                    src_sequence_length_max,
-                    batch,
-                    static_cast<unsigned long>(rnn_node->get_src_layer_feature_size())};
-                Shape src_iter_tz{
-                    num_fused_layers, direction, rnn_cell_n_states, batch, feature_size};
-                Shape wei_layer_tz{
-                    num_fused_layers,
-                    direction,
-                    static_cast<unsigned long>(rnn_node->get_src_layer_feature_size()),
-                    rnn_cell_n_gates,
-                    feature_size};
-                Shape wei_iter_tz{
-                    num_fused_layers, direction, feature_size, rnn_cell_n_gates, feature_size};
-                Shape bias_tz{num_fused_layers, direction, rnn_cell_n_gates, feature_size};
-                Shape dst_layer_tz{src_sequence_length_max, batch, feature_size};
-                Shape dst_iter_tz{
-                    num_fused_layers, direction, rnn_cell_n_states, batch, feature_size};
-
                 auto& mkldnn_emitter = external_function->get_mkldnn_emitter();
-
-                // We create the memory descriptors used by the user
-                auto src_layer_md = mkldnn_emitter->build_memory_descriptor(
-                    src_layer_tz, args[0].get_element_type(), mkldnn::memory::format::tnc);
-                auto src_iter_md = mkldnn_emitter->build_memory_descriptor(
-                    src_iter_tz, args[1].get_element_type(), mkldnn::memory::format::ldsnc);
-                auto wei_layer_md = mkldnn_emitter->build_memory_descriptor(
-                    wei_layer_tz, args[2].get_element_type(), mkldnn::memory::format::ldigo);
-                auto wei_iter_md = mkldnn_emitter->build_memory_descriptor(
-                    wei_iter_tz, args[3].get_element_type(), mkldnn::memory::format::ldigo);
-                auto bias_md = mkldnn_emitter->build_memory_descriptor(
-                    bias_tz, args[4].get_element_type(), mkldnn::memory::format::ldgo);
-                auto dst_layer_md = mkldnn_emitter->build_memory_descriptor(
-                    dst_layer_tz, out[0].get_element_type(), mkldnn::memory::format::tnc);
-                auto dst_iter_md = mkldnn_emitter->build_memory_descriptor(
-                    dst_iter_tz, out[1].get_element_type(), mkldnn::memory::format::ldsnc);
-
-                auto rnn_index = mkldnn_emitter->build_rnn_forward(src_layer_md,
-                                                                   src_iter_md,
-                                                                   wei_layer_md,
-                                                                   wei_iter_md,
-                                                                   bias_md,
-                                                                   dst_layer_md,
-                                                                   dst_iter_md);
+                auto rnn_index = mkldnn_emitter->build_rnn<ngraph::op::Rnn>(node, args, out);
                 auto& deps = mkldnn_emitter->get_primitive_deps(rnn_index);
-
                 auto functor = [&, rnn_index](CPURuntimeContext* ctx) {
                     cpu::mkldnn_utils::set_memory_ptr(ctx, deps[0], src_layer_tensor);
                     cpu::mkldnn_utils::set_memory_ptr(ctx, deps[1], src_iter_tensor);
