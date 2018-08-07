bd4c857a5052c1a175df79230586744aac60bce7
nikolay.korovaiko <korovaikon@gmail.com>
2018-08-07 14:12:34 -0700

    merge after jbobba's changes

--- src/ngraph/runtime/cpu/builder/lrn.cpp
+++ src/ngraph/runtime/cpu/builder/lrn.cpp
@@ -43,17 +43,9 @@ namespace ngraph
 
                 if (runtime::cpu::mkldnn_utils::use_mkldnn_kernel(node))
                 {
-                    auto input_format =
-                        runtime::cpu::mkldnn_utils::get_input_mkldnn_format(node, 0);
-
-                    auto output_format =
-                        runtime::cpu::mkldnn_utils::get_output_mkldnn_format(node, 0);
-
                     auto& mkldnn_emitter = external_function->get_mkldnn_emitter();
-                    auto input_data_desc =
-                        mkldnn_emitter->build_memory_descriptor(args[0], input_format);
-                    auto result_desc =
-                        mkldnn_emitter->build_memory_descriptor(out[0], output_format);
+                    auto input_data_desc = mkldnn_utils::get_input_mkldnn_md(node, 0);
+                    auto result_desc = mkldnn_utils::get_output_mkldnn_md(node, 0);
 
                     auto lrn_index =
                         mkldnn_emitter->build_lrn_forward(input_data_desc,
