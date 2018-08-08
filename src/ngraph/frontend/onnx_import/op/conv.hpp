9af61611364d146dfea6b708218afb26434c1931
Adam Rogowiec <adam.rogowiec@intel.com>
2018-08-08 15:36:47 +0200

    Refactoring.

--- src/ngraph/frontend/onnx_import/op/conv.hpp
+++ src/ngraph/frontend/onnx_import/op/conv.hpp
@@ -16,156 +16,147 @@
 
 #pragma once
 
-#include <cmath>
+#include <cstddef>
+#include <memory>
+#include <vector>
 
+#include "ngraph/frontend/onnx_import/exceptions.hpp"
 #include "ngraph/frontend/onnx_import/node.hpp"
+#include "ngraph/frontend/onnx_import/util/conv_pool.hpp"
+
+#include "ngraph/coordinate_diff.hpp"
+#include "ngraph/node.hpp"
 #include "ngraph/node_vector.hpp"
 #include "ngraph/op/add.hpp"
 #include "ngraph/op/broadcast.hpp"
 #include "ngraph/op/concat.hpp"
 #include "ngraph/op/convolution.hpp"
-#include "ngraph/op/slice.hpp"
 #include "ngraph/op/op.hpp"
+#include "ngraph/op/slice.hpp"
+#include "ngraph/strides.hpp"
 
 namespace ngraph
 {
-	namespace error
-	{	
-		namespace op
-		{
-			namespace conv
-			{
+    namespace onnx_import
+    {
+        namespace op
+        {
+            namespace detail
+            {
+                inline std::shared_ptr<ngraph::op::Op>
+                    make_ng_convolution(const std::shared_ptr<ngraph::Node>& data,
+                                        const std::shared_ptr<ngraph::Node>& filters,
+                                        const ngraph::Strides& strides,
+                                        const ngraph::Strides& dilations,
+                                        const ngrpah::CoordinateDiff& padding_below,
+                                        const ngrpah::CoordinateDiff& padding_above,
+                                        int groups)
+                {
+                    if (groups != 1)
+                    {
+                        // Split one convolution op to N ops where N is the number of groups
+                        // and concat results after computation.
+                        // reference: https://github.com/NervanaSystems/ngraph-mxnet/blob/fdd692/src/ngraph/ngraph_emitter.cc#L822-L856
+                        const size_t n_data_channels = data->get_shape().at(1);
+                        const size_t n_filters_channels = filters->get_shape().at(0);
+                        // TODO: ensure n_data_channels % groups = 0
+                        const size_t data_group_size = n_data_channels / groups;
+                        const size_t filters_group_size = n_filters_channels / groups;
+                        NodeVector convolution_nodes;
 
-	            struct op_value_error : ngraph_error
-	            {
-	                explicit op_value_error(const std::string& op_name,
-	                                        const std::string& name,
-	                                        const std::string& message)
-	                    : ngraph_error{op_name + " node (" + name + "): " + message}
-	                {
-	                }
-	            };
+                        // initial bounds for splice
+                        std::vector<size_t> data_lower_bounds(data->get_shape().size());
+                        std::vector<size_t> data_upper_bounds{data->get_shape()};
+                        std::vector<size_t> filters_lower_bounds(filters->get_shape().size());
+                        std::vector<size_t> filters_upper_bounds{filters->get_shape()};
 
-			} // namespace  conv
+                        for (std::size_t group{0}; group < groups; ++group)
+                        {
+                            // slice data
+                            data_lower_bounds[1] = group * data_group_size;
+                            data_upper_bounds[1] = (group + 1) * data_group_size;
+                            auto sliced_data = std::make_shared<ngraph::op::Slice>(
+                                data, data_lower_bounds, data_upper_bounds);
+                            // slice filters
+                            filters_lower_part[0] = group * filters_group_size;
+                            filters_upper_part[0] = (group + 1) * filters_group_size;
+                            auto sliced_filters = std::make_shared<ngraph::op::Slice>(
+                                filters, filters_lower_part, filters_upper_part);
 
-		} // namespace  op
+                            convolution_nodes.push_back(
+                                std::make_shared<ngraph::op::Convolution>(sliced_data,
+                                                                          sliced_filters,
+                                                                          strides,
+                                                                          dilations,
+                                                                          padding_below,
+                                                                          padding_above));
+                        }
+                        size_t concatenation_axis = 1;
+                        return std::make_shared<ngraph::op::Concat>(convolution_nodes,
+                                                                    concatenation_axis);
+                    }
+                    else
+                    {
+                        return std::make_shared<ngraph::op::Convolution>(
+                            data, filters, strides, dilations, padding_below, padding_above);
+                    }
+                }
 
-	} // namespace  error
-	
-	namespace onnx_import
-	{
-		namespace op
-		{
-			namespace detail
-			{
-				inline std::shared_ptr<ngraph::op::Op> 
-					make_ng_convolution(const std::shared_ptr<ngraph::Node>& data,
-										const std::shared_ptr<ngraph::Node>& weights,
-										std::vector<std::size_t> strides,
-										std::vector<std::size_t> dilations,
-										std::vector<std::size_t> weights,
-										std::vector<std::size_t> padding_below,
-										std::vector<std::size_t> padding_above,
-										int groups)
-					{
-						if (groups != 1)
-						{
-							// Split one convolution op to N ops where N is the number of groups 
-							// and concat results after computation.
-		    				// reference: https://github.com/NervanaSystems/ngraph-mxnet/blob/fdd692/src/ngraph/ngraph_emitter.cc#L822-L856
-							const size_t n_data_channels = data->get_shape().at(1);
-							const size_t n_weights_channels = weights->get_shape().at(0);
-							const size_t data_group_size = n_data_channels / groups;
-							const size_t weights_group_size = n_weights_channels / groups;
-							NodeVector convolution_nodes;
+            } // namespace  detail
 
-							// initial bounds for splice
-							std::vector<size_t> data_lower_bounds(data->get_shape().size());
-							std::vector<size_t> data_upper_bounds{data->get_shape()};
-							std::vector<size_t> weights_lower_bounds(weights->get_shape().size());
-							std::vector<size_t> weights_upper_bounds{weights->get_shape()};
+            /**
+             * @brief [brief description]
+             * @details [long description]
+             * 
+             * @param node [description]
+             * @param inputs [description]
+             * 
+             * @return [description]
+             */
+            inline NodeVector conv(const Node& node, const NodeVector& inputs)
+            {
+                auto data = inputs.at(0);
+                auto filters = inputs.at(1);
 
-							for (std::size_t group{0}; group < groups; ++group)
-							{
-								// slice data 
-					            data_lower_bounds[1] = group * data_group_size;
-					            data_upper_bounds[1] = (group + 1) * data_group_size;
-					            auto sliced_data = 
-					            	std::make_shared<ngraph::op::Slice>(data, data_lower_bounds, 
-					            					            		data_upper_bounds);
-					            // slice weights
-					            weights_lower_part[0] = group * weights_group_size;
-					            weights_upper_part[0] = std::max(
-					            							(group + 1) * weights_group_size, 1);
-					            auto sliced_weights = 
-				            		std::make_shared<ngraph::op::Slice>(weights, 
-				            				weights_lower_part, weights_upper_part);
+                int groups{node.get_attribute_value<int>("group", 1)};
+                // TODO validate groups value
+                if (groups <= 0)
+                {
+                    throw error::op::op_value_error("Conv",
+                                                    node.get_name(),
+                                                    "incorrect value of 'group' attribute: " +
+                                                        std::to_string(groups));
+                }
 
-					            convolution_nodes.push_back(
-					            	std::make_shared<ngraph::op::Convolution>(sliced_data, 
-					            		sliced_weights, strides, dilations, padding_below, 
-					            		padding_above));
-							}
-							size_t concatenation_axis = 1;
-							return std::make_shared<ngraph::op::Concat>(convolution_nodes, 
-								concatenation_axis);
-						}
-						else
-						{
-							return std::make_shared<ngraph::op::Convolution>(data, weights, 
-								strides, dilations, padding_below, padding_above);
-						}
-					}
+                auto strides = util::get_strides(node);
+                auto dilations = util::get_dilations(node);
+                auto paddings = util::get_pads(node);
+                auto padding_below = paddings.first;
+                auto padding_above = paddings.second;
 
-			} // namespace  detail
+                auto conv_node = detail::make_ng_convolution(
+                    data, filters, strides, dilations, padding_below, padding_above, groups);
 
-			/**
-			 * @brief [brief description]
-			 * @details [long description]
-			 * 
-			 * @param node [description]
-			 * @param inputs [description]
-			 * 
-			 * @return [description]
-			 */
-			inline NodeVector conv(const Node& node, const NodeVector& inputs)
-			{
-				auto data = inputs.at(0);
-				auto weights = inputs.at(1);
+                // no bias param
+                if (inputs.size() < 3)
+                {
+                    return conv_node;
+                }
 
-				int groups{node.get_attribute_value<int>("group", 1)};
-				// TODO validate groups value
-				if (groups <= 0 )
-				{
-					throw error::op::conv::op_value_error("Conv", node.get_name(), 
-						"incorrect value of 'group' attribute: " + std::to_string(groups));
-				}
-	
-				auto strides = get_strides(node);
-				auto dilations = get_dilations(node);
-				auto paddings = get_pads(node);
-				auto padding_below = paddings.at(0);
-				auto padding_above = paddings.at(1);
+                auto bias = inputs.at(2);
+                // const Shape& new_shape = conv_node->get_shape();
 
-				auto conv_node = detail::make_ng_convolution(data, weights, strides, dilations,
-									 padding_below, padding_above, groups);
+                throw error::not_supported_error("Conv", node.get_name(), "bias input");
 
-				if (inputs.size() >= 3)
-				{
-					auto bias = inputs.at(2);
-					const Shape& new_shape = conv_node->get_shape();
+                // auto broadcasted_bias = std::make_shared<ngraph::op::Broadcast>(bias, new_shape,
+                //      get_broadcast_axes(new_shape, bias->get_shape(), 1)
+                // // TODO impl get_broadcast_axes
+                // conv_node = std::make_shared<ngraph::op::Add>(conv_node, );
+                // return conv_node;
+            }
 
-					// TODO impl get_broadcast_axes
-					// conv_node = std::make_shared<ngraph::op::Add>(conv_node, 
-					// 	std::make_shared<ngraph::op::Broadcast>(bias, new_shape, 
-					// 		get_broadcast_axes(new_shape, bias->get_shape(), 1))));
-				}
+        } // namespace op
 
-				return conv_node;
-			}
-
-		} // namespace op
-
-	} // namespace onnx_import
+    } // namespace onnx_import
 
 } // namespace ngraph
