b94635d93de698f73b248bc9f7866a1eb5577ad3
Robert Kimball <robert.kimball@intel.com>
2018-08-14 07:00:18 -0700

    use all_close to compare floating point values

--- test/onnx_import.cpp
+++ test/onnx_import.cpp
@@ -20,8 +20,11 @@
 #include "gtest/gtest.h"
 #include "ngraph/frontend/onnx_import/onnx.hpp"
 #include "ngraph/ngraph.hpp"
+#include "util/all_close_f.hpp"
 #include "util/ndarray.hpp"
 #include "util/test_tools.hpp"
+
+using namespace ngraph;
 
 TEST(onnx, model_add_abc)
 {
@@ -40,7 +43,7 @@ TEST(onnx, model_add_abc)
     auto r{backend->create_tensor(ngraph::element::f32, shape)};
 
     backend->call(model.front(), {r}, {a, b, c});
-    EXPECT_EQ((std::vector<float>{6}), read_vector<float>(r));
+    EXPECT_TRUE(test::all_close_f((std::vector<float>{6}), read_vector<float>(r)));
 }
 
 TEST(onnx, model_add_abc_initializers)
@@ -57,7 +60,7 @@ TEST(onnx, model_add_abc_initializers)
     auto r{backend->create_tensor(ngraph::element::f32, shape)};
 
     backend->call(model.front(), {r}, {c});
-    EXPECT_EQ((std::vector<float>{3, 6, 9, 12}), read_vector<float>(r));
+    EXPECT_TRUE(test::all_close_f((std::vector<float>{3, 6, 9, 12}), read_vector<float>(r)));
 }
 
 TEST(onnx, model_split_equal_parts_default)
@@ -72,7 +75,7 @@ TEST(onnx, model_split_equal_parts_default)
     {
         auto result_vectors = execute(model[i], args, "CPU");
         EXPECT_EQ(result_vectors.size(), 1);
-        EXPECT_EQ(expected_output[i], result_vectors.front());
+        EXPECT_TRUE(test::all_close_f(expected_output[i], result_vectors.front()));
     }
 }
 
@@ -91,7 +94,7 @@ TEST(onnx, model_split_equal_parts_2d)
     {
         auto result_vectors = execute(model[i], args, "CPU");
         EXPECT_EQ(result_vectors.size(), 1);
-        EXPECT_EQ(expected_output[i], result_vectors[0]);
+        EXPECT_TRUE(test::all_close_f(expected_output[i], result_vectors[0]));
     }
 }
 
@@ -110,7 +113,7 @@ TEST(onnx, model_split_variable_parts_2d)
     {
         auto result_vectors = execute(model[i], args, "CPU");
         EXPECT_EQ(result_vectors.size(), 1);
-        EXPECT_EQ(expected_output[i], result_vectors[0]);
+        EXPECT_TRUE(test::all_close_f(expected_output[i], result_vectors[0]));
     }
 }
 
@@ -136,10 +139,10 @@ TEST(onnx, model_batchnorm_default)
     inputs.emplace_back(std::vector<float>{1., 1.5});
 
     // shape (1, 2, 1, 3)
-    auto expected_output = ngraph::test::NDArray<float, 4>(
-                               {{{{-0.999995, 0., 0.999995}}, {{-0.22474074, 1., 2.2247407}}}})
+    auto expected_output = ngraph::test::NDArray<float, 4>({{{{-0.999995f, 0.f, 0.999995f}},
+                                                             {{-0.22474074f, 1.f, 2.2247407f}}}})
                                .get_vector();
 
     auto result_vectors = execute(function, inputs, "CPU");
-    EXPECT_EQ(expected_output, result_vectors.front());
+    EXPECT_TRUE(test::all_close_f(expected_output, result_vectors.front()));
 }
