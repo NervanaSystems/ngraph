5ebaa219abe612ba2c1c7b22491b6decdc0cf07e
Artur Wojcik <artur.wojcik@intel.com>
2018-08-14 17:50:58 -0700

    onnx: fix Travis build configuration issue
    Signed-off-by: Artur Wojcik <artur.wojcik@intel.com>

--- cmake/external_protobuf.cmake
+++ cmake/external_protobuf.cmake
@@ -31,13 +31,11 @@ if (${CMAKE_VERSION} VERSION_LESS 3.2)
         PREFIX protobuf
         GIT_REPOSITORY ${PROTOBUF_GIT_REPO_URL}
         GIT_TAG ${PROTOBUF_GIT_BRANCH}
-        GIT_SHALLOW TRUE
-        # Disable install step
         INSTALL_COMMAND ""
         UPDATE_COMMAND ""
-        PATCH_COMMAND ./autogen.sh
-        CONFIGURE_COMMAND ./configure --disable-shared CXXFLAGS=-fPIC
-        BUILD_COMMAND "${MAKE}"
+        PATCH_COMMAND ""
+        CONFIGURE_COMMAND ./autogen.sh && ./configure --disable-shared CXXFLAGS=-fPIC
+        BUILD_COMMAND ${MAKE}
         TMP_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/tmp"
         STAMP_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/stamp"
         DOWNLOAD_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/download"
@@ -47,28 +45,49 @@ if (${CMAKE_VERSION} VERSION_LESS 3.2)
         EXCLUDE_FROM_ALL TRUE
         )
 else()
-    ExternalProject_Add(
-        ext_protobuf
-        PREFIX ext_protobuf
-        GIT_REPOSITORY ${PROTOBUF_GIT_REPO_URL}
-        GIT_TAG ${PROTOBUF_GIT_BRANCH}
-        GIT_SHALLOW TRUE
-        # Disable install step
-        INSTALL_COMMAND ""
-        UPDATE_COMMAND ""
-        PATCH_COMMAND ./autogen.sh
-        CONFIGURE_COMMAND ./configure --disable-shared CXXFLAGS=-fPIC
-        CMAKE_ARGS --disable-shared CXXFLAGS=-fPIC
-        BUILD_COMMAND ${MAKE}
-        TMP_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/tmp"
-        STAMP_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/stamp"
-        DOWNLOAD_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/download"
-        SOURCE_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/src"
-        BINARY_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/src"
-        INSTALL_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf"
-        BUILD_BYPRODUCTS "${EXTERNAL_PROJECTS_ROOT}/protobuf/src/src/.libs/libprotobuf.a"
-        EXCLUDE_FROM_ALL TRUE
-        )
+    if (${CMAKE_VERSION} VERSION_LESS 3.6)
+        ExternalProject_Add(
+            ext_protobuf
+            PREFIX ext_protobuf
+            GIT_REPOSITORY ${PROTOBUF_GIT_REPO_URL}
+            GIT_TAG ${PROTOBUF_GIT_BRANCH}
+            INSTALL_COMMAND ""
+            UPDATE_COMMAND ""
+            PATCH_COMMAND ""
+            CONFIGURE_COMMAND ./autogen.sh && ./configure --disable-shared CXXFLAGS=-fPIC
+            BUILD_COMMAND ${MAKE}
+            TMP_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/tmp"
+            STAMP_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/stamp"
+            DOWNLOAD_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/download"
+            SOURCE_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/src"
+            BINARY_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/src"
+            INSTALL_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf"
+            BUILD_BYPRODUCTS "${EXTERNAL_PROJECTS_ROOT}/protobuf/src/src/.libs/libprotobuf.a"
+            EXCLUDE_FROM_ALL TRUE
+            )
+    else()
+        # To speed things up prefer 'shallow copy' for CMake 3.6 and later
+        ExternalProject_Add(
+            ext_protobuf
+            PREFIX ext_protobuf
+            GIT_REPOSITORY ${PROTOBUF_GIT_REPO_URL}
+            GIT_TAG ${PROTOBUF_GIT_BRANCH}
+            GIT_SHALLOW TRUE
+            INSTALL_COMMAND ""
+            UPDATE_COMMAND ""
+            PATCH_COMMAND ""
+            CONFIGURE_COMMAND ./autogen.sh && ./configure --disable-shared CXXFLAGS=-fPIC
+            BUILD_COMMAND ${MAKE}
+            TMP_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/tmp"
+            STAMP_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/stamp"
+            DOWNLOAD_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/download"
+            SOURCE_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/src"
+            BINARY_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf/src"
+            INSTALL_DIR "${EXTERNAL_PROJECTS_ROOT}/protobuf"
+            BUILD_BYPRODUCTS "${EXTERNAL_PROJECTS_ROOT}/protobuf/src/src/.libs/libprotobuf.a"
+            EXCLUDE_FROM_ALL TRUE
+            )
+    endif()
 endif()
 
 # -----------------------------------------------------------------------------
@@ -84,20 +103,19 @@ set(Protobuf_SRC_ROOT_FOLDER ${SOURCE_DIR})
 set(Protobuf_PROTOC_EXECUTABLE ${Protobuf_SRC_ROOT_FOLDER}/src/protoc)
 set(Protobuf_INCLUDE_DIR ${Protobuf_SRC_ROOT_FOLDER}/src)
 set(Protobuf_LIBRARY ${Protobuf_SRC_ROOT_FOLDER}/src/.libs/libprotobuf.a)
-set(Protobuf_LIBRARIES "${Protobuf_LIBRARY}")
+set(Protobuf_LIBRARIES ${Protobuf_LIBRARY})
 
 if (NOT TARGET protobuf::libprotobuf)
     add_library(protobuf::libprotobuf UNKNOWN IMPORTED)
     set_target_properties(protobuf::libprotobuf PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${Protobuf_INCLUDE_DIR}")
-    if (EXISTS "${Protobuf_LIBRARY}")
-        set_target_properties(protobuf::libprotobuf PROPERTIES IMPORTED_LOCATION "${Protobuf_LIBRARY}")
-    endif()
-    add_dependencies(protobuf::libprotobuf ext_protobuf)    
+        INTERFACE_INCLUDE_DIRECTORIES "${Protobuf_INCLUDE_DIR}"
+        IMPORTED_LOCATION "${Protobuf_LIBRARY}")
+    add_dependencies(protobuf::libprotobuf ext_protobuf)
 endif()
 
 if (NOT TARGET protobuf::protoc)
-    add_executable(protobuf::protoc IMPORTED)            
-    set_target_properties(protobuf::protoc PROPERTIES IMPORTED_LOCATION "${Protobuf_PROTOC_EXECUTABLE}")
+    add_executable(protobuf::protoc IMPORTED)
+    set_target_properties(protobuf::protoc PROPERTIES
+        IMPORTED_LOCATION "${Protobuf_PROTOC_EXECUTABLE}")
     add_dependencies(protobuf::protoc ext_protobuf)
 endif()
