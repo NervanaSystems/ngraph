3f1948a7c9f84df1352830617365b281e410f694
pruthvi <pruthvi.gowda.thorehosur.appajigowda@intel.com>
2018-08-16 16:33:14 -0700

    style fix

--- src/ngraph/runtime/cpu/cpu_layout_descriptor.hpp
+++ src/ngraph/runtime/cpu/cpu_layout_descriptor.hpp
@@ -40,7 +40,6 @@ namespace ngraph
                 ~LayoutDescriptor() override {}
                 size_t get_size() override { return m_size; }
                 virtual size_t size() override { return m_mkldnn_memory_size; }
-                void compute_mkldnn_memory_size(const mkldnn::memory::desc md);
                 size_t get_offset() const { return m_offset; }
                 size_t get_index_offset(const std::vector<size_t>& indices) override;
 
