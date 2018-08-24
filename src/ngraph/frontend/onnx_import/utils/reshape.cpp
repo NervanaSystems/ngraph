1353713374637e40b86ce55fc25d5d0edc98c940
Michał Karzyński <postrational@users.noreply.github.com>
2018-08-25 00:54:20 +0200

    [ONNX] Fix reshape helper (#1490)

--- src/ngraph/frontend/onnx_import/utils/reshape.cpp
+++ src/ngraph/frontend/onnx_import/utils/reshape.cpp
@@ -14,6 +14,8 @@
  * limitations under the License.
  *******************************************************************************/
 
+#include <numeric>
+
 #include "ngraph/op/reshape.hpp"
 
 #include "utils/reshape.hpp"
