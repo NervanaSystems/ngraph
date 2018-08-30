204b40ced517d331cdae25ceb50403ebcb66d39f
Michal Karzynski <michal.karzynski@intel.com>
2018-08-30 13:08:48 +0200

    Remove redundant files

--- python/pyngraph/ops/util/regmodule_pyngraph_op_util.hpp
+++ python/pyngraph/ops/util/regmodule_pyngraph_op_util.hpp
@@ -18,13 +18,10 @@
 
 #include <pybind11/pybind11.h>
 #include "pyngraph/ops/util/arithmetic_reduction.hpp"
-#include "pyngraph/ops/util/binary_elementwise.hpp"
 #include "pyngraph/ops/util/binary_elementwise_arithmetic.hpp"
 #include "pyngraph/ops/util/binary_elementwise_comparison.hpp"
 #include "pyngraph/ops/util/binary_elementwise_logical.hpp"
 #include "pyngraph/ops/util/op_annotations.hpp"
-#include "pyngraph/ops/util/requires_tensor_view_args.hpp"
-#include "pyngraph/ops/util/unary_elementwise.hpp"
 #include "pyngraph/ops/util/unary_elementwise_arithmetic.hpp"
 
 namespace py = pybind11;
