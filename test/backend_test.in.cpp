5f40d9570eefeee69c98e57b62f6439b00acdee8
Fenglei <35274053+fengleitian@users.noreply.github.com>
2018-09-04 11:51:04 -0700

    nvgpu reduce to scalar optimization  (#1491)
    * add cuda reduce
    
    * clang format
    
    * fix bugs
    
    * fix bug
    
    * add 1d reduce
    
    * clang format
    
    * fix bugs
    
    * unroll loop
    
    * remove debug info
    
    * revert tests
    
    * unroll 1D reduce op
    
    * add comments
    
    * using cudnn for nd to scalar reduction
    
    * remove cuda 1d reduction since cudnn version is faster
    
    * remove 1D kernel
    
    * fix bugs
    
    * 1d multi block size
    
    * remove debug
    
    * change kernel name
    
    * add reduce to scalar optimization, add test
    
    * fix bugs and tune parameters
    
    * clang format
    
    * update comments
    
    * update comments
    
    * update comments
    
    * clang format
    
    * update comments
    
    * remove wrong comments, apply clang format
    
    * resolve Bob's comment
    
    * clang format
    
    * pass shared mem size from cuLaunchKernel, set unroll loop size through host code
    
    * remove unused code.clang format
    
    * change reduce to thread with shfl for each warp first
    
    * add seed
    
    * unroll size

--- test/backend_test.in.cpp
+++ test/backend_test.in.cpp
@@ -18,8 +18,8 @@
 #include <cinttypes>
 #include <cmath>
 #include <cstdlib>
+#include <random>
 #include <string>
-
 #include "gtest/gtest.h"
 
 #include "ngraph/autodiff/adjoints.hpp"
@@ -36,6 +36,8 @@
 #include "util/random.hpp"
 #include "util/test_control.hpp"
 #include "util/test_tools.hpp"
+
+static std::mt19937_64 random_generator;
 
 using namespace std;
 using namespace ngraph;
@@ -3500,6 +3502,33 @@ NGRAPH_TEST(${BACKEND_NAME}, sum_to_scalar)
     // For some reason I'm feeling extra paranoid about making sure reduction doesn't clobber the
     // input tensors, so let's do this too.
     EXPECT_EQ((vector<float>{1, 2, 3, 4}), read_vector<float>(a));
+}
+
+NGRAPH_TEST(${BACKEND_NAME}, sum_large_1d_to_scalar)
+{
+    Shape shape{1000000};
+    auto A = make_shared<op::Parameter>(element::f32, shape);
+    auto f = make_shared<Function>(make_shared<op::Sum>(A, AxisSet{0}), op::ParameterVector{A});
+
+    auto backend = runtime::Backend::create("${BACKEND_NAME}");
+
+    // Create some tensors for input/output
+    random_generator.seed(2);
+    vector<float> v_a(1000000, 0);
+    double r = 0;
+    for (int i = 0; i < 1000000; i++)
+    {
+        v_a[i] = static_cast<float>(random_generator() % 255);
+        r += static_cast<double>(v_a[i]);
+    }
+    auto a = backend->create_tensor(element::f32, shape);
+    copy_data(a, v_a);
+    auto result = backend->create_tensor(element::f32, Shape{});
+
+    backend->call_with_validate(f, {result}, {a});
+
+    EXPECT_TRUE(
+        test::all_close_f(vector<float>{static_cast<float>(r)}, read_vector<float>(result)));
 }
 
 NGRAPH_TEST(${BACKEND_NAME}, sum_matrix_columns)
