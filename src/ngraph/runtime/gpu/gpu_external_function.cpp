9daf9d3ae3f319410b6aa511b7bedd3a8a037e62
Chris Sullivan <chris.sullivan@intel.com>
2018-09-05 15:48:09 -0700

    Add back lstm op to dispatch table.

--- src/ngraph/runtime/gpu/gpu_external_function.cpp
+++ src/ngraph/runtime/gpu/gpu_external_function.cpp
@@ -106,6 +106,7 @@
 #include "ngraph/runtime/gpu/gpu_external_function.hpp"
 #include "ngraph/runtime/gpu/gpu_kernel_emitters.hpp"
 #include "ngraph/runtime/gpu/gpu_runtime_context.hpp"
+#include "ngraph/runtime/gpu/op/lstm.hpp"
 #include "ngraph/runtime/gpu/op/rnn.hpp"
 #include "ngraph/runtime/gpu/pass/gpu_layout.hpp"
 #include "ngraph/runtime/gpu/pass/gpu_rnn_fusion.hpp"
@@ -246,6 +247,7 @@ static const runtime::gpu::OpMap dispatcher{
      &runtime::gpu::GPU_Emitter::emit_elementwise<ngraph::op::SigmoidBackprop>},
     {TI(ngraph::op::And), &runtime::gpu::GPU_Emitter::emit_elementwise<ngraph::op::And>},
     {TI(ngraph::op::Or), &runtime::gpu::GPU_Emitter::emit_elementwise<ngraph::op::Or>},
+    {TI(ngraph::op::gpu::Lstm), &runtime::gpu::GPU_Emitter::emit<ngraph::op::gpu::Lstm>},
     {TI(ngraph::op::gpu::Rnn), &runtime::gpu::GPU_Emitter::emit<ngraph::op::gpu::Rnn>}};
 
 const size_t runtime::gpu::GPU_ExternalFunction::GPU_ExternalFunction::s_memory_pool_alignment = 64;
