8d58d5c5da8b1f2058724b8856e0df8262392c3d
jianyinglang <jianying.lang@intel.com>
2018-09-07 11:53:32 -0700

    rank_id debug

--- src/ngraph/runtime/cpu/builder/allreduce.cpp
+++ src/ngraph/runtime/cpu/builder/allreduce.cpp
@@ -39,6 +39,7 @@ namespace ngraph
 
                 int my_rank;
                 MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+                std::cout<<"rank_id =="<<my_rank;
 
                 if (args[0].get_element_type() == element::f32)
                 {
@@ -49,7 +50,7 @@ namespace ngraph
                     data_type = MPI_DOUBLE;
                 }
                 auto functor = [&, count, data_type](CPURuntimeContext* ctx) {
-                    std::cout<<"rank_id, count == "<<my_rank<<"  "<<count<<std::endl;
+                    std::cout<<"count == "<<my_rank<<"  "<<count<<std::endl;
                     MPI_Allreduce(
                         arg_tensor, out_tensor, count, data_type, MPI_SUM, MPI_COMM_WORLD);
                 };
