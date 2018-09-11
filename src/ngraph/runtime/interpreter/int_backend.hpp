4d59be996d8743cbc48cc6c3757100a46a8532d9
Robert Kimball <robert.kimball@intel.com>
2018-09-11 07:12:04 -0700

    remove switch warning as it is not supported in older gcc

--- src/ngraph/runtime/interpreter/int_backend.hpp
+++ src/ngraph/runtime/interpreter/int_backend.hpp
@@ -192,7 +192,7 @@ private:
 #pragma GCC diagnostic push
 #pragma GCC diagnostic error "-Wswitch"
 #pragma GCC diagnostic error "-Wswitch-enum"
-#pragma GCC diagnostic error "-Wcovered-switch-default"
+        // #pragma GCC diagnostic error "-Wcovered-switch-default"
         switch (node_wrapper.get_typeid())
         {
         case OP_TYPEID::Abs:
