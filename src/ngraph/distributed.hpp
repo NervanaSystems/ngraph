59f8b93f71d658829488529224c504bee2fd0f64
Sevin F. Varoglu <sevin.varoglu@intel.com>
2018-09-13 10:27:54 -0700

    add get_size and get_rank

--- src/ngraph/distributed.hpp
+++ src/ngraph/distributed.hpp
@@ -38,5 +38,18 @@ namespace ngraph
         }
 
         ~Distributed() { MPI_Finalize(); }
+        int get_size()
+        {
+            int size;
+            MPI_Comm_size(MPI_COMM_WORLD, &size);
+            return size;
+        }
+
+        int get_rank()
+        {
+            int rank;
+            MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+            return rank;
+        }
     };
 }
