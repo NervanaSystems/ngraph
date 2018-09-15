51bcb92d1bd85555786ff5a2dff51f75e4f9baef
Jayaram Bobba <jayaram.bobba@intel.com>
2018-09-14 18:51:26 -0700

    Move to MKLDNN v0.16. Git hash picks up some bug fixes post v0.16 (#1599)
    * Move to MKLDNN v0.16. Git hash picks up some bug fixes post v0.16
    
    * Temporary workaround for an MKLDNN bug

--- src/ngraph/runtime/cpu/pass/cpu_assignment.cpp
+++ src/ngraph/runtime/cpu/pass/cpu_assignment.cpp
@@ -118,26 +118,47 @@ namespace ngraph
                     }
                 }
 
+                template <typename T>
+                bool can_use_mkldnn_conv(ngraph::Node* node)
+                {
+                    auto convolution = static_cast<const T*>(node);
+                    auto arg0_rank = node->get_input_shape(0).size();
+
+                    for (size_t s : convolution->get_data_dilation_strides())
+                    {
+                        if (s != 1)
+                            return false;
+                    }
+                    if (arg0_rank != 4 && arg0_rank != 5)
+                    {
+                        return false;
+                    }
+                    if (node->get_input_element_type(0) != element::f32)
+                    {
+                        return false;
+                    }
+                    // Temporarily disable MKLDNN for large paddings due to
+                    // a bug in v0.16 - MKFDNN-982
+                    for (auto s : convolution->get_padding_below())
+                    {
+                        if (s >= 7)
+                            return false;
+                    }
+                    for (auto s : convolution->get_padding_above())
+                    {
+                        if (s >= 7)
+                            return false;
+                    }
+
+                    return true;
+                }
+
                 template <>
                 void CPUAssignment::ASSIGN_DECL(ngraph::op::Convolution)
                 {
                     auto convolution = static_cast<op::Convolution*>(node);
 
-                    auto arg0_shape = node->get_input_shape(0);
-                    auto arg1_shape = node->get_input_shape(1);
-                    auto result_shape = node->get_output_shape(0);
-                    auto arg0_rank = arg0_shape.size();
-                    auto arg1_rank = arg1_shape.size();
-
-                    bool data_dilated = false;
-                    for (size_t s : convolution->get_data_dilation_strides())
-                    {
-                        data_dilated = data_dilated || (s != 1);
-                    }
-
-                    if (!data_dilated && ((arg0_rank == 4 && arg1_rank == 4) ||
-                                          (arg0_rank == 5 && arg1_rank == 5)) &&
-                        node->get_input_element_type(0) == element::f32)
+                    if (can_use_mkldnn_conv<ngraph::op::Convolution>(node))
                     {
                         auto op_annotations =
                             std::make_shared<ngraph::runtime::cpu::CPUOpAnnotations>();
@@ -151,20 +172,7 @@ namespace ngraph
                 {
                     auto convolution = static_cast<op::GroupConvolution*>(node);
 
-                    auto arg0_shape = node->get_input_shape(0);
-                    auto arg1_shape = node->get_input_shape(1);
-                    auto result_shape = node->get_output_shape(0);
-                    auto arg0_rank = arg0_shape.size();
-                    auto arg1_rank = arg1_shape.size();
-
-                    bool data_dilated = false;
-                    for (size_t s : convolution->get_data_dilation_strides())
-                    {
-                        data_dilated = data_dilated || (s != 1);
-                    }
-
-                    if (!data_dilated && arg0_rank == 4 && arg1_rank == 4 &&
-                        node->get_input_element_type(0) == element::f32)
+                    if (can_use_mkldnn_conv<ngraph::op::GroupConvolution>(node))
                     {
                         auto op_annotations =
                             std::make_shared<ngraph::runtime::cpu::CPUOpAnnotations>();
@@ -178,17 +186,7 @@ namespace ngraph
                 {
                     auto convolution = static_cast<op::ConvolutionRelu*>(node);
 
-                    auto arg0_rank = node->get_input_shape(0).size();
-                    auto arg1_rank = node->get_input_shape(1).size();
-
-                    bool data_dilated = false;
-                    for (size_t s : convolution->get_data_dilation_strides())
-                    {
-                        data_dilated = data_dilated || (s != 1);
-                    }
-
-                    if (!data_dilated && arg0_rank == 4 && arg1_rank == 4 &&
-                        node->get_input_element_type(0) == element::f32)
+                    if (can_use_mkldnn_conv<ngraph::op::ConvolutionRelu>(node))
                     {
                         auto op_annotations =
                             std::make_shared<ngraph::runtime::cpu::CPUOpAnnotations>();
@@ -202,17 +200,7 @@ namespace ngraph
                 {
                     auto convolution = static_cast<op::ConvolutionBiasAdd*>(node);
 
-                    auto arg0_rank = node->get_input_shape(0).size();
-                    auto arg1_rank = node->get_input_shape(1).size();
-
-                    bool data_dilated = false;
-                    for (size_t s : convolution->get_data_dilation_strides())
-                    {
-                        data_dilated = data_dilated || (s != 1);
-                    }
-
-                    if (!data_dilated && arg0_rank == 4 && arg1_rank == 4 &&
-                        node->get_input_element_type(0) == element::f32)
+                    if (can_use_mkldnn_conv<ngraph::op::ConvolutionBiasAdd>(node))
                     {
                         auto op_annotations =
                             std::make_shared<ngraph::runtime::cpu::CPUOpAnnotations>();
@@ -229,17 +217,7 @@ namespace ngraph
                 {
                     auto convolution = static_cast<op::ConvolutionAdd*>(node);
 
-                    auto arg0_rank = node->get_input_shape(0).size();
-                    auto arg1_rank = node->get_input_shape(1).size();
-
-                    bool data_dilated = false;
-                    for (size_t s : convolution->get_data_dilation_strides())
-                    {
-                        data_dilated = data_dilated || (s != 1);
-                    }
-
-                    if (!data_dilated && arg0_rank == 4 && arg1_rank == 4 &&
-                        node->get_input_element_type(0) == element::f32)
+                    if (can_use_mkldnn_conv<ngraph::op::ConvolutionAdd>(node))
                     {
                         auto op_annotations =
                             std::make_shared<ngraph::runtime::cpu::CPUOpAnnotations>();
