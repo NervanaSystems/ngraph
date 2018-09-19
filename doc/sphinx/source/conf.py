601c4f0207406ac54449c234a1b67d859d4ab8fc
Leona <leona.cook@intel.com>
2018-09-19 13:38:59 -0700

    Try stable verison of Sphinx 1.7 instead

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -28,7 +28,7 @@ sys.path.insert(0, os.path.abspath('../../../python'))
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.7.9'
+needs_sphinx = '1.7.5'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -37,7 +37,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.mathjax',
-#   'sphinx.ext.numpy',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'breathe',
