35a4a32f0c5cc0131b43488da548bc30a35eb687
Anna Alberska <41912135+aalbersk@users.noreply.github.com>
2018-09-27 15:33:27 +0200

    IntelGPU backend: ArgMax, ArgMin, LRN operations + Add Quantize op into IntelGPU (#1697)
    * add quantize op into intelgpu
    
    * IntelGPU backend: ArgMax, ArgMin, LRN operations
    
    * PR1697. Comments addressed
    
    * PR1697. Empty lines added

--- src/ngraph/runtime/intelgpu/intelgpu_backend.cpp
+++ src/ngraph/runtime/intelgpu/intelgpu_backend.cpp
@@ -16,6 +16,7 @@
 
 #include <CPP/activation.hpp>
 #include <CPP/activation_grad.hpp>
+#include <CPP/arg_max_min.hpp>
 #include <CPP/batch_norm.hpp>
 #include <CPP/broadcast.hpp>
 #include <CPP/concatenation.hpp>
@@ -24,6 +25,7 @@
 #include <CPP/eltwise.hpp>
 #include <CPP/input_layout.hpp>
 #include <CPP/layout.hpp>
+#include <CPP/lrn.hpp>
 #include <CPP/permute.hpp>
 #include <CPP/pooling.hpp>
 #include <CPP/reorder.hpp>
@@ -49,6 +51,8 @@
 
 #include "ngraph/function.hpp"
 #include "ngraph/node.hpp"
+#include "ngraph/op/argmax.hpp"
+#include "ngraph/op/argmin.hpp"
 #include "ngraph/op/avg_pool.hpp"
 #include "ngraph/op/batch_norm.hpp"
 #include "ngraph/op/broadcast.hpp"
@@ -57,6 +61,7 @@
 #include "ngraph/op/convolution.hpp"
 #include "ngraph/op/dot.hpp"
 #include "ngraph/op/get_output_element.hpp"
+#include "ngraph/op/lrn.hpp"
 #include "ngraph/op/max.hpp"
 #include "ngraph/op/max_pool.hpp"
 #include "ngraph/op/min.hpp"
@@ -1232,11 +1237,85 @@ bool runtime::intelgpu::IntelGPUBackend::compile(shared_ptr<Function> func)
                                  one_hot_axis);
             break;
         }
-        case OP_TYPEID::AllReduce:
         case OP_TYPEID::ArgMax:
+        {
+            arguments_check(op, 1, 1);
+
+            const shared_ptr<op::ArgMax> arg_max_op = static_pointer_cast<op::ArgMax>(op);
+            const size_t reduction_axis = arg_max_op->get_reduction_axis();
+            const element::Type& index_elem_type = arg_max_op->get_element_type();
+
+            if (index_elem_type == element::i64 || index_elem_type == element::i32)
+            {
+                do_arg_max_min_operation(topology,
+                                         get_input_name(op),
+                                         get_input_shape(op),
+                                         get_input_type(op),
+                                         get_output_name(op),
+                                         get_output_shape(op),
+                                         get_output_type(op),
+                                         reduction_axis,
+                                         true);
+            }
+            else
+            {
+                cldnn::arg_max_min::axis_name axis =
+                    reduction_axis == 0 ? cldnn::arg_max_min::y : cldnn::arg_max_min::x;
+                const cldnn::arg_max_min arg_max_min(
+                    get_output_name(op), get_input_name(op), cldnn::arg_max_min::max, 1, axis);
+                topology.add(arg_max_min);
+            }
+            break;
+        }
         case OP_TYPEID::ArgMin:
-        case OP_TYPEID::FunctionCall:
+        {
+            arguments_check(op, 1, 1);
+
+            const shared_ptr<op::ArgMin> arg_min_op = static_pointer_cast<op::ArgMin>(op);
+            const size_t reduction_axis = arg_min_op->get_reduction_axis();
+            const element::Type& index_elem_type = arg_min_op->get_element_type();
+
+            if (index_elem_type == element::i64 || index_elem_type == element::i32)
+            {
+                do_arg_max_min_operation(topology,
+                                         get_input_name(op),
+                                         get_input_shape(op),
+                                         get_input_type(op),
+                                         get_output_name(op),
+                                         get_output_shape(op),
+                                         get_output_type(op),
+                                         reduction_axis,
+                                         false);
+            }
+            else
+            {
+                cldnn::arg_max_min::axis_name axis =
+                    reduction_axis == 0 ? cldnn::arg_max_min::y : cldnn::arg_max_min::x;
+                const cldnn::arg_max_min arg_max_min(
+                    get_output_name(op), get_input_name(op), cldnn::arg_max_min::min, 1, axis);
+                topology.add(arg_max_min);
+            }
+            break;
+        }
         case OP_TYPEID::LRN:
+        {
+            arguments_check(op, 1, 1);
+
+            const shared_ptr<op::LRN> lrn_op = static_pointer_cast<op::LRN>(op);
+
+            const cldnn::lrn lrn(get_output_name(op),
+                                 get_input_name(op),
+                                 lrn_op->get_nsize(),
+                                 lrn_op->get_bias(),
+                                 lrn_op->get_alpha(),
+                                 lrn_op->get_beta(),
+                                 cldnn_lrn_norm_region_across_channel);
+            topology.add(lrn);
+            break;
+        }
+        case OP_TYPEID::AllReduce:
+        case OP_TYPEID::FunctionCall:
+        case OP_TYPEID::Quantize:
         case OP_TYPEID::Reduce:
         case OP_TYPEID::ReduceWindow:
         case OP_TYPEID::ReplaceSlice:
