5eb01b515b415d5de6fa7d692c3e1db30cef2002
Adam Procter <adam.m.procter@intel.com>
2018-10-04 14:00:17 -0700

    Implement partial shape/type propagation for AllReduce

--- src/ngraph/op/allreduce.cpp
+++ src/ngraph/op/allreduce.cpp
@@ -27,13 +27,9 @@ op::AllReduce::AllReduce(const shared_ptr<Node>& arg)
 
 void op::AllReduce::validate_and_infer_types()
 {
-    if (validate_punt_if_incomplete())
-    {
-        return;
-    }
-
     NODE_VALIDATION_ASSERT(this,
-                           get_input_element_type(0) == element::f32 ||
+                           !get_input_element_type(0).is_determined() ||
+                               get_input_element_type(0) == element::f32 ||
                                get_input_element_type(0) == element::f64)
         << "Only element types f32 and f64 are supported (argument element type: "
         << get_input_element_type(0) << ").";
