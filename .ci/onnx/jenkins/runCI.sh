746be1b8aadad6eefabeab1a4f7f9f28c511c11f
Chruscinski, Michal <michal.chruscinski@intel.com>
2018-10-22 17:00:36 +0200

    Update runCI.sh
    * fixes
    * added rebuild parameter

--- .ci/onnx/jenkins/runCI.sh
+++ .ci/onnx/jenkins/runCI.sh
@@ -18,7 +18,7 @@
 
 NGRAPH_ONNX_REPO="https://github.com/NervanaSystems/ngraph-onnx"
 CI_PATH="$(pwd)"
-CI_ROOT=".ci/jenkins"
+CI_ROOT=".ci/onnx/jenkins"
 REPO_ROOT="${CI_PATH%$CI_ROOT}"
 DOCKER_CONTAINER="ngraph-onnx_ci_reproduction"
 
@@ -31,28 +31,47 @@ function run() {
     docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -f=./ubuntu-16_04.dockerfile -t ngraph-onnx:ubuntu-16_04 .
 
     cd "${CI_PATH}"    
-    if [[ -z $(docker ps -a | grep -i "${DOCKER_CONTAINER}") ]]; 
-    then 
-        docker run -h "$(hostname)" --privileged --name "${DOCKER_CONTAINER}" -v "${REPO_ROOT}":/root -d ngraph-onnx:ubuntu-16_04 tail -f /dev/null
-        BUILD_NGRAPH_CMD = "cd /root \
-            mkdir -p ./build && cd ./build \
-            cmake ../ -DNGRAPH_TOOLS_ENABLE=FALSE -DNGRAPH_UNIT_TEST_ENABLE=FALSE -DNGRAPH_USE_PREBUILT_LLVM=TRUE -DNGRAPH_ONNX_IMPORT_ENABLE=TRUE -DCMAKE_INSTALL_PREFIX=/root/ngraph_dist \
-            make -j $(lscpu --parse=CORE | grep -v '#' | sort | uniq | wc -l) \
-            make install \
-            cd /root/ngraph/python \
-            git clone --recursive https://github.com/pybind/pybind11.git \
-            export PYBIND_HEADERS_PATH=/root/ngraph/python/pybind11 \
-            export NGRAPH_CPP_BUILD_PATH=/root/ngraph_dist \
-            export NGRAPH_ONNX_IMPORT_ENABLE=TRUE \
-            python3 setup.py bdist_wheel \
-            cd /root
-            git clone ${NGRAPH_ONNX_REPO}"
-        docker exec "${DOCKER_CONTAINER}" bash -c ${BUILD_NGRAPH_CMD}
+    if [[ -z $(docker ps -a | grep -i "${DOCKER_CONTAINER}") ]];
+    then
+        docker run -h "$(hostname)" --privileged --name "${DOCKER_CONTAINER}" -v "${REPO_ROOT}":/root \ 
+            -d ngraph-onnx:ubuntu-16_04 tail -f /dev/null
+        BUILD="TRUE"
     fi
 
-    docker exec "${DOCKER_CONTAINER}" bash -c "cd /root && git clone ${NGRAPH_ONNX_REPO}"
-    NGRAPH_WHL=$(docker exec ${DOCKER_CONTAINER} find /home/ngraph/python/dist/ -name "ngraph*.whl")
+    if [[ "${BUILD}" == "TRUE" ]]; 
+    then         
+        BUILD_NGRAPH_CMD='cd /root &&\
+            mkdir -p ./build && cd ./build &&\
+            cmake ../ -DNGRAPH_TOOLS_ENABLE=FALSE -DNGRAPH_UNIT_TEST_ENABLE=FALSE -DNGRAPH_USE_PREBUILT_LLVM=TRUE \
+            -DNGRAPH_ONNX_IMPORT_ENABLE=TRUE -DCMAKE_INSTALL_PREFIX=/root/ngraph_dist && \
+            make -j $(lscpu --parse=CORE | grep -v '"'#'"' | sort | uniq | wc -l) && \
+            make install && \
+            cd /root/python && \
+            if [[ -z $(ls /root/ngraph-onnx 2>/dev/null) ]]; then
+                git clone --recursive https://github.com/pybind/pybind11.git;
+            fi
+            export PYBIND_HEADERS_PATH=/root/python/pybind11 && \
+            export NGRAPH_CPP_BUILD_PATH=/root/ngraph_dist && \
+            export NGRAPH_ONNX_IMPORT_ENABLE=TRUE && \
+            python3 setup.py bdist_wheel && \
+            cd /root'
+        docker exec "${DOCKER_CONTAINER}" bash -c "${BUILD_NGRAPH_CMD}"
+    fi
+
+    CLONE_CMD='cd /root &&\
+        if [[ -z $(ls /root/ngraph-onnx 2>/dev/null) ]]; then
+            git clone '"${NGRAPH_ONNX_REPO}"'; 
+        fi'
+    docker exec "${DOCKER_CONTAINER}" bash -c "${CLONE_CMD}"
+    NGRAPH_WHL=$(docker exec ${DOCKER_CONTAINER} find /root/python/dist/ -name "ngraph*.whl")
     docker exec -e TOX_INSTALL_NGRAPH_FROM="${NGRAPH_WHL}" "${DOCKER_CONTAINER}" tox -c /root/ngraph-onnx/
+}
+
+# Function cleanup() removes items related to nGraph, created during script execution
+function cleanup_ngraph() {
+    set -x
+    
+    docker exec "${DOCKER_CONTAINER}" bash -c 'rm -rf /root/ngraph_dist /root/python/dist'
 }
 
 # Function cleanup() removes items created during script execution
@@ -87,7 +106,8 @@ do
             exit 0
         ;;
         --rebuild*)
-            cleanup
+            cleanup_ngraph
+            BUILD="TRUE"
         ;;
     esac
 done
