61030f5ccd251e65828a81d0910850420152a674
Chris Lishka <christopher.r.lishka@intel.com>
2018-10-23 11:02:28 -0700

    Test new ngraph-ci-premerge.groovy changes on branch chrisl/test-general-ci

--- .ci/jenkins/jenkins-trigger.groovy
+++ .ci/jenkins/jenkins-trigger.groovy
@@ -24,7 +24,8 @@
 // Parameters which ngraph-unittest uses:
 String  PR_URL = CHANGE_URL
 String  PR_COMMIT_AUTHOR = CHANGE_AUTHOR
-String  JENKINS_BRANCH = "master"
+// ORIG  String  JENKINS_BRANCH = "master"
+String  JENKINS_BRANCH = "chrisl/revert-ci-distributed"
 Integer TIMEOUTTIME = "3600"
 // BRANCH parameter is no loner needed
 // TRIGGER_URL parameter is no longer needed
