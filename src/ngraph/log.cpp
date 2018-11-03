63d37becf282ca84cf44443189e2b38357ff90c6
Rob Earhart <robert.earhart@intel.com>
2018-11-02 22:09:58 -0700

    Add nil stream comments

--- src/ngraph/log.cpp
+++ src/ngraph/log.cpp
@@ -32,11 +32,17 @@ namespace
 {
     class NilStreamBuf final : public streambuf
     {
+        // N.B. We derive from the base streambuf implementation, in
+        //      which underflow() and overflow() both return
+        //      Traits::eof() -- any access returns a failure.
     };
 }
 
 ostream& ngraph::get_nil_stream()
 {
+    // N.B. When debug logging is disabled, multiple threads may
+    //      access the nil stream simultaneously, so it's important to
+    //      return a threadsafe nil stream implementation.
     static NilStreamBuf nil_buf;
     static ostream nil{&nil_buf};
     return nil;
