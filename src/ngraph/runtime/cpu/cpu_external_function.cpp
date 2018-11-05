e4c28c3122ecf48d4aa9cde67a4fa3cf46a57287
Jayaram Bobba <jayaram.bobba@intel.com>
2018-11-05 15:29:25 -0800

    Collapse dimensions for inputs to Dot

--- src/ngraph/runtime/cpu/cpu_external_function.cpp
+++ src/ngraph/runtime/cpu/cpu_external_function.cpp
@@ -1037,11 +1037,10 @@ void runtime::cpu::CPU_ExternalFunction::register_common_passes(ngraph::pass::Ma
     // pass_manager.register_pass<runtime::cpu::pass::ConcatInputs>();
     pass_manager.register_pass<runtime::cpu::pass::CPURnnMatFusion>();
     pass_manager.register_pass<runtime::cpu::pass::CPUBatchFusion>();
-
+    pass_manager.register_pass<runtime::cpu::pass::CPUCollapseDims>();
     pass_manager.register_pass<ngraph::pass::CoreFusion>();
     pass_manager.register_pass<runtime::cpu::pass::CPUFusion>();
-    // pass_manager.register_pass<runtime::cpu::pass::CPUHorizontalFusion>();
-    pass_manager.register_pass<runtime::cpu::pass::CPUCollapseDims>();
+// pass_manager.register_pass<runtime::cpu::pass::CPUHorizontalFusion>();
 #if defined(NGRAPH_HALIDE)
     pass_manager.register_pass<ngraph::runtime::cpu::pass::HalideSubgraphExtraction>();
 #endif
