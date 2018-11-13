b23c45fcc27749b2c7202829bbc39c1fc0112d6c
nishant.b.patel <nishant.b.patel@intel.com>
2018-11-13 14:36:30 -0800

    Test case for QCBSA

--- test/builder_quantization.cpp
+++ test/builder_quantization.cpp
@@ -323,7 +323,7 @@ TEST(builder, scaled_QC_with_bias_add_and_relu)
     auto G = op::Constant::create(element::f32, Shape{}, {22.0f});
     auto H = op::Constant::create(element::f32, Shape{}, {90.0f});
     auto I = op::Constant::create(element::f32, Shape{}, {22.0f});
-    auto J = op::Constant::create(element::f32, Shape{}, {90.0f});
+    auto J = op::Constant::create(element::f32, Shape{}, {180.0f});
     auto CV = ngraph::builder::ScaledQuantizedConvolutionBiasAdd(A,
                                                                  B,
                                                                  Bias,
@@ -355,7 +355,64 @@ TEST(builder, scaled_QC_with_bias_add_and_relu)
     copy_data(d, conv_2_data);
     auto result = backend->create_tensor(element::u8, shape_r);
     backend->call_with_validate(f, {result}, {a, b, c, d});
-    EXPECT_EQ((vector<uint8_t>{77, 112, 102, 109, 127, 224, 255, 136, 109, 163, 139, 129}),
+    EXPECT_EQ((vector<uint8_t>{78, 114, 105, 113, 132, 230, 255, 136, 110, 165, 142, 133}),
+              read_vector<uint8_t>(result));
+}
+
+TEST(builder, scaled_QC_with_bias_signed_add_and_relu)
+{
+    Shape shape_a{1, 1, 3, 4}; // input shape
+    Shape shape_b{1, 1, 3, 3}; // filter shape
+    Shape shape_r{1, 1, 3, 4}; // output shape
+    vector<uint8_t> a_data = {1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4};
+    vector<int8_t> b_data = {1, 2, 3, 4, 5, 0, 0, 1, 2};
+    vector<int32_t> c_data = {5};
+    vector<int8_t> conv_2_data = {-1, -2, -3, 4, 5, 6, -10, 0, 1, 2, 3, 4};
+    auto A = make_shared<op::Parameter>(element::u8, shape_a);
+    auto B = make_shared<op::Parameter>(element::i8, shape_b);
+    auto Add = make_shared<op::Parameter>(element::i8, shape_a);
+    auto Bias = make_shared<op::Parameter>(element::i32, Shape{1});
+    auto C = op::Constant::create(element::f32, Shape{}, {0.0f});
+    auto D = op::Constant::create(element::f32, Shape{}, {255.0f});
+    auto E = op::Constant::create(element::f32, Shape{}, {-127.0f});
+    auto F = op::Constant::create(element::f32, Shape{}, {127.0f});
+    auto G = op::Constant::create(element::f32, Shape{}, {22.0f});
+    auto H = op::Constant::create(element::f32, Shape{}, {90.0f});
+    auto I = op::Constant::create(element::f32, Shape{}, {22.0f});
+    auto J = op::Constant::create(element::f32, Shape{}, {90.0f});
+    auto CV =
+        ngraph::builder::ScaledQuantizedConvolutionBiasSignedAdd(A,
+                                                                 B,
+                                                                 Bias,
+                                                                 Add,
+                                                                 Strides{1, 1}, // move_strides
+                                                                 Strides{1, 1}, // filter_dilation
+                                                                 CoordinateDiff{1, 1}, // below_pads
+                                                                 CoordinateDiff{1, 1}, // above_pads
+                                                                 Strides{1, 1}, // data_dilation
+                                                                 C,
+                                                                 D,
+                                                                 E,
+                                                                 F,
+                                                                 G,
+                                                                 H,
+                                                                 I,
+                                                                 J,
+                                                                 true);
+    auto f = make_shared<Function>(NodeVector{CV}, op::ParameterVector{A, B, Bias, Add});
+    auto backend = runtime::Backend::create("CPU");
+    // Create some tensors for input/output
+    auto a = backend->create_tensor(element::u8, shape_a);
+    copy_data(a, a_data);
+    auto b = backend->create_tensor(element::i8, shape_b);
+    copy_data(b, b_data);
+    auto c = backend->create_tensor(element::i32, Shape{1});
+    copy_data(c, c_data);
+    auto d = backend->create_tensor(element::i8, shape_a);
+    copy_data(d, conv_2_data);
+    auto result = backend->create_tensor(element::u8, shape_r);
+    backend->call_with_validate(f, {result}, {a, b, c, d});
+    EXPECT_EQ((vector<uint8_t>{74, 106, 93, 113, 132, 230, 249, 136, 110, 165, 142, 133}),
               read_vector<uint8_t>(result));
 }
 
