70507fa33d5ebc8dc803c565f8414a70d4824f87
Yimei Sun <yimei.sun@intel.com>
2018-11-26 10:09:20 -0800

    Fix uninitialized parameter tensor data in nbench

--- src/tools/nbench/benchmark.cpp
+++ src/tools/nbench/benchmark.cpp
@@ -176,12 +176,15 @@ vector<runtime::PerformanceCounter> run_benchmark(shared_ptr<Function> f,
     vector<shared_ptr<runtime::HostTensor>> arg_data;
     vector<shared_ptr<runtime::Tensor>> args;
     vector<bool> args_cacheable;
-    for (shared_ptr<op::Parameter> param : f->get_parameters())
+    for (const shared_ptr<op::Parameter> param : f->get_parameters())
     {
         auto tensor = backend->create_tensor(param->get_element_type(), param->get_shape());
         auto tensor_data =
             make_shared<runtime::HostTensor>(param->get_element_type(), param->get_shape());
         random_init(tensor_data);
+        tensor->write(tensor_data->get_data_ptr(),
+                      0,
+                      tensor_data->get_element_count() * tensor_data->get_element_type().size());
         args.push_back(tensor);
         arg_data.push_back(tensor_data);
         args_cacheable.push_back(param->get_cacheable());
