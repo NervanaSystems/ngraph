e152e2ed5fc0e75d98f90f0a06531ec1b4d32e85
Tomasz Socha <tomasz.socha@intel.com>
2018-12-07 15:11:59 +0100

    FIX

--- python/ngraph/__init__.py
+++ python/ngraph/__init__.py
@@ -16,7 +16,7 @@
 """ngraph module namespace, exposing factory functions for all ops and other classes."""
 
 from pkg_resources import get_distribution
-__version__ = get_distribution('ngraph').version
+__version__ = get_distribution('ngraph-core').version
 
 from ngraph.ops import absolute
 from ngraph.ops import absolute as abs
