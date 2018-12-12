fc216f3938460cb84ac0703124398261312f8d2b
Adam Procter <adam.m.procter@intel.com>
2018-12-12 09:26:39 -0800

    "Any" and "All" ops (#2217)
    * Skip --exclude-libs linker flag on macOS
    
    * Change test to if(LINUX)
    
    * Add "Any" op and AnyAllReplacement pass
    
    * Add AnyAllReplacement to IGPU backend
    
    * Stub (error-out) handlers for GPU and INTELGPU
    
    * Add 'All' op
    
    * Add AnyAllInsertion pass, deprecate deprecable ops, add stubs for INTELGPU
    
    * Add failing unit tests to INTELGPU manifest
    
    * Reduce boilerplate
    
    * Reduce more boilerplate
    
    * Add static keywords

---
+++ src/ngraph/op/any.hpp
@@ -0,0 +1,48 @@
+//*****************************************************************************
+// Copyright 2017-2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+
+#pragma once
+
+#include "ngraph/axis_set.hpp"
+#include "ngraph/graph_util.hpp"
+#include "ngraph/op/op.hpp"
+#include "ngraph/op/util/logical_reduction.hpp"
+
+namespace ngraph
+{
+    namespace op
+    {
+        /// \brief Logical "any" reduction operation.
+        class Any : public util::LogicalReduction
+        {
+        public:
+            /// \brief Constructs an "any" reduction operation.
+            ///
+            /// \param arg The tensor view to be reduced.
+            /// \param reduction_axes The axis positions (0-based) to be eliminated.
+            Any(const std::shared_ptr<Node>& arg, const AxisSet& reduction_axes);
+
+            virtual std::shared_ptr<Node>
+                copy_with_new_args(const NodeVector& new_args) const override;
+
+            /// \return The default value for Any.
+            virtual std::shared_ptr<Node> get_default_value() const override
+            {
+                return ngraph::make_constant_from_string("0", get_element_type(), get_shape());
+            }
+        };
+    }
+}
--- src/ngraph/serializer.cpp
+++ src/ngraph/serializer.cpp
@@ -23,8 +23,10 @@
 #include "ngraph/op/abs.hpp"
 #include "ngraph/op/acos.hpp"
 #include "ngraph/op/add.hpp"
+#include "ngraph/op/all.hpp"
 #include "ngraph/op/allreduce.hpp"
 #include "ngraph/op/and.hpp"
+#include "ngraph/op/any.hpp"
 #include "ngraph/op/argmax.hpp"
 #include "ngraph/op/argmin.hpp"
 #include "ngraph/op/asin.hpp"
@@ -459,6 +461,12 @@ static shared_ptr<ngraph::Function>
                 node = make_shared<op::Add>(args[0], args[1]);
                 break;
             }
+            case OP_TYPEID::All:
+            {
+                auto reduction_axes = node_js.at("reduction_axes").get<set<size_t>>();
+                node = make_shared<op::All>(args[0], reduction_axes);
+                break;
+            }
             case OP_TYPEID::AllReduce:
             {
                 node = make_shared<op::AllReduce>(args[0]);
@@ -467,6 +475,12 @@ static shared_ptr<ngraph::Function>
             case OP_TYPEID::And:
             {
                 node = make_shared<op::And>(args[0], args[1]);
+                break;
+            }
+            case OP_TYPEID::Any:
+            {
+                auto reduction_axes = node_js.at("reduction_axes").get<set<size_t>>();
+                node = make_shared<op::Any>(args[0], reduction_axes);
                 break;
             }
             case OP_TYPEID::ArgMin:
@@ -1258,9 +1272,21 @@ static json write(const Node& n, bool binary_constant_data)
         node["index_element_type"] = write_element_type(tmp->get_element_type());
         break;
     }
+    case OP_TYPEID::All:
+    {
+        auto tmp = dynamic_cast<const op::All*>(&n);
+        node["reduction_axes"] = tmp->get_reduction_axes();
+        break;
+    }
     case OP_TYPEID::AllReduce: { break;
     }
     case OP_TYPEID::And: { break;
+    }
+    case OP_TYPEID::Any:
+    {
+        auto tmp = dynamic_cast<const op::Any*>(&n);
+        node["reduction_axes"] = tmp->get_reduction_axes();
+        break;
     }
     case OP_TYPEID::Asin: { break;
     }
