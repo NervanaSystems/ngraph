7d245a6151926ec64a2d701aca26d7cb8ce9e8ff
Tomasz Socha <tomasz.socha@intel.com>
2018-12-14 15:21:20 +0100

    Enable QuantConvolution

--- src/ngraph/frontend/onnx_import/ops_bridge.cpp
+++ src/ngraph/frontend/onnx_import/ops_bridge.cpp
@@ -72,6 +72,7 @@
 #include "op/pad.hpp"
 #include "op/pow.hpp"
 #include "op/prelu.hpp"
+#include "op/quant_conv.hpp"
 #include "op/reciprocal.hpp"
 #include "op/reduce.hpp"
 #include "op/relu.hpp"
@@ -203,6 +204,7 @@ namespace ngraph
             REGISTER_OPERATOR("Pad", 1, pad);
             REGISTER_OPERATOR("Pow", 1, pow);
             REGISTER_OPERATOR("PRelu", 1, prelu);
+            REGISTER_OPERATOR("QLinearConv", 1, quant_conv);
             REGISTER_OPERATOR("QuantizeLinear", 1, quantize_linear);
             REGISTER_OPERATOR("Reciprocal", 1, reciprocal);
             REGISTER_OPERATOR("ReduceLogSum", 1, reduce_log_sum);
