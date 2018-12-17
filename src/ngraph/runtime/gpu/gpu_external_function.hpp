186b330b687ffdd3180b27ead52b285e90bd20df
fenglei <fenglei.tian@intel.com>
2018-12-17 14:22:00 -0800

    change hash map to map

--- src/ngraph/runtime/gpu/gpu_external_function.hpp
+++ src/ngraph/runtime/gpu/gpu_external_function.hpp
@@ -103,7 +103,7 @@ namespace ngraph
                 std::shared_ptr<ngraph::Function> m_function;
 
                 std::map<std::string, size_t> m_name_index_map;
-                std::unordered_map<std::string, std::string> m_variable_name_map;
+                std::map<std::string, std::string> m_variable_name_map;
                 std::unordered_map<Node*, Node*> m_node_function_map;
                 std::unordered_map<std::shared_ptr<Function>, std::list<std::shared_ptr<Node>>>
                     m_function_ordered_ops;
