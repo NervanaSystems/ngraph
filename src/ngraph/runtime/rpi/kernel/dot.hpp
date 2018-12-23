9db6d5b59adca47f9df98e10113a25431c03dcab
Robert Kimball <robert.kimball@intel.com>
2018-12-23 06:16:44 -0800

    dot eigen

--- src/ngraph/runtime/rpi/kernel/dot.hpp
+++ src/ngraph/runtime/rpi/kernel/dot.hpp
@@ -16,6 +16,7 @@
 
 #pragma once
 
+#include <Eigen/Dense>
 #include <cmath>
 #include <omp.h>
 #include <utility>
@@ -40,86 +41,123 @@ namespace ngraph
                          const Shape& out_shape,
                          size_t reduction_axes_count)
                 {
-                    // Get the sizes of the dot axes. It's easiest to pull them from arg1 because they're
-                    // right up front.
-                    Shape dot_axis_sizes(reduction_axes_count);
-                    std::copy(arg1_shape.begin(),
-                              arg1_shape.begin() + reduction_axes_count,
-                              dot_axis_sizes.begin());
+                    NGRAPH_INFO << arg0_shape.size();
+                    NGRAPH_INFO << arg1_shape.size();
+                    NGRAPH_INFO << out_shape.size();
+                    if (arg0_shape.size() == 2 && arg1_shape.size() == 2)
+                    {
+                        Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic> stride(2, 1);
+                        Eigen::Map<
+                            Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>,
+                            Eigen::Unaligned,
+                            Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>>
+                            a0(const_cast<T*>(arg0), arg0_shape[0], arg0_shape[1], stride);
+                        Eigen::Map<
+                            Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>,
+                            Eigen::Unaligned,
+                            Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>>
+                            a1(const_cast<T*>(arg1), arg1_shape[0], arg1_shape[1], stride);
+                        Eigen::Map<
+                            Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>,
+                            Eigen::Unaligned,
+                            Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>>
+                            o(const_cast<T*>(out), out_shape[0], out_shape[1], stride);
+                        // Eigen::Map<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>> a1(
+                        //     const_cast<T*>(arg1),
+                        //     arg1_shape[0],
+                        //     arg1_shape[1],
+                        //     Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>(2, 1));
+                        // Eigen::Map<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>> o(
+                        //     out,
+                        //     out_shape[0],
+                        //     out_shape[1],
+                        //     Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>(2, 1));
+                        o = a0 * a1;
+                    }
+                    else
+                    {
+                        // Get the sizes of the dot axes. It's easiest to pull them from arg1 because they're
+                        // right up front.
+                        Shape dot_axis_sizes(reduction_axes_count);
+                        std::copy(arg1_shape.begin(),
+                                  arg1_shape.begin() + reduction_axes_count,
+                                  dot_axis_sizes.begin());
 
-                    CoordinateTransform arg0_transform(arg0_shape);
-                    CoordinateTransform arg1_transform(arg1_shape);
-                    CoordinateTransform output_transform(out_shape);
+                        CoordinateTransform arg0_transform(arg0_shape);
+                        CoordinateTransform arg1_transform(arg1_shape);
+                        CoordinateTransform output_transform(out_shape);
 
-                    // Create coordinate transforms for arg0 and arg1 that throw away the dotted axes.
-                    size_t arg0_projected_rank = arg0_shape.size() - reduction_axes_count;
-                    size_t arg1_projected_rank = arg1_shape.size() - reduction_axes_count;
+                        // Create coordinate transforms for arg0 and arg1 that throw away the dotted axes.
+                        size_t arg0_projected_rank = arg0_shape.size() - reduction_axes_count;
+                        size_t arg1_projected_rank = arg1_shape.size() - reduction_axes_count;
 
-                    Shape arg0_projected_shape(arg0_projected_rank);
-                    std::copy(arg0_shape.begin(),
-                              arg0_shape.begin() + arg0_projected_rank,
-                              arg0_projected_shape.begin());
+                        Shape arg0_projected_shape(arg0_projected_rank);
+                        std::copy(arg0_shape.begin(),
+                                  arg0_shape.begin() + arg0_projected_rank,
+                                  arg0_projected_shape.begin());
 
-                    Shape arg1_projected_shape(arg1_projected_rank);
-                    std::copy(arg1_shape.begin() + reduction_axes_count,
-                              arg1_shape.end(),
-                              arg1_projected_shape.begin());
+                        Shape arg1_projected_shape(arg1_projected_rank);
+                        std::copy(arg1_shape.begin() + reduction_axes_count,
+                                  arg1_shape.end(),
+                                  arg1_projected_shape.begin());
 
-                    CoordinateTransform arg0_projected_transform(arg0_projected_shape);
-                    CoordinateTransform arg1_projected_transform(arg1_projected_shape);
+                        CoordinateTransform arg0_projected_transform(arg0_projected_shape);
+                        CoordinateTransform arg1_projected_transform(arg1_projected_shape);
 
-                    // Create a coordinate transform that allows us to iterate over all possible values
-                    // for the dotted axes.
-                    CoordinateTransform dot_axes_transform(dot_axis_sizes);
+                        // Create a coordinate transform that allows us to iterate over all possible values
+                        // for the dotted axes.
+                        CoordinateTransform dot_axes_transform(dot_axis_sizes);
 
 #pragma omp parallel
-                    for (const Coordinate& arg0_projected_coord : arg0_projected_transform)
-                    {
-                        for (const Coordinate& arg1_projected_coord : arg1_projected_transform)
+                        for (const Coordinate& arg0_projected_coord : arg0_projected_transform)
                         {
-                            // The output coordinate is just the concatenation of the projected coordinates.
-                            Coordinate out_coord(arg0_projected_coord.size() +
-                                                 arg1_projected_coord.size());
-
-                            auto out_coord_it = std::copy(arg0_projected_coord.begin(),
-                                                          arg0_projected_coord.end(),
-                                                          out_coord.begin());
-                            std::copy(arg1_projected_coord.begin(),
-                                      arg1_projected_coord.end(),
-                                      out_coord_it);
-
-                            // Zero out to start the sum.
-                            T sum = 0;
-
-                            size_t out_index = output_transform.index(out_coord);
-
-                            // Walk along the dotted axes.
-                            Coordinate arg0_coord(arg0_shape.size());
-                            Coordinate arg1_coord(arg1_shape.size());
-                            auto arg0_it = std::copy(arg0_projected_coord.begin(),
-                                                     arg0_projected_coord.end(),
-                                                     arg0_coord.begin());
-                            for (const Coordinate& dot_axis_positions : dot_axes_transform)
+                            for (const Coordinate& arg1_projected_coord : arg1_projected_transform)
                             {
-                                // In order to find the points to multiply together, we need to inject our current
-                                // positions along the dotted axes back into the projected arg0 and arg1 coordinates.
-                                std::copy(
-                                    dot_axis_positions.begin(), dot_axis_positions.end(), arg0_it);
+                                // The output coordinate is just the concatenation of the projected coordinates.
+                                Coordinate out_coord(arg0_projected_coord.size() +
+                                                     arg1_projected_coord.size());
 
-                                auto arg1_it = std::copy(dot_axis_positions.begin(),
-                                                         dot_axis_positions.end(),
-                                                         arg1_coord.begin());
+                                auto out_coord_it = std::copy(arg0_projected_coord.begin(),
+                                                              arg0_projected_coord.end(),
+                                                              out_coord.begin());
                                 std::copy(arg1_projected_coord.begin(),
                                           arg1_projected_coord.end(),
-                                          arg1_it);
+                                          out_coord_it);
 
-                                // Multiply and add to the sum.
-                                sum += arg0[arg0_transform.index(arg0_coord)] *
-                                       arg1[arg1_transform.index(arg1_coord)];
+                                // Zero out to start the sum.
+                                T sum = 0;
+
+                                size_t out_index = output_transform.index(out_coord);
+
+                                // Walk along the dotted axes.
+                                Coordinate arg0_coord(arg0_shape.size());
+                                Coordinate arg1_coord(arg1_shape.size());
+                                auto arg0_it = std::copy(arg0_projected_coord.begin(),
+                                                         arg0_projected_coord.end(),
+                                                         arg0_coord.begin());
+                                for (const Coordinate& dot_axis_positions : dot_axes_transform)
+                                {
+                                    // In order to find the points to multiply together, we need to inject our current
+                                    // positions along the dotted axes back into the projected arg0 and arg1 coordinates.
+                                    std::copy(dot_axis_positions.begin(),
+                                              dot_axis_positions.end(),
+                                              arg0_it);
+
+                                    auto arg1_it = std::copy(dot_axis_positions.begin(),
+                                                             dot_axis_positions.end(),
+                                                             arg1_coord.begin());
+                                    std::copy(arg1_projected_coord.begin(),
+                                              arg1_projected_coord.end(),
+                                              arg1_it);
+
+                                    // Multiply and add to the sum.
+                                    sum += arg0[arg0_transform.index(arg0_coord)] *
+                                           arg1[arg1_transform.index(arg1_coord)];
+                                }
+
+                                // Write the sum back.
+                                out[out_index] = sum;
                             }
-
-                            // Write the sum back.
-                            out[out_index] = sum;
                         }
                     }
                 }
