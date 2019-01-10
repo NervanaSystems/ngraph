3a93619203c775c1689b5800b318ef1fde23eb11
Adam Procter <adam.m.procter@intel.com>
2019-01-09 18:28:07 -0800

    Maybe fix docs?

--- doc/sphinx/generate_python_api_doc.sh
+++ doc/sphinx/generate_python_api_doc.sh
@@ -8,19 +8,19 @@
 ###############################################################################
 
 # paths relative to this file location
-NGRAPH_REPO=../..
+CURRENT_DIR="$(pwd)"
+NGRAPH_REPO="${CURRENT_DIR}/../.."
 DOC_DIR=${NGRAPH_REPO}/doc
 TMP_DIR=/tmp/sphinx_auto_py_doc
 EXCLUDE_DIRS="${NGRAPH_REPO}/python/ngraph/impl*
               ${NGRAPH_REPO}/python/ngraph/utils*"
-CURRENT_DIR=.
 
-cd ${NGRAPH_REPO}/python/ngraph
-PYTHONPATH=. sphinx-autogen -t ${DOC_DIR}/sphinx/source/_templates/ -o ${TMP_DIR} \
-                             ${DOC_DIR}/sphinx/source/python_api/structure.rst
+pushd ${NGRAPH_REPO}/python
+PYTHONPATH="$(pwd)" sphinx-autogen -t ${DOC_DIR}/sphinx/source/_templates/ -o ${TMP_DIR} \
+                                      ${DOC_DIR}/sphinx/source/python_api/structure.rst
+popd
 sphinx-apidoc -f -M -d 1 -T -o ${TMP_DIR} ${CURRENT_DIR} ${EXCLUDE_DIRS}
 
-rm ${TMP_DIR}/ngraph.runtime.rst
 cp ${TMP_DIR}/* ${DOC_DIR}/sphinx/source/python_api/_autosummary/
 
 rm -rf ${TMP_DIR}
