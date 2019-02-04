e7cee397790301366d5af322a0666382f6de22b3
Adam Rogowiec <adam.rogowiec@intel.com>
2019-02-04 20:34:34 +0100

    Add missing template specialization for vector of strings.
    - For retrieving attribute value.

--- src/ngraph/frontend/onnx_import/core/node.cpp
+++ src/ngraph/frontend/onnx_import/core/node.cpp
@@ -248,6 +248,15 @@ namespace ngraph
         }
 
         template <>
+        std::vector<std::string>
+            Node::get_attribute_value(const std::string& name,
+                                      std::vector<std::string> default_value) const
+        {
+            return m_pimpl->template get_attribute_value<std::vector<std::string>>(
+                name, std::move(default_value));
+        }
+
+        template <>
         std::vector<Tensor> Node::get_attribute_value(const std::string& name,
                                                       std::vector<Tensor> default_value) const
         {
