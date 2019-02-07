8dc894d41c31b6a5c7f32cc1d44a01a8fd83cf1f
Tomasz Socha <tomasz.socha@intel.com>
2019-02-07 12:22:39 +0100

    Style check

--- src/ngraph/frontend/onnx_import/core/null_node.hpp
+++ src/ngraph/frontend/onnx_import/core/null_node.hpp
@@ -40,7 +40,6 @@ namespace ngraph
             NullNode();
 
             bool is_null() const final override { return true; }
-
             virtual std::shared_ptr<Node>
                 copy_with_new_args(const NodeVector& new_args) const override;
         };
