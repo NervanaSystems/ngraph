74cd2ca1c8b7e1577fa4c7acd4141e7cbab7f778
Ashok Emani <ashok.emani@intel.com>
2019-02-22 12:07:24 -0800

    style fix

--- src/ngraph/runtime/cpu/cpu_external_function.cpp
+++ src/ngraph/runtime/cpu/cpu_external_function.cpp
@@ -1108,7 +1108,7 @@ void runtime::cpu::CPU_ExternalFunction::register_common_passes(ngraph::pass::Ma
     REGISTER_KNOBBED_PASS(CPUBatchFusion, true, runtime::cpu::pass);
     // TODO: Remove this change before committing to master
     //       Reshape Sinking should be disabled by default
-    //       This is a workaround for ONNX fp32 baseline numbers 
+    //       This is a workaround for ONNX fp32 baseline numbers
     REGISTER_KNOBBED_PASS(ReshapeSinking, true, ngraph::pass);
     REGISTER_KNOBBED_PASS(ReshapeElimination, false, ngraph::pass);
     REGISTER_KNOBBED_PASS(CoreFusion, true, ngraph::pass);
