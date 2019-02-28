NervanaSystems/ngraph

commit b277627acb1945e1421868c4c53a590b1dc95510

Reuse memory for CPU backend. (#2238)
* Reuse memory for CPU backend.

* Use NGRAPH_REUSE_MEMORY to enable memory reuse.

* Add a test.

* Move make_function to test_tools.cpp.

* Add more comments.

* Address PR Feedback: add a method to CPU backend.

* *Add a member to CPUOpAnnotations to remove redundant code.

*Overload compile function for CPU backend.

* Move make_function out of test_tools.

* Address PR Feedback.

* Use modified liveness analysis in CPUMemoryAssignment pass.

* Use lambda expression.

* Fix style error.

* Check if any user of the tensor has destructive io when building tensor alias map.

* Fix a bug.

* Check if tensor has multiple users.

* Allow tensor alias for destructive oi node.

* Update multiple_users_tensor set along the chain of in place ops.

* No tensor alias if input is parameter or constant.

* Use buffer sets in cpu memory assignment,
tensors sharing the same memory buffer are put into the same set.

* Add more checks and do not combine sets when allowing destructive oi.

* Style fix.

* Do no allow destructive oi if the input tensor uses function input memory.

Update set label.

* Add unit tests.

* Style fix.

* Get the correct size for memcpy when the input is padded.

* Style fix.

* Address PR feedback.

* Address PR feedback.

* Move make_function in cpu_test after #if 0 and before the disabled test.

* Add utility functions.

Use iterator.

Rename variables.

* Add pass attributes and move cpu memory assignment to common passes (#2504)

src/ngraph/runtime/cpu/cpu_external_function.hpp
@@ -49,6 +49,7 @@
 #include "ngraph/runtime/cpu/mkldnn_emitter.hpp"
 #include "ngraph/runtime/performance_counter.hpp"
 #include "ngraph/state/state.hpp"
+#include "ngraph/util.hpp"
 
 namespace ngraph
 {
@@ -95,18 +96,11 @@ namespace ngraph
                 friend class CPU_Executable;
 
             public:
-                enum class CPUTensorRole
-                {
-                    INPUT,
-                    CONSTANT,
-                    OUTPUT,
-                    INTERMEDIATE
-                };
-
                 CPU_ExternalFunction(const std::shared_ptr<ngraph::Function>& function,
                                      bool release_function = true);
                 ~CPU_ExternalFunction();
-                std::shared_ptr<ngraph::runtime::cpu::CPU_CallFrame> make_call_frame();
+                std::shared_ptr<ngraph::runtime::cpu::CPU_CallFrame>
+                    make_call_frame(ngraph::pass::PassConfig& pass_config);
 
                 const LayoutDescriptorPtrs& get_parameter_layout_descriptors();
                 const LayoutDescriptorPtrs& get_result_layout_descriptors();
@@ -171,11 +165,11 @@ namespace ngraph
 #endif
 
             protected:
-                void build();
+                void build(ngraph::pass::PassConfig& pass_config);
 
 #if !defined(NGRAPH_DEX_ONLY)
 
-                void compile();
+                void compile(ngraph::pass::PassConfig& pass_config);
 
 #endif
 
@@ -183,37 +177,8 @@ namespace ngraph
 
             private:
                 // Register passes that are common to codegen and DEX
-                void register_common_passes(ngraph::pass::Manager& pass_manager);
-
-                // For non-destructive passthrough kernels, propagate function
-                // constant buffers to internal ops
-                void propagate_in_place_constant(ngraph::descriptor::Output* output,
-                                                 std::string input_name,
-                                                 bool dex);
-                // For non-destructive passthrough kernels, propagate function
-                // input buffers to internal ops
-                void propagate_in_place_input(ngraph::descriptor::Output* output,
-                                              std::string input_name,
-                                              bool dex);
-                // For in-place kernels, propagate function output buffers to
-                // internal ops
-                void propagate_in_place_output(ngraph::descriptor::Output* res_src_output,
-                                               std::string output_name,
-                                               bool dex);
-
-                // Find in-place concat ops and set appropriate memory pool offset for its arguments
-                void process_in_place_concat(std::list<std::shared_ptr<Node>> nodes);
-
-                // For a chain of concat ops, propagate memory pool offsets
-                void propagate_in_place_concat(std::shared_ptr<ngraph::op::Concat> concat);
-
-                // Find in-place slice ops and set appropriate memory pool offset for its output
-                void process_in_place_slice(std::list<std::shared_ptr<Node>> nodes);
-
-                // propagate slice when its arg comes from function input
-                void propagate_in_place_slice(ngraph::descriptor::Output* output,
-                                              size_t input_index,
-                                              size_t input_offset);
+                void register_common_passes(ngraph::pass::Manager& pass_manager,
+                                            ngraph::pass::PassConfig& pass_config);
 
                 bool computes_result(Node* node);
                 void release_function() { m_function = nullptr; }
@@ -237,6 +202,9 @@ namespace ngraph
                     const std::unordered_map<const Node*, std::string>& node_cache);
                 std::string emit_op_as_function(const Node&, const std::string& function_name);
                 std::string strip_comments(const std::string&);
+
+                std::unordered_set<descriptor::Tensor*>&
+                    get_tensor_set(descriptor::Tensor* output_tensor);
 
                 std::unique_ptr<codegen::Compiler> m_compiler;
                 std::unique_ptr<codegen::ExecutionEngine> m_execution_engine;
@@ -269,8 +237,10 @@ namespace ngraph
                 std::unordered_map<std::string, std::string> m_variable_name_map;
                 std::unordered_map<std::string, std::pair<std::size_t, std::size_t>>
                     m_variable_input_index_offset_map;
+                std::unordered_map<std::string, std::pair<std::size_t, std::size_t>>
+                    m_variable_output_index_offset_map;
 
-                std::unordered_map<std::string, CPUTensorRole> m_tensor_roles;
+                std::unordered_map<std::string, ngraph::CPUTensorRole> m_tensor_roles;
 
                 LayoutDescriptorPtrs parameter_layout_descriptors;
                 LayoutDescriptorPtrs result_layout_descriptors;
@@ -290,15 +260,33 @@ namespace ngraph
                     executor;
                 std::unordered_map<std::string, void*> tensor_data;
                 std::unordered_map<std::string, bool> tensor_stale;
+                // Each tensor is put into one buffer set.
+                // All the tensors in the same buffer set share the same memory buffer.
+                // bufferID_to_tensorSets maps bufferID to the pair of CPUTensorRole and buffer set.
+                // CPUTensorRole is INPUT, CONSTANT, OUTPUT, or INTERMEDIATE,
+                // which tells from where the memory buffer comes.
+                std::unordered_map<
+                    size_t,
+                    std::pair<ngraph::CPUTensorRole, std::unordered_set<descriptor::Tensor*>>>
+                    bufferID_to_tensorSets;
+                // tensor_to_bufferID maps tensor to the ID of the buffer set it belongs to.
+                std::unordered_map<descriptor::Tensor*, size_t> tensor_to_bufferID;
                 std::unordered_map<std::string, std::string> tensor_alias;
-                std::unordered_map<std::string, size_t> function_input_name_index;
+
+                // tensor pointer and its offset into the memory allocated for intermediates
+                // used to calculate the correct address at runtime
                 std::list<std::pair<std::reference_wrapper<void*>, size_t>> intermediates_offsets;
+                // tensor pointer, input index, offset into the input, and if the input is stale
+                // used to calculate the correct address at runtime
+                std::list<std::tuple<std::reference_wrapper<void*>,
+                                     size_t,
+                                     size_t,
+                                     std::reference_wrapper<bool>>>
+                    function_input_index_offset;
+                // tensor pointer, output index, and offset into the output
+                // used to calculate the correct address at runtime
                 std::list<std::tuple<std::reference_wrapper<void*>, size_t, size_t>>
-                    intermediate_input_index_offset;
-                std::list<
-                    std::tuple<std::reference_wrapper<void*>, size_t, std::reference_wrapper<bool>>>
-                    function_input_index;
-                std::list<std::pair<std::reference_wrapper<void*>, size_t>> function_output_index;
+                    function_output_index_offset;
                 std::unordered_map<std::string, std::shared_ptr<CPU_ExternalFunction>> callees;
                 bool m_is_built;
                 std::vector<runtime::PerformanceCounter> m_perf_counters;

src/ngraph/pass/pass_config.cpp
@@ -21,6 +21,7 @@
 
 using namespace ngraph;
 
+// TODO: Add file-based configuration support
 ngraph::pass::PassConfig::PassConfig()
 {
     /**
@@ -42,9 +43,37 @@ ngraph::pass::PassConfig::PassConfig()
             auto split_str = split(substr, ':', false);
             switch (split_str.size())
             {
-            case 1: m_enables.emplace(split_str[0], true); break;
-            case 2: m_enables.emplace(split_str[0], parse_string<bool>(split_str[1])); break;
-            default: throw ngraph_error("Unexpected string in get_pass_enables: " + substr);
+            case 1: m_pass_enables.emplace(split_str[0], true); break;
+            case 2: m_pass_enables.emplace(split_str[0], parse_string<bool>(split_str[1])); break;
+            default: throw ngraph_error("Unexpected string in NGRAPH_PASS_ENABLES: " + substr);
+            }
+        }
+    }
+    /**
+    * Parses the semi-colon separated environment string passed through NGRAPH_PASS_ATTRIBUTES
+    * and returns the pass attributes and whether they should be enabled or disabled in the
+    * provided unordered_map. Naming of pass attributes is up to the backends
+    * E.g., NGRAPH_PASS_ATTRIBUTES="OptimizeForMemory=0;MemoryAssignment::ReuseMemory=1;UseDefaultLayouts"
+    * would set false on "OptimizeForMemory", true on "MemoryAssignment::ReuseMemory" and true on
+    * "UseDefaultLayouts"
+    **/
+    env_str = std::getenv("NGRAPH_PASS_ATTRIBUTES");
+    if (env_str)
+    {
+        std::stringstream ss;
+        ss << env_str;
+        while (ss.good())
+        {
+            std::string substr;
+            std::getline(ss, substr, ';');
+            auto split_str = split(substr, '=', false);
+            switch (split_str.size())
+            {
+            case 1: m_pass_attributes.emplace(split_str[0], true); break;
+            case 2:
+                m_pass_attributes.emplace(split_str[0], parse_string<bool>(split_str[1]));
+                break;
+            default: throw ngraph_error("Unexpected string in NGRAPH_PASS_ATTRIBUTES: " + substr);
             }
         }
     }
@@ -52,14 +81,28 @@ ngraph::pass::PassConfig::PassConfig()
 
 void ngraph::pass::PassConfig::set_pass_enable(std::string name, bool enable)
 {
-    m_enables[name] = enable;
+    m_pass_enables[name] = enable;
 }
 
 bool ngraph::pass::PassConfig::get_pass_enable(std::string name)
 {
-    if (m_enables.find(name) == m_enables.end())
+    if (m_pass_enables.find(name) == m_pass_enables.end())
     {
         return false;
     }
-    return m_enables[name];
+    return m_pass_enables[name];
+}
+
+void ngraph::pass::PassConfig::set_pass_attribute(std::string name, bool enable)
+{
+    m_pass_attributes[name] = enable;
+}
+
+bool ngraph::pass::PassConfig::get_pass_attribute(std::string name)
+{
+    if (m_pass_attributes.find(name) == m_pass_attributes.end())
+    {
+        return false;
+    }
+    return m_pass_attributes[name];
 }

src/ngraph/pass/pass_config.hpp
@@ -30,10 +30,14 @@ class ngraph::pass::PassConfig
 {
 public:
     PassConfig();
-    const std::map<std::string, bool>& get_enables() { return m_enables; }
+    const std::map<std::string, bool>& get_enables() { return m_pass_enables; }
     void set_pass_enable(std::string name, bool enable);
     bool get_pass_enable(std::string name);
+    const std::map<std::string, bool>& get_pass_attributes() { return m_pass_attributes; }
+    void set_pass_attribute(std::string name, bool enable);
+    bool get_pass_attribute(std::string name);
 
 private:
-    std::map<std::string, bool> m_enables;
+    std::map<std::string, bool> m_pass_enables;
+    std::map<std::string, bool> m_pass_attributes;
 };

src/ngraph/pass/propagate_cacheability.cpp
@@ -39,13 +39,9 @@ bool ngraph::pass::PropagateCacheability::run_on_function(std::shared_ptr<Functi
                 op_annotations = op_annotations_factory();
                 op->set_op_annotations(op_annotations);
             }
-            if (std::dynamic_pointer_cast<op::Constant>(node))
+            if (node->is_parameter())
             {
-                op_annotations->set_cacheable(true);
-                NGRAPH_DEBUG << "propagate cacheability: cacheability is 1";
-            }
-            else if (auto parameter = std::dynamic_pointer_cast<op::Parameter>(node))
-            {
+                auto parameter = std::static_pointer_cast<op::Parameter>(node);
                 op_annotations->set_cacheable(parameter->get_cacheable());
                 NGRAPH_DEBUG << "propagate cacheability: cacheability is "
                              << parameter->get_cacheable();

src/ngraph/runtime/cpu/CMakeLists.txt
@@ -109,6 +109,7 @@ set(SRC
     pass/cpu_layout.cpp
     pass/cpu_loop_kernel_fusion.cpp
     pass/cpu_mat_fusion.cpp
+    pass/cpu_memory_assignment.cpp
     pass/cpu_memory_optimization.cpp
     pass/cpu_post_layout_optimizations.cpp
     pass/cpu_rnn_fusion.cpp

src/ngraph/runtime/cpu/builder/convolution.cpp
@@ -188,7 +188,7 @@ namespace ngraph
                 auto& arg2_tensor = external_function->get_tensor_data(args[2].get_name());
                 auto& arg3_tensor = external_function->get_tensor_data(args[3].get_name());
                 auto& out_tensor = external_function->get_tensor_data(out[0].get_name());
-                size_t arg3_size = args[3].get_size();
+                size_t arg3_size = node->get_inputs()[3].get_tensor().size();
 
                 if (runtime::cpu::mkldnn_utils::use_mkldnn_kernel(node))
                 {
@@ -229,7 +229,7 @@ namespace ngraph
                 auto& arg1_tensor = external_function->get_tensor_data(args[1].get_name());
                 auto& arg2_tensor = external_function->get_tensor_data(args[2].get_name());
                 auto& out_tensor = external_function->get_tensor_data(out[0].get_name());
-                size_t arg2_size = args[2].get_size();
+                size_t arg2_size = node->get_inputs()[2].get_tensor().size();
 
                 if (runtime::cpu::mkldnn_utils::use_mkldnn_kernel(node))
                 {

src/ngraph/runtime/cpu/builder/quantized_conv.cpp
@@ -203,8 +203,8 @@ namespace ngraph
                     auto& arg4_tensor = external_function->get_tensor_data(args[4].get_name());
                     auto& arg5_tensor = external_function->get_tensor_data(args[5].get_name());
                     auto& out0_tensor = external_function->get_tensor_data(out[0].get_name());
+                    size_t arg3_size = node->get_inputs()[3].get_tensor().size();
 
-                    size_t arg3_size = args[3].get_size();
                     auto scales_size = shape_size(args[4].get_shape());
                     auto sum_scales_size = shape_size(args[5].get_shape());
 
@@ -297,8 +297,8 @@ namespace ngraph
                     auto& arg4_tensor = external_function->get_tensor_data(args[4].get_name());
                     auto& arg5_tensor = external_function->get_tensor_data(args[5].get_name());
                     auto& out0_tensor = external_function->get_tensor_data(out[0].get_name());
+                    size_t arg3_size = node->get_inputs()[3].get_tensor().size();
 
-                    size_t arg3_size = args[3].get_size();
                     auto scales_size = shape_size(args[4].get_shape());
                     auto sum_scales_size = shape_size(args[5].get_shape());
 

commit d52d676b405deb75717f691dccdfd37f89228bfe

Fix bug introduced by #2238 on master (#2514)
* Fix bug introduced by #2238

* Apply style.

src/ngraph/runtime/cpu/cpu_external_function.cpp
@@ -1918,6 +1918,8 @@ string runtime::cpu::CPU_ExternalFunction::strip_comments(const string& s)
     return out.str();
 }
 
+#endif
+
 std::unordered_set<descriptor::Tensor*>&
     runtime::cpu::CPU_ExternalFunction::get_tensor_set(descriptor::Tensor* output_tensor)
 {
@@ -1928,5 +1930,3 @@ std::unordered_set<descriptor::Tensor*>&
     NGRAPH_ASSERT(output_buffer_it != bufferID_to_tensorSets.end());
     return output_buffer_it->second.second;
 }
-
-#endif

src/ngraph/runtime/cpu/cpu_external_function.hpp
@@ -203,9 +203,6 @@ namespace ngraph
                 std::string emit_op_as_function(const Node&, const std::string& function_name);
                 std::string strip_comments(const std::string&);
 
-                std::unordered_set<descriptor::Tensor*>&
-                    get_tensor_set(descriptor::Tensor* output_tensor);
-
                 std::unique_ptr<codegen::Compiler> m_compiler;
                 std::unique_ptr<codegen::ExecutionEngine> m_execution_engine;
 
@@ -216,6 +213,8 @@ namespace ngraph
                 // so they don't get freed before we are done with them
                 std::vector<std::shared_ptr<Node>> m_active_constants;
 #endif
+                std::unordered_set<descriptor::Tensor*>&
+                    get_tensor_set(descriptor::Tensor* output_tensor);
 
                 std::shared_ptr<ngraph::Function> m_function;
                 bool m_release_function;