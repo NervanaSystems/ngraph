0c39bd4c732fe383823fbc507e58a3fe5dd4984e
avijit-nervana <avijit.chakraborty@intel.com>
2019-02-28 00:36:42 -0800

    Added a debug logging macro: NGRAPH_DIST_DEBUG which prints the timestamp and MPI rank for distributed AllReduceops

--- src/ngraph/runtime/cpu/builder/allreduce.cpp
+++ src/ngraph/runtime/cpu/builder/allreduce.cpp
@@ -24,6 +24,55 @@
 #include "ngraph/op/allreduce.hpp"
 #include "ngraph/runtime/cpu/cpu_builder.hpp"
 
+#include <cstdarg>
+#include <iomanip>
+#include <locale>
+#include <sys/time.h>
+#include <unistd.h> /*for CLOCK_REALTIME? */
+
+#include "ngraph/distributed.hpp"
+
+std::string get_timestamp()
+{
+    using namespace std::chrono;
+
+    // get current time
+    auto now = system_clock::now();
+
+    // get number of nanoseconds for the current second
+    // (remainder after division into seconds)
+    auto ns = duration_cast<nanoseconds>(now.time_since_epoch()) % 1000000;
+
+    // convert to std::time_t in order to convert to std::tm (broken time)
+    auto timer = system_clock::to_time_t(now);
+
+    // convert to broken time
+    std::tm bt = *std::localtime(&timer);
+
+    std::ostringstream oss;
+    oss << std::put_time(&bt, "%H:%M:%S"); // HH:MM:SS
+    oss << '.' << std::setfill('0') << std::setw(3) << ns.count();
+
+    return oss.str();
+}
+
+inline void LogPrintf(const char* fmt, ...)
+{
+    va_list args1;
+    va_start(args1, fmt);
+    va_list args2;
+    va_copy(args2, args1);
+
+    std::vector<char> buf(1 + std::vsnprintf(nullptr, 0, fmt, args1));
+    va_end(args1);
+    std::vsnprintf(buf.data(), buf.size(), fmt, args2);
+    va_end(args2);
+
+    ngraph::Distributed dist;
+    std::printf("%s [RANK: %d]: %s\n", get_timestamp().c_str(), dist.get_rank(), buf.data());
+}
+#define NGRAPH_DIST_DEBUG(fmt, ...) LogPrintf(fmt, ##__VA_ARGS__)
+
 using namespace std;
 using namespace ngraph;
 
@@ -36,11 +85,22 @@ namespace ngraph
             template <>
             void Builder::BUILDER_DECL(ngraph::op::AllReduce)
             {
-                auto& functors = external_function->get_functors();
+                static int call_seq = 0;
 
+                auto& functors = external_function->get_functors();
                 auto& arg_tensor = external_function->get_tensor_data(args[0].get_name());
                 auto& out_tensor = external_function->get_tensor_data(out[0].get_name());
                 auto count = static_cast<int>(out[0].get_size());
+
+                auto external_function_name = external_function->get_function_name();
+                NGRAPH_DIST_DEBUG(
+                    "AllReduce Queued[%d]: Function: %s Node: %s %s Size: "
+                    "%d",
+                    call_seq,
+                    external_function_name.c_str(),
+                    node->get_name().c_str(),
+                    node->get_friendly_name().c_str(),
+                    count);
 
 #ifdef NGRAPH_DISTRIBUTED_MLSL_ENABLE
                 auto data_type = MLSL::DT_FLOAT;
@@ -72,8 +132,20 @@ namespace ngraph
                     data_type = MPI_DOUBLE;
                 }
 
-                auto functor = [&, count, data_type](CPURuntimeContext* ctx,
-                                                     CPUExecutionContext* ectx) {
+                auto node_friendly_name = node->get_friendly_name();
+                auto node_name = node->get_name();
+                auto func_name = external_function->get_function_name();
+                int id = call_seq;
+                call_seq++;
+
+                auto functor = [&, id, count, data_type, func_name, node_friendly_name, node_name](
+                                   CPURuntimeContext* ctx, CPUExecutionContext* ectx) {
+                    NGRAPH_DIST_DEBUG("AllReduce Execute[%d]: Function: %s  Node: %s %s Size: %d",
+                                      id,
+                                      func_name.c_str(),
+                                      node_name.c_str(),
+                                      node_friendly_name.c_str(),
+                                      count);
                     MPI_Allreduce(
                         arg_tensor, out_tensor, count, data_type, MPI_SUM, MPI_COMM_WORLD);
                 };
