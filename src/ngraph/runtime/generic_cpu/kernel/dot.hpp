914d0c7a0306761e772bb7740c614bf01c7f69e2
Robert Kimball <robert.kimball@intel.com>
2019-03-03 08:43:40 -0800

    directly call reference dot

--- src/ngraph/runtime/generic_cpu/kernel/dot.hpp
+++ src/ngraph/runtime/generic_cpu/kernel/dot.hpp
@@ -22,6 +22,7 @@
 #include <utility>
 
 #include "ngraph/coordinate_transform.hpp"
+#include "ngraph/runtime/reference/dot.hpp"
 #include "ngraph/shape_util.hpp"
 
 namespace ngraph
@@ -56,88 +57,13 @@ namespace ngraph
                     }
                     else
                     {
-                        // Get the sizes of the dot axes. It's easiest to pull them from arg1 because they're
-                        // right up front.
-                        Shape dot_axis_sizes(reduction_axes_count);
-                        std::copy(arg1_shape.begin(),
-                                  arg1_shape.begin() + reduction_axes_count,
-                                  dot_axis_sizes.begin());
-
-                        CoordinateTransform arg0_transform(arg0_shape);
-                        CoordinateTransform arg1_transform(arg1_shape);
-                        CoordinateTransform output_transform(out_shape);
-
-                        // Create coordinate transforms for arg0 and arg1 that throw away the dotted axes.
-                        size_t arg0_projected_rank = arg0_shape.size() - reduction_axes_count;
-                        size_t arg1_projected_rank = arg1_shape.size() - reduction_axes_count;
-
-                        Shape arg0_projected_shape(arg0_projected_rank);
-                        std::copy(arg0_shape.begin(),
-                                  arg0_shape.begin() + arg0_projected_rank,
-                                  arg0_projected_shape.begin());
-
-                        Shape arg1_projected_shape(arg1_projected_rank);
-                        std::copy(arg1_shape.begin() + reduction_axes_count,
-                                  arg1_shape.end(),
-                                  arg1_projected_shape.begin());
-
-                        CoordinateTransform arg0_projected_transform(arg0_projected_shape);
-                        CoordinateTransform arg1_projected_transform(arg1_projected_shape);
-
-                        // Create a coordinate transform that allows us to iterate over all possible values
-                        // for the dotted axes.
-                        CoordinateTransform dot_axes_transform(dot_axis_sizes);
-
-                        for (const Coordinate& arg0_projected_coord : arg0_projected_transform)
-                        {
-                            for (const Coordinate& arg1_projected_coord : arg1_projected_transform)
-                            {
-                                // The output coordinate is just the concatenation of the projected coordinates.
-                                Coordinate out_coord(arg0_projected_coord.size() +
-                                                     arg1_projected_coord.size());
-
-                                auto out_coord_it = std::copy(arg0_projected_coord.begin(),
-                                                              arg0_projected_coord.end(),
-                                                              out_coord.begin());
-                                std::copy(arg1_projected_coord.begin(),
-                                          arg1_projected_coord.end(),
-                                          out_coord_it);
-
-                                // Zero out to start the sum.
-                                T sum = 0;
-
-                                size_t out_index = output_transform.index(out_coord);
-
-                                // Walk along the dotted axes.
-                                Coordinate arg0_coord(arg0_shape.size());
-                                Coordinate arg1_coord(arg1_shape.size());
-                                auto arg0_it = std::copy(arg0_projected_coord.begin(),
-                                                         arg0_projected_coord.end(),
-                                                         arg0_coord.begin());
-                                for (const Coordinate& dot_axis_positions : dot_axes_transform)
-                                {
-                                    // In order to find the points to multiply together, we need to inject our current
-                                    // positions along the dotted axes back into the projected arg0 and arg1 coordinates.
-                                    std::copy(dot_axis_positions.begin(),
-                                              dot_axis_positions.end(),
-                                              arg0_it);
-
-                                    auto arg1_it = std::copy(dot_axis_positions.begin(),
-                                                             dot_axis_positions.end(),
-                                                             arg1_coord.begin());
-                                    std::copy(arg1_projected_coord.begin(),
-                                              arg1_projected_coord.end(),
-                                              arg1_it);
-
-                                    // Multiply and add to the sum.
-                                    sum += arg0[arg0_transform.index(arg0_coord)] *
-                                           arg1[arg1_transform.index(arg1_coord)];
-                                }
-
-                                // Write the sum back.
-                                out[out_index] = sum;
-                            }
-                        }
+                        reference::dot(arg0,
+                                       arg1,
+                                       out,
+                                       arg0_shape,
+                                       arg1_shape,
+                                       out_shape,
+                                       reduction_axes_count);
                     }
                 }
             }
