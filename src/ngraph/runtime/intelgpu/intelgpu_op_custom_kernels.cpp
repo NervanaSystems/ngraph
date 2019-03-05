ca5476b3bf163e615c0e5ec119a23335baa9147a
Sergey Shalnov <shssf@users.noreply.github.com>
2019-03-05 22:52:39 +0100

    IntelGPU backend: Concat operation custom kernel implemenation (#2551)

--- src/ngraph/runtime/intelgpu/intelgpu_op_custom_kernels.cpp
+++ src/ngraph/runtime/intelgpu/intelgpu_op_custom_kernels.cpp
@@ -1078,6 +1078,148 @@ void runtime::intelgpu::do_slice_operation(cldnn::topology& topology,
     topology.add(op_slice);
 }
 
+void runtime::intelgpu::do_concat_operation(cldnn::topology& topology,
+                                            const vector<string>& input_names,
+                                            const vector<Shape>& input_shapes,
+                                            const string& output_name,
+                                            const Shape& output_shape,
+                                            const element::Type& output_type,
+                                            size_t concat_axis)
+{
+    const cldnn::layout layout = IntelGPULayout::create_cldnn_layout(output_type, output_shape);
+    const string kernel_type_name = get_opencl_type_name(output_type);
+    string entry_point_name = "concat_" + output_name;
+
+    size_t bound_below = 0;
+    size_t idx = 0;
+    vector<string>::const_iterator input_name = input_names.cbegin();
+    string aux_output_name;
+
+    // this is quite non optimal because cldnn::custom_gpu_primitive
+    // does not provide an ability to run kernels simultaneously with the same output
+    // Also, need to make a chain of kernels to put kernel0::output0 as kernel1::input1
+    // with output name kernel1::output2
+    for (auto const& input_shape : input_shapes)
+    {
+        string name_suffix = to_string(idx);
+        const string entry_point_name_suffix = entry_point_name + "_" + name_suffix;
+        CodeWriter writer;
+        vector<size_t> gws;
+
+        if (idx == 0)
+        {
+            gen_func_def(writer,
+                         entry_point_name_suffix,
+                         {kernel_type_name},
+                         {input_shape},
+                         kernel_type_name,
+                         output_shape);
+        }
+        else
+        {
+            gen_func_def(writer,
+                         entry_point_name_suffix,
+                         {2, kernel_type_name},
+                         {input_shape, output_shape},
+                         kernel_type_name,
+                         output_shape);
+        }
+
+        writer.block_begin();
+        {
+            // Main loops
+            gws = generate_loops(writer, output_shape, true);
+
+            writer << kernel_type_name << " input_element;\n";
+
+            size_t bound_upper = input_shape.at(concat_axis);
+
+            // copy corresponding elements of input0 into output
+            writer << "if (((" << bound_below << " + 0) <= i" << concat_axis << ") && (i"
+                   << concat_axis << " < (" << bound_below << " + " << bound_upper << ")))\n";
+            writer.block_begin();
+            {
+                writer << "input_element = input0";
+
+                if (input_shape.empty())
+                {
+                    // it means scalar
+                    writer << "[0]";
+                }
+                else
+                {
+                    size_t var_idx = 0;
+                    for (auto const i : input_shape)
+                    {
+                        if (var_idx == concat_axis)
+                        {
+                            writer << "[i" << var_idx << " - " << bound_below << "]";
+                        }
+                        else
+                        {
+                            writer << "[i" << var_idx << "]";
+                        }
+                        ++var_idx;
+                    }
+                }
+                writer << ";\n";
+            }
+            writer.block_end();
+
+            // if not a first kernel, copy input1 into output
+            if (idx != 0)
+            {
+                writer << "else\n";
+                writer.block_begin();
+                {
+                    writer << "input_element = input1" << access_dims(output_shape) << ";\n";
+                }
+                writer.block_end();
+            }
+            bound_below += bound_upper;
+
+            writer << "output" << access_dims(output_shape) << " = input_element;\n";
+
+            // Closing brackets for main loops
+            generate_loops(writer, output_shape, false);
+        }
+        writer.block_end();
+
+        vector<cldnn::primitive_id> kernel_input;
+        vector<cldnn_arg> kernel_arguments;
+
+        kernel_input.push_back(*input_name);
+        if (idx == 0)
+        {
+            kernel_arguments = get_kernel_args(1, 1);
+        }
+        else
+        {
+            if (idx == input_shapes.size() - 1)
+            {
+                // last kernel should produce the output name as overall node required
+                name_suffix = "";
+            }
+            kernel_input.push_back(aux_output_name);
+            kernel_arguments = get_kernel_args(2, 1);
+        }
+
+        const cldnn::custom_gpu_primitive op_concat(output_name + name_suffix,
+                                                    kernel_input,
+                                                    {writer.get_code()},
+                                                    entry_point_name_suffix,
+                                                    kernel_arguments,
+                                                    "",
+                                                    layout,
+                                                    gws);
+        topology.add(op_concat);
+
+        ++input_name;
+        ++idx;
+        aux_output_name = output_name + name_suffix;
+    }
+}
+
 void runtime::intelgpu::do_select_operation(cldnn::topology& topology,
                                             const string& input0_name,
                                             const Shape& input0_shape,
