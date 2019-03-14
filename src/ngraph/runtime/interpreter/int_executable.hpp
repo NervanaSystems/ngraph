ef08d369d98c7f445c9342b2e44c237d2b7a7ea4
Mohammad Mahbubuzzaman <mohammad.mahbubuzzaman@intel.com>
2019-03-14 11:13:14 -0700

    Addresses code review comments.

--- src/ngraph/runtime/interpreter/int_executable.hpp
+++ src/ngraph/runtime/interpreter/int_executable.hpp
@@ -34,6 +34,7 @@
 #include "ngraph/op/convolution.hpp"
 #include "ngraph/op/dequantize.hpp"
 #include "ngraph/op/dot.hpp"
+#include "ngraph/op/experimental/dyn_broadcast.hpp"
 #include "ngraph/op/embedding_lookup.hpp"
 #include "ngraph/op/experimental/dyn_broadcast.hpp"
 #include "ngraph/op/experimental/generate_mask.hpp"
