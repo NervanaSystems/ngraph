718dfbd68246cf0543fc63fd7856a1cea20e3e93
Tomasz Socha <tomasz.socha@intel.com>
2019-03-19 12:57:17 +0100

    [ONNX] Change default broadcast dim value, gemm improvement

--- src/ngraph/frontend/onnx_import/utils/broadcasting.cpp
+++ src/ngraph/frontend/onnx_import/utils/broadcasting.cpp
@@ -45,9 +45,9 @@ ngraph::Shape calculate_broadcast_shape(ngraph::Shape left_shape, ngraph::Shape 
     auto max_rank = std::max(left_rank, right_rank);
 
     // left-pad the left_shape with ones
-    left_shape.insert(std::begin(left_shape), max_rank - left_rank, 1);
+    left_shape.insert(std::begin(left_shape), max_rank - left_rank, 0);
     // left-pad the right_shape with ones
-    right_shape.insert(std::begin(right_shape), max_rank - right_rank, 1);
+    right_shape.insert(std::begin(right_shape), max_rank - right_rank, 0);
 
     for (std::size_t index = 0; index < max_rank; ++index)
     {
