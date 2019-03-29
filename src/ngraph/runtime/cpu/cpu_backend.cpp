2bfec408806eb899abba9a020dd6985210989f57
fenglei <fenglei.tian@intel.com>
2019-03-29 12:54:18 -0700

    change cpu side

--- src/ngraph/runtime/cpu/cpu_backend.cpp
+++ src/ngraph/runtime/cpu/cpu_backend.cpp
@@ -102,12 +102,12 @@ runtime::cpu::CPU_Executable::CPU_Executable(shared_ptr<Function> func,
     FunctionInstance& instance = m_function_instance;
     if (instance.m_external_function == nullptr)
     {
-        instance.m_external_function = make_shared<CPU_ExternalFunction>(func);
+        set_parameters_and_results(*func);
+        instance.m_external_function = make_shared<CPU_ExternalFunction>(m_compiled_function);
         instance.m_external_function->m_emit_timing = performance_counters_enabled;
         auto cf = instance.m_external_function->make_call_frame(pass_config);
         instance.m_call_frame = dynamic_pointer_cast<CPU_CallFrame>(cf);
     }
-    set_parameters_and_results(*func);
 }
 
 std::shared_ptr<ngraph::runtime::cpu::CPU_CallFrame> runtime::cpu::CPU_Executable::get_call_frame()
