199ec73e8ebf4dfb27c13fd0eb7a95de662c1526
Robert Kimball <robert.kimball@intel.com>
2019-03-29 13:11:20 -0700

    move unit test to correct file (#2685)

--- test/backend_api.in.cpp
+++ test/backend_api.in.cpp
@@ -95,3 +95,42 @@ NGRAPH_TEST(${BACKEND_NAME}, tensor_copy_from)
     a->copy_from(*b);
     EXPECT_TRUE(test::all_close(bv, read_vector<float>(a), 0.0f, 0.0f));
 }
+
+NGRAPH_TEST(${BACKEND_NAME}, get_parameters_and_results)
+{
+    Shape shape{2, 2};
+    auto A = make_shared<op::Parameter>(element::f32, shape);
+    auto B = make_shared<op::Parameter>(element::f32, shape);
+    auto C = make_shared<op::Parameter>(element::f32, shape);
+    auto f = make_shared<Function>((A + B) * C, ParameterVector{A, B, C});
+
+    auto backend = runtime::Backend::create("${BACKEND_NAME}");
+
+    // Create some tensors for input/output
+    shared_ptr<runtime::Tensor> a = backend->create_tensor(element::f32, shape);
+    shared_ptr<runtime::Tensor> b = backend->create_tensor(element::f32, shape);
+    shared_ptr<runtime::Tensor> c = backend->create_tensor(element::f32, shape);
+    shared_ptr<runtime::Tensor> result = backend->create_tensor(element::f32, shape);
+
+    copy_data(a, test::NDArray<float, 2>({{1, 2}, {3, 4}}).get_vector());
+    copy_data(b, test::NDArray<float, 2>({{5, 6}, {7, 8}}).get_vector());
+    copy_data(c, test::NDArray<float, 2>({{9, 10}, {11, 12}}).get_vector());
+
+    auto handle = backend->compile(f);
+    auto parameters = handle->get_parameters();
+    auto results = handle->get_results();
+    ASSERT_EQ(parameters.size(), 3);
+    ASSERT_EQ(results.size(), 1);
+
+    // This part can't be enabled until we force backends to make a copy of the source graph
+    // auto func_parameters = f->get_parameters();
+    // auto func_results = f->get_results();
+    // for (size_t i = 0; i < 3; ++i)
+    // {
+    //     EXPECT_NE(parameters[i], func_parameters[i]);
+    // }
+    // for (size_t i = 0; i < 1; ++i)
+    // {
+    //     EXPECT_NE(results[i], func_results[i]);
+    // }
+}
