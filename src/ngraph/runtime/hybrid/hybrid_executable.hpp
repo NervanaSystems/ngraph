2975dd69f8cfd82ad5d458e643f2df5036a18f12
Fenglei Tian <fengleit@fengleit-mac01.local>
2019-04-04 15:46:42 -0700

    update compiled function for hybrid

--- src/ngraph/runtime/hybrid/hybrid_executable.hpp
+++ src/ngraph/runtime/hybrid/hybrid_executable.hpp
@@ -38,7 +38,7 @@ class ngraph::runtime::hybrid::HybridExecutable : public runtime::Executable
 {
 public:
     HybridExecutable(const std::vector<std::shared_ptr<runtime::Backend>>& backend_list,
-                     const std::shared_ptr<Function>& func,
+                     const std::shared_ptr<Function> func,
                      bool enable_performance_collection = false,
                      bool debug_enabled = false);
 
