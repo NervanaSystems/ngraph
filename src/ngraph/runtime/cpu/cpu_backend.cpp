dd42d5580378242a1451aec857ca24459f4ef760
fenglei <fenglei.tian@intel.com>
2019-04-04 11:55:14 -0700

    step by step, update gpu can cpu backend

--- src/ngraph/runtime/cpu/cpu_backend.cpp
+++ src/ngraph/runtime/cpu/cpu_backend.cpp
@@ -98,16 +98,16 @@ shared_ptr<runtime::Executable>
 runtime::cpu::CPU_Executable::CPU_Executable(shared_ptr<Function> func,
                                              ngraph::pass::PassConfig& pass_config,
                                              bool performance_counters_enabled)
+    : runtime::Executable(func)
 {
     FunctionInstance& instance = m_function_instance;
     if (instance.m_external_function == nullptr)
     {
-        instance.m_external_function = make_shared<CPU_ExternalFunction>(func);
+        instance.m_external_function = make_shared<CPU_ExternalFunction>(m_function);
         instance.m_external_function->m_emit_timing = performance_counters_enabled;
         auto cf = instance.m_external_function->make_call_frame(pass_config);
         instance.m_call_frame = dynamic_pointer_cast<CPU_CallFrame>(cf);
     }
-    set_parameters_and_results(*func);
 }
 
 std::shared_ptr<ngraph::runtime::cpu::CPU_CallFrame> runtime::cpu::CPU_Executable::get_call_frame()
