c069f3f92d9db2c223e7976679cd123033d4ac14
Sang Ik Lee <sang.ik.lee@intel.com>
2019-04-08 14:30:07 -0700

    Implement reference gather.

--- src/ngraph/runtime/reference/gather.hpp
+++ src/ngraph/runtime/reference/gather.hpp
@@ -60,42 +60,78 @@ namespace ngraph
                 Shape out_prime_shape(params_prime_shape);
                 out_prime_shape[0] = indices_shape[indices_ndim - 1];
 
+                // Create a CoordinateTransform for "out" that visits the outer "axis" dimensions
+                size_t out_ndim = static_cast<size_t>(out_shape.size());
+                Coordinate out_outer_start_corner(out_ndim, 0);
+                Coordinate out_outer_end_corner(out_shape);
+                for(size_t i = axis - 1; i < out_ndim; i++)
+                {
+                    out_outer_end_corner[i] = 1;
+                }
+                Strides out_outer_strides(out_ndim, 1);
+                AxisVector out_outer_axis_order(out_ndim);
+                iota(out_outer_axis_order.begin(), out_outer_axis_order.end(), 0);
+                CoordinateTransform out_outer_transform(
+                    out_shape, out_outer_start_corner, out_outer_end_corner, out_outer_strides, out_outer_axis_order);
+
+                // Create a CoordinateTransform for "params" that visits the outer "axis" dimensions
+                size_t params_ndim = static_cast<size_t>(params_shape.size());
+                Coordinate params_outer_start_corner(params_ndim, 0);
+                Coordinate params_outer_end_corner(params_shape);
+                for(size_t i = axis - 1; i < params_ndim; i++)
+                {
+                    params_outer_end_corner[i] = 1;
+                }
+                Strides params_outer_strides(params_ndim, 1);
+                AxisVector params_outer_axis_order(params_ndim);
+                iota(params_outer_axis_order.begin(), params_outer_axis_order.end(), 0);
+                CoordinateTransform params_outer_transform(
+                    params_shape, params_outer_start_corner, params_outer_end_corner, params_outer_strides, params_outer_axis_order);
 
                 // Create a CoordinateTransform for "indices" that visits only the first element along inner most axis
-                Coordinate indices_start_corner(indices_ndim, 0);
-                Coordinate indices_inner_end_corner(indices_shape);
-                size_t slice_rank = indices_shape[indices_ndim - 1];
-                for(size_t i = axis; i < indices_ndim; i++)
-                {
-                    indices_inner_end_corner[i] = 1;
-                }
-                Strides indices_strides(indices_ndim, 1);
-                AxisVector indices_axis_order(indices_ndim);
-                iota(indices_axis_order.begin(), indices_axis_order.end(), 0);
+                Coordinate indices_outer_start_corner(indices_ndim, 0);
+                Coordinate indices_outer_end_corner(indices_shape);
+                indices_outer_end_corner[indices_ndim - 1] = 1;
+                Strides indices_outer_strides(indices_ndim, 1);
+                AxisVector indices_outer_axis_order(indices_ndim);
+                iota(indices_outer_axis_order.begin(), indices_outer_axis_order.end(), 0);
                 CoordinateTransform indices_outer_transform(
-                    indices_shape, indices_start_corner, indices_inner_end_corner, indices_strides, indices_axis_order);
+                    indices_shape, indices_outer_start_corner, indices_outer_end_corner, indices_outer_strides, indices_outer_axis_order);
 
-                // Create a matching CoordinateTransform for "out" that visits the same outer coordinates
-                size_t out_ndim = static_cast<size_t>(out_shape.size());
-                Coordinate out_start_corner(out_ndim, 0);
-                Coordinate out_end_corner(out_shape);
-                for(size_t i = axis - 1; i < out_ndim; i++)
+                // Create an inner CoordinateTransfrom for "out"
+                size_t out_inner_ndim = out_ndim - axis;
+                Shape out_inner_shape(out_shape);
+                out_inner_shape.erase(out_inner_shape.begin(), out_inner_shape.begin() + axis);
+                Coordinate out_inner_start_corner(out_inner_ndim, 0);
+                Coordinate out_inner_end_corner(out_inner_shape);
+                for(size_t i = indices_ndim - 1; i < out_inner_ndim; i++)
                 {
                     out_inner_end_corner[i] = 1;
                 }
-                Strides out_strides(out_ndim, 1);
-                AxisVector out_axis_order(out_ndim);
-                iota(out_axis_order.begin(), out_axis_order.end(), 0);
-                CoordinateTransform out_transform(
-                    out_shape, out_start_corner, out_inner_end_corner, out_strides, out_axis_order);
+                Strides out_inner_strides(out_inner_ndim, 1);
+                AxisVector out_inner_axis_order(out_inner_ndim);
+                iota(out_inner_axis_order.begin(),out_inner_axis_order.end(), 0);
 
-                for (const Coordinate& indices_outer_coord : indices_outer_transform)
+                auto out_outer_coord_iter = out_outer_transform.begin();
+                for (const Coordinate& params_outer_coord : params_outer_transform)
                 {
-                    indices_prime = &indices[indices_outer_transform.index(indices_outer_coord)];
-                    gather_nd(params_prime, indices_prime, out_prime, params_prime_shape, indices_prime_shape, out_prime_shape);
+                    T *params_prime = &params[params_outer_transform.index(params_outer_coord)];
+                    T *out_outer = &out[out_outer_transform.index(*out_outer_coord_iter)];
+                    for(size_t i = axis; i < out_inner_ndim; i++)
+                    {
+                        indices_outer_end_corner[i] = 1;
+                    }
+                    //
+                    auto out_inner_coord_iter = out_inner_transform.begin();
+                    for (const Coordinate& indices_outer_coord : indices_outer_transform)
+                    {
+                        U *indices_prime = &indices[indices_outer_transform.index(indices_outer_coord)];
+                        T* out_prime = &out_outer[out_inner_transform.index(*out_inner_coord_iter)];
+                        gather_nd(params_prime, indices_prime, out_prime, params_prime_shape, indices_prime_shape, out_prime_shape);
+                        out_inner_coord_iter++;
+                    }
+                    out_outer_coord_iter++;
                 }
-                // reshape out
-                //
             }
         }
     }
