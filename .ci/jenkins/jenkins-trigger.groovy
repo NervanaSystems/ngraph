60088d874c6a46e175cea2d47314732f209ddb53
aslepko <44713115+aslepko@users.noreply.github.com>
2019-04-16 10:01:12 -0700

    Update jenkins-trigger.groovy

--- .ci/jenkins/jenkins-trigger.groovy
+++ .ci/jenkins/jenkins-trigger.groovy
@@ -24,7 +24,7 @@
 // Parameters which ngraph-unittest uses:
 String  PR_URL = CHANGE_URL
 String  PR_COMMIT_AUTHOR = CHANGE_AUTHOR
-String  JENKINS_BRANCH = "aslepko/winclean"
+String  JENKINS_BRANCH = "Aslepko/winclean"
 Integer TIMEOUTTIME = "3600"
 // BRANCH parameter is no loner needed
 // TRIGGER_URL parameter is no longer needed
