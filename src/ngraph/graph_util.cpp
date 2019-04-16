e3612ca5e836d60817aca28d252af2978ab6e292
Adam Procter <adam.m.procter@intel.com>
2019-04-16 12:09:48 -0700

    Fix merge boo-boo

--- src/ngraph/graph_util.cpp
+++ src/ngraph/graph_util.cpp
@@ -150,20 +150,6 @@ void ngraph::replace_node(std::shared_ptr<Node> target, std::shared_ptr<Node> re
     // Fix input/output descriptors
     NGRAPH_CHECK(target->get_output_size() == replacement->get_output_size());
 
-    auto set_replacement_prov = [replacement](std::shared_ptr<Node> node) {
-        replacement->merge_provenance_tags_from(node);
-    };
-    traverse_nodes({target}, set_replacement_prov, false, replacement->get_arguments());
-
-    auto propagate_replacement_prov = [replacement](std::shared_ptr<Node> node) {
-        if (is_post_dominated(node.get(), replacement.get()))
-        {
-            node->merge_provenance_tags_from(replacement);
-        }
-    };
-
-    traverse_nodes({replacement}, propagate_replacement_prov, false);
-
     // For each of target's output O with replacement output O_rep:
     //     For each O's connected downstream input I:
     //         Change I's connected upstream output to O_rep
