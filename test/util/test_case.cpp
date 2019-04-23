da874068767b5b5ef31d4b7d4d9fc0aa1cd9a601
tomdol <tomasz.dolbniak@intel.com>
2019-04-23 18:32:54 +0200

    [ONNX] Extended types support for NgraphTestCase

--- test/util/test_case.cpp
+++ test/util/test_case.cpp
@@ -21,6 +21,45 @@
 #include "ngraph/assertion.hpp"
 #include "test_tools.hpp"
 
+namespace
+{
+    template <typename T>
+    typename std::enable_if<std::is_floating_point<T>::value, ::testing::AssertionResult>::type
+        compare_values(const std::shared_ptr<ngraph::op::Constant>& expected_results,
+                       const std::shared_ptr<ngraph::runtime::Tensor>& results)
+    {
+        const auto expected = expected_results->get_vector<T>();
+        const auto result = read_vector<T>(results);
+        return ngraph::test::all_close_f(expected, result);
+    }
+
+    template <typename T>
+    typename std::enable_if<std::is_integral<T>::value, ::testing::AssertionResult>::type
+        compare_values(const std::shared_ptr<ngraph::op::Constant>& expected_results,
+                       const std::shared_ptr<ngraph::runtime::Tensor>& results)
+    {
+        const auto expected = expected_results->get_vector<T>();
+        const auto result = read_vector<T>(results);
+        return ngraph::test::all_close(expected, result);
+    }
+
+    using value_comparator_function =
+        std::function<::testing::AssertionResult(const std::shared_ptr<ngraph::op::Constant>&,
+                                                 const std::shared_ptr<ngraph::runtime::Tensor>&)>;
+
+    const std::map<ngraph::element::Type_t, value_comparator_function> value_comparators = {
+        {ngraph::element::Type_t::f32, compare_values<float>},
+        {ngraph::element::Type_t::f64, compare_values<double>},
+        {ngraph::element::Type_t::i8, compare_values<int8_t>},
+        {ngraph::element::Type_t::i16, compare_values<int16_t>},
+        {ngraph::element::Type_t::i32, compare_values<int32_t>},
+        {ngraph::element::Type_t::i64, compare_values<int64_t>},
+        {ngraph::element::Type_t::u8, compare_values<uint8_t>},
+        {ngraph::element::Type_t::u16, compare_values<uint16_t>},
+        {ngraph::element::Type_t::u32, compare_values<uint32_t>},
+        {ngraph::element::Type_t::u64, compare_values<uint64_t>}};
+}
+
 void ngraph::test::NgraphTestCase::run()
 {
     const auto& function_results = m_function->get_results();
@@ -36,22 +75,16 @@ void ngraph::test::NgraphTestCase::run()
         const auto& expected_result_constant = m_expected_outputs.at(i);
         const auto& element_type = result_tensor->get_element_type();
 
-        if (element_type == ngraph::element::f32)
+        if (value_comparators.count(element_type.get_type_enum()) == 0)
         {
-            const auto result = read_vector<float>(result_tensor);
-            const auto expected = expected_result_constant->get_vector<float>();
-            EXPECT_TRUE(test::all_close_f(expected, result));
-        }
-        else if (element_type == ngraph::element::u8)
-        {
-            const auto result = read_vector<uint8_t>(result_tensor);
-            const auto expected = expected_result_constant->get_vector<uint8_t>();
-            EXPECT_TRUE(test::all_close(expected, result));
+            NGRAPH_FAIL() << "Please add support for " << element_type
+                          << " to ngraph::test::NgraphTestCase::run()";
         }
         else
         {
-            NGRAPH_FAIL() << "Please add support for " << element_type
-                          << " to ngraph::test::NgraphTestCase::run().";
+            auto values_match = value_comparators.at(element_type.get_type_enum());
+
+            EXPECT_TRUE(values_match(expected_result_constant, result_tensor));
         }
     }
 }
