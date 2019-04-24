f116d560a5e5cae0eaf7a31ff7174fb67ae3039e
Adam Procter <adam.m.procter@intel.com>
2019-04-24 15:55:27 -0700

    Fix broken test in shape specialization pass

--- src/ngraph/op/add.hpp
+++ src/ngraph/op/add.hpp
@@ -43,6 +43,8 @@ namespace ngraph
             virtual std::shared_ptr<Node>
                 copy_with_new_args(const NodeVector& new_args) const override;
 
+            virtual std::vector<std::shared_ptr<op::Constant>> as_constants() const override;
+
         protected:
             virtual void generate_adjoints(autodiff::Adjoints& adjoints,
                                            const NodeVector& deltas) override;
