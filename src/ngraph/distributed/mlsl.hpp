ee9de5b228741c09a8e3839a56a3674f60cfade2
Scott Cyphers <scott.cyphers@intel.com>
2019-04-26 22:59:05 -0700

    Return name

--- src/ngraph/distributed/mlsl.hpp
+++ src/ngraph/distributed/mlsl.hpp
@@ -47,7 +47,7 @@ namespace ngraph
                 }
             }
 
-            const std::string& get_name() const override {}
+            const std::string& get_name() const override { return m_name; }
             int get_size() override
             {
                 return static_cast<int>(MLSL::Environment::GetEnv().GetProcessCount());
