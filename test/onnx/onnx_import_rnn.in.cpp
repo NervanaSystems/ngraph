737645c094ebb043297aaf369b7e4ec31bb778dd
Adam Rogowiec <adam.rogowiec@intel.com>
2019-04-30 08:27:11 +0200

    UT for lstm with hardsigmoid activation function.

--- test/onnx/onnx_import_rnn.in.cpp
+++ test/onnx/onnx_import_rnn.in.cpp
@@ -30,6 +30,7 @@
 #include "util/all_close.hpp"
 #include "util/all_close_f.hpp"
 #include "util/ndarray.hpp"
+#include "util/test_case.hpp"
 #include "util/test_control.hpp"
 #include "util/test_tools.hpp"
 
@@ -218,4 +219,33 @@ NGRAPH_TEST(onnx_${BACKEND_NAME}, model_lstm_fwd_mixed_seq)
         // The discrepancies may occur at most on 7th decimal position.
         EXPECT_TRUE(test::all_close_f(expected_output.at(i), outputs.at(i), 3));
     }
+}
+
+NGRAPH_TEST(onnx_${BACKEND_NAME}, model_lstm_fwd_hardsigmoid_activation)
+{
+    auto function = onnx_import::import_onnx_model(
+        file_util::path_join(SERIALIZED_ZOO, "onnx/lstm_fwd_hardsigmoid_activation.prototxt"));
+
+    auto test_case = ngraph::test::NgraphTestCase(function, "${BACKEND_NAME}");
+
+    // X
+    test_case.add_input<float>({-0.455351f, -0.276391f, -0.185934f, -0.269585f});
+    // W
+    test_case.add_input<float>({-0.494659f, 0.0453352f, -0.487793f, 0.417264f, -0.0175329f,
+         0.489074f, -0.446013f, 0.414029f, -0.0091708f, -0.255364f, -0.106952f, -0.266717f,
+        -0.0888852f, -0.428709f, -0.283349f, 0.208792f});
+    // R
+    test_case.add_input<float>({0.146626f, -0.0620289f, -0.0815302f, 0.100482f, -0.219535f,
+        -0.306635f, -0.28515f, -0.314112f, -0.228172f, 0.405972f, 0.31576f, 0.281487f, -0.394864f,
+         0.42111f, -0.386624f, -0.390225f});
+
+    // Y
+    test_case.add_expected_output<float>(
+        Shape{2, 1, 1, 2}, {0.09086666f, 0.04378549f, 0.12914555f, 0.00257774f});
+    // Y_h
+    test_case.add_expected_output<float>(Shape{1, 1, 2}, {0.12914555f, 0.00257774f});
+    // Y_c
+    test_case.add_expected_output<float>(Shape{1, 1, 2}, {0.19017234f, 0.00356848f});
+
+    test_case.run();
 }
