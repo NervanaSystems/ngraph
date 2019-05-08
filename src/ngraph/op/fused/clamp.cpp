4576c7bac8d574e2fdc2a561fb9a9e128a1b4789
tomdol <tomasz.dolbniak@intel.com>
2019-05-08 18:13:24 +0200

    Validate the min and max params for Clamp

--- src/ngraph/op/fused/clamp.cpp
+++ src/ngraph/op/fused/clamp.cpp
@@ -27,16 +27,18 @@ op::Clamp::Clamp(const shared_ptr<Node>& data, const double min, const double ma
     , m_min{min}
     , m_max{max}
 {
+    NODE_VALIDATION_CHECK(
+        this, min < max, "The 'min' parameter has to be less than 'max' for Clamp.");
     constructor_validate_and_infer_types();
 }
 
 NodeVector op::Clamp::decompose_op() const
 {
-    auto data = get_argument(0);
-    auto data_shape = data->get_shape();
+    const auto data = get_argument(0);
+    const auto data_shape = data->get_shape();
 
-    auto clamp_min = builder::make_constant<float>(data->get_element_type(), data_shape, m_min);
-    auto clamp_max = builder::make_constant<float>(data->get_element_type(), data_shape, m_max);
+    const auto clamp_min = builder::make_constant(data->get_element_type(), data_shape, m_min);
+    const auto clamp_max = builder::make_constant(data->get_element_type(), data_shape, m_max);
 
     return {std::make_shared<ngraph::op::Minimum>(
         clamp_max, std::make_shared<ngraph::op::Maximum>(clamp_min, data))};
@@ -44,9 +46,10 @@ NodeVector op::Clamp::decompose_op() const
 
 shared_ptr<Node> op::Clamp::copy_with_new_args(const NodeVector& new_args) const
 {
-    NGRAPH_CHECK(new_args.size() == 1,
-                 "Expected 1 element in new_args for the Clamp op but got ",
-                 new_args.size());
+    NODE_VALIDATION_CHECK(this,
+                          new_args.size() == 1,
+                          "Expected 1 element in new_args for the Clamp op but got ",
+                          new_args.size());
 
     return make_shared<Clamp>(new_args.at(0), m_min, m_max);
 }
