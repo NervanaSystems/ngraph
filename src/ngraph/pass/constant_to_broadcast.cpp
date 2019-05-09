7c337e5ddbac7b31337eb0104a94e4e44ff2d8a7
Robert Kimball <robert.kimball@intel.com>
2019-05-09 16:05:06 -0700

    Store Constant arrays where data is constant as a single value (#2880)
    * wip
    
    * Add support for storing constant array of constant values as a single values that is automatically broadcast on deserialize
    
    * revert some changes to serializer.cpp
    
    * fix all_close_f to support nan and inf to allow for unit test
    
    * update unit tests to pass for all_close_f update
    
    * fix bug with i64
    
    * address compile issues?
    
    * change function name to be more accurate
    
    * fix compiler error

--- src/ngraph/pass/constant_to_broadcast.cpp
+++ src/ngraph/pass/constant_to_broadcast.cpp
@@ -22,40 +22,6 @@
 using namespace std;
 using namespace ngraph;
 
-template <typename T>
-static bool is_data_constant(shared_ptr<op::Constant> constant)
-{
-    const size_t size = shape_size(constant->get_shape());
-    bool data_is_constant = true;
-    if (size > 0)
-    {
-        const T* data = constant->get_data_ptr<T>();
-        const T compare = data[0];
-        for (size_t i = 1; i < size; i++)
-        {
-            if (data[i] != compare)
-            {
-                data_is_constant = false;
-                break;
-            }
-        }
-        if (data_is_constant)
-        {
-            auto scalar_constant = make_shared<op::Constant>(
-                constant->get_element_type(), Shape{}, constant->get_data_ptr());
-            AxisSet broadcast_axes;
-            for (size_t i = 0; i < constant->get_output_shape(0).size(); i++)
-            {
-                broadcast_axes.insert(i);
-            }
-            auto broadcast = make_shared<op::Broadcast>(
-                scalar_constant, constant->get_output_shape(0), broadcast_axes);
-            replace_node(constant, broadcast);
-        }
-    }
-    return data_is_constant;
-}
-
 bool pass::ConstantToBroadcast::run_on_node(shared_ptr<Node> node)
 {
     const size_t minimum_size_of_interest = 32;
@@ -66,38 +32,18 @@ bool pass::ConstantToBroadcast::run_on_node(shared_ptr<Node> node)
         size_t size = shape_size(constant->get_shape());
         if (size > minimum_size_of_interest)
         {
-            switch (constant->get_element_type().get_type_enum())
+            if (constant->are_all_data_elements_bitwise_identical())
             {
-            case element::Type_t::boolean:
-            case element::Type_t::i8:
-            case element::Type_t::u8:
-            {
-                modified = is_data_constant<uint8_t>(constant);
-                break;
-            }
-            case element::Type_t::bf16:
-            case element::Type_t::i16:
-            case element::Type_t::u16:
-            {
-                modified = is_data_constant<uint16_t>(constant);
-                break;
-            }
-            case element::Type_t::f32:
-            case element::Type_t::i32:
-            case element::Type_t::u32:
-            {
-                modified = is_data_constant<uint32_t>(constant);
-                break;
-            }
-            case element::Type_t::f64:
-            case element::Type_t::i64:
-            case element::Type_t::u64:
-            {
-                modified = is_data_constant<uint64_t>(constant);
-                break;
-            }
-            case element::Type_t::undefined:
-            case element::Type_t::dynamic: break;
+                auto scalar_constant = make_shared<op::Constant>(
+                    constant->get_element_type(), Shape{}, constant->get_data_ptr());
+                AxisSet broadcast_axes;
+                for (size_t i = 0; i < constant->get_output_shape(0).size(); i++)
+                {
+                    broadcast_axes.insert(i);
+                }
+                auto broadcast = make_shared<op::Broadcast>(
+                    scalar_constant, constant->get_output_shape(0), broadcast_axes);
+                replace_node(constant, broadcast);
             }
         }
     }
