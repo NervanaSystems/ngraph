9a89bb858db2811d74289150c7186aec8fe8a9dc
Tomasz Socha <tomasz.socha@intel.com>
2019-05-09 14:47:32 +0200

    Add UT for ScaleShift

--- test/backend_fused_op.in.cpp
+++ test/backend_fused_op.in.cpp
@@ -377,3 +377,44 @@ NGRAPH_TEST(${BACKEND_NAME}, gemm_broadcast_input_C)
     test_case.add_expected_output<double>(Shape{3, 4}, vector<double>(12, 7));
     test_case.run();
 }
+
+NGRAPH_TEST(${BACKEND_NAME}, scale_shift_no_broadcast)
+{
+    auto data = make_shared<op::Parameter>(element::f64, Shape{3, 6});
+    auto scale = make_shared<op::Parameter>(element::f64, Shape{3, 6});
+    auto shift = make_shared<op::Parameter>(element::f64, Shape{3, 6});
+
+    auto scale_shift_func = make_shared<op::ScaleShift>(data, scale, shift);
+    auto function = make_shared<Function>(NodeVector{scale_shift_func}, ParameterVector{data, scale, shift});
+    auto test_case = ngraph::test::NgraphTestCase(function, "${BACKEND_NAME}");
+    // Data
+    test_case.add_input<double>(vector<double>(18, 2));
+    // Scale
+    test_case.add_input<double>(vector<double>(18, 2));
+    // Shift
+    test_case.add_input<double>(vector<double>(18, 2));
+    //output
+    test_case.add_expected_output<double>(Shape{3, 6}, vector<double>(18, 6));
+    test_case.run();
+}
+
+NGRAPH_TEST(${BACKEND_NAME}, scale_shift)
+{
+    auto data = make_shared<op::Parameter>(element::f64, Shape{3, 6});
+    auto scale = make_shared<op::Parameter>(element::f64, Shape{3, 6});
+    auto shift = make_shared<op::Parameter>(element::f64, Shape{});
+
+    auto scale_shift_func = make_shared<op::ScaleShift>(data, scale, shift);
+    auto function =
+        make_shared<Function>(NodeVector{scale_shift_func}, ParameterVector{data, scale, shift});
+    auto test_case = ngraph::test::NgraphTestCase(function, "${BACKEND_NAME}");
+    // Data
+    test_case.add_input<double>(vector<double>(18, 2));
+    // Scale
+    test_case.add_input<double>(vector<double>(18, 2));
+    // Shift
+    test_case.add_input<double>(vector<double>{2});
+    //output
+    test_case.add_expected_output<double>(Shape{3, 6}, vector<double>(18, 6));
+    test_case.run();
+}
