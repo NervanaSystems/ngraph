fff927b729a46be4c04c80f75913fbf450980d78
Scott Cyphers <scott.cyphers@intel.com>
2019-05-09 17:22:40 -0700

    Migrate #2864

--- src/ngraph/pass/manager.cpp
+++ src/ngraph/pass/manager.cpp
@@ -111,7 +111,7 @@ void pass::Manager::run_passes(shared_ptr<Function> func, bool transitive)
                 // This checks is to skip the graph optimization when the graph pass relies on static shape
                 // but the function state is dynamic.
                 // we update the function dynamic state only if we run the graph pass successfully.
-                if (function_pass->get_property(pass::PassProperty::REQUIRE_STATIC_SHAPE) &&
+                if (function_pass->get_property(PassProperty::REQUIRE_STATIC_SHAPE) &&
                     f_pair.second)
                 {
                     continue;
@@ -136,7 +136,7 @@ void pass::Manager::run_passes(shared_ptr<Function> func, bool transitive)
             for (auto f_pair : fs)
             {
                 shared_ptr<Function> f = f_pair.first;
-                if (call_graph_pass->get_property(pass::PassProperty::REQUIRE_STATIC_SHAPE) &&
+                if (call_graph_pass->get_property(PassProperty::REQUIRE_STATIC_SHAPE) &&
                     f_pair.second)
                 {
                     continue;
@@ -160,19 +160,18 @@ void pass::Manager::run_passes(shared_ptr<Function> func, bool transitive)
             std::string index_str = std::to_string(index);
             index_str = std::string(num_digits_in_pass_index - index_str.length(), '0') + index_str;
             auto base_filename = f_array.at(0)->get_name() + std::string("_") + index_str +
-                                 std::string("_") + m_pass_names.at(index) + std::string(".");
+                                 std::string("_") + m_pass_names.at(index);
 
             if (m_visualize)
             {
-                pass::VisualizeTree vt(base_filename + pass::VisualizeTree::get_file_ext());
+                pass::VisualizeTree vt(base_filename);
                 vt.set_ops_to_details(get_state().get_visualize_tree_ops_map());
                 vt.run_on_module(f_array);
             }
 
             if (m_serialize)
             {
-                // no "." in the extension
-                pass::Serialization st(base_filename + "json");
+                pass::Serialization st(base_filename + ".json");
                 st.run_on_module(f_array);
             }
         }
