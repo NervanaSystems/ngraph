fb8e44add8c1ce8e29880249e1219d64260561ba
Adam Procter <adam.m.procter@intel.com>
2019-05-10 18:17:14 -0700

    Remove runtime/cpu includes from things outside runtime/cpu (#2910)

--- src/ngraph/runtime/backend.cpp
+++ src/ngraph/runtime/backend.cpp
@@ -19,7 +19,6 @@
 #include "ngraph/file_util.hpp"
 #include "ngraph/runtime/backend.hpp"
 #include "ngraph/runtime/backend_manager.hpp"
-#include "ngraph/runtime/cpu/cpu_tensor_view.hpp"
 #include "ngraph/util.hpp"
 
 using namespace std;
