10fbe4b2d35775c033b7a007a109695386bc210a
Scott Cyphers <scott.cyphers@intel.com>
2019-05-13 16:35:48 -0700

    style

--- src/ngraph/runtime/plaidml/plaidml_backend.cpp
+++ src/ngraph/runtime/plaidml/plaidml_backend.cpp
@@ -31,8 +31,7 @@ ngraph::runtime::plaidml::PlaidML_Backend::PlaidML_Backend(const char* configura
 std::shared_ptr<ngraph::runtime::Tensor> ngraph::runtime::plaidml::PlaidML_Backend::create_tensor(
     const ngraph::element::Type& element_type, const ngraph::Shape& shape)
 {
-    return std::make_shared<PlaidML_Tensor>(
-        &m_config, element_type, shape, "direct_data", nullptr);
+    return std::make_shared<PlaidML_Tensor>(&m_config, element_type, shape, "direct_data", nullptr);
 }
 
 std::shared_ptr<ngraph::runtime::Tensor> ngraph::runtime::plaidml::PlaidML_Backend::create_tensor(
