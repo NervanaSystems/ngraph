859a0fedefe69d0369781d7a3973f2d231c7c19e
Nishant Patel <nishant.b.patel@intel.com>
2019-05-17 13:23:51 -0700

    Generic Dot & builders for QLinearMatmul  (#2857)
    * Add builders for QLinearMatmul for onnxruntime
    
    * Generic Dot
    
    * Change the onnx bridge and address PR feedback
    
    * Fix date
    
    * Fix CI failure
    
    * Change varibale filter to input1
    
    * const & reference
    
    * update branch
    
    * Comment
    
    * Introduce QuantizedMatmul
    
    * change pattern
    
    * QDot tests passing
    
    * style
    
    * PR feedback
    
    * Fix pattern
    
    * style

---
+++ src/ngraph/runtime/cpu/op/quantized_matmul.hpp
@@ -0,0 +1,50 @@
+//*****************************************************************************
+// Copyright 2018-2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+
+#pragma once
+
+#include <utility>
+
+#include "ngraph/op/op.hpp"
+
+namespace ngraph
+{
+    namespace op
+    {
+        class QuantizedMatmul : public Op
+        {
+        public:
+            QuantizedMatmul(const std::shared_ptr<Node>& data,
+                            const std::shared_ptr<Node>& weights,
+                            const std::shared_ptr<Node>& scale,
+                            bool requantize = true,
+                            bool with_relu = false);
+
+            virtual std::shared_ptr<Node>
+                copy_with_new_args(const NodeVector& new_args) const override
+            {
+                check_new_args_count(this, new_args);
+                return std::make_shared<QuantizedMatmul>(
+                    new_args.at(0), new_args.at(1), new_args.at(2), m_requantize, m_with_relu);
+            }
+            bool with_relu() const { return m_with_relu; }
+            bool requantize() const { return m_requantize; }
+        protected:
+            bool m_requantize;
+            bool m_with_relu;
+        };
+    } // namespace op
+} // namespace ngraph
