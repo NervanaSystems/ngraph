4ec94acc11084a5d53418f565529310fa584899a
Jayaram Bobba <jayaram.bobba@intel.com>
2019-05-24 13:37:17 -0700

    Added accessor methods for layer op attributes (#2964)
    * Added accessor methods for layer op attributes
    
    * style fixes and addressed PR feedback

--- src/ngraph/op/experimental/layers/proposal.hpp
+++ src/ngraph/op/experimental/layers/proposal.hpp
@@ -22,6 +22,38 @@ namespace ngraph
 {
     namespace op
     {
+        // base_size       Anchor sizes
+        // pre_nms_topn    Number of boxes before nms
+        // post_nms_topn   Number of boxes after nms
+        // nms_threshold   Threshold for nms
+        // feature_stride  Feature stride
+        // min_size        Minimum box size
+        // anchor_ratios   Ratios for anchor generation
+        // anchor_scales   Scales for anchor generation
+        // clip_before_nms Clip before NMs
+        // clip_after_nms  Clip after NMs
+        // normalize       Normalize boxes to [0,1]
+        // box_size_scale  Scale factor for scaling box size logits
+        // box_coord_scale Scale factor for scaling box coordiate logits
+        // algo            Calculation algorithm to use
+        struct ProposalAttrs
+        {
+            size_t base_size;
+            size_t pre_nms_topn;
+            size_t post_nms_topn;
+            float nms_threshold = 0.0f;
+            size_t feature_stride = 1;
+            size_t min_size = 1;
+            std::vector<float> anchor_ratios;
+            std::vector<float> anchor_scales;
+            bool clip_before_nms = false;
+            bool clip_after_nms = false;
+            bool normalize = false;
+            float box_size_scale = 1.0f;
+            float box_coord_scale = 1.0f;
+            std::string algo;
+        };
+
         class Proposal : public Op
         {
         public:
@@ -30,58 +62,20 @@ namespace ngraph
             /// \param class_probs     Class probability scores
             /// \param class_logits    Class prediction logits
             /// \param image_shape     Shape of image
-            /// \param base_size       Anchor sizes
-            /// \param pre_nms_topn    Number of boxes before nms
-            /// \param post_nms_topn   Number of boxes after nms
-            /// \param nms_threshold   Threshold for nms
-            /// \param feature_stride  Feature stride
-            /// \param min_size        Minimum box size
-            /// \param anchor_ratios   Ratios for anchor generation
-            /// \param anchor_scales   Scales for anchor generation
-            /// \param clip_before_nms Clip before NMs
-            /// \param clip_after_nms  Clip after NMs
-            /// \param normalize       Normalize boxes to [0,1]
-            /// \param box_size_scale  Scale factor for scaling box size logits
-            /// \param box_coord_scale Scale factor for scaling box coordiate logits
-            /// \param algo            Calculation algorithm to use
+            /// \param attrs           Proposal op attributes
             Proposal(const std::shared_ptr<Node>& class_probs,
                      const std::shared_ptr<Node>& class_logits,
                      const std::shared_ptr<Node>& image_shape,
-                     const size_t base_size,
-                     const size_t pre_nms_topn,
-                     const size_t post_nms_topn,
-                     const float nms_threshold,
-                     const size_t feature_stride,
-                     const size_t min_size,
-                     const std::vector<float>& anchor_ratios,
-                     const std::vector<float>& anchor_scales,
-                     const bool clip_before_nms,
-                     const bool clip_after_nms,
-                     const bool normalize,
-                     const float box_size_scale,
-                     const float box_coord_scale,
-                     const std::string& algo);
+                     const ProposalAttrs& attrs);
 
             void validate_and_infer_types() override;
 
             virtual std::shared_ptr<Node>
                 copy_with_new_args(const NodeVector& new_args) const override;
 
+            const ProposalAttrs& get_attrs() const { return m_attrs; }
         private:
-            size_t m_base_size;
-            size_t m_pre_nms_topn;
-            size_t m_post_nms_topn;
-            float m_nms_threshold;
-            size_t m_feature_stride;
-            size_t m_min_size;
-            std::vector<float> m_anchor_ratios;
-            std::vector<float> m_anchor_scales;
-            bool m_clip_before_nms;
-            bool m_clip_after_nms;
-            bool m_normalize;
-            float m_box_size_scale;
-            float m_box_coord_scale;
-            std::string m_algo;
+            ProposalAttrs m_attrs;
         };
     }
 }
