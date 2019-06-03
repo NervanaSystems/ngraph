189198daa0f962bd10955f5f4a96ad579b7377ca
Amy Zhuang <amyzhuang97@gmail.com>
2019-06-03 14:54:02 -0700

    Rename variables.
    Fix bugs.
    
    Use helper function.
    
    Add one unit test.

--- src/ngraph/runtime/cpu/kernel/scatter_add.hpp
+++ src/ngraph/runtime/cpu/kernel/scatter_add.hpp
@@ -31,22 +31,25 @@ namespace ngraph
         {
             namespace kernel
             {
+                static void
+                    get_leading_indices(const Shape& shape, int index, std::vector<int>& indices)
+                {
+                    auto rank = shape.size();
+                    std::vector<int> partial_sum(rank);
+
+                    partial_sum[rank - 1] = 1;
+                    for (int j = rank - 2; j >= 0; j--)
+                    {
+                        partial_sum[j] = partial_sum[j + 1] * shape[j + 1];
+                    }
+                    for (int j = 0; j < rank; j++)
+                    {
+                        indices[j] = index / partial_sum[j];
+                        index = index % partial_sum[j];
+                    }
+                }
+
                 // ScatterAdd is to update bunch of slices of the inputs. The rank of slice is 1 less than the rank of the inputs.
-
-                // For Eigen slice op, both parameters (offsets and extents) need to have the same rank.
-                // Here *_indices and *_slice_dims have the same rank.
-                // Suppose the slice would have shape {3, 3} ignoring the leading 1s.
-                // And if the shape of *_indices has zeros at position 0, ..., rank - 1 - 2(rank of slice without leading 1s),
-                // the shape of *_slice_dimes should have ones at the same position.
-                // These works: {1, 1, 0, 0} and {1, 1, 3, 3}
-                //              {0, 0, 0, 0} and {1, 1, 3, 3}
-                //              {0, 0, 0} and {1, 3, 3}
-                //              {0, 0} and {3, 3}
-                // This does not work: {0, 0, 0, 0} and {1, 0, 3, 3}
-                // Change 0 to 1 at those positions:
-                // For in_slice_dims, we only need to check the its shape[0] since its rank is always the rank of slice without leading 1s + 1.
-                // For updates_slice_dims, we need to check its shape[0], ..., shape[rank of indices - 1] if indices is not scalar.
-
                 template <typename ElementType,
                           typename IndicesType,
                           unsigned int Rank1,
@@ -60,19 +63,22 @@ namespace ngraph
                                  const Shape& updates_shape,
                                  int arena)
                 {
-                    Eigen::array<Eigen::Index, Rank1> in_dims, in_slice_dims, in_indices;
-                    Eigen::array<Eigen::Index, Rank2> updates_dims, updates_slice_dims,
-                        updates_indices;
+                    // For Eigen slice op, both parameters (offsets and extents) need to have the same rank.
+                    // Here *_offsets and *_extents have the same rank.
+                    Eigen::array<Eigen::Index, Rank1> in_dims, in_extents, in_offsets;
+                    Eigen::array<Eigen::Index, Rank2> updates_dims, updates_extents,
+                        updates_offsets;
 
                     for (int i = 0; i < Rank1; i++)
                     {
-                        in_slice_dims[i] = in_dims[i] = inputs_shape[i];
-                        in_indices[i] = 0;
+                        in_extents[i] = in_dims[i] = inputs_shape[i];
+                        in_offsets[i] = 0;
                     }
+                    in_extents[0] = 1;
                     for (int i = 0; i < Rank2; i++)
                     {
-                        updates_slice_dims[i] = updates_dims[i] = updates_shape[i];
-                        updates_indices[i] = 0;
+                        updates_extents[i] = updates_dims[i] = updates_shape[i];
+                        updates_offsets[i] = 0;
                     }
 
                     Eigen::TensorMap<Eigen::Tensor<ElementType, Rank1, Eigen::RowMajor>> out(
@@ -90,54 +96,31 @@ namespace ngraph
                     }
 
                     auto indices_ptr = static_cast<IndicesType*>(indices);
-                    auto size = indices_shape.size();
-                    if (size == 0)
+                    auto indices_rank = indices_shape.size();
+                    if (indices_rank == 0)
                     {
-                        in_slice_dims[0] = in_indices[0] = indices_ptr[0];
-                        // change to 1 if 0.
-                        in_slice_dims[0] = in_slice_dims[0] == 0 ? 1 : in_slice_dims[0];
-
-                        out.slice(in_indices, in_slice_dims)
+                        in_offsets[0] = indices_ptr[0];
+                        out.slice(in_offsets, in_extents)
                             .device(ngraph::runtime::cpu::executor::GetCPUExecutor().get_device(
-                                arena)) =
-                            out.slice(in_indices, in_slice_dims) +
-                            up.slice(updates_indices, updates_slice_dims).reshape(in_slice_dims);
+                                arena)) = out.slice(in_offsets, in_extents) +
+                                          up.slice(updates_offsets, updates_extents);
                     }
                     else
                     {
-                        // This is used to calculate the leading indices for updates_indices and updates_slice_dims
-                        // when size > 1.
-                        std::vector<int> ele_counts(size);
-                        ele_counts[size - 1] = 0;
-                        if (size > 1)
-                        {
-                            ele_counts[size - 2] = indices_shape[size - 1];
-                        }
-                        for (int j = size - 3; j >= 0; j--)
-                        {
-                            ele_counts[j] = indices_shape[j + 2] * indices_shape[j + 1];
-                        }
+                        std::vector<int> leading_indices(indices_rank);
                         for (int i = 0; i < shape_size(indices_shape); i++)
                         {
-                            in_slice_dims[0] = in_indices[0] = indices_ptr[i];
-                            int k = i;
-                            for (int j = 0; j < size - 1; j++)
+                            in_offsets[0] = indices_ptr[i];
+                            get_leading_indices(indices_shape, i, leading_indices);
+                            for (int j = 0; j < indices_rank; j++)
                             {
-                                updates_slice_dims[j] = updates_indices[j] = k / ele_counts[j];
-                                // change to 1 if 0.
-                                updates_slice_dims[j] =
-                                    updates_slice_dims[j] == 0 ? 1 : updates_slice_dims[j];
-                                k = k % ele_counts[j];
+                                updates_extents[j] = 1;
+                                updates_offsets[j] = leading_indices[j];
                             }
-                            updates_indices[size - 1] = k;
-                            in_slice_dims[0] = in_slice_dims[0] == 0 ? 1 : in_slice_dims[0];
-                            // change to 1 if 0.
-                            updates_slice_dims[size - 1] = k == 0 ? 1 : k;
-                            out.slice(in_indices, in_slice_dims)
+                            out.slice(in_offsets, in_extents)
                                 .device(ngraph::runtime::cpu::executor::GetCPUExecutor().get_device(
-                                    arena)) = out.slice(in_indices, in_slice_dims) +
-                                              up.slice(updates_indices, updates_slice_dims)
-                                                  .reshape(in_slice_dims);
+                                    arena)) = out.slice(in_offsets, in_extents) +
+                                              up.slice(updates_offsets, updates_extents);
                         }
                     }
                 }
