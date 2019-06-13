3c46986388c45924d18841b4df171d665b7686b7
Adam Procter <adam.m.procter@intel.com>
2019-06-12 17:29:51 -0700

    Implement DynElimination for DynReplaceSlice

--- src/ngraph/pass/dyn_elimination.hpp
+++ src/ngraph/pass/dyn_elimination.hpp
@@ -30,7 +30,8 @@ namespace ngraph
 
         private:
             void construct_transpose();
-            void construct_dyn_reshape();
+            void construct_dyn_slice();
+            void construct_dyn_replace_slice();
         };
     }
 }
