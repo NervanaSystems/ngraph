5b2b1fa50204829368a5521c9327b3a5810e8529
tomdol <tomasz.dolbniak@intel.com>
2019-06-18 13:07:19 +0200

    Enable INTELGPU backend for python tests

--- python/test/conftest.py
+++ python/test/conftest.py
@@ -19,7 +19,7 @@ import test
 
 def pytest_addoption(parser):
     parser.addoption('--backend', default='INTERPRETER',
-                     choices=['INTERPRETER', 'CPU', 'GPU', 'NNP', 'PlaidML'],
+                     choices=['INTERPRETER', 'CPU', 'GPU', 'NNP', 'PlaidML', 'INTELGPU'],
                      help='Select from available backends')
 
 
@@ -36,6 +36,7 @@ def pytest_collection_modifyitems(config, items):
     nnp_skip = pytest.mark.skip(reason='Skipping test on the NNP backend.')
     interpreter_skip = pytest.mark.skip(reason='Skipping test on the INTERPRETER backend.')
     plaidml_skip = pytest.mark.skip(reason='Skipping test on the PlaidML backend.')
+    igpu_skip = pytest.mark.skip(reason='Skipping test on the INTELGPU backend.')
 
     for item in items:
         if backend_name == 'GPU' and 'skip_on_gpu' in item.keywords:
@@ -48,3 +49,5 @@ def pytest_collection_modifyitems(config, items):
             item.add_marker(interpreter_skip)
         if backend_name == 'PlaidML' and 'skip_on_plaidml' in item.keywords:
             item.add_marker(plaidml_skip)
+        if backend_name == 'INTELGPU' and 'skip_on_intelgpu' in item.keywords:
+            item.add_marker(igpu_skip)
