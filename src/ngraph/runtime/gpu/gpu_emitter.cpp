95eb7c5c89e39d2431a205cb4f484bb0285f6aa3
Adam Procter <adam.m.procter@intel.com>
2019-06-19 16:29:38 -0700

    Add missing include for GPU

--- src/ngraph/runtime/gpu/gpu_emitter.cpp
+++ src/ngraph/runtime/gpu/gpu_emitter.cpp
@@ -72,6 +72,7 @@
 #include "ngraph/op/experimental/quantized_dot.hpp"
 #include "ngraph/op/experimental/quantized_dot_bias.hpp"
 #include "ngraph/op/experimental/quantized_max_pool.hpp"
+#include "ngraph/op/experimental/range.hpp"
 #include "ngraph/op/experimental/shape_of.hpp"
 #include "ngraph/op/experimental/tile.hpp"
 #include "ngraph/op/experimental/transpose.hpp"
