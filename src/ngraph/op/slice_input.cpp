a76901fb8fce9b4a95572b9493001ffaec8ef3e0
Scott Cyphers <scott.cyphers@intel.com>
2019-06-21 17:57:39 -0700

    Add explicit conversion from PartialShape to dimension vector validate and infer types for SliceInput

--- src/ngraph/op/slice_input.cpp
+++ src/ngraph/op/slice_input.cpp
@@ -14,6 +14,8 @@
 // limitations under the License.
 //*****************************************************************************
 
+#include <vector>
+
 #include "ngraph/op/slice_input.hpp"
 
 using namespace std;
@@ -39,7 +41,48 @@ op::SliceInput::SliceInput(const Output<Node>& value,
 
 void op::SliceInput::validate_and_infer_types()
 {
-    validate_and_infer_elementwise_args();
+    // For now, keep things simple; no padding, forwards stepping, etc.
+    NODE_VALIDATION_CHECK(this, 0 <= m_axis, "Negative axis is not supported.");
+    NODE_VALIDATION_CHECK(this, 0 <= m_start, "Negative start is not supported.");
+    NODE_VALIDATION_CHECK(this, 0 <= m_end, "Negative end is not supported.");
+    NODE_VALIDATION_CHECK(this, 1 <= m_part_size, "Part size must be positive.");
+    NODE_VALIDATION_CHECK(this, 1 <= m_stride, "Stride must be positive.");
+
+    PartialShape result_shape;
+    const PartialShape& value_shape = get_input_partial_shape(0);
+    if (value_shape.is_dynamic())
+    {
+        result_shape = PartialShape::dynamic();
+    }
+    else
+    {
+        vector<Dimension> value_dimensions = static_cast<vector<Dimension>>(value_shape);
+        NODE_VALIDATION_CHECK(
+            this, m_axis < value_dimensions.size(), "Axis is not within the input shape.");
+
+        Dimension value_dimension = value_dimensions.at(m_axis);
+        value_dimensions[m_axis] = m_part_size;
+        Dimension sequence_dimension;
+        if (value_dimension.is_dynamic())
+        {
+            // Can't tell how long the sequence is
+            sequence_dimension = Dimension();
+        }
+        else
+        {
+            size_t axis_size = static_cast<size_t>(value_dimension);
+            NODE_VALIDATION_CHECK(
+                this, m_start + m_part_size - 1 <= axis_size, "start part is out of range.");
+            NODE_VALIDATION_CHECK(
+                this, m_end + m_part_size - 1 <= axis_size, "end part is out of range.");
+            size_t number_slices = (m_end - m_start) / m_part_size;
+            sequence_dimension = Dimension(number_slices);
+        }
+        value_dimensions.insert(value_dimensions.begin(), sequence_dimension);
+        result_shape = PartialShape(value_dimensions);
+    }
+    set_output_size(1);
+    set_output_type(0, get_input_element_type(0), result_shape);
 }
 
 shared_ptr<Node> op::SliceInput::copy_with_new_args(const NodeVector& new_args) const
