0e12d5db6c58d971aec9503fb3b3f62a642588a0
jianyinglang <jianying.lang@intel.com>
2019-06-28 16:32:45 -0700

    debug

--- src/ngraph/runtime/cpu/builder/allreduce.cpp
+++ src/ngraph/runtime/cpu/builder/allreduce.cpp
@@ -43,13 +43,14 @@ namespace ngraph
 
                 auto external_function_name = external_function->get_function_name();
                 NGRAPH_DEBUG_PRINT(
-                    "AllReduce Queued[%d]: Function: %s Node: %s %s Size: "
+                    "AllReduce Queued[%d]: Function: %s Node: %s %s %s Size: "
                     "%d",
                     call_seq,
                     external_function_name.c_str(),
                     node->get_name().c_str(),
                     node->get_provenance_tags().size() == 1 ? 
                     (*(node->get_provenance_tags()).begin()).c_str() :
+                    node->get_friendly_name().c_str(),
                     node->get_friendly_name().c_str(),
                     count);
 
