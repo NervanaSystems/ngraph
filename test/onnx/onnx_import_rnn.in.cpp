f9f951bfc314ac3251d2ac5a4b6a1a03e141bf6d
Adam Rogowiec <adam.rogowiec@intel.com>
2019-07-09 21:50:15 +0200

    UT for LSTM with sequence_lens shorter than input sequence size.

--- test/onnx/onnx_import_rnn.in.cpp
+++ test/onnx/onnx_import_rnn.in.cpp
@@ -249,3 +249,64 @@ NGRAPH_TEST(onnx_${BACKEND_NAME}, model_lstm_fwd_large_batch_no_clip)
 
     test_case.run();
 }
+
+NGRAPH_TEST(onnx_${BACKEND_NAME}, model_lstm_bdir_short_input_seq)
+{
+    auto function = onnx_import::import_onnx_model(
+        file_util::path_join(SERIALIZED_ZOO, "onnx/lstm_bdir_short_input_seq.prototxt"));
+
+    auto test_case = ngraph::test::NgraphTestCase(function, "${BACKEND_NAME}");
+
+    // X
+    test_case.add_input<float>({-0.455351f, -0.276391f, -0.185934f, -0.269585f});
+    // W
+    test_case.add_input<float>(
+        {-0.494659f,  0.0453352f,  -0.487793f, 0.417264f,   -0.0175329f, 0.489074f,   -0.446013f,
+         0.414029f,   -0.0091708f, -0.255364f, -0.106952f,  -0.266717f,  -0.0888852f, -0.428709f,
+         -0.283349f,  0.208792f,   -0.494659f, 0.0453352f,  -0.487793f,  0.417264f,   -0.0175329f,
+         0.489074f,   -0.446013f,  0.414029f,  -0.0091708f, -0.255364f,  -0.106952f,  -0.266717f,
+         -0.0888852f, -0.428709f,  -0.283349f, 0.208792f});
+    // R
+    test_case.add_input<float>(
+        {0.146626f,  -0.0620289f, -0.0815302f, 0.100482f,   -0.219535f,  -0.306635f, -0.28515f,
+         -0.314112f, -0.228172f,  0.405972f,   0.31576f,    0.281487f,   -0.394864f, 0.42111f,
+         -0.386624f, -0.390225f,  0.146626f,   -0.0620289f, -0.0815302f, 0.100482f,  -0.219535f,
+         -0.306635f, -0.28515f,   -0.314112f,  -0.228172f,  0.405972f,   0.31576f,   0.281487f,
+         -0.394864f, 0.42111f,    -0.386624f,  -0.390225f});
+    // B
+    test_case.add_input<float>(
+        {0.381619f, 0.0323954f, -0.14449f, 0.420804f, -0.258721f, 0.45056f, -0.250755f, 0.0967895f,
+         0.0f,      0.0f,       0.0f,      0.0f,      0.0f,       0.0f,     0.0f,       0.0f,
+         0.381619f, 0.0323954f, -0.14449f, 0.420804f, -0.258721f, 0.45056f, -0.250755f, 0.0967895f,
+         0.0f,      0.0f,       0.0f,      0.0f,      0.0f,       0.0f,     0.0f,       0.0f});
+    // sequence_lens
+    test_case.add_input<int>({1});
+    // initial_h
+    test_case.add_input<float>({0.0f, 0.0f, -0.0306872f, 0.028035f});
+    // initial_c
+    test_case.add_input<float>({0.0f, 0.0f, -0.07243599f, 0.0467052f});
+    // P
+    test_case.add_input<float>({0.2345f,
+                                0.5235f,
+                                0.4378f,
+                                0.3475f,
+                                0.8927f,
+                                0.3456f,
+                                0.2345f,
+                                0.5235f,
+                                0.4378f,
+                                0.3475f,
+                                0.8927f,
+                                0.3456f});
+
+    // Y
+    test_case.add_expected_output<float>(
+        Shape{2, 2, 1, 2},
+        {-0.0251062f, 0.0561262f, -0.0318928f, 0.0762679f, 0.0f, 0.0f, 0.0f, 0.0f});
+    // Y_h
+    test_case.add_expected_output<float>(Shape{2, 1, 2},
+                                         {-0.0251062f, 0.0561262f, -0.0318928f, 0.0762679f});
+
+    test_case.set_tolerance(DEFAULT_FLOAT_TOLERANCE_BITS + 3);
+    test_case.run();
+}
