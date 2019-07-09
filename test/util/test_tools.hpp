9b3d5732986824c28996903d6eaf3712141757dc
Scott Cyphers <scott.cyphers@intel.com>
2019-07-09 15:25:53 -0700

    Fix top sort

--- test/util/test_tools.hpp
+++ test/util/test_tools.hpp
@@ -25,6 +25,8 @@
 #include <random>
 #include <vector>
 
+#include "gtest/gtest.h"
+
 #include "ngraph/descriptor/layout/tensor_layout.hpp"
 #include "ngraph/file_util.hpp"
 #include "ngraph/log.hpp"
@@ -276,3 +278,5 @@ std::vector<T> read_binary_file(const std::string& path)
     inputs_fs.read(reinterpret_cast<char*>(file_content.data()), size);
     return file_content;
 }
+
+testing::AssertionResult test_ordered_ops(std::shared_ptr<ngraph::Function> f);
