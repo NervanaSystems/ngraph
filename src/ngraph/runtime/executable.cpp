f13257e4115b70c4c5de6a1a17a974979a5f1f93
Robert Kimball <robert.kimball@intel.com>
2019-07-11 09:31:56 -0700

    add comments and new methods if you only want to createe non-pipelined tensors

--- src/ngraph/runtime/executable.cpp
+++ src/ngraph/runtime/executable.cpp
@@ -124,14 +124,24 @@ void runtime::Executable::save(std::ostream& output_stream)
     throw runtime_error("save opertion unimplemented.");
 }
 
-vector<shared_ptr<runtime::Tensor>> runtime::Executable::create_input_tensor(size_t input_number,
+shared_ptr<runtime::Tensor> runtime::Executable::create_input_tensor(size_t input_index)
+{
+    throw runtime_error("create_input_tensor unimplemented");
+}
+
+shared_ptr<runtime::Tensor> runtime::Executable::create_output_tensor(size_t output_index)
+{
+    throw runtime_error("create_output_tensor unimplemented");
+}
+
+vector<shared_ptr<runtime::Tensor>> runtime::Executable::create_input_tensor(size_t input_index,
                                                                  size_t pipeline_depth)
 {
     throw runtime_error("create_input_tensor unimplemented");
 }
 
-vector<shared_ptr<runtime::Tensor>> runtime::Executable::create_output_tensor(size_t input_number,
+vector<shared_ptr<runtime::Tensor>> runtime::Executable::create_output_tensor(size_t output_index,
                                                                   size_t pipeline_depth)
 {
-    throw runtime_error("create_input_tensor unimplemented");
+    throw runtime_error("create_output_tensor unimplemented");
 }
