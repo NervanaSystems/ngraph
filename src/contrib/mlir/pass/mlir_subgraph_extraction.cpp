c8657861202a22cb35f1714f21ef714fd77e144d
pruthvi <pruthvi.gowda.thorehosur.appajigowda@intel.com>
2019-07-16 16:21:45 -0700

    - return after the conditional check

--- src/contrib/mlir/pass/mlir_subgraph_extraction.cpp
+++ src/contrib/mlir/pass/mlir_subgraph_extraction.cpp
@@ -282,27 +282,49 @@ bool MLIRSubgraphExtractionPass::is_supported_mlir_op(std::shared_ptr<Node> node
         {
             return false;
         }
+        else
+        {
+            return true;
+        }
     }
 
     if (TI(ngraph::op::ArgMin) == TI(*node) || TI(ngraph::op::ArgMax) == TI(*node))
     {
         // TODO: Remove this when MLIR has float point cmp support
         if (!node->input(0).get_element_type().is_integral())
+        {
             return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
     if (TI(ngraph::op::Maximum) == TI(*node) || TI(ngraph::op::Minimum) == TI(*node))
     {
         // TODO: Remove this when MLIR has float point cmp support
         if (!node->input(0).get_element_type().is_integral())
+        {
             return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
     if (TI(ngraph::op::Greater) == TI(*node) || TI(ngraph::op::Less) == TI(*node))
     {
         // TODO: Remove this when MLIR has float point cmp support
         if (!node->input(0).get_element_type().is_integral())
+        {
             return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
     // Relu is supported for integer types only until MLIR adds support for lowering !std.CmpF to LLVM dialect
@@ -311,6 +333,10 @@ bool MLIRSubgraphExtractionPass::is_supported_mlir_op(std::shared_ptr<Node> node
         if (!node->get_element_type().is_integral())
         {
             return false;
+        }
+        else
+        {
+            return true;
         }
     }
     return true;
