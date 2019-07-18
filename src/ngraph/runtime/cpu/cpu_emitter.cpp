e7b7fd824d2cf9fd01f22fadc753b5a203f0a851
Gauri1 Deshpande <gauri1.deshpande@intel.com>
2019-07-18 15:08:11 -0700

    Different implementation

--- src/ngraph/runtime/cpu/cpu_emitter.cpp
+++ src/ngraph/runtime/cpu/cpu_emitter.cpp
@@ -4013,39 +4013,54 @@ namespace ngraph
             void CPU_Emitter::EMITTER_DECL(ngraph::op::Dropout)
             {
                 auto dropout = static_cast<const ngraph::op::Dropout*>(node);
-                size_t element_count = out[0].get_size();
-                size_t nthr = ngraph::runtime::cpu::executor::GetCPUExecutor().get_num_cores();
-                size_t chunk_size = (element_count + nthr - 1) / nthr;
+                size_t ncr = ngraph::runtime::cpu::executor::GetCPUExecutor().get_num_cores();
 
                 writer.block_begin();
-                writer << "bool use_seed = " << to_string(dropout->get_use_seed()) << ";\n";
-
-                writer << "std::vector<std::minstd_rand> vmsr(" << to_string(nthr) << ");\n";
-                writer << "if (use_seed)\n";
-                writer << "{\n";
-                writer << "    uint64_t seed = " << to_string(dropout->get_seed()) << ";\n";
-                writer << "    for (size_t i = 0; i < " << to_string(nthr) << "; i++)\n\
-                    {\n\
-                        std::minstd_rand msr;\n\
-                        msr.seed(seed);\n\
-                        msr.discard(i * "
-                       << to_string(chunk_size) << ");\n\
-                        vmsr[i] = msr;\n\
-                    }\n";
-                writer << "}\n";
-
                 writer << "bool training = static_cast<bool>(" << args[1].get_name() << "[0]);\n";
+                writer << "bool use_seed = " << to_string(dropout->get_use_seed()) << ";\n";
+                writer << "int32_t seed = use_seed ? " << to_string(dropout->get_seed())
+                       << " : rand();\n";
                 writer << "double keep_prob = static_cast<double>(" << args[4].get_name()
                        << "[0]);\n";
-                writer << "cpu::kernel::generate_dropout(";
-                writer << "            " << args[0].get_name() << ",\n";
-                writer << "            " << out[0].get_name() << ",\n";
-                writer << "            " << out[1].get_name() << ",\n";
-                writer << "            " << out[0].get_size() << ",\n";
-                writer << "             training,\n";
-                writer << "             keep_prob,\n";
-                writer << "             vmsr,\n";
-                writer << "             use_seed);\n";
+                writer << "size_t count = " << args[0].get_size() << ";\n";
+                writer << "size_t nthr = " << to_string(ncr) << ";\n";
+                //writer << "size_t nthr = " << to_string(ngraph::runtime::cpu::executor::GetCPUExecutor().get_num_cores()) << ";\n";
+                writer << "size_t chunk_size = (count + nthr - 1) / nthr;\n";
+                writer << "std::vector<std::minstd_rand> vmsr(nthr);\n";
+                writer << "for (size_t i = 0; i < nthr; i++)\n\
+                {\n\
+                    std::minstd_rand msr;\n\
+                    msr.seed(seed+i);\n\
+                    vmsr[i] = msr;\n\
+                }\n";
+
+                writer << "double dropout_prob = 1 - keep_prob;\n";
+                writer << "std::uniform_real_distribution<> gen(0, 1);\n";
+                writer << "#pragma omp parallel num_threads(nthr)\n";
+                writer << "{\n";
+                writer << "size_t tid = omp_get_thread_num();\n";
+                writer << "std::minstd_rand msr;\n msr.seed(seed+tid);\n";
+                writer << "size_t idx_start = tid * chunk_size;\n";
+                writer << "size_t idx_end = std::min(idx_start + chunk_size, count);\n";
+                writer << "for (size_t i = idx_start; i < idx_end; i++)\n";
+                writer << "{\n";
+                writer << "    //out[i] = training ? static_cast<T>(bd(gen)) : "
+                          "static_cast<float>(1);\n";
+                writer << "    //out0[i] = training ? input[i] : static_cast<float>(1);\n";
+                writer << "    if (static_cast<float>(gen(msr)) < dropout_prob)\n";
+                writer << "    {\n";
+                writer << "        " << out[0].get_name() << "[i] = 0;\n";
+                writer << "        " << out[1].get_name() << "[i] = 0;\n";
+                writer << "    }\n";
+                writer << "    else\n";
+                writer << "    {\n";
+                writer << "        " << out[1].get_name() << "[i] = 1;\n";
+                writer << "        " << out[0].get_name() << "[i] = " << args[0].get_name()
+                       << "[i] / static_cast<float>(keep_prob);\n";
+                writer << "    }\n";
+                writer << "}\n"; // for loop ends
+                writer << "}\n"; //#pragma ends
+
                 writer.block_end();
             }
 
