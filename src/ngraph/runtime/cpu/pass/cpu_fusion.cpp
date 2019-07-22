9d0f1cde75f8b995c09f0ade23b1e3e8577f4af2
nishant.b.patel <nishant.b.patel@intel.com>
2019-07-22 10:07:03 -0700

    Change fusion pattern

--- src/ngraph/runtime/cpu/pass/cpu_fusion.cpp
+++ src/ngraph/runtime/cpu/pass/cpu_fusion.cpp
@@ -2410,7 +2410,7 @@ void ngraph::runtime::cpu::pass::CPUQuantFusion::construct_quantized_matmul()
         }
 
         auto reshape_input1 = std::make_shared<op::Reshape>(
-            input_1, AxisVector{0, 1}, Shape{input_1->get_shape()[1], input_1->get_shape()[0]});
+            input_1, AxisVector{1, 0}, Shape{input_1->get_shape()[1], input_1->get_shape()[0]});
         auto qmatmul = std::make_shared<ngraph::op::QuantizedMatmul>(
             input_0, reshape_input1, scale_new, qdot->get_output_type());
 
