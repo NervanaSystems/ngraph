52ffb663d0ae842372b5f2e4d3f11fc0b0b2883f
Adam Procter <adam.m.procter@intel.com>
2019-08-07 17:08:03 -0700

    Dynamic version of builder::flatten

--- src/ngraph/ngraph.hpp
+++ src/ngraph/ngraph.hpp
@@ -47,6 +47,7 @@
 #include "ngraph/builder/numpy_transpose.hpp"
 #include "ngraph/builder/quantized_conv_builder.hpp"
 #include "ngraph/builder/reduce_ops.hpp"
+#include "ngraph/builder/reshape.hpp"
 #include "ngraph/builder/tensor_mask.hpp"
 #include "ngraph/coordinate_transform.hpp"
 #include "ngraph/descriptor/input.hpp"
