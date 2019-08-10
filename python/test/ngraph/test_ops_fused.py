9611ceb98bd119e3a4f025abfbcd20a42be5f871
Ewa21 <ewa.tusien@intel.com>
2019-08-10 15:00:38 +0200

    [Py] Undo removed test.

--- python/test/ngraph/test_ops_fused.py
+++ python/test/ngraph/test_ops_fused.py
@@ -69,6 +69,33 @@ def test_elu_operator_with_scalar():
     assert np.allclose(result, expected)
 
 
+def test_depth_to_space():
+    runtime = get_runtime()
+
+    data_value = np.array([[[[0, 1, 2],
+                            [3, 4, 5]],
+                            [[6, 7, 8],
+                            [9, 10, 11]],
+                            [[12, 13, 14],
+                            [15, 16, 17]],
+                            [[18, 19, 20],
+                            [21, 22, 23]]]], dtype=np.float32)
+    block_size = np.float32(2)
+
+    data_shape = [1, 4, 2, 3]
+    parameter_data = ng.parameter(data_shape, name='Data', dtype=np.float32)
+
+    model = ng.depth_to_space(parameter_data, block_size)
+    computation = runtime.computation(model, parameter_data)
+
+    result = computation(data_value)
+    expected = np.array([[[[0, 6, 1, 7, 2, 8],
+                        [12, 18, 13, 19, 14, 20],
+                        [3, 9, 4, 10, 5, 11],
+                        [15, 21, 16, 22, 17, 23]]]], dtype=np.float32)
+    assert np.allclose(result, expected)
+
+
 def test_gemm_operator():
     runtime = get_runtime()
 
