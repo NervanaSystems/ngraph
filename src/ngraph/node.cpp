802bca81d1cc342f3578968029d063c626315a18
Adam Procter <adam.m.procter@intel.com>
2019-08-19 14:46:32 -0700

    Add .clang-format with comment wrap to src/ngraph (#3461)
    * Plant an updated .clang-format in src/ngraph, with overrides in each subdir thereof
    
    * Whoops, forgot to commit the actual .clang-formats
    
    * Remove src/ngraph/type/.clang-format (it was having no effect)
    
    * Remove src/ngraph/distributed/.clang-format (it was having no effect)
    
    * Remove src/ngraph/codegen/.clang-format (only one file was affected, so it's a wash)
    
    * Remove src/ngraph/autodiff/.clang-format (only one file was affected, so it's a wash)
    
    * Un-relax comment wrapping in src/ngraph/state
    
    * Revert "Un-relax comment wrapping in src/ngraph/state"
    
    This reverts commit 41fc50fb92bffb7f5aca4126eb1267f00dcca727.

--- src/ngraph/node.cpp
+++ src/ngraph/node.cpp
@@ -67,7 +67,8 @@ Node::~Node()
     {
         if (input.has_output())
         {
-            // This test adds 1 to the actual count, so a count of 2 means this input is the only reference to the node.
+            // This test adds 1 to the actual count, so a count of 2 means this input is the only
+            // reference to the node.
             if (input.get_output().get_node().use_count() == 2)
             {
                 // Don't want to trigger a deep recursive delete
@@ -122,7 +123,8 @@ void Node::safe_delete(NodeVector& nodes, bool recurse)
     {
         if (input.has_output())
         {
-            // This test adds 1 to the actual count, so a count of 2 means this input is the only reference to the node.
+            // This test adds 1 to the actual count, so a count of 2 means this input is the only
+            // reference to the node.
             auto node = input.get_output().get_node();
             if (node.use_count() == 2)
             {
