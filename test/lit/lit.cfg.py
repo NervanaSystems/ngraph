fee4416ce332ef1ef86f386fc5a1c5833ef00ec3
Diego Caballero <diego.caballero@intel.com>
2019-08-26 10:33:46 -0700

    Progress on LIT test enablement

--- test/lit/lit.cfg.py
+++ test/lit/lit.cfg.py
@@ -1,53 +1,44 @@
-# -*- Python -*-
-
-import os
-import platform
-import re
-import subprocess
-import tempfile
+#*****************************************************************************
+# Copyright 2017-2019 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#*****************************************************************************
 
 import lit.formats
-import lit.util
-
 from lit.llvm import llvm_config
 from lit.llvm.subst import ToolSubst
-from lit.llvm.subst import FindTool
-
-# Configuration file for the 'lit' test runner.
 
 # name: The name of this test suite.
-config.name = 'NGRAPH'
+config.name = 'NGRAPH MLIR Compiler'
 
 config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
 
-# suffixes: A list of file extensions to treat as test files.
-config.suffixes = ['.ngraph', '.mlir']
-
 # test_source_root: The root path where tests are located.
-config.test_source_root = os.path.dirname(__file__)
-
-llvm_config.with_system_environment(
-    ['HOME', 'INCLUDE', 'LIB', 'TMP', 'TEMP'])
-
-llvm_config.use_default_substitutions()
-
-# excludes: A list of directories to exclude from the testsuite. The 'Inputs'
-# subdirectories contain auxiliary inputs for various tests in their parent
-# directories.
-#config.excludes = ['Inputs', 'CMakeLists.txt', 'README.txt', 'LICENSE.txt']
-
-# test_source_root: The root path where tests are located.
-config.test_source_root = os.path.dirname(__file__)
+config.test_source_root = config.ngraph_mlir_test_dir
 
 # test_exec_root: The root path where tests should be run.
-config.test_exec_root = os.path.join(config.mlir_obj_root, 'test/lit')
+config.test_exec_root = config.ngraph_mlir_test_dir
+
+llvm_config.use_default_substitutions()
 
 # Tweak the PATH to include the tools dir.
 llvm_config.with_environment('PATH', config.llvm_tools_dir, append_path=True)
 
-tool_dirs = [config.mlir_tools_dir, config.llvm_tools_dir]
-tools = [
-    'ngraph-opt', 'mlir-opt', 'mlir-tblgen', 'mlir-translate',
+tool_dirs = [
+    config.mlir_tools_dir, config.llvm_tools_dir
 ]
-
+tool_names = [
+    'mlir-opt', 'mlir-translate'
+]
+tools = [ToolSubst(s, unresolved='ignore') for s in tool_names]
 llvm_config.add_tool_substitutions(tools, tool_dirs)
