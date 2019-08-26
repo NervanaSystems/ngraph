269f33495cf7b7382504e9574eb109fb5bf80c55
Robert Kimball <robert.kimball@intel.com>
2019-08-26 09:54:31 -0700

    Catch a few more I missed

--- src/ngraph/frontend/onnx_import/core/node.cpp
+++ src/ngraph/frontend/onnx_import/core/node.cpp
@@ -148,7 +148,7 @@ namespace ngraph
             return result;
         }
 
-        const std::string& Node::Impl::description() const
+        std::string Node::Impl::description() const
         {
             if (m_description.empty())
             {
@@ -187,7 +187,7 @@ namespace ngraph
         NodeVector Node::get_ng_nodes() const { return m_pimpl->get_ng_nodes(*this); }
         const std::string& Node::domain() const { return m_pimpl->domain(); }
         const std::string& Node::op_type() const { return m_pimpl->op_type(); }
-        const std::string& Node::get_description() const { return m_pimpl->description(); }
+        std::string Node::get_description() const { return m_pimpl->description(); }
         const std::string& Node::get_name() const { return m_pimpl->name(); }
         const std::vector<std::reference_wrapper<const std::string>>& Node::get_output_names() const
         {
