85e608c742b5c710254a934d1fbf2f938d2e3329
gleb.kazantaev <gkazanta@nncv666el.inn.intel.com>
2019-08-28 23:25:34 +0300

    Fixed Proposal output type infer

--- src/ngraph/op/experimental/layers/proposal.cpp
+++ src/ngraph/op/experimental/layers/proposal.cpp
@@ -76,11 +76,11 @@ void op::Proposal::validate_and_infer_types()
             ").");
 
         auto batch_size = class_probs_shape[0];
-        set_output_type(0, element::f32, Shape{batch_size * m_attrs.post_nms_topn, 5});
+        set_output_type(0, get_input_element_type(0), Shape{batch_size * m_attrs.post_nms_topn, 5});
     }
     else
     {
-        set_output_type(0, element::f32, PartialShape::dynamic());
+        set_output_type(0, get_input_element_type(0), PartialShape::dynamic());
     }
 }
 
