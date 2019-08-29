483666472d575103b73b12d462425129c5dbe0af
mbencer <mateusz.bencer@intel.com>
2019-08-29 23:27:39 +0200

    Code review remarks introduced

--- src/ngraph/pass/opset1_transform.cpp
+++ src/ngraph/pass/opset1_transform.cpp
@@ -68,7 +68,7 @@ bool pass::Opset1Transformation::run_on_node(shared_ptr<Node> node)
                  op_version,
                  " found.");
 
-// Not all enumeration values not explicitly handled in switch
+// Not all enumeration values explicitly handled in switch
 #if defined(__clang__)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wswitch"
