d1aeaa8b9c0d7da5a5f68bac5ac120cde27e4299
gaurides <gauri1.deshpande@intel.com>
2019-09-06 13:24:56 -0700

    Fix reshape error for onnx (#3573)
    * Fix reshape error for onnx
    
    * Change to fix CI error
    
    * Removed old comments

--- .ci/jenkins/jenkins-trigger.groovy
+++ .ci/jenkins/jenkins-trigger.groovy
@@ -24,7 +24,7 @@
 // Parameters which ngraph-unittest uses:
 String  PR_URL = CHANGE_URL
 String  PR_COMMIT_AUTHOR = CHANGE_AUTHOR
-String  JENKINS_BRANCH = "ngraph-core-r0.22.0.rc1"
+String  JENKINS_BRANCH = "aslepko/r0.22"
 Integer TIMEOUTTIME = "3600"
 // BRANCH parameter is no loner needed
 // TRIGGER_URL parameter is no longer needed
