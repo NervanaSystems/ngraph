ad5868f34684e7da8d827988f0e2a9216f59146d
nmostafa <nagy.h.mostafa@intel.com>
2019-09-11 16:30:41 -0700

    Remove dump call

--- src/contrib/mlir/compiler/compiler.cpp
+++ src/contrib/mlir/compiler/compiler.cpp
@@ -354,7 +354,6 @@ void MLIRCompiler::lower_ng_dialect()
         NGRAPH_CHECK(false, "Incorrect module after dialect lowering");
     }
 
-    dump_mlir_module("Affine Dialect Conversion");
     optimize();
 
     NGRAPH_CHECK(m_module, "MLIR module is not ready.");
