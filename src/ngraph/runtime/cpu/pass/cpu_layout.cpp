324cbfa28a34085ab8f2c9c80117948142a21553
Scott Cyphers <scott.cyphers@intel.com>
2019-09-12 08:49:16 -0700

    md used in mkldnn1.0

--- src/ngraph/runtime/cpu/pass/cpu_layout.cpp
+++ src/ngraph/runtime/cpu/pass/cpu_layout.cpp
@@ -1879,8 +1879,9 @@ namespace ngraph
                 }
 
                 static bool can_be_rotated(const ngraph::op::Reshape* reshape,
-                                           const mkldnn::memory::desc& /* md */)
+                                           const mkldnn::memory::desc& md)
                 {
+                    (void)md;
                     auto axis_order = reshape->get_input_order();
                     auto input_shape = reshape->get_input_shape(0);
                     auto output_shape = reshape->get_output_shape();
