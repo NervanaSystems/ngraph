b77d90c52efe675c1e182be8a8649eacd66e0577
Adam Procter <adam.m.procter@intel.com>
2019-09-11 17:17:45 -0700

    Simplify SpecParser

--- opgen/opgen/spec/parser.py
+++ opgen/opgen/spec/parser.py
@@ -24,44 +24,14 @@ class SpecParser():
     def __init__(self):
         pass
 
-    def parse_argument(self,j):
-        if isinstance(j,str):
-            j = json.loads(j)
-
-        return OpArgument(**j)
-
-    def parse_result(self,j):
-        if isinstance(j,str):
-            j = json.loads(j)
-
-        return OpResult(**j)
-
-    def parse_attribute(self,j):
-        if isinstance(j,str):
-            j = json.loads(j)
-
-        return OpAttribute(**j)
-
     def parse_class(self,j):
-        if isinstance(j,str):
-            j = json.loads(j)
-
         if 'arguments' in j:
-            args_parsed = []
-            for arg_j in j['arguments']:
-                args_parsed.append(self.parse_argument(arg_j))
-            j['arguments'] = args_parsed
+            j['arguments'] = [OpArgument(**arg) for arg in j['arguments']]
 
         if 'results' in j:
-            results_parsed = []
-            for res_j in j['results']:
-                results_parsed.append(self.parse_result(res_j))
-            j['results'] = results_parsed
+            j['results'] = [OpResult(**res) for res in j['results']]
 
         if 'attributes' in j:
-            attributes_parsed = []
-            for attr_j in j['attributes']:
-                attributes_parsed.append(self.parse_attribute(attr_j))
-            j['attributes'] = attributes_parsed
+            j['attributes'] = [OpAttribute(**attr) for attr in j['attributes']]
 
         return OpClass(**j)
