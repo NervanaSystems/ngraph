20b5d20d8f41a23b641e8ac6afee0849768d9c15
Sang Ik Lee <sang.ik.lee@intel.com>
2019-09-17 17:46:31 -0700

    Add dummy type prop test.

---
+++ test/type_prop/layer_norm.cpp
@@ -0,0 +1,33 @@
+//*****************************************************************************
+// Copyright 2017-2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+
+#include "gtest/gtest.h"
+#include "ngraph/ngraph.hpp"
+#include "util/type_prop.hpp"
+
+using namespace std;
+using namespace ngraph;
+
+TEST(type_prop, layer_norm_dummy_prop)
+{
+    //auto tv0_2_4_param_0 = make_shared<op::Parameter>(element::boolean, Shape{2, 4});
+    //auto tv0_2_4_param_1 = make_shared<op::Parameter>(element::f32, Shape{2, 4});
+    //auto tv0_2_4_param_2 = make_shared<op::Parameter>(element::f32, Shape{2, 4});
+    //auto bc = make_shared<op::Select>(tv0_2_4_param_0, tv0_2_4_param_1, tv0_2_4_param_2);
+    //ASSERT_EQ(bc->get_element_type(), element::f32);
+    //ASSERT_EQ(bc->get_shape(), (Shape{2, 4}));
+}
+
