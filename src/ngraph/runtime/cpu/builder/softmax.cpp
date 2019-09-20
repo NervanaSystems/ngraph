a8720c297c2f85fbd3745dd6db6797be220b0391
Nishant Patel <nishant.b.patel@intel.com>
2019-09-19 23:42:28 -0700

    Fix softmax

--- src/ngraph/runtime/cpu/builder/softmax.cpp
+++ src/ngraph/runtime/cpu/builder/softmax.cpp
@@ -188,9 +188,10 @@ namespace ngraph
                 }
                 NGRAPH_WARN << "Falling back to refernce kernel for softmax " << arg_shape
                             << " over " << axes;
-                std::function<decltype(runtime::cpu::kernel::ref_softmax<double>)> kernel;
-                kernel = runtime::cpu::kernel::ref_softmax<double>;
-                auto functor = [&, arg_shape, axes, arg_buffer_index, out_buffer_index](
+                std::function<decltype(runtime::cpu::kernel::ref_softmax<float>)> kernel;
+                SELECT_KERNEL(
+                    kernel, args[0].get_element_type(), runtime::cpu::kernel::ref_softmax);
+                auto functor = [&, kernel, arg_shape, axes, arg_buffer_index, out_buffer_index](
                     CPURuntimeContext* ctx, CPUExecutionContext* /*ectx*/) {
                     kernel(ctx->buffer_data[arg_buffer_index],
                            ctx->buffer_data[out_buffer_index],
