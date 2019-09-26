79e36f6a0e8144288c636d49e0aa0486fb11e2e0
mbencer <mateusz.bencer@intel.com>
2019-09-26 14:51:49 +0200

    Added downgrade pass for reduce_prod

--- src/ngraph/pass/opset1_downgrade.cpp
+++ src/ngraph/pass/opset1_downgrade.cpp
@@ -15,8 +15,14 @@
 //*****************************************************************************
 #include "ngraph/pass/opset1_downgrade.hpp"
 #include "ngraph/graph_util.hpp"
+#include "ngraph/graph_util.hpp"
 #include "ngraph/op/get_output_element.hpp"
 #include "ngraph/op/pad.hpp"
+#include "ngraph/op/product.hpp"
+#include "ngraph/op/reduce_prod.hpp"
+#include "ngraph/op/reduce_sum.hpp"
+#include "ngraph/op/reshape.hpp"
+#include "ngraph/op/sum.hpp"
 
 using namespace std;
 using namespace ngraph;
@@ -84,6 +90,48 @@ bool pass::Opset1Downgrade::run_on_node(shared_ptr<Node> node)
                                                          tmp->get_pads_end(),
                                                          tmp->get_pad_mode());
 
+        replace_node(node, replacement_node);
+        modified = true;
+        break;
+    }
+    case OP_TYPEID::Product:
+    {
+        auto tmp = dynamic_cast<const op::v1::ReduceProd*>(node.get());
+        auto replacement_node = make_shared<op::v0::Product>(node->input(0).get_source_output(),
+                                                             node->input(1).get_source_output());
+        if (tmp->get_keep_dims())
+        {
+            NGRAPH_CHECK(tmp->reduction_axes_constant(),
+                "Unable to convert ReduceProd:v1 to Product:v0 "
+                "if reduction axes are not constant (for keep_dims=true). Node: ",
+                *node);
+            auto output_pshape = replacement_node->get_output_partial_shape(0);
+            NGRAPH_CHECK(output_pshape.is_static(),
+                         "Unable to convert ReduceProd:v1 to Product:v0 "
+                         "if output shape is dynamic (for keep_dims=true). Node: ",
+                         *node);
+            const auto output_shape = output_pshape.to_shape();
+            auto reshaped_output_shape = output_shape;
+            for (const auto& axis : tmp->get_reduction_axes())
+            {
+                reshaped_output_shape.insert(reshaped_output_shape.begin() + axis, 1);
+            }
+            auto reshaped_product = make_shared<op::Reshape>(
+                replacement_node->output(0), get_default_order(output_shape), reshaped_output_shape);
+            replace_node(node, reshaped_product);
+        }
+        else
+        {
+            replace_node(node, replacement_node);
+        }
+        modified = true;
+        break;
+    }
+    case OP_TYPEID::Sum:
+    {
+        bool keep_dims = false;
+        auto replacement_node = make_shared<op::v1::ReduceSum>(
+            node->input(0).get_source_output(), node->input(1).get_source_output(), keep_dims);
         replace_node(node, replacement_node);
         modified = true;
         break;
