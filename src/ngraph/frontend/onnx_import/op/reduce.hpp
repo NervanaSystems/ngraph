342d26fb3133f4f0a132ee303b30f3d190b81041
mbencer <mateusz.bencer@intel.com>
2019-09-27 09:37:42 +0200

    Added ReduceProd:1 downgrade support

--- src/ngraph/frontend/onnx_import/op/reduce.hpp
+++ src/ngraph/frontend/onnx_import/op/reduce.hpp
@@ -28,8 +28,8 @@
 #include "ngraph/op/max.hpp"
 #include "ngraph/op/min.hpp"
 #include "ngraph/op/multiply.hpp"
-#include "ngraph/op/product.hpp"
 #include "ngraph/op/reduce_prod.hpp"
+#include "ngraph/op/reduce_sum.hpp"
 #include "ngraph/op/sum.hpp"
 #include "ngraph/op/util/broadcasting.hpp"
 #include "utils/reduction.hpp"
@@ -229,12 +229,13 @@ namespace ngraph
                 ///
                 inline NodeVector reduce_sum(const Node& node)
                 {
-                    return {reduction::make_ng_reduction_op(
+                    return {reduction::make_ng_dyn_reduction_op(
                         node,
                         node.get_ng_inputs().at(0),
-                        std::make_shared<ngraph::op::Sum,
+                        std::make_shared<ngraph::op::v1::ReduceSum,
                                          const std::shared_ptr<ngraph::Node>&,
-                                         const ngraph::AxisSet&>)};
+                                         const std::shared_ptr<ngraph::Node>&,
+                                         bool>) };
                 }
 
                 /// \brief      Compute the sum square of the input tensor's element along the
