afab340628df1f89715fce2c90a08c8504fe4d4f
Tomasz Socha <tomasz.socha@intel.com>
2019-10-02 15:34:45 +0200

    Review fix I

--- src/ngraph/frontend/onnx_import/op/unsqueeze.cpp
+++ src/ngraph/frontend/onnx_import/op/unsqueeze.cpp
@@ -31,8 +31,9 @@ namespace ngraph
                 {
                     auto data = node.get_ng_inputs().at(0);
                     auto axes = node.get_attribute_value<std::vector<std::int64_t>>("axes", {});
+                    const auto expanded_rank = data->get_shape().size() + axes.size();
                     std::vector<std::size_t> valid_axes =
-                        common::validate_axes(node, axes, data->get_shape().size() + axes.size());
+                        common::validate_axes(node, axes, expanded_rank);
                     auto axes_node = std::make_shared<ngraph::op::Constant>(
                         element::i64, Shape{valid_axes.size()}, valid_axes);
                     return {std::make_shared<ngraph::op::Unsqueeze>(data, axes_node)};
