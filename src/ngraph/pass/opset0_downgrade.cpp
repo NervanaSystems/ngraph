51a859ec691bdd78ad937eae1cde38e4a477d9bb
mbencer <mateusz.bencer@intel.com>
2019-10-09 11:49:11 +0200

    styles applied

--- src/ngraph/pass/opset0_downgrade.cpp
+++ src/ngraph/pass/opset0_downgrade.cpp
@@ -97,18 +97,18 @@ bool pass::Opset0Downgrade::run_on_node(shared_ptr<Node> node)
     {
         auto tmp = as_type_ptr<op::v1::ReduceProd>(node);
         auto replacement_node = make_shared<op::v0::Product>(node->input(0).get_source_output(),
-            node->input(1).get_source_output());
+                                                             node->input(1).get_source_output());
         if (tmp->get_keep_dims())
         {
             NGRAPH_CHECK(tmp->reduction_axes_constant(),
-                "Unable to convert ReduceProd:v1 to Product:v0 "
-                "if reduction axes are not constant (for keep_dims=true). Node: ",
-                *node);
+                         "Unable to convert ReduceProd:v1 to Product:v0 "
+                         "if reduction axes are not constant (for keep_dims=true). Node: ",
+                         *node);
             auto output_pshape = replacement_node->get_output_partial_shape(0);
             NGRAPH_CHECK(output_pshape.is_static(),
-                "Unable to convert ReduceProd:v1 to Product:v0 "
-                "if output shape is dynamic (for keep_dims=true). Node: ",
-                *node);
+                         "Unable to convert ReduceProd:v1 to Product:v0 "
+                         "if output shape is dynamic (for keep_dims=true). Node: ",
+                         *node);
             const auto output_shape = output_pshape.to_shape();
             auto reshaped_output_shape = output_shape;
             for (const auto& axis : tmp->get_reduction_axes())
@@ -116,8 +116,8 @@ bool pass::Opset0Downgrade::run_on_node(shared_ptr<Node> node)
                 reshaped_output_shape.insert(reshaped_output_shape.begin() + axis, 1);
             }
             auto reshaped_product = make_shared<op::Reshape>(replacement_node->output(0),
-                get_default_order(output_shape),
-                reshaped_output_shape);
+                                                             get_default_order(output_shape),
+                                                             reshaped_output_shape);
             replace_node(node, reshaped_product);
         }
         else
@@ -131,18 +131,18 @@ bool pass::Opset0Downgrade::run_on_node(shared_ptr<Node> node)
     {
         auto tmp = as_type_ptr<op::v1::ReduceSum>(node);
         auto replacement_node = make_shared<op::v0::Sum>(node->input(0).get_source_output(),
-            node->input(1).get_source_output());
+                                                         node->input(1).get_source_output());
         if (tmp->get_keep_dims())
         {
             NGRAPH_CHECK(tmp->reduction_axes_constant(),
-                "Unable to convert ReduceSum:v1 to Sum:v0 "
-                "if reduction axes are not constant (for keep_dims=true). Node: ",
-                *node);
+                         "Unable to convert ReduceSum:v1 to Sum:v0 "
+                         "if reduction axes are not constant (for keep_dims=true). Node: ",
+                         *node);
             auto output_pshape = replacement_node->get_output_partial_shape(0);
             NGRAPH_CHECK(output_pshape.is_static(),
-                "Unable to convert ReduceSum:v1 to Sum:v0 "
-                "if output shape is dynamic (for keep_dims=true). Node: ",
-                *node);
+                         "Unable to convert ReduceSum:v1 to Sum:v0 "
+                         "if output shape is dynamic (for keep_dims=true). Node: ",
+                         *node);
             const auto output_shape = output_pshape.to_shape();
             auto reshaped_output_shape = output_shape;
             for (const auto& axis : tmp->get_reduction_axes())
@@ -150,8 +150,8 @@ bool pass::Opset0Downgrade::run_on_node(shared_ptr<Node> node)
                 reshaped_output_shape.insert(reshaped_output_shape.begin() + axis, 1);
             }
             auto reshaped_product = make_shared<op::Reshape>(replacement_node->output(0),
-                get_default_order(output_shape),
-                reshaped_output_shape);
+                                                             get_default_order(output_shape),
+                                                             reshaped_output_shape);
             replace_node(node, reshaped_product);
         }
         else
