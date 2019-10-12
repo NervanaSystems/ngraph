bbc7d99f6bccaa7308eb88b3561830434b38b248
Avijit Chakraborty <avijit.chakraborty@intel.com>
2019-10-12 12:04:52 -0700

    Added a callback registration to the event class so that frameworks can decide how to write the events. Note: This is FULLY backwards compatible i.e., no change in the API so - won't break existing users

--- src/ngraph/event_tracing.hpp
+++ src/ngraph/event_tracing.hpp
@@ -30,6 +30,7 @@
 #else
 #include <unistd.h>
 #endif
+#include <functional>
 
 namespace ngraph
 {
@@ -90,14 +91,30 @@ namespace ngraph
             m_stop = std::chrono::high_resolution_clock::now();
         }
 
+        const std::string& get_name() const { return m_name; }
+        const std::string& get_category() const { return m_category; }
+        const std::string& get_agrs() const { return m_args; }
+        const std::chrono::time_point<std::chrono::high_resolution_clock>& get_start() const
+        {
+            return m_start;
+        }
+        const std::chrono::time_point<std::chrono::high_resolution_clock>& get_stop() const
+        {
+            return m_stop;
+        }
+
+        static void register_event_writer(std::function<void(const Event& event)> callback)
+        {
+            std::lock_guard<std::mutex> lock(s_file_mutex);
+
+            s_event_writer_registered = true;
+            s_event_writer = callback;
+        }
         static void write_trace(const Event& event);
         static bool is_tracing_enabled() { return s_tracing_enabled; }
         static void enable_event_tracing();
         static void disable_event_tracing();
         std::string to_json() const;
-
-        Event(const Event&) = delete;
-        Event& operator=(Event const&) = delete;
 
     private:
         int m_pid;
@@ -111,6 +128,8 @@ namespace ngraph
         static std::mutex s_file_mutex;
         static std::ofstream s_event_log;
         static bool s_tracing_enabled;
+        static std::function<void(const Event& event)> s_event_writer;
+        static bool s_event_writer_registered;
     };
 
 } // namespace ngraph
