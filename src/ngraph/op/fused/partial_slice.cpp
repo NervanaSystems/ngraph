c14116179b1b48a769139cfe71e9ae8f53958e44
baojun-nervana <baojun.liu@intel.com>
2019-10-18 17:37:37 -0700

    fprop implemented

--- src/ngraph/op/fused/partial_slice.cpp
+++ src/ngraph/op/fused/partial_slice.cpp
@@ -26,9 +26,9 @@
 #include "ngraph/op/multiply.hpp"
 #include "ngraph/op/negative.hpp"
 #include "ngraph/op/reshape.hpp"
+#include "ngraph/op/slice.hpp"
 #include "ngraph/op/sqrt.hpp"
 #include "ngraph/op/subtract.hpp"
-#include "ngraph/op/sum.hpp"
 #include "ngraph/op/util/broadcasting.hpp"
 
 using namespace std;
@@ -59,9 +59,43 @@ NodeVector op::PartialSlice::decompose_op() const
     {
         throw ngraph_error("Data needs to have static shape to decompose");
     }
+    auto input = input_value(0);
+    auto input_shape = input.get_shape();
+    auto axes = get_axes();
+    auto starts = get_lower_bounds();
+    auto ends = get_upper_bounds();
+    ngraph::Coordinate ng_start, ng_end;
+    int axis, start, end;
+    for (size_t i = 0; i < input_shape.size(); ++i)
+    {
+        ng_start.push_back(0);
+        ng_end.push_back(input_shape[i]);
+    }
+    for (size_t i = 0; i < axes.size(); ++i)
+    {
+        axis = input_shape[axes[i]];
+        start = starts[i] < 0 ? (starts[i] + axis) : starts[i];
+        end = ends[i] < 0 ? (ends[i] + axis) : ends[i];
+        start = std::max(start, 0);
+        end = std::max(end, 0);
+        start = std::min(start, axis);
+        end = std::min(end, axis);
+        start = std::min(start, end);
+        ng_start[axes[i]] = start;
+        ng_end[axes[i]] = end;
+    }
+    auto out = std::make_shared<op::Slice>(input, ng_start, ng_end);
+
+    auto out_shape = out->get_shape();
+
+    std::vector<size_t> out_axis_vec(out_shape.size());
+    std::iota(out_axis_vec.begin(), out_axis_vec.end(), 0);
+
+    // paddle::platform::TrimTrailingSingularDims(&out_shape);
+    auto out_dim = std::make_shared<op::Reshape>(
+        out, ngraph::AxisVector(out_axis_vec), ngraph::Shape(out_shape));
     // Return output nodes
-    NodeVector retval;
-    return retval;
+    return {out_dim};
 }
 
 shared_ptr<Node> op::PartialSlice::copy_with_new_args(const NodeVector& new_args) const
@@ -74,7 +108,7 @@ shared_ptr<Node> op::PartialSlice::copy_with_new_args(const NodeVector& new_args
         new_args.at(0), m_axes, m_lower_bounds, m_upper_bounds, m_decrease_axes);
 }
 
-void op::PartialSlice::validate_and_infer_types()
+void op::PartialSlice::pre_validate_and_infer_types()
 {
     element::Type input_element_type = get_input_element_type(0);
 
@@ -118,7 +152,7 @@ shared_ptr<Node> op::PartialSliceBackprop::copy_with_new_args(const NodeVector& 
 {
 }
 
-void op::PartialSliceBackprop::validate_and_infer_types()
+void op::PartialSliceBackprop::pre_validate_and_infer_types()
 {
     element::Type input_element_type = get_input_element_type(0);
 
