7bb5edfb97d1c2533dd58e14d4c61436c22c000b
baojun-nervana <baojun.liu@intel.com>
2019-10-21 16:23:27 -0700

    implement bprop

--- src/ngraph/op/fused/partial_slice.cpp
+++ src/ngraph/op/fused/partial_slice.cpp
@@ -18,18 +18,11 @@
 #include <numeric>
 
 #include "ngraph/builder/make_constant.hpp"
-#include "ngraph/builder/reduce_ops.hpp"
-#include "ngraph/op/add.hpp"
 #include "ngraph/op/constant.hpp"
-#include "ngraph/op/divide.hpp"
 #include "ngraph/op/fused/partial_slice.hpp"
-#include "ngraph/op/multiply.hpp"
-#include "ngraph/op/negative.hpp"
+#include "ngraph/op/replace_slice.hpp"
 #include "ngraph/op/reshape.hpp"
 #include "ngraph/op/slice.hpp"
-#include "ngraph/op/sqrt.hpp"
-#include "ngraph/op/subtract.hpp"
-#include "ngraph/op/util/broadcasting.hpp"
 
 using namespace std;
 using namespace ngraph;
@@ -54,48 +47,42 @@ op::PartialSlice::PartialSlice(const Output<Node>& data,
 // All input shape should be static by this point
 NodeVector op::PartialSlice::decompose_op() const
 {
-    const PartialShape& data_shape = get_input_partial_shape(0);
-    if (data_shape.is_dynamic())
+    const PartialShape& data_pshape = get_input_partial_shape(0);
+    if (data_pshape.is_dynamic())
     {
         throw ngraph_error("Data needs to have static shape to decompose");
     }
-    auto input = input_value(0);
-    auto input_shape = input.get_shape();
+
+    auto data = input_value(0);
+    auto data_shape = data.get_shape();
     auto axes = get_axes();
     auto starts = get_lower_bounds();
     auto ends = get_upper_bounds();
-    ngraph::Coordinate ng_start, ng_end;
+
+    Coordinate ng_start, ng_end;
     int axis, start, end;
-    for (size_t i = 0; i < input_shape.size(); ++i)
+    for (size_t i = 0; i < data_shape.size(); ++i)
     {
         ng_start.push_back(0);
-        ng_end.push_back(input_shape[i]);
+        ng_end.push_back(data_shape[i]);
     }
+
     for (size_t i = 0; i < axes.size(); ++i)
     {
-        axis = input_shape[axes[i]];
+        axis = data_shape[axes[i]];
         start = starts[i] < 0 ? (starts[i] + axis) : starts[i];
         end = ends[i] < 0 ? (ends[i] + axis) : ends[i];
-        start = std::max(start, 0);
-        end = std::max(end, 0);
-        start = std::min(start, axis);
-        end = std::min(end, axis);
-        start = std::min(start, end);
+        start = max(start, 0);
+        end = max(end, 0);
+        start = min(start, axis);
+        end = min(end, axis);
+        start = min(start, end);
         ng_start[axes[i]] = start;
         ng_end[axes[i]] = end;
     }
-    auto out = std::make_shared<op::Slice>(input, ng_start, ng_end);
 
-    auto out_shape = out->get_shape();
-
-    std::vector<size_t> out_axis_vec(out_shape.size());
-    std::iota(out_axis_vec.begin(), out_axis_vec.end(), 0);
-
-    // paddle::platform::TrimTrailingSingularDims(&out_shape);
-    auto out_dim = std::make_shared<op::Reshape>(
-        out, ngraph::AxisVector(out_axis_vec), ngraph::Shape(out_shape));
-    // Return output nodes
-    return {out_dim};
+    auto sliced = std::make_shared<op::Slice>(data, ng_start, ng_end);
+    return {sliced};
 }
 
 shared_ptr<Node> op::PartialSlice::copy_with_new_args(const NodeVector& new_args) const
@@ -121,6 +108,7 @@ void op::PartialSlice::pre_validate_and_infer_types()
 
 void op::PartialSlice::generate_adjoints(autodiff::Adjoints& adjoints, const NodeVector& deltas)
 {
+    throw ngraph_error("op::PartialSlice::generate_adjoints function is not implemented yet");
 }
 
 op::PartialSliceBackprop::PartialSliceBackprop(const Output<Node>& data,
@@ -128,7 +116,7 @@ op::PartialSliceBackprop::PartialSliceBackprop(const Output<Node>& data,
                                                const AxisVector& axes,
                                                const std::vector<int64_t>& lower_bounds,
                                                const std::vector<int64_t>& upper_bounds)
-    : FusedOp({data})
+    : FusedOp({data, dout})
     , m_axes(axes)
     , m_lower_bounds(lower_bounds)
     , m_upper_bounds(upper_bounds)
@@ -139,17 +127,62 @@ op::PartialSliceBackprop::PartialSliceBackprop(const Output<Node>& data,
 // All input shape should be static by this point
 NodeVector op::PartialSliceBackprop::decompose_op() const
 {
-    const PartialShape& data_shape = get_input_partial_shape(0);
-    if (data_shape.is_dynamic())
+    const PartialShape& data_pshape = get_input_partial_shape(0);
+    if (data_pshape.is_dynamic())
     {
         throw ngraph_error("Data needs to have static shape to decompose");
     }
-    NodeVector retval;
-    return retval;
+
+    auto data = input_value(0);
+    auto dout = input_value(1);
+    auto data_shape = data.get_shape();
+    auto axes = get_axes();
+    auto starts = get_lower_bounds();
+    auto ends = get_upper_bounds();
+
+    Coordinate ng_start, ng_end;
+    int axis, start, end;
+
+    auto reshape = data_shape;
+    for (size_t i = 0; i < data_shape.size(); ++i)
+    {
+        ng_start.push_back(0);
+        ng_end.push_back(data_shape[i]);
+    }
+    for (size_t i = 0; i < axes.size(); ++i)
+    {
+        axis = data_shape[axes[i]];
+        start = starts[i] < 0 ? (starts[i] + axis) : starts[i];
+        end = ends[i] < 0 ? (ends[i] + axis) : ends[i];
+        start = max(start, 0);
+        end = max(end, 0);
+        start = min(start, axis);
+        end = min(end, axis);
+        start = min(start, end);
+        ng_start[axes[i]] = start;
+        ng_end[axes[i]] = end;
+        reshape[axes[i]] = end - start;
+    }
+
+    auto dout_reshape =
+        std::make_shared<op::Reshape>(dout, get_default_order(dout.get_shape()), reshape);
+
+    std::shared_ptr<ngraph::Node> input0 =
+        op::Constant::create(dout.get_element_type(), data_shape, {0});
+
+    auto din = std::make_shared<op::ReplaceSlice>(input0, dout_reshape, ng_start, ng_end);
+    return {din};
 }
 
 shared_ptr<Node> op::PartialSliceBackprop::copy_with_new_args(const NodeVector& new_args) const
 {
+    if (new_args.size() != 2)
+    {
+        throw ngraph_error("Incorrect number of new arguments");
+    }
+
+    return make_shared<PartialSliceBackprop>(
+        new_args.at(0), new_args.at(1), m_axes, m_lower_bounds, m_upper_bounds);
 }
 
 void op::PartialSliceBackprop::pre_validate_and_infer_types()
