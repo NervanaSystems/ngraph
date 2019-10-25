485b29dc4247ce329f60304e5578faa9ac3f98f2
nmostafa <nagy.h.mostafa@intel.com>
2019-10-25 15:16:56 -0700

    Added liveness info

--- src/contrib/mlir/compiler/pass/memory_optimization.cpp
+++ src/contrib/mlir/compiler/pass/memory_optimization.cpp
@@ -47,22 +47,39 @@ namespace
     // If NoAlias[T] contains S, then T and S cannot alias. 
     // The relationship is transitive and we compute transitive closure on each update to the 
     // the strucutre. 
-    class AliasMap {
+    class AliasRelation 
+    {
 
         public:
-        AliasMap();
+        AliasRelation();
         /// Checks if values a and b can alias
         bool canAlias(Value* a, Value* b);
-        bool insertNoAlias(Value* a, Value* b);
+        void insertNoAlias(Value* a, Value* b);
 
         private: 
-        void computeClosure();
+        void computeTransitiveClosure();
+#ifdef NGRAPH_DEBUG_ENABLE
+        void checkInvariance();
+#endif
         private:
-        using Row = SmallVector<int8_t,4>;
+        using Row = SmallVector<int8_t,10>;
         std::unordered_map<Value*, unsigned> m_valueToIdx;
-        SmallVector<Row, 4> m_reachability;
-        unsigned m_maxIdx;
+        SmallVector<Row, 10> m_reachability;
+        unsigned m_maxIdx; 
         bool m_needsTransitiveClosure;
+    };
+
+    class Liveness
+    {
+        public:
+        bool isLive(Value* v);
+        void setLive(Value* v);
+        void kill(Value* v);
+        void reset();
+        private: 
+        unsigned m_maxIdx = 0;
+        SmallVector<bool, 10> m_liveness;
+        std::unordered_map<Value*, unsigned> m_valueToIdx;
     };
 
     /// Memory Optimization pass
@@ -87,6 +104,29 @@ namespace
     };
 
     unsigned MemoryOptimizationPass::bufferId = 0;
+
+// Aliasing conditions:
+// General conditions:
+//      Operand cannot be argument or output of the sub-graph
+// Destructive in-place:
+//      Find first operand where:
+//          It is last use (operand is dead). If both are las-use, then pick one with lower # of uses. 
+//      Assert operand has no prior buffer Id assigned (since last use). 
+//      If result has bufferId + Offset, then copy them to operand. If not create new BufferId and Offset = 0. 
+//
+// Non-Destructive in-place:
+//      Concat:
+//          Concat axis is most-significant non-one axis. 
+//          All operands can alias dest. 
+//          Compute buffer ID and offset for each operand and try to assign, if any of the operands already have an attribute that doesnt match what we want to assign, bail out. 
+//
+//
+//      Slice: TBD
+
+
+
+
+
 
     void MemoryOptimizationPass::runOnFunction()
     {
@@ -166,6 +206,7 @@ namespace
         });
     }
 
+    // TODO Change this to use interfaces. 
     bool MemoryOptimizationPass::isSafeInPlace(mlir::Operation* op)
     {
         auto it = m_inplaceOps.find(op->getName().getStringRef().str());
@@ -173,7 +214,7 @@ namespace
     }
 
 
-    AliasMap::AliasMap()
+    AliasRelation::AliasRelation()
     {
         for (auto i = 0; i < m_reachability.size(); i++)
         {
@@ -183,7 +224,7 @@ namespace
         m_maxIdx = 0;
         m_needsTransitiveClosure = false;
     }
-    bool AliasMap::canAlias(Value* a, Value* b)
+    bool AliasRelation::canAlias(Value* a, Value* b)
     {
         NGRAPH_CHECK(m_needsTransitiveClosure == false, "Relationship needs transitive closure");
         auto a_it = m_valueToIdx.find(a);
@@ -193,28 +234,39 @@ namespace
             // at least one value doesn't exist in the cannot-alias relationship
             return true;
         }
-        auto a_idx = *a_it;
-        auto b_idx = *b_it;
-        return !m_reachability[a_idx][b_idx] && m_reachability[b_idx][a_idx]);
+        auto a_idx = a_it->second;
+        auto b_idx = b_it->second;
+        return (!m_reachability[a_idx][b_idx] && m_reachability[b_idx][a_idx]);
     }
-    void AliasMap::insertNoAlias(Value* a, Value* b)
+    void AliasRelation::insertNoAlias(Value* a, Value* b)
     {
         m_needsTransitiveClosure = true;
         int rowsToAdd = 0;
+        unsigned a_idx, b_idx;
         auto it = m_valueToIdx.find(a);
         if (it == m_valueToIdx.end())
         {
             rowsToAdd++;
-            m_valueToIdx[a] = m_maxIdx++;
+            a_idx = m_maxIdx++;
+            m_valueToIdx[a] = a_idx;
+        }
+        else
+        {
+            a_idx = it->second;
         }
 
         it = m_valueToIdx.find(b);
         if (it == m_valueToIdx.end())
         {
             rowsToAdd++;
-            m_valueToIdx[b] = m_maxIdx++;
+            b_idx = m_maxIdx++;
+            m_valueToIdx[b] = b_idx;
         }
-
+        else
+        {
+            b_idx = it->second;
+        }
+        
         if (rowsToAdd)
         {
             // expand existing rows with 1 or 2 additional columns
@@ -227,12 +279,15 @@ namespace
                 }
             }
             // add 1 or 2 additional rows
-            m_reachability.push_back(Row(m_maxIdx, 0));
+            m_reachability.push_back(Row(m_maxIdx /* size */, 0));
             if (rowsToAdd > 1)
             {
-                m_reachability.push_back(Row(m_maxIdx, 0));
+                m_reachability.push_back(Row(m_maxIdx /* size */, 0));
             }
         }
+
+        m_reachability[a_idx][b_idx] = 1;
+        m_reachability[b_idx][a_idx] = 1;
 
 #ifdef NGRAPH_DEBUG_ENABLE
         checkInvariance();
@@ -240,7 +295,7 @@ namespace
         computeTransitiveClosure();
     }
 
-    void AliasMap::computeTransitiveClosure()
+    void AliasRelation::computeTransitiveClosure()
     {
         for (unsigned k = 0; k < m_maxIdx; k++)
         {
@@ -258,7 +313,8 @@ namespace
             
     }
 
-    void AliasMap::checkInvariance() 
+#ifdef NGRAPH_DEBUG_ENABLE
+    void AliasRelation::checkInvariance() 
     {
         NGRAPH_CHECK(m_reachability.size() == m_maxIdx);
         for (auto& v : m_reachability)
@@ -273,6 +329,50 @@ namespace
                 NGRAPH_CHECK(m_reachability[i][j] == m_reachability[j][i], "Non-symmetric relationship");
             }
         }
+    }
+#endif
+
+    void Liveness::reset()
+    {
+        m_valueToIdx.clear();
+        m_liveness.clear();
+        m_maxIdx = 0;
+    }
+
+    bool Liveness::isLive(Value* v)
+    {
+        auto it = m_valueToIdx.find(v);
+        if (it == m_valueToIdx.end())
+        {
+            return false;
+        }
+        return m_liveness[it->second];
+    }
+
+    void Liveness::setLive(Value* v)
+    {
+        auto it = m_valueToIdx.find(v);
+        if (it == m_valueToIdx.end())
+        {
+            m_valueToIdx[v] = m_maxIdx++;
+            NGRAPH_CHECK(m_liveness.size() == m_maxIdx);
+            m_liveness.push_back(true);
+        }
+        else
+        {
+            m_liveness[it->second] = true;            
+        }
+    }
+
+    void Liveness::kill(Value* v)
+    {
+        auto it = m_valueToIdx.find(v);
+        if (it == m_valueToIdx.end())
+        {
+            // already dead
+            return;
+        }
+        m_liveness[it->second] = false;
     }
 }
 
