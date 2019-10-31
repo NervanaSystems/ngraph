e3ed0673074d5377b99fffbe8cb55fe22c0f12cf
Ewa21 <ewa.tusien@intel.com>
2019-10-31 12:08:01 +0100

    Added support for ONNX's clip op in set_11.

--- src/ngraph/frontend/onnx_import/op/clip.cpp
+++ src/ngraph/frontend/onnx_import/op/clip.cpp
@@ -18,7 +18,11 @@
 #include <memory>
 
 #include "clip.hpp"
+#include "ngraph/builder/make_constant.hpp"
+#include "ngraph/op/constant.hpp"
 #include "ngraph/op/fused/clamp.hpp"
+#include "ngraph/op/maximum.hpp"
+#include "ngraph/op/minimum.hpp"
 
 namespace ngraph
 {
@@ -43,8 +47,44 @@ namespace ngraph
 
             } // namespace set_1
 
-        } // namespace op
+            namespace set_11
+            {
+                NodeVector clip(const Node& node)
+                {
+                    NodeVector inputs{node.get_ng_inputs()};
+                    std::shared_ptr<ngraph::Node> data = inputs.at(0);
+                    const ngraph::element::Type data_type = data->get_element_type();
+                    const ngraph::Shape data_shape = data->get_shape();
+                    const double max_value = std::numeric_limits<double>::max();
+                    const double min_value = std::numeric_limits<double>::lowest();
+                    std::shared_ptr<ngraph::Node> min =
+                        builder::make_constant(data_type, data_shape, min_value);
+                    std::shared_ptr<ngraph::Node> max = =
+                        builder::make_constant(data_type, data_shape, max_value);
 
-    } // namespace onnx_import
+                    if (inputs.size() > 1)
+                    {
+                        if (!inputs.at(1)->is_null())
+                        {
+                            min = inputs.at(1);
+                        }
 
-} // namespace ngraph
+                        if (inputs.size() == 3)
+                        {
+                            if (!inputs.at(2)->is_null())
+                            {
+                                max = inputs.at(2);
+                            }
+                        }
+
+                        return {std::make_shared<ngraph::op::Minimum>(
+                            min, std::make_shared<ngraph::op::Maximum>(max, data))};
+                    }
+
+                } // namespace set_11
+
+            } // namespace op
+
+        } // namespace onnx_import
+
+    } // namespace ngraph
