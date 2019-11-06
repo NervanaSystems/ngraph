0b4695bdff82cf6f8db0741465f99affc6bfa398
tomdol <tomasz.dolbniak@intel.com>
2019-11-06 22:45:51 +0100

    Code formatting

--- src/ngraph/op/fused/softplus.hpp
+++ src/ngraph/op/fused/softplus.hpp
@@ -30,7 +30,6 @@ namespace ngraph
             NGRAPH_API
             static constexpr NodeTypeInfo type_info{"Softplus", 0};
             const NodeTypeInfo& get_type_info() const override { return type_info; }
-
             Softplus() = default;
 
             /// \brief   Constructs a Softplus operation.
