aaae165e18760f9fd86e4d67cfbdfd64cd590fd7
Pruthvi Gowda <pruthvi.gowda.thorehosur.appajigowda@intel.com>
2019-11-11 18:51:32 -0800

    - Op defination for cummalative sum

---
+++ src/ngraph/op/cum_sum.cpp
@@ -0,0 +1,51 @@
+//*****************************************************************************
+// Copyright 2017-2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+
+#include "ngraph/op/cum_sum.hpp"
+#include "ngraph/graph_util.hpp"
+#include "ngraph/op/broadcast.hpp"
+
+using namespace std;
+using namespace ngraph;
+
+constexpr NodeTypeInfo op::v0::CumSum::type_info;
+
+op::v0::CumSum::CumSum(const Output<Node>& arg, const Output<Node>& axis)
+    : ArithmeticReduction(arg, axis)
+{
+    constructor_validate_and_infer_types();
+}
+
+shared_ptr<Node> op::CumSum::copy_with_new_args(const NodeVector& new_args) const
+{
+    check_new_args_count(this, new_args);
+    return make_shared<op::v0::CumSum>(new_args.at(0), new_args.at(1));
+}
+
+void op::v0::CumSum::generate_adjoints(autodiff::Adjoints& adjoints, const NodeVector& deltas)
+{
+    auto delta = deltas.at(0);
+
+    auto x = input_value(0);
+    auto& x_shape = x.get_shape();
+
+    adjoints.add_delta(x, make_shared<op::Broadcast>(delta, x_shape, get_axis()));
+}
+
+shared_ptr<Node> op::v0::CumSum::get_default_value() const
+{
+    return ngraph::make_constant_from_string("0", get_element_type(), get_shape());
+}
