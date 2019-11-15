e0a8e13d30bbb76d2a692fddcc1a3b0c20f07a6d
Scott Cyphers <scott.cyphers@intel.com>
2019-11-15 08:22:41 -0800

    Cut and pasteo

--- src/ngraph/op/crop_and_resize.hpp
+++ src/ngraph/op/crop_and_resize.hpp
@@ -35,7 +35,7 @@ namespace ngraph
             NGRAPH_API
             static const std::string type_name;
             const std::string& description() const override { return type_name; }
-            /// \brief Constructs an absolute value operation.
+            /// \brief Constructs a crop and resize operation.
             CropAndResize() = default;
 
             /// \param image [N, H, W, C]
