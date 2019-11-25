45fef03ea619ed451401d41e961c38bf40444383
mbencer <mateusz.bencer@intel.com>
2019-11-25 21:25:02 +0100

    Moved Mod to op_v1_tbl

--- src/ngraph/runtime/interpreter/int_executable.hpp
+++ src/ngraph/runtime/interpreter/int_executable.hpp
@@ -1884,7 +1884,7 @@ private:
         case OP_TYPEID::MaxPool_v1:
         case OP_TYPEID::Maximum_v1:
         case OP_TYPEID::Minimum_v1:
-        case OP_TYPEID::Mod:
+        case OP_TYPEID::Mod_v1:
         case OP_TYPEID::Multiply_v1:
         case OP_TYPEID::MVN:
         case OP_TYPEID::Negative_v1:
