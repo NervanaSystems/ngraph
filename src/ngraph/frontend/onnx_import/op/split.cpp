34d7930f206e058b4b7f78f1ee1a48b68ef7cba2
mbencer <mateusz.bencer@intel.com>
2019-11-26 11:10:11 +0100

    refactored split

--- src/ngraph/frontend/onnx_import/op/split.cpp
+++ src/ngraph/frontend/onnx_import/op/split.cpp
@@ -19,8 +19,20 @@
 
 #include "ngraph/op/constant.hpp"
 #include "ngraph/op/fused/split.hpp"
+#include "ngraph/op/get_output_element.hpp"
 #include "op/split.hpp"
 #include "utils/common.hpp"
+
+/// \return Return the outputs of the node.
+static ngraph::NodeVector get_outputs(const std::shared_ptr<ngraph::Node>& node, int64_t outputs_numer)
+{
+    ngraph::NodeVector outputs(outputs_numer);
+    for (int i = 0; i < outputs_numer; ++i)
+    {
+        outputs[i] = std::make_shared<ngraph::op::GetOutputElement>(node, i);
+    }
+    return outputs;
+}
 
 namespace ngraph
 {
@@ -33,31 +45,25 @@ namespace ngraph
                 NodeVector split(const Node& node)
                 {
                     const auto input = node.get_ng_inputs().at(0);
-                    const auto outputs_number = node.get_output_names().size();
                     const auto axis = node.get_attribute_value<int64_t>("axis", 0);
-                    int64_t valid_axis =
-                        common::validate_axis(node, axis, input->get_shape().size()); //TODO
-                    const auto axis_node = ngraph::op::Constant::create(element::i64, Shape{}, { valid_axis });
+                    const auto axis_node = ngraph::op::Constant::create(element::i64, Shape{}, {axis});
 
+                    std::shared_ptr<ngraph::op::Split> fused_split;
                     try
                     {
                         const auto length_parts =
                             node.get_attribute_value<std::vector<std::size_t>>("split");
-                        const auto fused_split =
-                            std::make_shared<ngraph::op::Split>(input, axis_node, length_parts);
-
-                        return fused_split->decompose_op();
+                        fused_split = std::make_shared<ngraph::op::Split>(input, axis_node, length_parts);
                     }
                     catch (const error::node::UnknownAttribute&)
                     {
                         // an exception will be caught if the input node does not contain
                         // the 'split' attribute - this means we should split the input tensor
                         // into same-length parts equal to the number of node outputs
-                        const auto fused_split =
-                            std::make_shared<ngraph::op::Split>(input, axis_node, outputs_number);
-
-                        return fused_split->decompose_op();
+                        const auto outputs_number = node.get_output_names().size();
+                        fused_split = std::make_shared<ngraph::op::Split>(input, axis_node, outputs_number);
                     }
+                    return get_outputs(fused_split, fused_split->get_splits().size());
                 }
 
             } // namespace set_1
