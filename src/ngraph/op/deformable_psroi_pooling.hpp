e22de083689c1932c786a946115886dbaf3cfe7b
mbencer <mateusz.bencer@intel.com>
2019-11-27 15:02:36 +0100

    Added serialization

--- src/ngraph/op/deformable_psroi_pooling.hpp
+++ src/ngraph/op/deformable_psroi_pooling.hpp
@@ -28,7 +28,7 @@ namespace ngraph
             {
             public:
                 NGRAPH_API
-                static constexpr NodeTypeInfo type_info{"DeformablePSROIPooling", 0};
+                static constexpr NodeTypeInfo type_info{"DeformablePSROIPooling", 1};
                 const NodeTypeInfo& get_type_info() const override { return type_info; }
                 DeformablePSROIPooling() = default;
                 /// \brief Constructs a PSROIPooling operation
