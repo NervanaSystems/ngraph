48c86a651d7102d15daffac33cc121a52c943afa
mbencer <mateusz.bencer@intel.com>
2019-11-27 10:16:54 +0100

    Added missing fields

--- src/ngraph/op/deformable_psroi_pooling.cpp
+++ src/ngraph/op/deformable_psroi_pooling.cpp
@@ -22,20 +22,52 @@ using namespace ngraph;
 constexpr NodeTypeInfo op::v1::DeformablePSROIPooling::type_info;
 
 op::v1::DeformablePSROIPooling::DeformablePSROIPooling(const Output<Node>& input,
-                               const Output<Node>& coords,
-                               const size_t output_dim,
-                               const size_t group_size,
-                               const float spatial_scale,
-                               int spatial_bins_x,
-                               int spatial_bins_y,
-                               const string& mode)
+                                                       const Output<Node>& coords,
+                                                       const Output<Node>& offsets,
+                                                       const int64_t output_dim,
+                                                       const int64_t group_size,
+                                                       float spatial_scale,
+                                                       const std::string mode,
+                                                       int64_t spatial_bins_x,
+                                                       int64_t spatial_bins_y,
+                                                       bool no_trans,
+                                                       float trans_std,
+                                                       int64_t part_size)
+    : Op({input, coords, offsets})
+    , m_output_dim(output_dim)
+    , m_group_size(group_size)
+    , m_spatial_scale(spatial_scale)
+    , m_mode(mode)
+    , m_spatial_bins_x(spatial_bins_x)
+    , m_spatial_bins_y(spatial_bins_y)
+    , m_no_trans(no_trans)
+    , m_trans_std(trans_std)
+    , m_part_size(part_size)
+{
+    constructor_validate_and_infer_types();
+}
+
+op::v1::DeformablePSROIPooling::DeformablePSROIPooling(const Output<Node>& input,
+                                                       const Output<Node>& coords,
+                                                       const int64_t output_dim,
+                                                       const int64_t group_size,
+                                                       float spatial_scale,
+                                                       const std::string mode,
+                                                       int64_t spatial_bins_x,
+                                                       int64_t spatial_bins_y,
+                                                       bool no_trans,
+                                                       float trans_std,
+                                                       int64_t part_size)
     : Op({input, coords})
     , m_output_dim(output_dim)
     , m_group_size(group_size)
     , m_spatial_scale(spatial_scale)
+    , m_mode(mode)
     , m_spatial_bins_x(spatial_bins_x)
     , m_spatial_bins_y(spatial_bins_y)
-    , m_mode(mode)
+    , m_no_trans(no_trans)
+    , m_trans_std(trans_std)
+    , m_part_size(part_size)
 {
     constructor_validate_and_infer_types();
 }
@@ -47,14 +79,16 @@ void op::v1::DeformablePSROIPooling::validate_and_infer_types()
     {
         Shape input_shape = get_input_partial_shape(0).to_shape();
         Shape coords_shape = get_input_partial_shape(1).to_shape();
-        NODE_VALIDATION_CHECK(this,
-                              input_shape.size() >= 3,
-                              "DeformablePSROIPooling expects 3 or higher dimensions for input. Got ",
-                              input_shape.size());
-        NODE_VALIDATION_CHECK(this,
-                              coords_shape.size() == 2,
-                              "DeformablePSROIPooling expects 2 dimensions for box coordinates. Got ",
-                              coords_shape.size());
+        NODE_VALIDATION_CHECK(
+            this,
+            input_shape.size() >= 3,
+            "DeformablePSROIPooling expects 3 or higher dimensions for input. Got ",
+            input_shape.size());
+        NODE_VALIDATION_CHECK(
+            this,
+            coords_shape.size() == 2,
+            "DeformablePSROIPooling expects 2 dimensions for box coordinates. Got ",
+            coords_shape.size());
         Shape output_shape{coords_shape[0], m_output_dim};
         for (size_t i = 2; i < input_shape.size(); i++)
         {
@@ -68,15 +102,41 @@ void op::v1::DeformablePSROIPooling::validate_and_infer_types()
     }
 }
 
-shared_ptr<Node> op::v1::DeformablePSROIPooling::copy_with_new_args(const NodeVector& new_args) const
+shared_ptr<Node>
+    op::v1::DeformablePSROIPooling::copy_with_new_args(const NodeVector& new_args) const
 {
     check_new_args_count(this, new_args);
-    return make_shared<v1::DeformablePSROIPooling>(new_args.at(0),
-                                     new_args.at(1),
-                                     m_output_dim,
-                                     m_group_size,
-                                     m_spatial_scale,
-                                     m_spatial_bins_x,
-                                     m_spatial_bins_y,
-                                     m_mode);
+    if (new_args.size() == 3)
+    {
+        return make_shared<v1::DeformablePSROIPooling>(new_args.at(0),
+                                                       new_args.at(1),
+                                                       new_args.at(2),
+                                                       m_output_dim,
+                                                       m_group_size,
+                                                       m_spatial_scale,
+                                                       m_mode,
+                                                       m_spatial_bins_x,
+                                                       m_spatial_bins_y,
+                                                       m_no_trans,
+                                                       m_trans_std,
+                                                       m_part_size);
+    }
+    else if (new_args.size() == 2)
+    {
+        return make_shared<v1::DeformablePSROIPooling>(new_args.at(0),
+                                                       new_args.at(1),
+                                                       m_output_dim,
+                                                       m_group_size,
+                                                       m_spatial_scale,
+                                                       m_mode,
+                                                       m_spatial_bins_x,
+                                                       m_spatial_bins_y,
+                                                       m_no_trans,
+                                                       m_trans_std,
+                                                       m_part_size);
+    }
+    else
+    {
+        throw ngraph_error("Not supported number of DeformablePSROIPooling args");
+    }
 }
--- src/ngraph/op/deformable_psroi_pooling.hpp
+++ src/ngraph/op/deformable_psroi_pooling.hpp
@@ -28,7 +28,7 @@ namespace ngraph
             {
             public:
                 NGRAPH_API
-                    static constexpr NodeTypeInfo type_info{ "DeformablePSROIPooling", 0 };
+                static constexpr NodeTypeInfo type_info{"DeformablePSROIPooling", 0};
                 const NodeTypeInfo& get_type_info() const override { return type_info; }
                 DeformablePSROIPooling() = default;
                 /// \brief Constructs a PSROIPooling operation
@@ -38,37 +38,61 @@ namespace ngraph
                 /// \param output_dim     Output channel number
                 /// \param group_size     Number of groups to encode position-sensitive scores
                 /// \param spatial_scale  Ratio of input feature map over input image size
-                /// \param spatial_bins_x Numbers of bins to divide the input feature maps over width
-                /// \param spatial_bins_y Numbers of bins to divide the input feature maps over height
+                /// \param spatial_bins_x Numbers of bins to divide the input feature maps over
+                /// width
+                /// \param spatial_bins_y Numbers of bins to divide the input feature maps over
+                /// height
                 /// \param mode           Mode of pooling - Avg or Bilinear
-                //TODO
+                // TODO
                 DeformablePSROIPooling(const Output<Node>& input,
-                    const Output<Node>& coords,
-                    const size_t output_dim,
-                    const size_t group_size,
-                    const float spatial_scale,
-                    int spatial_bins_x,
-                    int spatial_bins_y,
-                    const std::string& mode);
+                                       const Output<Node>& coords,
+                                       const Output<Node>& offsets,
+                                       const int64_t output_dim,
+                                       const int64_t group_size,
+                                       const float spatial_scale,
+                                       const std::string mode = "bilinear_deformable",
+                                       int64_t spatial_bins_x = 1,
+                                       int64_t spatial_bins_y = 1,
+                                       bool no_trans = true,
+                                       float trans_std = 1,
+                                       int64_t part_size = 1);
+
+                DeformablePSROIPooling(const Output<Node>& input,
+                                       const Output<Node>& coords,
+                                       const int64_t output_dim,
+                                       const int64_t group_size,
+                                       const float spatial_scale,
+                                       const std::string mode = "bilinear_deformable",
+                                       int64_t spatial_bins_x = 1,
+                                       int64_t spatial_bins_y = 1,
+                                       bool no_trans = true,
+                                       float trans_std = 1,
+                                       int64_t part_size = 1);
 
                 void validate_and_infer_types() override;
 
                 virtual std::shared_ptr<Node>
                     copy_with_new_args(const NodeVector& new_args) const override;
 
-                size_t get_output_dim() const { return m_output_dim; }
-                size_t get_group_size() const { return m_group_size; }
+                int64_t get_output_dim() const { return m_output_dim; }
+                int64_t get_group_size() const { return m_group_size; }
                 float get_spatial_scale() const { return m_spatial_scale; }
-                int get_spatial_bins_x() const { return m_spatial_bins_x; }
-                int get_spatial_bins_y() const { return m_spatial_bins_y; }
                 const std::string& get_mode() const { return m_mode; }
+                int64_t get_spatial_bins_x() const { return m_spatial_bins_x; }
+                int64_t get_spatial_bins_y() const { return m_spatial_bins_y; }
+                bool get_no_trans() const { return m_no_trans; }
+                float get_trans_std() const { return m_trans_std; }
+                int64_t get_part_size() const { return m_part_size; }
             private:
-                size_t m_output_dim;
-                size_t m_group_size;
+                int64_t m_output_dim;
+                int64_t m_group_size;
                 float m_spatial_scale;
-                int m_spatial_bins_x;
-                int m_spatial_bins_y;
                 std::string m_mode;
+                int64_t m_spatial_bins_x;
+                int64_t m_spatial_bins_y;
+                bool m_no_trans;
+                float m_trans_std;
+                int64_t m_part_size;
             };
         }
     }
