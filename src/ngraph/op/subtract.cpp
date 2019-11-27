318a27813d66de821501ba754b803f1aae1ff7f6
mitruska <katarzyna.mitrus@intel.com>
2019-11-27 17:17:23 +0100

    Add v1 namespace

--- src/ngraph/op/subtract.cpp
+++ src/ngraph/op/subtract.cpp
@@ -71,7 +71,7 @@ op::v1::Subtract::Subtract(const Output<Node>& arg0,
     constructor_validate_and_infer_types();
 }
 
-shared_ptr<Node> op::Subtract::copy_with_new_args(const NodeVector& new_args) const
+shared_ptr<Node> op::v1::Subtract::copy_with_new_args(const NodeVector& new_args) const
 {
     check_new_args_count(this, new_args);
     return make_shared<Subtract>(new_args.at(0), new_args.at(1), this->get_autob());
