ca31b515aa56753673147ce8f56a652f903a9e1b
mbencer <mateusz.bencer@intel.com>
2019-11-28 10:00:18 +0100

    Moved to NGRAPH_API

--- src/ngraph/op/deformable_psroi_pooling.hpp
+++ src/ngraph/op/deformable_psroi_pooling.hpp
@@ -24,10 +24,9 @@ namespace ngraph
     {
         namespace v1
         {
-            class DeformablePSROIPooling : public Op
+            class NGRAPH_API DeformablePSROIPooling : public Op
             {
             public:
-                NGRAPH_API
                 static constexpr NodeTypeInfo type_info{"DeformablePSROIPooling", 1};
                 const NodeTypeInfo& get_type_info() const override { return type_info; }
                 DeformablePSROIPooling() = default;
