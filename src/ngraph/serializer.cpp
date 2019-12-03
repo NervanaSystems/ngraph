edc5d6baa9a13b70dec72d4c80d47ac46698b1c3
Tomasz Socha <tomasz.socha@intel.com>
2019-12-03 21:55:23 +0100

    [SPEC] Add new v1::GroupConvolution and v1::GroupConvolutionBackpropData operators. (#3956)
    * [SPEC] Add new v1::GroupConvolution and v1::GroupConvolutionBackpropData ops.
    
    * Sort downgrade passes
    
    * Add downgrade pass fro GroupConvolution
    
    * WIP I
    
    * Fix GroupConvolution validate and infer types
    
    * Add upgrade pass for GroupConvolution
    
    * Remove unnecesary get_static_groups() method
    
    * Disable pass for cases when groups are in filters
    
    * Review Fix I
    
    * Move ops to fused/group_conv.hpp
    
    * Use Op instead of FusedOp again
    
    * Move v1::GroupConvolution and v1::GroupConvolutionBackpropData to FusedOp but temporarily disable decomposition

--- src/ngraph/serializer.cpp
+++ src/ngraph/serializer.cpp
@@ -1178,6 +1178,51 @@ shared_ptr<Node> JSONDeserializer::deserialize_node(json node_js)
             }
             break;
         }
+        case OP_TYPEID::GroupConvolution_v1:
+        {
+            auto strides = node_js.at("strides").get<vector<size_t>>();
+            auto dilations = node_js.at("dilations").get<vector<size_t>>();
+            auto pads_begin = node_js.at("pads_begin").get<vector<std::ptrdiff_t>>();
+            auto pads_end = node_js.at("pads_end").get<vector<std::ptrdiff_t>>();
+
+            op::PadType auto_pad = read_pad_type(node_js);
+
+            node = make_shared<op::v1::GroupConvolution>(
+                args[0], args[1], strides, pads_begin, pads_end, dilations, auto_pad);
+            break;
+        }
+        case OP_TYPEID::GroupConvolutionBackpropData_v1:
+        {
+            auto strides = node_js.at("strides").get<vector<size_t>>();
+            auto dilations = node_js.at("dilations").get<vector<size_t>>();
+            auto pads_begin = node_js.at("pads_begin").get<vector<std::ptrdiff_t>>();
+            auto pads_end = node_js.at("pads_end").get<vector<std::ptrdiff_t>>();
+            auto output_padding = node_js.at("output_padding").get<vector<std::ptrdiff_t>>();
+            if (args.size() == 3)
+            {
+                node = make_shared<op::v1::GroupConvolutionBackpropData>(args[0],
+                                                                         args[1],
+                                                                         args[2],
+                                                                         strides,
+                                                                         pads_begin,
+                                                                         pads_end,
+                                                                         dilations,
+                                                                         read_pad_type(node_js),
+                                                                         output_padding);
+            }
+            else
+            {
+                node = make_shared<op::v1::GroupConvolutionBackpropData>(args[0],
+                                                                         args[1],
+                                                                         strides,
+                                                                         pads_begin,
+                                                                         pads_end,
+                                                                         dilations,
+                                                                         read_pad_type(node_js),
+                                                                         output_padding);
+            }
+            break;
+        }
         case OP_TYPEID::ConvolutionBackpropFilters:
         {
             auto filters_shape = node_js.at("filters_shape").get<vector<size_t>>();
@@ -3288,6 +3333,27 @@ json JSONSerializer::serialize_node(const Node& n)
     case OP_TYPEID::ConvolutionBackpropData_v1:
     {
         auto tmp = static_cast<const op::v1::ConvolutionBackpropData*>(&n);
+        node["strides"] = tmp->get_strides();
+        node["dilations"] = tmp->get_dilations();
+        node["pads_begin"] = tmp->get_pads_begin();
+        node["pads_end"] = tmp->get_pads_end();
+        node["auto_pad"] = tmp->get_auto_pad();
+        node["output_padding"] = tmp->get_output_padding();
+        break;
+    }
+    case OP_TYPEID::GroupConvolution_v1:
+    {
+        auto tmp = static_cast<const op::v1::GroupConvolution*>(&n);
+        node["strides"] = tmp->get_strides();
+        node["dilations"] = tmp->get_dilations();
+        node["pads_begin"] = tmp->get_pads_begin();
+        node["pads_end"] = tmp->get_pads_end();
+        node["auto_pad"] = tmp->get_auto_pad();
+        break;
+    }
+    case OP_TYPEID::GroupConvolutionBackpropData_v1:
+    {
+        auto tmp = static_cast<const op::v1::GroupConvolutionBackpropData*>(&n);
         node["strides"] = tmp->get_strides();
         node["dilations"] = tmp->get_dilations();
         node["pads_begin"] = tmp->get_pads_begin();
