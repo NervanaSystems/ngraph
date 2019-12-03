6c63a4bd9e5cff4bd848a940d7a9a9319ebf2a61
Scott Cyphers <scott.cyphers@intel.com>
2019-12-03 11:21:43 -0800

    Opset op insertion

--- src/ngraph/opsets/opset.hpp
+++ src/ngraph/opsets/opset.hpp
@@ -16,33 +16,49 @@
 
 #pragma once
 
+#include <map>
+#include <mutex>
 #include <set>
 
+#include "ngraph/factory.hpp"
 #include "ngraph/node.hpp"
 
 namespace ngraph
 {
     class OpSet
     {
+        static std::mutex& get_mutex();
+
     public:
-        OpSet(const std::set<NodeTypeInfo>& op_types)
-            : m_op_types(op_types)
+        OpSet() = default;
+
+        template <typename T>
+        void insert()
         {
+            std::lock_guard<std::mutex> guard(get_mutex());
+            m_op_types.insert(T::type_info);
+            m_name_type_info_map[T::type_info.name] = T::type_info;
+            ngraph::FactoryRegistry<Node>::get().register_factory<T>();
         }
+
+        ngraph::Node* create(const std::string& name);
 
         template <typename T>
         bool contains_type() const
         {
+            std::lock_guard<std::mutex> guard(get_mutex());
             return m_op_types.find(T::type_info) != m_op_types.end();
         }
 
         bool contains_op_type(Node* node) const
         {
+            std::lock_guard<std::mutex> guard(get_mutex());
             return m_op_types.find(node->get_type_info()) != m_op_types.end();
         }
 
     protected:
         std::set<NodeTypeInfo> m_op_types;
+        std::map<std::string, NodeTypeInfo> m_name_type_info_map;
     };
 
     const OpSet& get_opset0();
