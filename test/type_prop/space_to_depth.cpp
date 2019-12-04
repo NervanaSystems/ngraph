46f3385d8ea27cc15c07509145e9ee3df854e126
mbencer <mateusz.bencer@intel.com>
2019-12-04 11:40:53 +0100

    Added type_prop tests for SpaceToDepth

--- test/type_prop/space_to_depth.cpp
+++ test/type_prop/space_to_depth.cpp
@@ -33,12 +33,32 @@ TEST(type_prop, space_to_depth_output_shape_block_first_4D)
 
 TEST(type_prop, space_to_depth_output_shape_block_first_4D_2)
 {
-    auto A = make_shared<op::Parameter>(element::f32, Shape{ 1, 2, 64, 64 });
+    auto A = make_shared<op::Parameter>(element::f32, Shape{ 1, 12, 1080, 1616 });
     const auto mode = ngraph::op::SpaceToDepth::SpaceToDepthMode::BLOCKS_FIRST;
-    auto space_to_depth = make_shared<op::SpaceToDepth>(A, mode, 8);
+    auto space_to_depth = make_shared<op::SpaceToDepth>(A, mode, 2);
 
     ASSERT_EQ(space_to_depth->get_element_type(), element::f32);
-    ASSERT_EQ(space_to_depth->get_shape(), (Shape{ 1, 128, 8, 8 }));
+    ASSERT_EQ(space_to_depth->get_shape(), (Shape{ 1, 12 * 4, 1080 / 2, 1616 / 2 }));
+}
+
+TEST(type_prop, space_to_depth_output_shape_depth_first_4D)
+{
+    auto A = make_shared<op::Parameter>(element::f32, Shape{ 1, 12, 1080, 1616 });
+    const auto mode = ngraph::op::SpaceToDepth::SpaceToDepthMode::DEPTH_FIRST;
+    auto space_to_depth = make_shared<op::SpaceToDepth>(A, mode, 2);
+
+    ASSERT_EQ(space_to_depth->get_element_type(), element::f32);
+    ASSERT_EQ(space_to_depth->get_shape(), (Shape{ 1, 12 * 4, 1080 / 2, 1616 / 2 }));
+}
+
+TEST(type_prop, space_to_depth_output_shape_depth_first_5D)
+{
+    auto A = make_shared<op::Parameter>(element::f32, Shape{ 1, 12, 4, 1080, 1616 });
+    const auto mode = ngraph::op::SpaceToDepth::SpaceToDepthMode::DEPTH_FIRST;
+    auto space_to_depth = make_shared<op::SpaceToDepth>(A, mode, 2);
+
+    ASSERT_EQ(space_to_depth->get_element_type(), element::f32);
+    ASSERT_EQ(space_to_depth->get_shape(), (Shape{ 1, 12 * 8, 4/2, 1080 / 2, 1616 / 2 }));
 }
 
 TEST(type_prop, space_to_depth_input_rank_not_supported)
