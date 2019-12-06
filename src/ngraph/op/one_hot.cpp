64b635343b5d6a59bb52a44ac888e593cf196514
Michał Karzyński <postrational@users.noreply.github.com>
2019-12-06 12:46:16 +0100

    Review comment
    Co-Authored-By: Tomasz Socha <tomasz.socha@intel.com>

--- src/ngraph/op/one_hot.cpp
+++ src/ngraph/op/one_hot.cpp
@@ -166,7 +166,7 @@ void op::v1::OneHot::validate_and_infer_types()
         auto depth_element_type = depth->get_output_element_type(0);
         NODE_VALIDATION_CHECK(this,
                               depth_element_type.is_integral(),
-                              "'depth' input element type must be i8, i32 or i64 (got ",
+                               "'depth' input element type must be an integer (got ",
                               depth_element_type,
                               ").");
 
