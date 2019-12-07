375c5a88e9d8b10ab490c8aafbe5a6ca9248297e
Robert Kimball <robert.kimball@intel.com>
2019-12-07 10:55:26 -0800

    Fix serializer op name

--- src/ngraph/serializer.cpp
+++ src/ngraph/serializer.cpp
@@ -3055,7 +3055,8 @@ json JSONSerializer::serialize_node(const Node& n)
     {
         node["friendly_name"] = n.get_friendly_name();
     }
-    node["op"] = n.type_info.name;
+    // rhk
+    node["op"] = type_info.name;
     // TODO Multiple outputs
     json inputs = json::array();
     json control_deps = json::array();
