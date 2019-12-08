54c17941d7ec94de05e8daca0d6164ec99e84737
Robert Kimball <robert.kimball@intel.com>
2019-12-08 12:56:55 -0800

    style

--- src/ngraph/serializer.cpp
+++ src/ngraph/serializer.cpp
@@ -3056,7 +3056,7 @@ json JSONSerializer::serialize_node(const Node& n)
         node["friendly_name"] = n.get_friendly_name();
     }
     node["op"] = type_info.name;
-    
+
     // TODO Multiple outputs
     json inputs = json::array();
     json control_deps = json::array();
