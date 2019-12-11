f803feb74454ee1e3f89b28410c7d17c668cb477
Robert Kimball <robert.kimball@intel.com>
2019-12-10 18:35:58 -0800

    Remove GOE from Adjoints class (#3973)
    * Change generate_adjoints to take an OutputVector instead of a NodeVector for deltas.
    
    * Cleanup
    
    * Adjoints class convert to use Output<Node>
    
    * More cleanup
    
    * More cleanup
    
    * Post-merge build issues
    
    * Don't push initial bprops multiple times
    
    * Eliminate GOE correctly
    
    * back-compatibility, unit test

--- src/ngraph/op/parameter.hpp
+++ src/ngraph/op/parameter.hpp
@@ -32,7 +32,7 @@ namespace ngraph
         {
         protected:
             virtual void generate_adjoints(autodiff::Adjoints& adjoints,
-                                           const NodeVector& deltas) override;
+                                           const OutputVector& deltas) override;
 
         public:
             static constexpr NodeTypeInfo type_info{"Parameter", 0};
