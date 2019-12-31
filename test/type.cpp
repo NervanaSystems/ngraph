c7a7bc74787c8db8a6dabfdb97a580fc74449ba4
Robert Kimball <robert.kimball@intel.com>
2019-12-31 14:46:31 -0800

    Add unit test

---
+++ test/type.cpp
@@ -0,0 +1,32 @@
+//*****************************************************************************
+// Copyright 2017-2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+
+#include "gtest/gtest.h"
+
+#include "ngraph/ngraph.hpp"
+
+#include <memory>
+using namespace std;
+using namespace ngraph;
+
+TEST(type, as_type_ptr)
+{
+    Shape shape{2, 2, 2};
+    auto A = op::Constant::create(element::f32, shape, {1, 2, 3, 4, 5, 6, 7, 8});
+
+    auto scl = as_type_ptr<op::ScalarConstantLikeBase>(A);
+    EXPECT_EQ(scl, nullptr);
+}
