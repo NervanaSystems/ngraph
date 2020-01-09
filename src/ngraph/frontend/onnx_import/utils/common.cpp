b2877b75b1266a47c332e70dce260b08c0c9614b
tomdol <tomasz.dolbniak@intel.com>
2020-01-09 10:20:53 +0100

    Missing <numeric> include

--- src/ngraph/frontend/onnx_import/utils/common.cpp
+++ src/ngraph/frontend/onnx_import/utils/common.cpp
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //*****************************************************************************
+#include <numeric>
 #include <onnx/onnx_pb.h> // onnx types
 
 #include "common.hpp"
