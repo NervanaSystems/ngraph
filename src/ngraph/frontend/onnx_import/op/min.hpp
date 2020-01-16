e8c0282c140914b5e3c200d090e1096606cdc2aa
Ewa Tusień <ewa.tusien@intel.com>
2020-01-16 18:24:53 +0100

    [ONNX] Update Min, Max, Mean and Sum ops to use v1 version of ops (set_1 namespace) (#4157)
    * Changes version from v0 to default for Min and Max ops.
    
    * Added broadcast option in variadic function.
    
    * Added tests.
    
    * Added test for opset8.
    
    * Undo changes for Mean op.
    
    Co-authored-by: Tomasz Dołbniak <tomasz.dolbniak@intel.com>
    Co-authored-by: Scott Cyphers <diyessi@users.noreply.github.com>

--- src/ngraph/frontend/onnx_import/op/min.hpp
+++ src/ngraph/frontend/onnx_import/op/min.hpp
@@ -19,7 +19,7 @@
 #include "core/node.hpp"
 #include "default_opset.hpp"
 #include "ngraph/node.hpp"
-#include "ngraph/opsets/opset0.hpp"
+#include "ngraph/op/util/broadcasting.hpp"
 #include "utils/variadic.hpp"
 
 namespace ngraph
@@ -32,7 +32,8 @@ namespace ngraph
             {
                 inline NodeVector min(const Node& node)
                 {
-                    return variadic::make_ng_variadic_op<ngraph::opset0::Minimum>(node);
+                    return variadic::make_ng_variadic_op<default_opset::Minimum>(
+                        node, ngraph::op::AutoBroadcastSpec::NONE);
                 }
 
             } // namespace set_1
