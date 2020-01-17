06045b9669cf5563618f1f0337bae87601dd1cdc
Adam Osewski <adam.osewski@intel.com>
2020-01-17 10:51:10 +0100

    User helper broadcasting function for arithmetic operators.

--- src/ngraph/frontend/onnx_import/op/add.cpp
+++ src/ngraph/frontend/onnx_import/op/add.cpp
@@ -29,21 +29,16 @@ namespace ngraph
             {
                 NodeVector add(const Node& node)
                 {
-                    auto lhs_node = node.get_ng_inputs().at(0);
-                    auto rhs_node = node.get_ng_inputs().at(1);
-                    auto lhs_shape = lhs_node->get_shape();
-                    auto rhs_shape = rhs_node->get_shape();
+                    const Output<ngraph::Node> lhs_node = node.get_ng_inputs().at(0);
+                    Output<ngraph::Node> rhs_node = node.get_ng_inputs().at(1);
+                    auto lhs_shape = lhs_node.get_shape();
+                    auto rhs_shape = rhs_node.get_shape();
                     auto lhs_rank = lhs_shape.size();
                     auto rhs_rank = rhs_shape.size();
-                    auto axis =
-                        node.get_attribute_value<std::int64_t>("axis", lhs_rank - rhs_rank);
+                    auto axis = node.get_attribute_value<std::int64_t>("axis", lhs_rank - rhs_rank);
                     // Unidirectional broadcast right node to left shape.
-                    rhs_node = std::make_shared<default_opset::Broadcast>(
-                        rhs_node,
-                        default_opset::Constant::create(
-                            ngraph::element::i64, Shape{lhs_rank}, lhs_shape),
-                        ngraph::op::opset1::get_axes_mapping_output(lhs_shape, rhs_shape, axis));
-
+                    rhs_node = ngraph::op::opset1::legacy_style_broadcast_for_binary_operation(
+                        lhs_node, rhs_node, axis);
                     return {std::make_shared<default_opset::Add>(
                         lhs_node, rhs_node, ngraph::op::AutoBroadcastSpec::NONE)};
                 }
