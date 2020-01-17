50efdb62deb4a53ae241d72f9a24e6b658702727
mitruska <katarzyna.mitrus@intel.com>
2020-01-17 14:19:37 +0100

    Add provenance tags to all nodes created while downgrade

--- src/ngraph/pass/opset0_downgrade.cpp
+++ src/ngraph/pass/opset0_downgrade.cpp
@@ -849,8 +849,9 @@ namespace
         auto downgraded_node = op_cast(as_type_ptr<T>(node));
         if (downgraded_node)
         {
-            std::string provenance_tag = "<Downgraded from v1 " + std::string(node->get_type_name()) + ">";
+            std::string provenance_tag = "<Opset0_Downgrade (v1 " + std::string(node->get_type_name()) + ")>";
             downgraded_node->add_provenance_tag(provenance_tag);
+            downgraded_node->add_provenance_tags_above(node->input_values(), {provenance_tag});
             return true;
         }
         return false;
