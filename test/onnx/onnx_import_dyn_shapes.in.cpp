39cc669dd09ae4da4a46d88616efc9f269906e4a
tomdol <tomasz.dolbniak@intel.com>
2020-01-18 00:47:28 +0100

    Test the execution of an ONNX model with dynamic dimensions

--- test/onnx/onnx_import_dyn_shapes.in.cpp
+++ test/onnx/onnx_import_dyn_shapes.in.cpp
@@ -19,6 +19,7 @@
 #include "ngraph/frontend/onnx_import/onnx.hpp"
 #include "util/test_control.hpp"
 #include "util/type_prop.hpp"
+#include "util/test_tools.hpp"
 
 using namespace ngraph;
 using namespace ngraph::onnx_import;
@@ -41,7 +42,7 @@ NGRAPH_TEST(onnx_${BACKEND_NAME}, onnx_dynamic_dims_to_ngraph_dynamic_dims)
         EXPECT_EQ(static_cast<size_t>(input_ps.rank()), 2);
         EXPECT_TRUE(input_ps[0].is_dynamic());
         EXPECT_TRUE(input_ps[1].is_static());
-        EXPECT_EQ(static_cast<size_t>(input_ps[1]), 5);
+        EXPECT_EQ(static_cast<size_t>(input_ps[1]), 2);
     }
 
     const auto& graph_outputs = function->get_results();
@@ -53,5 +54,32 @@ NGRAPH_TEST(onnx_${BACKEND_NAME}, onnx_dynamic_dims_to_ngraph_dynamic_dims)
     EXPECT_EQ(static_cast<size_t>(out_ps.rank()), 2);
     EXPECT_TRUE(out_ps[0].is_dynamic());
     EXPECT_TRUE(out_ps[1].is_static());
-    EXPECT_EQ(static_cast<size_t>(out_ps[1]), 5);
+    EXPECT_EQ(static_cast<size_t>(out_ps[1]), 2);
+}
+
+NGRAPH_TEST(onnx_${BACKEND_NAME}, ab_plus_c_dynamic_execution)
+{
+    const auto function = onnx_import::import_onnx_model(
+        file_util::path_join(SERIALIZED_ZOO, "onnx/dynamic_shapes/ab_plus_c.prototxt"));
+
+    auto backend = runtime::Backend::create("${BACKEND_NAME}", true);
+    auto executable = backend->compile(function);
+
+    auto out_tensor = backend->create_dynamic_tensor(function->get_output_element_type(0),
+                                                     function->get_output_partial_shape(0));
+
+    auto input_A = backend->create_tensor(element::i64, Shape{1, 2});
+    auto input_B = backend->create_tensor(element::i64, Shape{1, 2});
+    auto input_C = backend->create_tensor(element::i64, Shape{1, 2});
+
+    copy_data(input_A, std::vector<int64_t>{1, 2});
+    copy_data(input_B, std::vector<int64_t>{3, 4});
+    copy_data(input_C, std::vector<int64_t>{5, 6});
+
+    executable->call_with_validate({out_tensor}, {input_A, input_B, input_C});
+
+    const auto results = read_vector<int64_t>(out_tensor);
+    const auto expected_results = std::vector<int64_t>{8, 14};
+
+    EXPECT_TRUE(results == expected_results);
 }
