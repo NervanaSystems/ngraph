65ecb315b2a69e5cd1d8c9a4834478ae5f67d7da
Sang Ik Lee <sang.ik.lee@intel.com>
2020-01-27 13:53:43 -0800

    Use modern CMake for gtest.

--- cmake/external_gtest.cmake
+++ cmake/external_gtest.cmake
@@ -21,77 +21,42 @@ include(ExternalProject)
 # Download and install GoogleTest ...
 #------------------------------------------------------------------------------
 
-SET(GTEST_GIT_REPO_URL https://github.com/google/googletest.git)
-SET(GTEST_GIT_LABEL release-1.8.1)
+set(GTEST_GIT_REPO_URL https://github.com/google/googletest.git)
+set(GTEST_GIT_LABEL release-1.8.1)
+set(GTEST_PROJECT_ROOT ${EXTERNAL_PROJECTS_ROOT}/gtest-project)
+set(GTEST_SOURCE_DIR ${GTEST_PROJECT_ROOT}/gtest-src)
+set(GTEST_BINARY_DIR ${GTEST_PROJECT_ROOT}/gtest-build)
 
-set(GMOCK_OUTPUT_DIR ${EXTERNAL_PROJECTS_ROOT}/gtest/build/googlemock)
-set(GTEST_OUTPUT_DIR ${GMOCK_OUTPUT_DIR}/gtest)
+configure_file(${CMAKE_SOURCE_DIR}/cmake/gtest_fetch.cmake.in ${GTEST_PROJECT_ROOT}/CMakeLists.txt @ONLY)
 
-if(WIN32)
-    list(APPEND GTEST_CMAKE_ARGS
-        -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE=${GTEST_OUTPUT_DIR}
-        -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG=${GTEST_OUTPUT_DIR}
-        -Dgtest_force_shared_crt=TRUE
-    )
-    set(GMOCK_OUTPUT_DIR ${GTEST_OUTPUT_DIR})
+execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}"
+    -DCMAKE_GENERATOR_PLATFORM:STRING=${CMAKE_GENERATOR_PLATFORM}
+    -DCMAKE_GENERATOR_TOOLSET:STRING=${CMAKE_GENERATOR_TOOLSET}
+    -DCMAKE_CXX_FLAGS:STRING=${GTEST_CXX_FLAGS} .
+    WORKING_DIRECTORY "${GTEST_PROJECT_ROOT}")
+
+# clone and build googletest
+include(ProcessorCount)
+ProcessorCount(N)
+if(("${CMAKE_GENERATOR}" STREQUAL "Unix Makefiles") AND (NOT N EQUAL 0))
+    execute_process(COMMAND "${CMAKE_COMMAND}" --build . -- -j${N}
+        WORKING_DIRECTORY "${GTEST_PROJECT_ROOT}")
+else()
+    execute_process(COMMAND "${CMAKE_COMMAND}" --build .
+        WORKING_DIRECTORY "${GTEST_PROJECT_ROOT}")
 endif()
 
-if(CMAKE_BUILD_TYPE)
-    list(APPEND GTEST_CMAKE_ARGS
-        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-    )
+add_subdirectory("${GTEST_SOURCE_DIR}"
+                 "${GTEST_BINARY_DIR}"
+)
+
+if(WIN32)
+    target_compile_definitions(gtest PRIVATE gtest_force_shared_crt=ON)
 endif()
 
 if(UNIX)
     # workaround for compile error
     # related: https://github.com/intel/mkl-dnn/issues/55
-    set(GTEST_CXX_FLAGS "-Wno-unused-result ${CMAKE_ORIGINAL_CXX_FLAGS} -Wno-undef")
-else()
-    set(GTEST_CXX_FLAGS ${CMAKE_ORIGINAL_CXX_FLAGS})
+    target_compile_options(gtext PRIVATE -Wno-unused-result -Wno-undef)
 endif()
 
-#Build for ninja
-SET(GTEST_PATHS ${GTEST_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gtestd${CMAKE_STATIC_LIBRARY_SUFFIX}
-    ${GMOCK_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gmockd${CMAKE_STATIC_LIBRARY_SUFFIX}
-    ${GTEST_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}
-    ${GMOCK_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gmock${CMAKE_STATIC_LIBRARY_SUFFIX})
-
-ExternalProject_Add(
-    ext_gtest
-    PREFIX gtest
-    GIT_REPOSITORY ${GTEST_GIT_REPO_URL}
-    GIT_TAG ${GTEST_GIT_LABEL}
-    # Disable install step
-    INSTALL_COMMAND ""
-    UPDATE_COMMAND ""
-    CMAKE_GENERATOR ${CMAKE_GENERATOR}
-    CMAKE_GENERATOR_PLATFORM ${CMAKE_GENERATOR_PLATFORM}
-    CMAKE_GENERATOR_TOOLSET ${CMAKE_GENERATOR_TOOLSET}
-    CMAKE_ARGS
-        ${NGRAPH_FORWARD_CMAKE_ARGS}
-        -DCMAKE_CXX_FLAGS=${GTEST_CXX_FLAGS}
-        ${GTEST_CMAKE_ARGS}
-    BINARY_DIR "${EXTERNAL_PROJECTS_ROOT}/gtest/build"
-    EXCLUDE_FROM_ALL TRUE
-    BUILD_BYPRODUCTS ${GTEST_PATHS}
-    )
-
-#------------------------------------------------------------------------------
-
-ExternalProject_Get_Property(ext_gtest SOURCE_DIR BINARY_DIR)
-
-add_library(libgtest INTERFACE)
-add_dependencies(libgtest ext_gtest ext_gmock)
-target_include_directories(libgtest SYSTEM INTERFACE
-    ${SOURCE_DIR}/googletest/include
-    ${SOURCE_DIR}/googlemock/include)
-
-if(LINUX OR APPLE OR WIN32)
-    target_link_libraries(libgtest INTERFACE
-        debug ${GTEST_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gtestd${CMAKE_STATIC_LIBRARY_SUFFIX}
-        debug ${GMOCK_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gmockd${CMAKE_STATIC_LIBRARY_SUFFIX}
-        optimized ${GTEST_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}
-        optimized ${GMOCK_OUTPUT_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gmock${CMAKE_STATIC_LIBRARY_SUFFIX})
-else()
-    message(FATAL_ERROR "libgtest: Unsupported platform.")
-endif()
