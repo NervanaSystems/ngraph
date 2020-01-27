23f2e21f1f8a78c5bd10310fb0250c9772ca000d
nbpatel <nishant.b.patel@intel.com>
2020-01-27 15:05:50 -0800

    MinMaxShapePropagation

---
+++ src/ngraph/pass/min_max_propagation.cpp
@@ -0,0 +1,45 @@
+//*****************************************************************************
+// Copyright 2017-2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+
+#include "ngraph/pass/min_max_propagation.hpp"
+#include "ngraph/graph_util.hpp"
+
+using namespace ngraph;
+
+bool pass::MinMaxShapePropagation::run_on_function(std::shared_ptr<Function> f)
+{
+    for (auto& node : f->get_ordered_ops())
+    {
+        if (node->is_parameter() || node->is_constant())
+        {
+            continue;
+        }
+
+        // Check if the output shape of the node is dynamic
+        if (node->get_output_partial_shape(0).is_dynamic())
+        {
+            // if max shape is set for the output shape, propagate the max.
+            if (!node->output(0).get_max_partial_shape().is_dynamic())
+            {
+                node->set_output_max_partial_shape(0, node->output(0).get_max_partial_shape());
+                // Propagate the shape
+                f->validate_nodes_and_infer_types();
+                // propagate the max ?
+            }
+        }
+    }
+    return true;
+}
