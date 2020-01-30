fea36e6935e8c5f1fd66355f1ea45bdf675dd7ed
Robert Kimball <robert.kimball@intel.com>
2020-01-30 12:54:31 -0800

    Fix compile error

--- src/ngraph/op/constant.cpp
+++ src/ngraph/op/constant.cpp
@@ -303,7 +303,7 @@ op::Constant::Constant(const Constant& other)
     : m_element_type(other.m_element_type)
     , m_shape(other.m_shape)
     , m_data(other.m_data)
-    , m_all_elements_bitwise_identical(m_all_elements_bitwise_identical)
+    , m_all_elements_bitwise_identical(other.m_all_elements_bitwise_identical)
 {
     constructor_validate_and_infer_types();
 }
