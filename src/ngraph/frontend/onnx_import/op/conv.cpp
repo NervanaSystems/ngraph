b364d096c5918256e9e21e19ea729db15e81aa33
Gleb Kazantaev <gleb.kazantaev@intel.com>
2020-01-30 16:46:22 +0300

    Update src/ngraph/frontend/onnx_import/op/conv.cpp
    Co-Authored-By: Michał Karzyński <postrational@users.noreply.github.com>

--- src/ngraph/frontend/onnx_import/op/conv.cpp
+++ src/ngraph/frontend/onnx_import/op/conv.cpp
@@ -134,7 +134,7 @@ namespace ngraph
 
                     auto reshaped_bias = std::make_shared<ngraph::opset1::Reshape>(
                         bias,
-                        ngraph::op::Constant::create(
+                        default_opset::Constant::create(
                             element::i64, Shape{new_shape.size()}, new_shape),
                         true);
                     return {std::make_shared<default_opset::Add>(conv_node, reshaped_bias)};
