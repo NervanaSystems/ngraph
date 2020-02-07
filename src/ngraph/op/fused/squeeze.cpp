0320f2d5a2b3d39e946ce6a3b6d77ace99097601
Evgenya Stepyreva <evgenya.stepyreva@intel.com>
2020-02-08 00:40:56 +0300

    Discussion resolved

--- src/ngraph/op/fused/squeeze.cpp
+++ src/ngraph/op/fused/squeeze.cpp
@@ -37,21 +37,15 @@ void op::Squeeze::pre_validate_and_infer_types()
     auto data = input_value(0);
     auto axes_node = input_value(1).get_node_shared_ptr();
 
-    // Currently only support Constant node for axes.
-    NODE_VALIDATION_CHECK(this,
-                          axes_node->is_constant(),
-                          "doesn't support 'axes' input of other type than a Constant.");
+     if (data.get_partial_shape().is_dynamic() || !axes_node->is_constant())
+    {
+        set_output_type(0, get_input_element_type(0), PartialShape::dynamic());
+        return;
+     }
 
     // Get value of axes from Constant
     auto axes_constant = as_type_ptr<op::Constant>(axes_node);
     auto axes = axes_constant->cast_vector<size_t>();
-
-    if (data.get_partial_shape().is_dynamic())
-    {
-        set_output_type(0, get_input_element_type(0), PartialShape::dynamic());
-        return;
-    }
-
     auto data_shape = data.get_shape();
     std::vector<uint64_t> axes_to_squeeze(data_shape.size());
 
