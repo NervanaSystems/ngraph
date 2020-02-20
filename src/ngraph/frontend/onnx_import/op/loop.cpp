9c36b0a9fe10353335c81873cb1420f5670cea62
Adam Osewski <adam.osewski@intel.com>
2020-02-20 17:55:54 +0100

    Multiple refinements
    - Additional check for not supported loop_cond
    - Fix loop body attribute name
    - Use Range to create sliceable input for loop iteration count.
    - Fix configuring of final output values
    - Manually convert TensorIterator outputs from Output<Node> to Node

--- src/ngraph/frontend/onnx_import/op/loop.cpp
+++ src/ngraph/frontend/onnx_import/op/loop.cpp
@@ -18,10 +18,10 @@
 #include <iterator>
 #include <memory>
 
+#include "core/graph.hpp"
+#include "default_opset.hpp"
 #include "exceptions.hpp"
-#include "graph.hpp"
 #include "ngraph/function.hpp"
-#include "ngraph/op/tensor_iterator.hpp"
 
 namespace ngraph
 {
@@ -41,14 +41,18 @@ namespace ngraph
                     // At this moment nGraph TensorIterator doesn't have support for conditional
                     // termination of iterations.
                     CHECK_VALID_NODE(node,
-                                     !trip_count.is_null(),
+                                     !trip_count->is_null(),
                                      "Currently nGraph requires trip count input to be provided.");
+                    CHECK_VALID_NODE(node,
+                                     loop_cond->is_null(),
+                                     "Currently nGraph doesn't support conditional loop "
+                                     "termination.");
 
                     const OutputVector loop_carried_dependencies{std::next(ng_inputs.begin(), 2),
                                                                  ng_inputs.end()};
 
                     // required
-                    const Graph& body_graph{node.get_attribute_value<Graph>("graph")};
+                    const Graph& body_graph{node.get_attribute_value<Graph>("body")};
                     const auto& graph_outputs{
                         ngraph::as_output_vector(body_graph.get_ng_outputs())};
                     const auto& graph_inputs{body_graph.get_ng_parameters()};
@@ -71,17 +75,33 @@ namespace ngraph
                                      loop_carried_dependencies.size() + 1);
 
                     // create the loop body
-                    const auto& body = std::make_shared<ngraph::op::TensorIterator::BodyLambda>(
+                    const auto body = std::make_shared<ngraph::op::TensorIterator::BodyLambda>(
                         graph_outputs, graph_inputs);
-                    auto tensor_iterator = std::make_shared<op::TensorIterator>();
+                    auto tensor_iterator = std::make_shared<ngraph::op::TensorIterator>();
                     tensor_iterator->set_body(body);
 
-                    // We itearate over trip_count input.
+                    // TensorIterator need to iterate over some input, thus we have to create
+                    // 1 dim tensor with number of values equal to value provided by trip_count
+                    // input.
+                    const auto loop_trip_count = std::make_shared<default_opset::Range>(
+                        default_opset::Constant::create(
+                            trip_count->get_element_type(), Shape{}, {0}),
+                        trip_count,
+                        default_opset::Constant::create(
+                            trip_count->get_element_type(), Shape{}, {1}));
+
+                    // We iterate over trip_count input.
+                    // start=0, stride=1, part_size=1, end=-1, axis=0
                     tensor_iterator->set_sliced_input(
-                        trip_count, graph_inputs.at(0), 0, 1, 1, -1, 0);
+                        graph_inputs.at(0), loop_trip_count, 0, 1, 1, -1, 0);
+
+                    // TODO: Remove when loop condition would be supported.
+                    const auto& cond_node =
+                        default_opset::Constant::create(element::boolean, Shape{}, {true});
+
                     // Set loop condition input, which should be changing over the iterations.
                     tensor_iterator->set_merged_input(
-                        graph_inputs.at(1), loop_cond, graph_outputs.at(0));
+                        graph_inputs.at(1), cond_node, graph_outputs.at(0));
 
                     // Setting up other Loop body inputs.
                     auto graph_inputs_it = std::next(graph_inputs.begin(), 2);
@@ -89,12 +109,12 @@ namespace ngraph
 
                     // Set-up loop carried dependencies and final output values
                     OutputVector final_values;
-                    for (const& dep : loop_carried_dependencies)
+                    for (const auto& dep : loop_carried_dependencies)
                     {
                         tensor_iterator->set_merged_input(
                             *graph_inputs_it++, dep, *graph_outputs_it);
-                        final_values->get_iter_value(*graph_outputs_it, -1);
-                        graph_outputs_it++;
+                        final_values.push_back(
+                            tensor_iterator->get_iter_value(*graph_outputs_it++, -1));
                     }
 
                     // Set-up scan outputs
@@ -102,14 +122,19 @@ namespace ngraph
                     for (; graph_outputs_it != graph_outputs.end(); graph_outputs_it++)
                     {
                         // TODO: does concatenating along 0 axis is right?
+                        // start=0, stride=1, part_size=1, end=-1, axis=0
                         scan_outputs.push_back(tensor_iterator->get_concatenated_slices(
                             *graph_outputs_it, 0, 1, 1, -1, 0));
                     }
 
-                    NodeVector node_outputs(ngraph::as_node_vector(final_values));
-                    for (const& value : scan_outputs)
+                    NodeVector node_outputs;
+                    for (const auto& v : final_values)
                     {
-                        node_outputs.push_back(value.as_single_output_node());
+                        node_outputs.push_back(v.as_single_output_node(false));
+                    }
+                    for (const auto& v : scan_outputs)
+                    {
+                        node_outputs.push_back(v.as_single_output_node(false));
                     }
                     return node_outputs;
                 }
