ea948943763d05634f50ee4943db2bc694f634cc
tomdol <tomasz.dolbniak@intel.com>
2020-02-26 14:42:48 +0100

    Code review

--- python/ngraph/impl/__init__.py
+++ python/ngraph/impl/__init__.py
@@ -27,6 +27,9 @@ import six
 if sys.platform == 'win32':
     # ngraph.dll is installed 3 directories above by default
     # and this path needs to be visible to the _pyngraph module
+    #
+    # If you're using a custom installation of nGraph,
+    # add the location of ngraph.dll to your system PATH.
     ngraph_dll = os.path.join(os.path.dirname(__file__), '..', '..', '..')
     os.environ['PATH'] = os.path.abspath(ngraph_dll) + ';' + os.environ['PATH']
 else:
