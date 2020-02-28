c39ec0aa1e72a9567a4f0d31409812b2b2e08c8a
mitruska <katarzyna.mitrus@intel.com>
2020-02-28 15:30:55 +0100

    Add test for Elu

--- test/attributes.cpp
+++ test/attributes.cpp
@@ -260,6 +260,20 @@ protected:
     NodeSaver m_values;
 };
 
+TEST(attributes, elu_op)
+{
+    FactoryRegistry<Node>::get().register_factory<opset1::Elu>();
+    auto data = make_shared<op::Parameter>(element::f32, Shape{2, 4});
+
+    double alpha = 0.1;
+
+    const auto elu = make_shared<opset1::Elu>(data, alpha);
+    NodeBuilder builder(elu);
+    auto g_elu = as_type_ptr<opset1::Elu>(builder.create());
+
+    EXPECT_EQ(g_elu->get_alpha(), elu->get_alpha());
+}
+
 TEST(attributes, lrn_op)
 {
     FactoryRegistry<Node>::get().register_factory<opset1::LRN>();
