13b621b9cb77a39e6e4edc4e03c866a1e94b0fbf
mitruska <katarzyna.mitrus@intel.com>
2020-02-28 13:32:22 +0100

    Add test for PSROIPooling

--- test/attributes.cpp
+++ test/attributes.cpp
@@ -407,3 +407,29 @@ TEST(attributes, one_hot_op)
 
     EXPECT_EQ(g_one_hot->get_axis(), one_hot->get_axis());
 }
+
+TEST(attributes, psroi_pooling_op)
+{
+    FactoryRegistry<Node>::get().register_factory<opset1::PSROIPooling>();
+    auto input = make_shared<op::Parameter>(element::f32, Shape{1, 1024, 63, 38});
+    auto coords = make_shared<op::Parameter>(element::f32, Shape{300, 5});
+
+    const int64_t output_dim = 882;
+    const int64_t group_size = 3;
+    const float spatial_scale = 0.0625;
+    int spatial_bins_x = 1;
+    int spatial_bins_y = 1;
+    string mode = "Avg";
+
+    auto psroi_pool = make_shared<opset1::PSROIPooling>(
+        input, coords, output_dim, group_size, spatial_scale, spatial_bins_x, spatial_bins_y, mode);
+    NodeBuilder builder(psroi_pool);
+    auto g_psroi_pool = as_type_ptr<opset1::PSROIPooling>(builder.create());
+
+    EXPECT_EQ(g_psroi_pool->get_output_dim(), psroi_pool->get_output_dim());
+    EXPECT_EQ(g_psroi_pool->get_group_size(), psroi_pool->get_group_size());
+    EXPECT_EQ(g_psroi_pool->get_spatial_scale(), psroi_pool->get_spatial_scale());
+    EXPECT_EQ(g_psroi_pool->get_spatial_bins_x(), psroi_pool->get_spatial_bins_x());
+    EXPECT_EQ(g_psroi_pool->get_spatial_bins_y(), psroi_pool->get_spatial_bins_y());
+    EXPECT_EQ(g_psroi_pool->get_mode(), psroi_pool->get_mode());
+}
