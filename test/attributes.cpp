e339b8278c8daeecb9ce5c5ac76a2644dfa63301
mitruska <katarzyna.mitrus@intel.com>
2020-03-02 10:57:33 +0100

    Add tests for GroupConvolution and GroupCinvolutionBackpropData

--- test/attributes.cpp
+++ test/attributes.cpp
@@ -334,6 +334,60 @@ TEST(attributes, grn_op)
     EXPECT_EQ(g_grn->get_bias(), grn->get_bias());
 }
 
+TEST(attributes, group_conv_op)
+{
+    FactoryRegistry<Node>::get().register_factory<opset1::GroupConvolution>();
+    auto data = make_shared<op::Parameter>(element::f32, Shape{1, 12, 224, 224});
+    auto filters = make_shared<op::Parameter>(element::f32, Shape{4, 1, 3, 5, 5});
+    auto strides = Strides{1, 1};
+    auto pads_begin = CoordinateDiff{1, 2};
+    auto pads_end = CoordinateDiff{1, 2};
+    auto dilations = Strides{1, 1};
+    auto group_conv = make_shared<opset1::GroupConvolution>(
+        data, filters, strides, pads_begin, pads_end, dilations, op::PadType::VALID);
+    NodeBuilder builder(group_conv);
+    auto g_group_conv = as_type_ptr<opset1::GroupConvolution>(builder.create());
+    EXPECT_EQ(g_group_conv->get_strides(), group_conv->get_strides());
+    EXPECT_EQ(g_group_conv->get_pads_begin(), group_conv->get_pads_begin());
+    EXPECT_EQ(g_group_conv->get_pads_end(), group_conv->get_pads_end());
+    EXPECT_EQ(g_group_conv->get_dilations(), group_conv->get_dilations());
+    EXPECT_EQ(g_group_conv->get_auto_pad(), group_conv->get_auto_pad());
+}
+
+TEST(attributes, group_conv_backprop_data_op)
+{
+    FactoryRegistry<Node>::get().register_factory<opset1::GroupConvolutionBackpropData>();
+    const auto data = make_shared<op::Parameter>(element::f32, Shape{1, 20, 224, 224});
+    const auto filter = make_shared<op::Parameter>(element::f32, Shape{4, 5, 2, 3, 3});
+    const auto output_shape = make_shared<op::Parameter>(element::f32, Shape{1, 8, 447, 447});
+
+    const auto strides = Strides{2, 1};
+    const auto pads_begin = CoordinateDiff{3, 4};
+    const auto pads_end = CoordinateDiff{4, 6};
+    const auto dilations = Strides{3, 1};
+    const auto auto_pad = op::PadType::EXPLICIT;
+    const auto output_padding = CoordinateDiff{3, 4};
+
+    const auto gcbd = make_shared<opset1::GroupConvolutionBackpropData>(data,
+                                                                        filter,
+                                                                        output_shape,
+                                                                        strides,
+                                                                        pads_begin,
+                                                                        pads_end,
+                                                                        dilations,
+                                                                        auto_pad,
+                                                                        output_padding);
+    NodeBuilder builder(gcbd);
+    const auto g_gcbd = as_type_ptr<opset1::GroupConvolutionBackpropData>(builder.create());
+
+    EXPECT_EQ(g_gcbd->get_strides(), gcbd->get_strides());
+    EXPECT_EQ(g_gcbd->get_pads_begin(), gcbd->get_pads_begin());
+    EXPECT_EQ(g_gcbd->get_pads_end(), gcbd->get_pads_end());
+    EXPECT_EQ(g_gcbd->get_dilations(), gcbd->get_dilations());
+    EXPECT_EQ(g_gcbd->get_auto_pad(), gcbd->get_auto_pad());
+    EXPECT_EQ(g_gcbd->get_output_padding(), gcbd->get_output_padding());
+}
+
 TEST(attributes, lrn_op)
 {
     FactoryRegistry<Node>::get().register_factory<opset1::LRN>();
