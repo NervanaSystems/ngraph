44dbff2e63b6744600398fcf6d2db40b7d5c042c
Robert Kimball <robert.kimball@intel.com>
2020-03-03 14:10:58 -0800

    wip

--- src/ngraph/pass/constant_folding_pad.cpp
+++ src/ngraph/pass/constant_folding_pad.cpp
@@ -30,7 +30,7 @@ shared_ptr<op::Constant> fold_constant_pad(shared_ptr<op::Constant> constant,
     runtime::AlignedBuffer buffer(shape_size(out_shape) * sizeof(T));
     T* data_ptr = buffer.get_ptr<T>();
     auto pad_value = std::static_pointer_cast<op::Constant>(
-        pad->input(1).get_source_output().get_node_shared_ptr());
+        pad->get_input_node_shared_ptr(1));
 
     if (func != nullptr)
     {
