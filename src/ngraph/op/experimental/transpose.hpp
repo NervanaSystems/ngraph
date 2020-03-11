84e2735d4dac614c9c82c6a15d7fc80878b0bea3
Robert Kimball <robert.kimball@intel.com>
2020-03-11 13:07:24 -0700

    Temporary fix for dldt

--- src/ngraph/op/experimental/transpose.hpp
+++ src/ngraph/op/experimental/transpose.hpp
@@ -48,6 +48,9 @@ namespace ngraph
                 virtual std::shared_ptr<Node>
                     clone_with_new_inputs(const OutputVector& new_args) const override;
 
+                virtual std::shared_ptr<Node>
+                    copy_with_new_args(const NodeVector& new_args) const override;
+
             protected:
                 virtual void generate_adjoints(autodiff::Adjoints& adjoints,
                                                const OutputVector& deltas) override;
