61545c703b439b0f7ad4b1c10928e43a48c51a73
mitruska <katarzyna.mitrus@intel.com>
2020-03-20 10:49:11 +0100

    More type_prop tests

--- test/type_prop/scatter_elements_update.cpp
+++ test/type_prop/scatter_elements_update.cpp
@@ -21,7 +21,7 @@
 using namespace std;
 using namespace ngraph;
 
-TEST(type_prop, scatter_elements_update_check_output_shape)
+TEST(type_prop, scatter_elements_update_output_shape)
 {
     Shape data_shape{2, 4, 5, 7};
     Shape indices_shape{2, 2, 2, 2};
@@ -39,17 +39,17 @@ TEST(type_prop, scatter_elements_update_check_output_shape)
     EXPECT_EQ(scatter->get_output_shape(0), expected_output_shape);
 }
 
-TEST(type_prop, scatter_elements_update_check_axis_validation)
+TEST(type_prop, scatter_elements_update_axis_validation)
 {
     Shape data_shape{2, 4, 5, 7};
-    Shape indices_shape{2, 2};
+    Shape indices_shape{2, 2, 2, 2};
     Shape updates_shape{2, 2, 2, 2};
     Shape axis_shape{};
 
     auto data = make_shared<op::Parameter>(element::f32, data_shape);
     auto indices = make_shared<op::Parameter>(element::i16, indices_shape);
     auto updates = make_shared<op::Parameter>(element::f32, updates_shape);
-    auto axis = make_shared<op::Constant>(element::i16, axis_shape, 8);
+    auto axis = make_shared<op::Constant>(element::i16, axis_shape, std::vector<int>{8});
 
     try
     {
@@ -59,6 +59,88 @@ TEST(type_prop, scatter_elements_update_check_axis_validation)
     catch (const NodeValidationFailure& error)
     {
         EXPECT_HAS_SUBSTRING(error.what(), std::string("Axis value has to be in range"));
+    }
+    catch (...)
+    {
+        FAIL() << "Deduced type check failed for unexpected reason";
+    }
+}
+
+
+TEST(type_prop, scatter_elements_updates_indices_shape)
+{
+    Shape data_shape{2, 4, 5, 7};
+    Shape indices_shape{3, 3, 3, 3};
+    Shape updates_shape{2, 2, 2, 2};
+    Shape axis_shape{};
+
+    auto data = make_shared<op::Parameter>(element::f32, data_shape);
+    auto indices = make_shared<op::Parameter>(element::i16, indices_shape);
+    auto updates = make_shared<op::Parameter>(element::f32, updates_shape);
+    auto axis = make_shared<op::Constant>(element::i16, axis_shape, std::vector<int>{1});
+
+    try
+    {
+        auto scatter = make_shared<op::v3::ScatterElementsUpdate>(data, indices, updates, axis);
+        FAIL() << "Not detected incompatibile indices and updates shape";
+    }
+    catch (const NodeValidationFailure& error)
+    {
+        EXPECT_HAS_SUBSTRING(error.what(), std::string("Indices and updates input shapes are required to be equal"));
+    }
+    catch (...)
+    {
+        FAIL() << "Deduced type check failed for unexpected reason";
+    }
+}
+
+TEST(type_prop, scatter_elements_updates_indices_rank)
+{
+    Shape data_shape{2, 4};
+    Shape indices_shape{2, 2};
+    Shape updates_shape{2, 2, 2, 2};
+    Shape axis_shape{};
+
+    auto data = make_shared<op::Parameter>(element::f32, data_shape);
+    auto indices = make_shared<op::Parameter>(element::i16, indices_shape);
+    auto updates = make_shared<op::Parameter>(element::f32, updates_shape);
+    auto axis = make_shared<op::Constant>(element::i16, axis_shape, std::vector<int>{1});
+
+    try
+    {
+        auto scatter = make_shared<op::v3::ScatterElementsUpdate>(data, indices, updates, axis);
+        FAIL() << "Not detected incompatibile indices and updates shape";
+    }
+    catch (const NodeValidationFailure& error)
+    {
+        EXPECT_HAS_SUBSTRING(error.what(), std::string("Indices and updates input shapes are required to be equal"));
+    }
+    catch (...)
+    {
+        FAIL() << "Deduced type check failed for unexpected reason";
+    }
+}
+
+TEST(type_prop, scatter_elements_data_indices_rank)
+{
+    Shape data_shape{2, 4, 5, 7};
+    Shape indices_shape{2, 2};
+    Shape updates_shape{2, 2};
+    Shape axis_shape{};
+
+    auto data = make_shared<op::Parameter>(element::f32, data_shape);
+    auto indices = make_shared<op::Parameter>(element::i16, indices_shape);
+    auto updates = make_shared<op::Parameter>(element::f32, updates_shape);
+    auto axis = make_shared<op::Constant>(element::i16, axis_shape, std::vector<int>{1});
+
+    try
+    {
+        auto scatter = make_shared<op::v3::ScatterElementsUpdate>(data, indices, updates, axis);
+        FAIL() << "Not detected incompatibile indices and data rank";
+    }
+    catch (const NodeValidationFailure& error)
+    {
+        EXPECT_HAS_SUBSTRING(error.what(), std::string("Indices rank and data rank are required to be equal"));
     }
     catch (...)
     {
