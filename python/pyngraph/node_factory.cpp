04538827776eff5d0aa40883226bf747913fba26
Adam Osewski <adam.osewski@intel.com>
2020-04-03 18:37:45 +0200

    Remove dependency from ngraph::to_lower which symbol is unvisible.

--- python/pyngraph/node_factory.cpp
+++ python/pyngraph/node_factory.cpp
@@ -14,6 +14,7 @@
 // limitations under the License.
 //*****************************************************************************
 
+#include <algorithm>
 #include <memory>
 #include <string>
 #include <vector>
@@ -139,7 +140,9 @@ public:
 private:
     const ngraph::OpSet& get_opset(const std::string& opset_name)
     {
-        const std::string& opset_name_{ngraph::to_lower(opset_name)};
+        std::string opset_name_ = opset_name;
+        std::transform(opset_name_.begin(), opset_name_.end(), opset_name_.begin(), std::tolower);
+
         if (opset_name_ == "opset0")
         {
             return ngraph::get_opset0();
