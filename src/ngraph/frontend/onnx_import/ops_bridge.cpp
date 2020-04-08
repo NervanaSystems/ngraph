d90be4070a1f17f804c728dac5337e6ed77bc70c
mitruska <katarzyna.mitrus@intel.com>
2020-04-08 12:48:25 +0200

    Merge Resize-10 mitruska/onnx-resize

--- src/ngraph/frontend/onnx_import/ops_bridge.cpp
+++ src/ngraph/frontend/onnx_import/ops_bridge.cpp
@@ -102,6 +102,7 @@
 #include "op/reduce.hpp"
 #include "op/relu.hpp"
 #include "op/reshape.hpp"
+#include "op/resize.hpp"
 #include "op/reverse_sequence.hpp"
 #include "op/roi_align.hpp"
 #include "op/round.hpp"
@@ -340,6 +341,7 @@ namespace ngraph
             REGISTER_OPERATOR("ReduceSumSquare", 1, reduce_sum_square);
             REGISTER_OPERATOR("Relu", 1, relu);
             REGISTER_OPERATOR("Reshape", 1, reshape);
+            REGISTER_OPERATOR("Resize", 1, resize);
             REGISTER_OPERATOR("ReverseSequence", 1, reverse_sequence);
             REGISTER_OPERATOR("RoiAlign", 1, roi_align);
             REGISTER_OPERATOR("Round", 1, round);
