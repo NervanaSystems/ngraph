c93f80e41592567310d109c26dbdb896638a777d
mbencer <mateusz.bencer@intel.com>
2020-04-14 17:38:02 +0200

    added normalized axes casting

--- src/ngraph/frontend/onnx_import/op/slice.cpp
+++ src/ngraph/frontend/onnx_import/op/slice.cpp
@@ -185,8 +185,10 @@ namespace ngraph
                     std::vector<uint64_t> axes_vec;
                     if (data_rank.is_static())
                     {
-                        const auto normalized_axes_vec = normalize_axes(node.get_description(), raw_axes_vec, data_rank);
-                        axes_vec = std::vector<uint64_t>(std::begin(normalized_axes_vec), std::begin(normalized_axes_vec));
+                        const auto normalized_axes_vec =
+                            normalize_axes(node.get_description(), raw_axes_vec, data_rank);
+                        axes_vec = std::vector<uint64_t>(std::begin(normalized_axes_vec),
+                                                         std::begin(normalized_axes_vec));
                     }
                     else
                     {
