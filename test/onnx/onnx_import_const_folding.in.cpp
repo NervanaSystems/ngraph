4d93bead494cc78374ad515e818eee07371a3fec
mitruska <katarzyna.mitrus@intel.com>
2020-04-14 20:50:22 +0200

    Rewrite all of the tests to use new helper

--- test/onnx/onnx_import_const_folding.in.cpp
+++ test/onnx/onnx_import_const_folding.in.cpp
@@ -63,78 +63,39 @@ namespace
     }
 }
 
-
-
-NGRAPH_TEST(onnx_${BACKEND_NAME}, model_scatter_elements_import_only)
+NGRAPH_TEST(onnx_${BACKEND_NAME}_const_folding, model_scatter_elements)
 {
-    const auto scatter_fn = onnx_import::import_onnx_model(
+    const auto fn = onnx_import::import_onnx_model(
         file_util::path_join(SERIALIZED_ZOO, "onnx/scatter_elements_opset11.prototxt"));
 
-    const Shape data_shape{1, 5};
-
-    EXPECT_EQ(scatter_fn->get_output_size(), 1);
-    EXPECT_EQ(scatter_fn->get_output_shape(0), data_shape);
-    EXPECT_EQ(count_ops_of_type<op::v3::ScatterElementsUpdate>(scatter_fn), 1);
-    EXPECT_EQ(count_ops_of_type<op::v0::Constant>(scatter_fn), 4);
+    test_constant_folding<float>(fn, {1.0, 1.1, 3.0, 2.1, 5.0}, Shape{1, 5});
 }
 
-NGRAPH_TEST(onnx_${BACKEND_NAME}, model_scatter_elements_const_folding)
-{
-    const auto scatter_fn = onnx_import::import_onnx_model(
-        file_util::path_join(SERIALIZED_ZOO, "onnx/scatter_elements_opset11.prototxt"));
-
-    ngraph::pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ConstantFolding>();
-    pass_manager.run_passes(scatter_fn);
-
-    const Shape data_shape{1, 5};
-
-    EXPECT_EQ(scatter_fn->get_output_size(), 1);
-    EXPECT_EQ(scatter_fn->get_output_shape(0), data_shape);
-    EXPECT_EQ(count_ops_of_type<op::v3::ScatterElementsUpdate>(scatter_fn), 0);
-    EXPECT_EQ(count_ops_of_type<op::v0::Constant>(scatter_fn), 1);
-
-    const auto result_node =
-        as_type_ptr<op::Constant>(scatter_fn->get_results().at(0)->get_argument(0));
-    const auto expected_output = std::vector<float>{1.0, 1.1, 3.0, 2.1, 5.0};
-    EXPECT_TRUE(ngraph::test::all_close(result_node->cast_vector<float>(), expected_output));
-}
-
-
-
-NGRAPH_TEST(onnx_${BACKEND_NAME}, model_non_zero_scalar)
+NGRAPH_TEST(onnx_${BACKEND_NAME}_const_folding, model_non_zero_scalar)
 {
     const auto fn = onnx_import::import_onnx_model(
         file_util::path_join(SERIALIZED_ZOO, "onnx/non_zero_scalar.prototxt"));
 
-    test_non_zero_constant_folding(fn, {0}, Shape{1, 1});
+    test_constant_folding<int64_t>(fn, {0}, Shape{1, 1});
 }
 
-NGRAPH_TEST(onnx_${BACKEND_NAME}, model_non_zero_1d)
+NGRAPH_TEST(onnx_${BACKEND_NAME}_const_folding, model_non_zero_1d)
 {
     const auto fn = onnx_import::import_onnx_model(
         file_util::path_join(SERIALIZED_ZOO, "onnx/non_zero_1d.prototxt"));
 
-    ngraph::pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ConstantFolding>();
-    pass_manager.run_passes(fn);
-
-    test_non_zero_constant_folding(fn, {1, 2, 4}, Shape{1, 3});
+    test_constant_folding<int64_t>(fn, {1, 2, 4}, Shape{1, 3});
 }
 
-NGRAPH_TEST(onnx_${BACKEND_NAME}, model_non_zero_1d_float)
+NGRAPH_TEST(onnx_${BACKEND_NAME}_const_folding, model_non_zero_1d_float)
 {
     const auto fn = onnx_import::import_onnx_model(
         file_util::path_join(SERIALIZED_ZOO, "onnx/non_zero_1d_float.prototxt"));
 
-    ngraph::pass::Manager pass_manager;
-    pass_manager.register_pass<pass::ConstantFolding>();
-    pass_manager.run_passes(fn);
-
-    test_non_zero_constant_folding(fn, {0, 1, 3, 4, 5, 6, 7, 8, 9});
+    test_constant_folding<int64_t>(fn, {0, 1, 3, 4, 5, 6, 7, 8, 9});
 }
 
-NGRAPH_TEST(onnx_${BACKEND_NAME}, model_non_zero_3d)
+NGRAPH_TEST(onnx_${BACKEND_NAME}_const_folding, model_non_zero_3d)
 {
     const auto fn = onnx_import::import_onnx_model(
         file_util::path_join(SERIALIZED_ZOO, "onnx/non_zero_3d.prototxt"));
@@ -143,14 +104,14 @@ NGRAPH_TEST(onnx_${BACKEND_NAME}, model_non_zero_3d)
     // {0, 0, 0, 1, 1, 2, 2}
     // {0, 0, 1, 0, 1, 0, 1}
     // {0, 1, 1, 1, 0, 0, 1}
-    test_non_zero_constant_folding(fn,
+    test_constant_folding<int64_t>(fn,
                                    {0, 0, 0, 1, 1, 2, 2, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1});
 }
 
-NGRAPH_TEST(onnx_${BACKEND_NAME}, model_non_zero_2d_bool)
+NGRAPH_TEST(onnx_${BACKEND_NAME}_const_folding, model_non_zero_2d_bool)
 {
     const auto fn = onnx_import::import_onnx_model(
         file_util::path_join(SERIALIZED_ZOO, "onnx/non_zero_2d_bool.prototxt"));
 
-    test_non_zero_constant_folding(fn, {0, 1, 1, 0});
+    test_constant_folding<int64_t>(fn, {0, 1, 1, 0});
 }
