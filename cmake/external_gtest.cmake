b59c88c7a924f3fc3d2550a44a5e1eb4ff20e8f1
Sang Ik Lee <sang.ik.lee@intel.com>
2020-04-15 15:35:29 -0700

    Use shallow clone.

--- cmake/external_gtest.cmake
+++ cmake/external_gtest.cmake
@@ -21,6 +21,7 @@ if(NOT TARGET gtest)
       googletest
       GIT_REPOSITORY https://github.com/google/googletest.git
       GIT_TAG        release-1.8.1
+      GIT_SHALLOW    1
     )
 
     set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
