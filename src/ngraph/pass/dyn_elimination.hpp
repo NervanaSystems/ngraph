acc9ad9a8a57ebc144c544da1719cfb05a1dfd4d
Yimei Sun <yimei.sun@intel.com>
2020-04-16 09:23:09 -0700

    Add doxygen description for dynamic elimination pass

--- src/ngraph/pass/dyn_elimination.hpp
+++ src/ngraph/pass/dyn_elimination.hpp
@@ -23,6 +23,33 @@ namespace ngraph
 {
     namespace pass
     {
+        /// \brief The DynElimination pass finds dynamic operations in a graph whose
+        /// shape relevant inputs have already been resolved to static values, and
+        /// replaces those dynamic operations with the equivalent operations using
+        /// statical inputs and attributes.
+        /// \details This pass should be executed after the ConstantFolding pass.
+        ///
+        /// The ConstantFolding and DynElimination pass are used together to transform
+        /// dynamic operations in a computation graph to static operations when the
+        /// graph is executed with input data.
+        ///
+        /// In the example shown below, the original graph is constructed with dynamic
+        /// broadcast operation. When the graph is executed with input data, the input
+        /// shapes become available, by applying the ConstantFolding and DynElimination
+        /// pass, the graph is updated with dynamic broadcast being replaced by static
+        /// broadcast operation.
+        /// <table>
+        /// <tr>
+        ///     <th>Original</th>
+        ///     <th>After %ConstantFolding
+        ///     <th>After %DynElimination</th>
+        /// </tr>
+        /// <tr>
+        ///      <td> \image html dyn_broadcast_pre_constfld.svg </td>
+        ///      <td> \image html dyn_broadcast_post_constfld.svg </td>
+        ///      <td> \image html dyn_broadcast_post_dyneliminate.svg </td>
+        /// </tr>
+        /// </table>
         class NGRAPH_API DynElimination : public GraphRewrite
         {
         public:
