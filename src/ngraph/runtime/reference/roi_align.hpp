a076389bf05a112d4ea9c63b5b7f6a8341557871
Wenzhe Xue <wenzhe.xue@intel.com>
2020-04-28 15:06:38 -0700

    add comments

--- src/ngraph/runtime/reference/roi_align.hpp
+++ src/ngraph/runtime/reference/roi_align.hpp
@@ -26,8 +26,8 @@ namespace ngraph
         namespace reference
         {
             template <typename T>
-            inline void roi_align(T* in,
-                                  T* boxes,
+            inline void roi_align(T* in, // [N, C, H, W]
+                                  T* boxes, // each row [x_1, y_1, x_2, y_2]
                                   T* batch_indices,
                                   T* out,
                                   const int pooled_h,
@@ -36,6 +36,35 @@ namespace ngraph
                                   const float spatial_scale,
                                   const op::v3::ROIAlign::PoolingMode& mode)
             {
+                // x1 = boxes[0] * spatial_scale
+                // y1 = boxes[1] * spatial_scale
+                // x2 = boxes[2] * spatial_scale
+                // y2 = boxes[3] * spatial_scale
+                // roi_w = max(x2 - x1, 1);
+                // roi_h = max(y2 - y1, 1);
+                // bin_w = roi_w / pooled_w;
+                // bin_h = roi_h / pooled_h;
+
+                // sampling_w = sampling_ratio * pooled_w;
+                // sampling_h = sampling_ratio * pooled_h;
+                
+                // sampling_x1 = x1 + 0.5 * bin_w / sampling_ratio;
+                // sampling_y1 = y1 + 0.5 * bin_h / sampling_ratio;
+                // sampling_x2 = x2 - 0.5 * bin_w / sampling_ratio;
+                // sampling_y2 = y2 - 0.5 * bin_h / sampling_ratio;
+                // sampling_boxes = [sampling_x1, sampling_y1,
+                //                   sampling_x2, sampling_y2];
+                // samples = crop_and_resize(in, sampling_boxes, batch_indices, 
+                //                 shape{sampling_h, sampling_w}, ResizeMthod::bilinear);
+
+                // do pooling with no padding
+                // switch(mode)
+                // case(PoolingMode::max)
+                //     out = max_pool(samples, Shape{sampling_ratio, sampling_ratio});
+                // case(PoolingMode::avg)
+                //     out = avg_pool(samples, Shape{sampling_ratio, sampling_ratio});
+                
+
 
             }
                                 
