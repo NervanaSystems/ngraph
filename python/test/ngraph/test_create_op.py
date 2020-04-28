b14764f4da3477bb201c842dcf87f4407e2b90d8
mitruska <katarzyna.mitrus@intel.com>
2020-04-28 21:28:47 +0200

    Update create_op types list

--- python/test/ngraph/test_create_op.py
+++ python/test/ngraph/test_create_op.py
@@ -19,8 +19,7 @@ import pytest
 import ngraph as ng
 
 
-np_types = [np.float32, np.float64, np.int8, np.int16, np.int32, np.int64,
-            np.uint8, np.uint16, np.uint32, np.uint64]
+np_types = [np.float32, np.int32]
 
 
 @pytest.mark.parametrize('dtype', np_types)
