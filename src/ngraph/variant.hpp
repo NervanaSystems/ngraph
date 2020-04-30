7ba96d14999748a41dd7b18b5c49857087a8a549
gleb.kazantaev <gkazanta@nncv666el.inn.intel.com>
2020-04-30 09:36:36 +0300

    Added NGRAPH_API for  VariantWrapper

--- src/ngraph/variant.hpp
+++ src/ngraph/variant.hpp
@@ -66,10 +66,9 @@ namespace ngraph
     };
 
     template <>
-    class VariantWrapper<std::string> : public VariantImpl<std::string>
+    class NGRAPH_API VariantWrapper<std::string> : public VariantImpl<std::string>
     {
     public:
-        NGRAPH_API
         static constexpr VariantTypeInfo type_info{"Variant::std::string", 0};
         const VariantTypeInfo& get_type_info() const override { return type_info; }
         VariantWrapper(const value_type& value)
@@ -79,10 +78,9 @@ namespace ngraph
     };
 
     template <>
-    class VariantWrapper<int64_t> : public VariantImpl<int64_t>
+    class NGRAPH_API VariantWrapper<int64_t> : public VariantImpl<int64_t>
     {
     public:
-        NGRAPH_API
         static constexpr VariantTypeInfo type_info{"Variant::int64_t", 0};
         const VariantTypeInfo& get_type_info() const override { return type_info; }
         VariantWrapper(const value_type& value)
