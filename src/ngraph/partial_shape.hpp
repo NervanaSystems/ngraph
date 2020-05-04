4c7ed24f43bfc28bed6736fd3884d4a4f3c75b79
Robert Kimball <robert.kimball@intel.com>
2020-05-04 16:59:59 -0700

    More fixed to PartialShape

--- src/ngraph/partial_shape.hpp
+++ src/ngraph/partial_shape.hpp
@@ -176,18 +176,11 @@ namespace ngraph
         /// \brief Index operator for PartialShape.
         /// \param i The index of the dimension being selected.
         /// \return A reference to the `i`th Dimension of this shape.
-        const Dimension& operator[](size_t i) const
-        {
-            if (i >= m_dimensions.size())
-            {
-                throw std::out_of_range("Accessing out-of-range dimension in Dimension[]");
-            }
-            return m_dimensions[i];
-        }
+        const Dimension& operator[](size_t i) const;
         /// \brief Index operator for PartialShape.
         /// \param i The index of the dimension being selected.
         /// \return A reference to the `i`th Dimension of this shape.
-        Dimension& operator[](size_t i) { return m_dimensions[i]; }
+        Dimension& operator[](size_t i);
         /// \brief Returns a vector of the dimensions. This has no meaning if dynamic.
         explicit operator std::vector<Dimension>() const { return m_dimensions; }
         friend NGRAPH_API std::ostream& operator<<(std::ostream& str, const PartialShape& shape);
