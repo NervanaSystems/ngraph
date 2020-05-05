43fc218fe82fb044b396b849dcf35c8eac82d79c
tomdol <tomasz.dolbniak@intel.com>
2020-05-05 14:10:35 +0200

    Code formatting

--- src/ngraph/serializer.cpp
+++ src/ngraph/serializer.cpp
@@ -2325,7 +2325,9 @@ shared_ptr<Node> JSONDeserializer::deserialize_node(json node_js)
         }
         case OP_TYPEID::RegionYolo: { break;
         }
-        case OP_TYPEID::ReorgYolo: { break;
+        case OP_TYPEID::ReorgYolo:
+        {
+            break;
             const auto strides = node_js.at("strides").get<vector<size_t>>();
             node = make_shared<op::ReorgYolo>(args[0], strides);
             break;
@@ -3050,7 +3052,8 @@ json JSONSerializer::serialize_node(const Node& n)
     }
     case OP_TYPEID::RegionYolo: { break;
     }
-    case OP_TYPEID::ReorgYolo: {
+    case OP_TYPEID::ReorgYolo:
+    {
         auto tmp = static_cast<const op::ReorgYolo*>(&n);
         node["strides"] = tmp->get_strides();
         break;
