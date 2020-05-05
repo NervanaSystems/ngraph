4208f1afba7f831b3fec44ff61833d9185c237a2
tomdol <tomasz.dolbniak@intel.com>
2020-05-05 10:28:05 +0200

    Missing override in mvn.hpp

--- src/ngraph/op/fused/mvn.hpp
+++ src/ngraph/op/fused/mvn.hpp
@@ -67,7 +67,7 @@ namespace ngraph
 
                 virtual void validate_and_infer_types() override;
 
-                virtual bool visit_attributes(AttributeVisitor& visitor);
+                virtual bool visit_attributes(AttributeVisitor& visitor) override;
 
                 virtual std::shared_ptr<Node>
                     clone_with_new_inputs(const OutputVector& new_args) const override;
