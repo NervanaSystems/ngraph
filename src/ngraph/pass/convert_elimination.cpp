39c51510716bb4307b81c8221c962acffa55858c
swapna.iyer <iyeswa@aipg-ra-clx-122.ra.intel.com>
2020-05-06 16:13:58 -0700

    Add new files

---
+++ src/ngraph/pass/convert_elimination.cpp
@@ -0,0 +1,101 @@
+//*****************************************************************************
+// Copyright 2017-2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+
+#include <functional>
+#include <memory>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+
+#include "convert_elimination.hpp"
+#include "ngraph/graph_util.hpp"
+#include "ngraph/log.hpp"
+#include "ngraph/op/convert.hpp"
+#include "ngraph/opsets/opset3.hpp"
+
+using namespace std;
+using namespace ngraph;
+
+#define TI(x) x::type_info
+
+//************************
+// Eliminates 3 types of spines with converts:
+//
+// 1. [ op1--> convert2(type A->B)--> ReduceMin--> convert1(type B->A)--> op2 ] to
+//                    [ op1--> ReduceMin--> op2 ]
+// 2. [ op3--> convert1-->  NonZero--> op4 ] to
+//                    [ op3--> NonZero--> op4 ]
+// 3. [ op5--> convert1--> convert2--> op6 ] to
+//                    [op5--> convert2--> op6 ]
+//
+//*************************
+static bool eliminate_convert(const std::shared_ptr<Node>& node)
+{
+    auto convert = as_type_ptr<opset3::Convert>(node);
+    auto destination_type = convert->get_destination_type();
+    auto input_op = convert->input_value(0);
+
+    // case 1
+    if (is_type<opset3::ReduceMin>(input_op.get_node()))
+    {
+        auto convert_2 = input_op.get_node()->input_value(0).get_node();
+        if (is_type<opset3::Convert>(convert_2) && convert_2->get_users().size() == 1 &&
+            destination_type == convert_2->get_input_element_type(0))
+        {
+            if (destination_type == element::i64 && convert_2->get_element_type() == element::i32)
+            {
+                // replace ReplaceMin as convert's output
+                if (replace_output_update_name(convert->output(0), input_op))
+                {
+                    input_op = convert_2->input_value(0);
+                    // replace input to convert_2 as convert_2's output
+                    return replace_output_update_name(convert_2->output(0), input_op);
+                }
+            }
+        }
+    }
+    bool is_out_type_agnostic = false;
+    static const std::set<NodeTypeInfo> type_agnostic{TI(opset3::NonZero)};
+    // case 2 & 3
+    if (convert->get_users().size() == 1)
+    {
+        is_out_type_agnostic = type_agnostic.count(convert->get_users()[0]->get_type_info()) == 1;
+    }
+
+    if (destination_type == input_op.get_element_type() || is_out_type_agnostic)
+    {
+        if (is_out_type_agnostic && is_type<opset3::Convert>(input_op.get_node()))
+        {
+            input_op = input_op.get_node()->input_value(0);
+        }
+        return replace_output_update_name(convert->output(0), input_op);
+    }
+    return false;
+}
+
+bool pass::ConvertElimination::run_on_function(std::shared_ptr<Function> function)
+{
+    bool clobbered = false;
+
+    for (const auto& n : function->get_ops())
+    {
+        // Work around a warning [-Wpotentially-evaluated-expression]
+        //        const Node& node = *n;
+        clobbered = eliminate_convert(n) || clobbered;
+    }
+
+    return clobbered;
+}
