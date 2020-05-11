5404f41d06b67f37f066c89016a5b0cad017ae58
tomdol <tomasz.dolbniak@intel.com>
2020-05-11 16:32:28 +0200

    Revert the softmax downgrade pass changes

--- src/ngraph/pass/opset0_downgrade.cpp
+++ src/ngraph/pass/opset0_downgrade.cpp
@@ -774,8 +774,11 @@ namespace
     {
         const auto axis = node->get_axis();
         const auto data = node->input(0);
+        const auto data_shape = data.get_shape();
+        std::vector<size_t> axes(data_shape.size() - axis);
+        std::iota(std::begin(axes), std::end(axes), axis);
         const auto replacement_node =
-            make_shared<op::v0::Softmax>(node->input_value(0), AxisSet{axis});
+            make_shared<op::v0::Softmax>(node->input_value(0), axes);
         replace_node(node, replacement_node);
         return replacement_node;
     }
