ada49b781950426d869d6d6c92fcb74d530f7368
Adam Osewski <adam.osewski@intel.com>
2020-05-14 19:14:44 +0200

    Add ONNX GRU operator.

--- src/ngraph/frontend/onnx_import/ops_bridge.cpp
+++ src/ngraph/frontend/onnx_import/ops_bridge.cpp
@@ -65,6 +65,7 @@
 #include "op/global_average_pool.hpp"
 #include "op/global_max_pool.hpp"
 #include "op/greater.hpp"
+#include "op/gru.hpp"
 #include "op/hard_sigmoid.hpp"
 #include "op/hardmax.hpp"
 #include "op/identity.hpp"
@@ -292,6 +293,7 @@ namespace ngraph
             REGISTER_OPERATOR("GlobalLpPool", 1, global_lp_pool);
             REGISTER_OPERATOR("GlobalMaxPool", 1, global_max_pool);
             REGISTER_OPERATOR("Greater", 1, greater);
+            REGISTER_OPERATOR("GRU", 1, gru);
             REGISTER_OPERATOR("Hardmax", 1, hardmax);
             REGISTER_OPERATOR("HardSigmoid", 1, hard_sigmoid);
             REGISTER_OPERATOR("Identity", 1, identity);
