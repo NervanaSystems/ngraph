00a2c4cab43c7c6e2b8fa84803877775b679cd64
Wenzhe Xue <wenzhe.xue@intel.com>
2020-05-14 22:24:52 -0700

    add a test case for reverse, disabled for now

--- test/op_eval/strided_slice.cpp
+++ test/op_eval/strided_slice.cpp
@@ -129,9 +129,9 @@ TEST(op_eval, strided_slice2)
 
 // A Shape{3, 2, 3}
 // A[:2, 1:, ::2]
-// result Shape{2, 1, 3}
-// [[ 3 5]
-//  [ 9 11]] 
+// result Shape{2, 1, 2}
+// [[[3 5]]
+//  [[9 11]]]
 TEST(op_eval, strided_slice3)
 {
     auto A_shape = Shape{3, 2, 3};
@@ -162,6 +162,53 @@ TEST(op_eval, strided_slice3)
 
     std::vector<int64_t> expected{3, 5, 9, 11};
     Shape expected_shape{2, 1, 2};
+
+    auto result = make_shared<HostTensor>();
+    ASSERT_TRUE(f->evaluate({result},
+                            {make_host_tensor<element::Type_t::i64>(A_shape, A_vec),
+                             make_host_tensor<element::Type_t::i64>(Shape{3}, begin_vec),
+                             make_host_tensor<element::Type_t::i64>(Shape{3}, end_vec),
+                             make_host_tensor<element::Type_t::i64>(Shape{3}, strides_vec)}));
+    EXPECT_EQ(result->get_element_type(), element::i64);
+    EXPECT_EQ(result->get_shape(), expected_shape);
+    EXPECT_EQ(read_vector<int64_t>(result), expected);
+}
+
+// A Shape{3, 2, 3}
+// A[0:1, :, ::-1]
+// result Shape{1, 2, 3}
+// [[[2 1 0]
+//   [5 4 3]]] 
+TEST(op_eval, DISABLED_strided_slice_reverse)
+{
+    auto A_shape = Shape{3, 2, 3};
+    auto A = make_shared<op::Parameter>(element::i64, A_shape);
+    auto begin = make_shared<op::Parameter>(element::i64, Shape{3});
+    auto end = make_shared<op::Parameter>(element::i64, Shape{3});
+    auto strides = make_shared<op::Parameter>(element::i64, Shape{3});
+
+    std::vector<int64_t> begin_vec{0, 0, 0};
+    std::vector<int64_t> end_vec{1, 0, 0};
+    std::vector<int64_t> strides_vec{1, 1, -1};
+    std::vector<int64_t> begin_mask{0, 1, 1};
+    std::vector<int64_t> end_mask{0, 1, 1};
+
+    auto r = make_shared<op::v1::StridedSlice>(A,
+                                               begin,
+                                               end,
+                                               strides,
+                                               begin_mask,
+                                               end_mask,
+                                               std::vector<int64_t>(3, 0),
+                                               std::vector<int64_t>(3, 0),
+                                               std::vector<int64_t>(3, 0));
+    auto f = make_shared<Function>(r, ParameterVector{A, begin, end, strides});
+
+    std::vector<int64_t> A_vec(3 * 2 * 3);
+    std::iota(A_vec.begin(), A_vec.end(), 0);
+
+    std::vector<int64_t> expected{2, 1, 0, 5, 4, 3};
+    Shape expected_shape{1, 2, 3};
 
     auto result = make_shared<HostTensor>();
     ASSERT_TRUE(f->evaluate({result},
