091ae1747ddd9a2836022f2b6f153ba10764a377
Tomasz Socha <tomasz.socha@intel.com>
2020-05-15 16:35:02 +0200

    [ONNX] ONNX Importer as shared library. (#4623)
    * Change onnx_importer into shared library
    
    * Rename onnx importer targets
    
    * Ignore warnings in external onnx library
    
    * Merge onnx importer libraries
    
    * Fix symbols visibility
    
    * Fix typo
    
    * Fix visibility of v3::InterpolateMode
    
    * Skip Softmax_0D test on IE backend
    
    Co-authored-by: Robert Kimball <robert.kimball@intel.com>

--- python/setup.py
+++ python/setup.py
@@ -87,6 +87,10 @@ NGRAPH_CPP_LIBRARY_NAME = 'ngraph'
 if len([fn for fn in os.listdir(NGRAPH_CPP_LIBRARY_DIR) if re.search('ngraphd', fn)]):
     NGRAPH_CPP_LIBRARY_NAME = 'ngraphd'
 
+ONNX_IMPORTER_CPP_LIBRARY_NAME = 'onnx_importer'
+if len([fn for fn in os.listdir(NGRAPH_CPP_LIBRARY_DIR) if re.search('onnx_importerd', fn)]):
+    ONNX_IMPORTER_CPP_LIBRARY_NAME = 'onnx_importerd'
+
 
 def parallelCCompile(
     self,
@@ -227,7 +231,7 @@ include_dirs = [PYNGRAPH_SRC_DIR, NGRAPH_CPP_INCLUDE_DIR, PYBIND11_INCLUDE_DIR]
 
 library_dirs = [NGRAPH_CPP_LIBRARY_DIR]
 
-libraries = [NGRAPH_CPP_LIBRARY_NAME]
+libraries = [NGRAPH_CPP_LIBRARY_NAME, ONNX_IMPORTER_CPP_LIBRARY_NAME]
 
 extra_compile_args = []
 if NGRAPH_ONNX_IMPORT_ENABLE in ['TRUE', 'ON', True]:
