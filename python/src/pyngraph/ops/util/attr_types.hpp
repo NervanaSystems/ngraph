9addbe00d2a1d3eeb649b5ddee0c076c3eedca44
Michal Karzynski <michal.karzynski@intel.com>
2020-05-15 10:18:35 +0200

    Remove temp file

--- python/src/pyngraph/ops/util/attr_types.hpp
+++
@@ -1,8 +0,0 @@
-    py::enum_<ngraph::op::PadType>(groupconvolution, "PadType", py::arithmetic())
-        .value("EXPLICIT", ngraph::op::PadType::EXPLICIT)
-        .value("SAME_LOWER", ngraph::op::PadType::SAME_LOWER)
-        .value("SAME_UPPER", ngraph::op::PadType::SAME_UPPER)
-        .value("VALID", ngraph::op::PadType::VALID)
-        .value("AUTO", ngraph::op::PadType::AUTO)
-        .value("NOTSET", ngraph::op::PadType::NOTSET)
-        .export_values();
