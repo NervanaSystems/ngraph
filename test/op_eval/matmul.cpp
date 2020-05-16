361a66887b918f6230ccf604e49241c12cb46528
Yimei Sun <yimei.sun@intel.com>
2020-05-15 21:00:30 -0700

    Update include header in test

--- test/op_eval/matmul.cpp
+++ test/op_eval/matmul.cpp
@@ -14,6 +14,7 @@
 // limitations under the License.
 //*****************************************************************************
 
+#include <numeric>
 #include <vector>
 
 #include "gtest/gtest.h"
