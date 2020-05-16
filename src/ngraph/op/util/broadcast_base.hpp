0a09423744448f6d062e82513097e303670a762e
Scott Cyphers <scott.cyphers@intel.com>
2020-05-16 06:38:00 -0700

    Compiler error

--- src/ngraph/op/util/broadcast_base.hpp
+++ src/ngraph/op/util/broadcast_base.hpp
@@ -60,7 +60,7 @@ namespace ngraph
                 /// \return true and the AxisSet if broadcast axes can be fully determined.
                 virtual std::pair<bool, AxisSet> get_broadcast_axes() const;
 
-                bool evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs);
+                bool evaluate(const HostTensorVector& outputs, const HostTensorVector& inputs) override;
 
             protected:
                 virtual void generate_adjoints(autodiff::Adjoints& adjoints,
