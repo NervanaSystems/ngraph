db68247b1c7d9f6ed840dccb697f4f34e9530986
Robert Kimball <robert.kimball@intel.com>
2020-06-13 10:00:18 -0700

    Fix MLIR

--- src/contrib/mlir/core/pass/mlir_subgraph_extraction.cpp
+++ src/contrib/mlir/core/pass/mlir_subgraph_extraction.cpp
@@ -233,6 +233,29 @@ void MLIRSubgraphExtractionPass::build_subgraphs(
   }
 }
 
+namespace
+{
+    std::shared_ptr<ngraph::Node> output_to_node(ngraph::Output<ngraph::Node> output)
+    {
+        std::shared_ptr<ngraph::Node> node = output.get_node_shared_ptr();
+        if (output.get_index() == 0 && output.get_node()->get_output_size() == 1)
+        {
+            return node;
+        }
+        else
+        {
+            for (auto in : output.get_target_inputs())
+            {
+                if (ngraph::is_type<ngraph::op::GetOutputElement>(in.get_node()))
+                {
+                    return in.get_node()->shared_from_this();
+                }
+            }
+            return std::make_shared<ngraph::op::GetOutputElement>(node, output.get_index());
+        }
+    }
+}
+
 ngraph::NodeVector
 MLIRSubgraphExtractionPass::build_ck_nodes(std::shared_ptr<Function> func) {
   NodeVector ck_nodes;
@@ -292,6 +315,19 @@ MLIRSubgraphExtractionPass::build_ck_nodes(std::shared_ptr<Function> func) {
         if (node_set.find(input.get_node()->shared_from_this()) ==
             node_set.end()) {
           input.replace_source_output(ck->output(i));
+        }
+      }
+    }
+  }
+  for (auto &node : ck_nodes) {
+    auto ck = std::static_pointer_cast<CompiledKernel>(node);
+    if (ck->get_output_size() > 1) {
+      for (auto &old_output : ck->outputs()) {
+        auto inputs = old_output.get_target_inputs();
+        auto goe_node = output_to_node(old_output);
+        auto new_output = goe_node->output(0);
+        for (auto &input : inputs) {
+          input.replace_source_output(new_output);
         }
       }
     }
