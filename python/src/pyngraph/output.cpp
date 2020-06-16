adb62df68c4d4993b3d337b8e64ae7bb0c9c5924
Robert Kimball <robert.kimball@intel.com>
2020-06-16 11:20:19 -0700

    style

--- python/src/pyngraph/output.cpp
+++ python/src/pyngraph/output.cpp
@@ -17,7 +17,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "ngraph/node_output.hpp"     // ngraph::Output<Node>
+#include "ngraph/node_output.hpp"  // ngraph::Output<Node>
 #include "ngraph/op/parameter.hpp" // ngraph::op::Parameter
 #include "pyngraph/output.hpp"
 
