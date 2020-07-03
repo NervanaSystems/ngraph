da46bc525c1673c69d9c6bbae604185d0a098cd9
Robert Kimball <robert.kimball@intel.com>
2020-07-03 07:35:45 -0700

    Cleanup

--- cmake/Modules/gen_op_version.cmake
+++ cmake/Modules/gen_op_version.cmake
@@ -1,14 +1,21 @@
 cmake_policy(SET CMP0074 NEW)
+cmake_policy(SET CMP0007 NEW)
 
 # Make the string OPSETS into a list by replacing spaces with semicolons
 string(REPLACE " " ";" OPSETS "${OPSETS}")
 set(ALL_OPS)
 foreach(OPSET ${OPSETS})
-    message(STATUS "Processing ${CURRENT_DIR}/${OPSET}")
     file(STRINGS ${CURRENT_DIR}/${OPSET} CONTENTS)
+
+    # Remove // style comments
     list(TRANSFORM CONTENTS REPLACE "( |)//.*" "")
+
+    # Remove # C preprocessor statements
     list(TRANSFORM CONTENTS REPLACE "#.*" "")
+
+    # Convert ngraph::op::vN to N
     list(TRANSFORM CONTENTS REPLACE "ngraph::op::v" "")
+
     list(APPEND ALL_OPS ${CONTENTS})
 endforeach()
 list(REMOVE_DUPLICATES ALL_OPS)
