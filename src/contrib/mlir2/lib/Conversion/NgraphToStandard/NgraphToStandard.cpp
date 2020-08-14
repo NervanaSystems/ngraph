54ab86481111e2a3b8411ae1009e5014c87f307b
Robert Kimball <robert.kimball@intel.com>
2020-08-14 07:32:30 -0700

    convert pass compiling

--- src/contrib/mlir2/lib/Conversion/NgraphToStandard/NgraphToStandard.cpp
+++ src/contrib/mlir2/lib/Conversion/NgraphToStandard/NgraphToStandard.cpp
@@ -15,71 +15,76 @@
 //*****************************************************************************
 
 #include "Conversion/NgraphToStandard/NgraphToStandard.hpp"
-#include <llvm/ADT/DenseSet.h>
-#include <llvm/Support/Debug.h>
-#include <mlir/Dialect/Affine/EDSC/Builders.h>
-#include <mlir/Dialect/Affine/EDSC/Intrinsics.h>
-#include <mlir/Dialect/LLVMIR/LLVMDialect.h>
-#include <mlir/Dialect/StandardOps/EDSC/Intrinsics.h>
-#include <mlir/IR/AffineExpr.h>
-#include <mlir/IR/Function.h>
-#include <mlir/IR/IntegerSet.h>
-#include <mlir/IR/MLIRContext.h>
-#include <mlir/IR/StandardTypes.h>
-#include <mlir/Transforms/DialectConversion.h>
+#include "mlir/Conversion/AffineToStandard/AffineToStandard.h"
+#include "mlir/Conversion/SCFToStandard/SCFToStandard.h"
+#include "mlir/Conversion/StandardToLLVM/ConvertStandardToLLVM.h"
+#include "mlir/Conversion/StandardToLLVM/ConvertStandardToLLVMPass.h"
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/SCF/SCF.h"
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "llvm/ADT/Sequence.h"
 
-using namespace mlir;
-using namespace mlir::edsc;
-using namespace mlir::edsc::intrinsics;
-using namespace mlir::edsc::op;
+using namespace ::mlir;
+// using namespace mlir::edsc;
+// using namespace mlir::edsc::intrinsics;
+// using namespace mlir::edsc::op;
 
 namespace
 {
-    // class LowerNgraphPass : public ConvertNgraphToStandardBase<LowerNgraphPass>
-    // {
-    //     void runOnOperation() override
-    //     {
-    //         OwningRewritePatternList patterns;
-    //         populateNgraphToStdConversionPatterns(patterns, &getContext());
-    //         populateNgraphToVectorConversionPatterns(patterns, &getContext());
-    //         ConversionTarget target(getContext());
-    //         target.addLegalDialect<scf::SCFDialect, StandardOpsDialect, VectorDialect>();
-    //         if (failed(applyPartialConversion(getOperation(), target, patterns)))
-    //             signalPassFailure();
-    //     }
-    // };
+    struct NgraphToLLVMLoweringPass
+        : public ::mlir::PassWrapper<NgraphToLLVMLoweringPass, ::mlir::OperationPass<::mlir::ModuleOp>>
+    {
+        void runOnOperation() final;
+    };
+}
 
-    // /// Base class for nGraph operation conversions to affine/standard dialect.
-    // /// Provides conversion patterns with an access to the DialectLoweringPass
-    // /// which holds the state of the conversion.
-    // class NGraphOpConversionBase : public ::mlir::ConversionPattern
-    // {
-    // public:
-    //     NGraphOpConversion(llvm::StringRef rootOpName, ::mlir::MLIRContext* context,
-    //     DialectLoweringPass& pass)
-    //         : ConversionPattern(rootOpName, /*benefit=*/1, context)
-    //         , pass(pass)
-    //         , context(context){};
+void NgraphToLLVMLoweringPass::runOnOperation()
+{
+    // The first thing to define is the conversion target. This will define the
+    // final target for this lowering. For this lowering, we are only targeting
+    // the LLVM dialect.
+    ::mlir::LLVMConversionTarget target(getContext());
+    target.addLegalOp<::mlir::ModuleOp, ::mlir::ModuleTerminatorOp>();
 
-    // protected:
-    //     // Back-reference to the lowering pass which contains the lowering state,
-    //     // including the nGraph type converter.
-    //     DialectLoweringPass& pass;
-    //     MLIRContext* context;
-    // };
+    // During this lowering, we will also be lowering the MemRef types, that are
+    // currently being operated on, to a representation in LLVM. To perform this
+    // conversion we use a TypeConverter as part of the lowering. This converter
+    // details how one type maps to another. This is necessary now that we will be
+    // doing more complicated lowerings, involving loop region arguments.
+    LLVMTypeConverter typeConverter(&getContext());
 
-    // class AddOpConversion : public NGraphOpLowering
-    // {
-    // public:
-    //     explicit AddOpConversion(mlir::MLIRContext* context, DialectLoweringPass& pass)
-    //         : NGraphOpLowering(mlir::Ngraph_AddOp::getOperationName(), context, pass)
-    //     {
-    //     }
+    // Now that the conversion target has been defined, we need to provide the
+    // patterns used for lowering. At this point of the compilation process, we
+    // have a combination of `ngraph`, `affine`, and `std` operations. Luckily, there
+    // are already exists a set of patterns to transform `affine` and `std`
+    // dialects. These patterns lowering in multiple stages, relying on transitive
+    // lowerings. Transitive lowering, or A->B->C lowering, is when multiple
+    // patterns must be applied to fully transform an illegal operation into a
+    // set of legal ones.
+    OwningRewritePatternList patterns;
+    populateAffineToStdConversionPatterns(patterns, &getContext());
+    populateLoopToStdConversionPatterns(patterns, &getContext());
+    populateStdToLLVMConversionPatterns(typeConverter, patterns);
 
-    //     LogicalResult matchAndRewrite(Operation* op,
-    //                                   ArrayRef<Value> operands,
-    //                                   ConversionPatternRewriter& rewriter) const override
-    //     {
-    //     }
-    // };
+    // The only remaining operation to lower from the `ngraph` dialect, is the
+    // PrintOp.
+    // patterns.insert<PrintOpLowering>(&getContext());
+
+    // We want to completely lower to LLVM, so we use a `FullConversion`. This
+    // ensures that only legal operations will remain after the conversion.
+    auto module = getOperation();
+    if (failed(applyFullConversion(module, target, patterns)))
+    {
+        signalPassFailure();
+    }
+}
+
+/// Create a pass for lowering operations the remaining `Toy` operations, as
+/// well as `Affine` and `Std`, to the LLVM dialect for codegen.
+std::unique_ptr<mlir::Pass> mlir::ngraph::createLowerToLLVMPass()
+{
+    return std::make_unique<NgraphToLLVMLoweringPass>();
 }
