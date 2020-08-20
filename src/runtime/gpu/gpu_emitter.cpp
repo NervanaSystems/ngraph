2b23ed318a005f009bec20676364854552090c6b
Robert Kimball <robert.kimball@intel.com>
2020-08-20 08:29:55 -0700

    Fix GPU build

--- src/runtime/gpu/gpu_emitter.cpp
+++ src/runtime/gpu/gpu_emitter.cpp
@@ -512,7 +512,7 @@ std::string runtime::gpu::GPU_Emitter::emit_Greater(EMIT_ARGS)
         compiled_function, function_name, node, args, out);
 }
 
-std::string runtime::gpu::GPU_Emitter::emit_GreaterEq(EMIT_ARGS)
+std::string runtime::gpu::GPU_Emitter::emit_GreaterEqual(EMIT_ARGS)
 {
     return emit_elementwise<ngraph::op::v1::GreaterEqual>(
         compiled_function, function_name, node, args, out);
@@ -524,7 +524,7 @@ std::string runtime::gpu::GPU_Emitter::emit_Less(EMIT_ARGS)
         compiled_function, function_name, node, args, out);
 }
 
-std::string runtime::gpu::GPU_Emitter::emit_LessEq(EMIT_ARGS)
+std::string runtime::gpu::GPU_Emitter::emit_LessEqual(EMIT_ARGS)
 {
     return emit_elementwise<ngraph::op::v1::LessEqual>(
         compiled_function, function_name, node, args, out);
